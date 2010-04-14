d55ac96da132a9f33159a843ef1958495421c6a0
Kai Germaschewski <kai.germaschewski@unh.edu>
2010-04-14 00:00:50 -0400

    run testcase with 1000 particles
    and fix a stupid bug in the generic_c copying to/from Fortran.

--- src/generic_c/psc_generic_c.c
+++ src/generic_c/psc_generic_c.c
@@ -17,15 +17,15 @@ genc_particles_from_fortran()
     struct f_particle *f_part = &psc.f_part[n];
     struct c_particle *part = &genc->part[n];
 
-    part[n].xi  = f_part[n].xi;
-    part[n].yi  = f_part[n].yi;
-    part[n].zi  = f_part[n].zi;
-    part[n].pxi = f_part[n].pxi;
-    part[n].pyi = f_part[n].pyi;
-    part[n].pzi = f_part[n].pzi;
-    part[n].qni = f_part[n].qni;
-    part[n].mni = f_part[n].mni;
-    part[n].wni = f_part[n].wni;
+    part->xi  = f_part->xi;
+    part->yi  = f_part->yi;
+    part->zi  = f_part->zi;
+    part->pxi = f_part->pxi;
+    part->pyi = f_part->pyi;
+    part->pzi = f_part->pzi;
+    part->qni = f_part->qni;
+    part->mni = f_part->mni;
+    part->wni = f_part->wni;
   }
 }
 
@@ -38,15 +38,15 @@ genc_particles_to_fortran()
     struct f_particle *f_part = &psc.f_part[n];
     struct c_particle *part = &genc->part[n];
 
-    f_part[n].xi  = part[n].xi;
-    f_part[n].yi  = part[n].yi;
-    f_part[n].zi  = part[n].zi;
-    f_part[n].pxi = part[n].pxi;
-    f_part[n].pyi = part[n].pyi;
-    f_part[n].pzi = part[n].pzi;
-    f_part[n].qni = part[n].qni;
-    f_part[n].mni = part[n].mni;
-    f_part[n].wni = part[n].wni;
+    f_part->xi  = part->xi;
+    f_part->yi  = part->yi;
+    f_part->zi  = part->zi;
+    f_part->pxi = part->pxi;
+    f_part->pyi = part->pyi;
+    f_part->pzi = part->pzi;
+    f_part->qni = part->qni;
+    f_part->mni = part->mni;
+    f_part->wni = part->wni;
   }
 }
 
