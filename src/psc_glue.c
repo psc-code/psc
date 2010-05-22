a23ec2cfb9e9feed38d56b9d5139274cf84cf778
Kai Germaschewski <kai.germaschewski@unh.edu>
2010-05-21 22:47:12 -0400

    add PIC_set_variables()
    which allows to set (some) module variables from C, which in the end
    is still less ugly than passing those long lists of arguments...

--- src/psc_glue.c
+++ src/psc_glue.c
@@ -3,6 +3,7 @@
 
 #include <assert.h>
 
+#define PIC_set_variables_F77 F77_FUNC(pic_set_variables,PIC_SET_VARIABLES)
 #define PIC_push_part_yz_F77 F77_FUNC(pic_push_part_yz,PIC_PUSH_PART_YZ)
 #define PIC_push_part_yz_a_F77 F77_FUNC(pic_push_part_yz_a,PIC_PUSH_PART_YZ_A)
 #define PIC_push_part_yz_b_F77 F77_FUNC(pic_push_part_yz_b,PIC_PUSH_PART_YZ_B)
@@ -15,13 +16,14 @@
 #define C_push_part_z_F77 F77_FUNC(c_push_part_z,C_PUSH_PART_Z)
 #define C_sort_F77 F77_FUNC(c_sort,C_SORT)
 
+void PIC_set_variables_F77(f_int *i1mn, f_int *i2mn, f_int *i3mn,
+			   f_int *i1mx, f_int *i2mx, f_int *i3mx,
+			   f_int *rd1, f_int *rd2, f_int *rd3);
+
 void PIC_push_part_yz_F77(f_int *niloc, struct f_particle *p_niloc,
 			  f_real *cori, f_real *alpha, f_real *eta, 
 			  f_real *p2A, f_real *p2B,
 			  f_real *dt, f_real *dx, f_real *dy, f_real *dz,
-			  f_int *i1mn, f_int *i2mn, f_int *i3mn,
-			  f_int *i1mx, f_int *i2mx, f_int *i3mx,
-			  f_int *rd1, f_int *rd2, f_int *rd3,
 			  f_real *ne, f_real *ni, f_real *nn,
 			  f_real *jxi, f_real *jyi, f_real *jzi,
 			  f_real *ex, f_real *ey, f_real *ez,
@@ -31,9 +33,6 @@ void PIC_push_part_yz_a_F77(f_int *niloc, struct f_particle *p_niloc,
 			    f_real *cori, f_real *alpha, f_real *eta, 
 			    f_real *p2A, f_real *p2B,
 			    f_real *dt, f_real *dx, f_real *dy, f_real *dz,
-			    f_int *i1mn, f_int *i2mn, f_int *i3mn,
-			    f_int *i1mx, f_int *i2mx, f_int *i3mx,
-			    f_int *rd1, f_int *rd2, f_int *rd3,
 			    f_real *ne, f_real *ni, f_real *nn,
 			    f_real *jxi, f_real *jyi, f_real *jzi,
 			    f_real *ex, f_real *ey, f_real *ez,
@@ -43,9 +42,6 @@ void PIC_push_part_yz_b_F77(f_int *niloc, struct f_particle *p_niloc,
 			    f_real *cori, f_real *alpha, f_real *eta, 
 			    f_real *p2A, f_real *p2B,
 			    f_real *dt, f_real *dx, f_real *dy, f_real *dz,
-			    f_int *i1mn, f_int *i2mn, f_int *i3mn,
-			    f_int *i1mx, f_int *i2mx, f_int *i3mx,
-			    f_int *rd1, f_int *rd2, f_int *rd3,
 			    f_real *ne, f_real *ni, f_real *nn,
 			    f_real *jxi, f_real *jyi, f_real *jzi,
 			    f_real *ex, f_real *ey, f_real *ez,
@@ -56,9 +52,6 @@ void PIC_push_part_z_F77(f_int *niloc, struct f_particle *p_niloc,
 			 f_real *wl, f_real *wp,
 			 f_real *p2A, f_real *p2B,
 			 f_int *n, f_real *dt, f_real *dx, f_real *dy, f_real *dz,
-			 f_int *i1mn, f_int *i2mn, f_int *i3mn,
-			 f_int *i1mx, f_int *i2mx, f_int *i3mx,
-			 f_int *rd1, f_int *rd2, f_int *rd3,
 			 f_real *ne, f_real *ni, f_real *nn,
 			 f_real *jxi, f_real *jyi, f_real *jzi,
 			 f_real *ex, f_real *ey, f_real *ez,
@@ -70,17 +63,23 @@ void PIC_randomize_F77(f_int *niloc, struct f_particle *p_niloc);
 // ----------------------------------------------------------------------
 // Wrappers to be called from C that call into Fortran
 
-void
-PIC_push_part_yz()
+static void
+PIC_set_variables()
 {
   int i0mx = psc.ihi[0] - 1, i1mx = psc.ihi[1] - 1, i2mx = psc.ihi[2] - 1;
 
+  PIC_set_variables_F77(&psc.ilo[0], &psc.ilo[1], &psc.ilo[2],
+			&i0mx, &i1mx, &i2mx,
+			&psc.ibn[0], &psc.ibn[1], &psc.ibn[2]);
+}
+
+void
+PIC_push_part_yz()
+{
+  PIC_set_variables();
   PIC_push_part_yz_F77(&psc.n_part, &psc.f_part[-1], &psc.prm.cori, &psc.prm.alpha,
 		       &psc.prm.eta, &psc.p2A, &psc.p2B,
 		       &psc.dt, &psc.dx[0], &psc.dx[1], &psc.dx[2],
-		       &psc.ilo[0], &psc.ilo[1], &psc.ilo[2],
-		       &i0mx, &i1mx, &i2mx,
-		       &psc.ibn[0], &psc.ibn[1], &psc.ibn[2],
 		       psc.f_fields[NE], psc.f_fields[NI], psc.f_fields[NN],
 		       psc.f_fields[JXI], psc.f_fields[JYI], psc.f_fields[JZI],
 		       psc.f_fields[EX], psc.f_fields[EY], psc.f_fields[EZ],
@@ -90,15 +89,11 @@ PIC_push_part_yz()
 void
 PIC_push_part_yz_a()
 {
-  int i0mx = psc.ihi[0] - 1, i1mx = psc.ihi[1] - 1, i2mx = psc.ihi[2] - 1;
-
+  PIC_set_variables();
   PIC_push_part_yz_a_F77(&psc.n_part, &psc.f_part[-1],
 			 &psc.prm.cori, &psc.prm.alpha,
 			 &psc.prm.eta, &psc.p2A, &psc.p2B,
 			 &psc.dt, &psc.dx[0], &psc.dx[1], &psc.dx[2],
-			 &psc.ilo[0], &psc.ilo[1], &psc.ilo[2],
-			 &i0mx, &i1mx, &i2mx,
-			 &psc.ibn[0], &psc.ibn[1], &psc.ibn[2],
 			 psc.f_fields[NE], psc.f_fields[NI], psc.f_fields[NN],
 			 psc.f_fields[JXI], psc.f_fields[JYI], psc.f_fields[JZI],
 			 psc.f_fields[EX], psc.f_fields[EY], psc.f_fields[EZ],
@@ -108,15 +103,11 @@ PIC_push_part_yz_a()
 void
 PIC_push_part_yz_b()
 {
-  int i0mx = psc.ihi[0] - 1, i1mx = psc.ihi[1] - 1, i2mx = psc.ihi[2] - 1;
-
+  PIC_set_variables();
   PIC_push_part_yz_b_F77(&psc.n_part, &psc.f_part[-1],
 			 &psc.prm.cori, &psc.prm.alpha,
 			 &psc.prm.eta, &psc.p2A, &psc.p2B,
 			 &psc.dt, &psc.dx[0], &psc.dx[1], &psc.dx[2],
-			 &psc.ilo[0], &psc.ilo[1], &psc.ilo[2],
-			 &i0mx, &i1mx, &i2mx,
-			 &psc.ibn[0], &psc.ibn[1], &psc.ibn[2],
 			 psc.f_fields[NE], psc.f_fields[NI], psc.f_fields[NN],
 			 psc.f_fields[JXI], psc.f_fields[JYI], psc.f_fields[JZI],
 			 psc.f_fields[EX], psc.f_fields[EY], psc.f_fields[EZ],
@@ -126,14 +117,10 @@ PIC_push_part_yz_b()
 void
 PIC_push_part_z()
 {
-  int i0mx = psc.ihi[0] - 1, i1mx = psc.ihi[1] - 1, i2mx = psc.ihi[2] - 1;
-
+  PIC_set_variables();
   PIC_push_part_z_F77(&psc.n_part, &psc.f_part[-1], &psc.prm.cori, &psc.prm.alpha,
 		      &psc.prm.eta,  &psc.prm.wl, &psc.prm.wp, &psc.p2A, &psc.p2B,
 		      &psc.timestep, &psc.dt, &psc.dx[0], &psc.dx[1], &psc.dx[2],
-		      &psc.ilo[0], &psc.ilo[1], &psc.ilo[2],
-		      &i0mx, &i1mx, &i2mx,
-		      &psc.ibn[0], &psc.ibn[1], &psc.ibn[2],
 		      psc.f_fields[NE], psc.f_fields[NI], psc.f_fields[NN],
 		      psc.f_fields[JXI], psc.f_fields[JYI], psc.f_fields[JZI],
 		      psc.f_fields[EX], psc.f_fields[EY], psc.f_fields[EZ],
