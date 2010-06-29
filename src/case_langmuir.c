754642a6555100db2dcdf9036a90bd7d9ab6d9a2
Kai Germaschewski <kai.germaschewski@unh.edu>
2010-06-29 19:38:19 -0400

    case langmuir: make some parameters command line selectable
    This is a demonstration on how to change hard-coded parameters
    to parameters which can be selected on the command line --
    copy&paste as needed to other initial conditions.
    
    langmuir_create() will create a struct of parameters needed for this
    particular case. Defaults will be set as specified, which can be
    overridden on the command line.
    
    Other functions, like langmuir_init_nvt() then access those parameters
    instead of the hard-coded ones.

--- src/case_langmuir.c
+++ src/case_langmuir.c
@@ -1,38 +1,77 @@
 
 #include "psc.h"
+#include "util/params.h"
 
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
 
+struct psc_langmuir {
+  double x0, y0, z0; // location of density center in m
+  double Lx, Ly, Lz; // gradient of density profile in m
+  double widthx, widthy, widthz; // width of transverse / longitudinal 
+                                 // density profile in m
+  double rot;        // rotation angle in degrees
+  double mass_ratio; // M_i / M_e
+};
+
+#define VAR(x) (void *)offsetof(struct psc_langmuir, x)
+
+static struct param psc_langmuir_descr[] = {
+  { "x0"            , VAR(x0)              , PARAM_DOUBLE(.5  * 1e-6)     },
+  { "y0"            , VAR(y0)              , PARAM_DOUBLE(.5  * 1e-6)     },
+  { "z0"            , VAR(z0)              , PARAM_DOUBLE(10. * 1e-6)     },
+  { "Lx"            , VAR(Lx)              , PARAM_DOUBLE(.1  * 1e-6)     },
+  { "Ly"            , VAR(Ly)              , PARAM_DOUBLE(.1  * 1e-6)     },
+  { "Lz"            , VAR(Lz)              , PARAM_DOUBLE(.1  * 1e-6)     },
+  { "widthx"        , VAR(widthx)          , PARAM_DOUBLE(1.  * 1e-6)     },
+  { "widthy"        , VAR(widthy)          , PARAM_DOUBLE(1.  * 1e-6)     },
+  { "widthz"        , VAR(widthz)          , PARAM_DOUBLE(8.  * 1e-6)     },
+  { "rot"           , VAR(rot)             , PARAM_DOUBLE(0.)             },
+  { "mass_ratio"    , VAR(mass_ratio)      , PARAM_DOUBLE(1836.)          },
+  {},
+};
+
+#undef VAR
+
+static void
+langmuir_create()
+{
+  struct psc_langmuir *langmuir = malloc(sizeof(*langmuir));
+  memset(langmuir, 0, sizeof(*langmuir));
+
+  params_parse_cmdline(langmuir, psc_langmuir_descr, "PSC Langmuir", MPI_COMM_WORLD);
+  params_print(langmuir, psc_langmuir_descr, "PSC Langmuir", MPI_COMM_WORLD);
+
+  psc.case_data = langmuir;
+}
+
+static void
+langmuir_destroy()
+{
+  free(psc.case_data);
+  psc.case_data = NULL;
+}
+
 static void
 langmuir_init_nvt(int kind, double x[3], double *q, double *m, double *n,
 		  double v[3], double T[3])
 {
-  real x0 = 0.5*1.0e-6;
-  real y0 = 0.5*1.0e-6;
-  real z0 = 10.0*1.0e-6;
-  real Lx = 0.1*1.0e-6;
-  real Ly = 0.1*1.0e-6;
-  real Lz = 0.1*1.0e-6;
-  real widthx = 1.0*1.0e-6;
-  real widthy = 1.0*1.0e-6;
-  real widthz = 8.0*1.0e-6;
-  real rot = 0.0;
+  struct psc_langmuir *langmuir = psc.case_data;
 
   real ld = psc.coeff.ld;
 
-  x0 = x0 / ld;
-  y0 = y0 / ld;
-  z0 = z0 / ld;
-  Lx = Lx / ld;
-  Ly = Ly / ld;
-  Lz = Lz / ld;
-  widthx = widthx / ld;
-  widthy = widthy / ld;
-  widthz = widthz / ld;
-  rot = rot * 2.*M_PI / 360.;
+  real x0 = langmuir->x0 / ld;
+  real y0 = langmuir->y0 / ld;
+  real z0 = langmuir->z0 / ld;
+  real Lx = langmuir->Lx / ld;
+  real Ly = langmuir->Ly / ld;
+  real Lz = langmuir->Lz / ld;
+  real widthx = langmuir->widthx / ld;
+  real widthy = langmuir->widthy / ld;
+  real widthz = langmuir->widthz / ld;
+  real rot = langmuir->rot * 2.*M_PI / 360.;
 
   real xr = x[0];
   real yr = cos(rot) * (x[1]-y0) - sin(rot) * (x[2]-z0) + y0;
@@ -58,7 +97,7 @@ langmuir_init_nvt(int kind, double x[3], double *q, double *m, double *n,
     break;
   case 1: // ions
     *q = 1.;
-    *m = 1836.;
+    *m = langmuir->mass_ratio;
     *n = dens;
     T[2] = 0.;
     break;
@@ -74,5 +113,7 @@ langmuir_init_nvt(int kind, double x[3], double *q, double *m, double *n,
 
 struct psc_case_ops psc_case_ops_langmuir = {
   .name       = "langmuir",
+  .create     = langmuir_create,
+  .destroy    = langmuir_destroy,
   .init_nvt   = langmuir_init_nvt,
 };
