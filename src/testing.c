d2c6accc648b08ae6a717185cedc9320856579d6
Kai Germaschewski <kai.germaschewski@unh.edu>
2010-08-06 19:13:14 -0400

    test: relax test criteria
    When a number is very close to zero, the relative error is not
    a good measure, so let's use the threshold as an absolute error, too.

--- src/testing.c
+++ src/testing.c
@@ -12,7 +12,10 @@
 void
 __assert_equal(double x, double y, const char *xs, const char *ys, double thres)
 {
-  double max = fmax(fabs(x), fabs(y)) + 1e-10;
+  double max = fmax(fabs(x), fabs(y));
+  if (max < thres)
+    return;
+
   double eps = fabs((x - y) / max);
   if (eps > thres) {
     fprintf(stderr, "assert_equal: fail %s = %g, %s = %g rel err = %g thres = %g\n",
