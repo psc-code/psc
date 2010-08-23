e2f089ddf5e5b79bf28975ea580298046ba157de
Hartmut Ruhl <hartmut.ruhl@physik.uni-muenchen.de>
2010-08-23 10:38:55 -0400

    pml: fix: particles must not be placed in UPML region.
    Modified init_particles.c accordingly.

--- src/init_particles.c
+++ src/init_particles.c
@@ -118,11 +118,44 @@ psc_init_particles(int particle_label_offset)
   double beta = psc.coeff.beta;
   int i = 0;
 
+
+  // particles must not be placed in the pml regions
+  // check if pml bnds are set and restrict avoid particle placement inside pml regions
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank, size;
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &size);
+
+  int bnd_proc_flag[3];
+  int r = rank;
+  bnd_proc_flag[0] = r % psc.domain.nproc[0]; r /= psc.domain.nproc[0];
+  bnd_proc_flag[1] = r % psc.domain.nproc[1]; r /= psc.domain.nproc[1];
+  bnd_proc_flag[2] = r;
+
+  int pml_size_lo[3];
+  int pml_size_hi[3];
+  for (int d = 0; d < 3; d++) {
+    pml_size_lo[d] = 0;
+    pml_size_hi[d] = 0;
+    if (bnd_proc_flag[d] == 0 && // left-most proc in this dir
+	(psc.domain.bnd_fld_lo[d] == BND_FLD_UPML || 
+	 psc.domain.bnd_fld_lo[d] == BND_FLD_TIME)) {
+      pml_size_lo[d] = psc.pml.size+1;
+    }
+    if (bnd_proc_flag[d] == psc.domain.nproc[d] - 1 && // right-most proc in this dir
+	(psc.domain.bnd_fld_hi[d] == BND_FLD_UPML || 
+	 psc.domain.bnd_fld_hi[d] == BND_FLD_TIME)) {
+      pml_size_hi[d] = psc.pml.size+1;
+    }
+  }
+
+
   if (psc.Case->ops->init_npt) {
     for (int kind = 0; kind < 2; kind++) {
-      for (int jz = psc.ilo[2]; jz < psc.ihi[2]; jz++) {
-	for (int jy = psc.ilo[1]; jy < psc.ihi[1]; jy++) {
-	  for (int jx = psc.ilo[0]; jx < psc.ihi[0]; jx++) {
+      for (int jz = psc.ilo[2]+pml_size_lo[2]; jz < psc.ihi[2]-pml_size_hi[2]; jz++) {
+	for (int jy = psc.ilo[1]+pml_size_lo[1]; jy < psc.ihi[1]-pml_size_hi[1]; jy++) {
+	  for (int jx = psc.ilo[0]+pml_size_lo[0]; jx < psc.ihi[0]-pml_size_hi[0]; jx++) {
+
 	    double dx = psc.dx[0], dy = psc.dx[1], dz = psc.dx[2];
 	    double xx[3] = { jx * dx, jy * dy, jz * dz };
 	    struct psc_particle_npt npt = { // init to all zero
