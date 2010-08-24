a5d17a0293e38aca6aea00c173d66ab4f7834df4
Kai Germaschewski <kai.germaschewski@unh.edu>
2010-08-24 00:21:38 -0400

    debug: make HERE macro MPI-aware

--- src/psc.h
+++ src/psc.h
@@ -540,6 +540,6 @@ void SERV_write(particles_fortran_t *pp, fields_fortran_t *pf);
 
 #define sqr(a) ((a) * (a))
 
-#define HERE printf("HERE: in %s() at %s:%d\n", __FUNCTION__, __FILE__, __LINE__)
+#define HERE do { int __rank; MPI_Comm_rank(MPI_COMM_WORLD, &__rank); printf("[%d] HERE: in %s() at %s:%d\n", __rank, __FUNCTION__, __FILE__, __LINE__); } while(0)
 
 #endif
