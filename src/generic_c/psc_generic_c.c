8b10eda6a31dbefe42fa296d240a5dd1135ef3b6
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-02-10 17:03:25 -0500

    psc: add particle pusher xyz in C

--- src/generic_c/psc_generic_c.c
+++ src/generic_c/psc_generic_c.c
@@ -3,6 +3,7 @@
 
 struct psc_ops psc_ops_generic_c = {
   .name                   = "generic_c",
+  .push_part_xyz          = genc_push_part_xyz,
   .push_part_xy           = genc_push_part_xy,
   .push_part_xz           = genc_push_part_xz,
   .push_part_yz           = genc_push_part_yz,
