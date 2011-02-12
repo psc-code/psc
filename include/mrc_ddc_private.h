270655889e3d90cddb67d7919a8667e634230353
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-02-11 23:39:29 -0500

    libmrc/ddc: reintroduce mrc_ddc_private.h

--- include/mrc_ddc_private.h
+++ include/mrc_ddc_private.h
@@ -6,27 +6,28 @@
 
 #define N_DIR (27)
 
-struct mrc_ddc_dir {
-  int dir[3];
-  int recv_ib[3], recv_ie[3];
-  int send_ib[3], send_ie[3];
-  int in[3], n_bnd; // len of bnd box (x,y,z), total
-  int recv_rank;
-  int send_rank;
-  float *recv_buf;
-  float *send_buf;
+struct mrc_ddc_sendrecv {
+  int ilo[3], ihi[3];
+  int rank_nei;
+  int len;
+  void *buf;
+};
+
+struct mrc_ddc_pattern {
+  struct mrc_ddc_sendrecv send[N_DIR];
+  struct mrc_ddc_sendrecv recv[N_DIR];
 };
 
 struct mrc_ddc {
-  struct mrc_ddc_ops *ops;
   MPI_Comm comm;
-  struct mrc_ddc_params params;
-  int rank;  // rank (in comm)
-  int size;  // size (in comm), must == np[0]*np[1]*np[2]
-  int ip[3]; // rank split in terms of crd direction
-  struct mrc_ddc_dir dir[N_DIR];
-  MPI_Request recv_reqs[N_DIR], send_reqs[N_DIR];
-  int n_recv_reqs, n_send_reqs;
+  struct mrc_ddc_params prm;
+  struct mrc_ddc_ops *ops;
+  int rank, size;
+  int proc[3]; // this proc's position in the 3D proc grid
+  struct mrc_ddc_pattern add_ghosts;
+  struct mrc_ddc_pattern fill_ghosts;
+  MPI_Request send_reqs[N_DIR];
+  MPI_Request recv_reqs[N_DIR];
 };
 
 #endif
