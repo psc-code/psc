953bd730e550855fe75d44baeb96ec51cf1d628e
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-03-03 16:19:37 -0500

    libmrc/multi: small test update
    Use diff instead of cmp.

--- src/libmrc/tests/test_mrc_domain_multi_1.sh
+++ src/libmrc/tests/test_mrc_domain_multi_1.sh
@@ -7,13 +7,13 @@ openmpirun -n 4 ./test_mrc_domain_multi --npx 3 --npy 2 --mrc_io_type xdmf2
 TEST=1
 for a in reference_results/$TEST/*.xdmf; do 
     b=`basename $a`
-    cmp $b $a
+    diff -u $a $b
 done
 
 for a in reference_results/$TEST/*.h5.dump; do 
     b=`basename $a .dump`
     h5dump $b > $b.dump
-    cmp $b.dump $a
+    diff -u $a $b.dump
     rm $b.dump
 done
 
