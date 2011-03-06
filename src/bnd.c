91a5752bae865bff7a2618bf39821b49fb995eb9
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-03-05 22:27:53 -0500

    psc/exchg: patchify actually communication routine
    the test now passes with > 1 patch / proc.

--- src/bnd.c
+++ src/bnd.c
@@ -131,7 +131,7 @@ ddc_particles_create(struct mrc_ddc *ddc)
 	  nei->send_buf_size = 8;
 	  nei->send_buf = malloc(nei->send_buf_size * sizeof(*nei->send_buf));
 	  nei->n_send = 0;
-	  mrc_ddc_get_nei_rank_patch(ddc, 0, dir, &nei->rank, &nei->patch);
+	  mrc_ddc_get_nei_rank_patch(ddc, p, dir, &nei->rank, &nei->patch);
 	}
       }
     }
@@ -158,87 +158,117 @@ ddc_particles_queue(struct ddcp_patch *patch, int dir[3], particle_base_t *p)
 static void
 ddc_particles_comm(struct ddc_particles *ddcp, mparticles_base_t *particles)
 {
-  particles_base_t *pp = &particles->p[0];
-
   int sz = sizeof(particle_base_t) / sizeof(particle_base_real_t);
   int dir[3];
 
-  struct ddcp_patch *patch = &ddcp->patches[0];
   // post receives for # particles we'll receive in the next step
-  for (dir[2] = -1; dir[2] <= 1; dir[2]++) {
-    for (dir[1] = -1; dir[1] <= 1; dir[1]++) {
-      for (dir[0] = -1; dir[0] <= 1; dir[0]++) {
-	int dir1 = mrc_ddc_dir2idx(dir);
-	int dir1neg = mrc_ddc_dir2idx((int[3]) { -dir[0], -dir[1], -dir[2] });
-	struct ddcp_nei *nei = &patch->nei[dir1];
-	if (nei->rank < 0) {
-	  ddcp->recv_reqs[dir1] = MPI_REQUEST_NULL;
-	  continue;
+  foreach_patch(p) {
+    struct ddcp_patch *patch = &ddcp->patches[p];
+
+    for (dir[2] = -1; dir[2] <= 1; dir[2]++) {
+      for (dir[1] = -1; dir[1] <= 1; dir[1]++) {
+	for (dir[0] = -1; dir[0] <= 1; dir[0]++) {
+	  int dir1 = mrc_ddc_dir2idx(dir);
+	  int dir1neg = mrc_ddc_dir2idx((int[3]) { -dir[0], -dir[1], -dir[2] });
+	  struct ddcp_nei *nei = &patch->nei[dir1];
+	  if (nei->rank < 0) {
+	    continue;
+	  }
+#if 0
+	  mprintf(":%d irecv # from %d:%d tag %d\n", p, nei->rank, nei->patch,
+		  dir1neg + p * N_DIR);
+#endif
+	  MPI_Irecv(&nei->n_recv, 1, MPI_INT, nei->rank, dir1neg + p * N_DIR,
+		    MPI_COMM_WORLD, &ddcp->recv_reqs[dir1 + p * N_DIR]);
 	}
-	MPI_Irecv(&nei->n_recv, 1, MPI_INT, nei->rank, 2000 + dir1neg,
-		  MPI_COMM_WORLD, &ddcp->recv_reqs[dir1]);
       }
     }
   }
 
   // post sends for # particles and then the actual particles
-  for (dir[2] = -1; dir[2] <= 1; dir[2]++) {
-    for (dir[1] = -1; dir[1] <= 1; dir[1]++) {
-      for (dir[0] = -1; dir[0] <= 1; dir[0]++) {
-	int dir1 = mrc_ddc_dir2idx(dir);
-	struct ddcp_nei *nei = &patch->nei[dir1];
-	if (nei->rank < 0) {
-	  continue;
-	}
+  foreach_patch(p) {
+    struct ddcp_patch *patch = &ddcp->patches[p];
 
-	MPI_Isend(&nei->n_send, 1, MPI_INT, nei->rank, 2000 + dir1,
-		  MPI_COMM_WORLD, &ddcp->send_reqs[dir1]);
-	MPI_Isend(nei->send_buf, sz * nei->n_send, MPI_PARTICLES_BASE_REAL, nei->rank,
-		  2100 + dir1, MPI_COMM_WORLD, &ddcp->sendp_reqs[dir1]);
+    for (dir[2] = -1; dir[2] <= 1; dir[2]++) {
+      for (dir[1] = -1; dir[1] <= 1; dir[1]++) {
+	for (dir[0] = -1; dir[0] <= 1; dir[0]++) {
+	  int dir1 = mrc_ddc_dir2idx(dir);
+	  struct ddcp_nei *nei = &patch->nei[dir1];
+	  if (nei->rank < 0) {
+	    continue;
+	  }
+#if 0
+	  mprintf(":%d isend # to   %d:%d tag %d: len %d\n", p, nei->rank, nei->patch,
+		  dir1 + nei->patch * N_DIR, nei->n_send);
+	  mprintf(":%d isend P to   %d:%d tag %d\n", p, nei->rank, nei->patch,
+		  dir1 + nei->patch * N_DIR);
+#endif
+	  MPI_Isend(&nei->n_send, 1, MPI_INT,
+		    nei->rank, dir1 + nei->patch * N_DIR, MPI_COMM_WORLD,
+		    &ddcp->send_reqs[dir1 + p * N_DIR]);
+	  MPI_Isend(nei->send_buf, sz * nei->n_send, MPI_PARTICLES_BASE_REAL,
+		    nei->rank, dir1 + nei->patch * N_DIR, MPI_COMM_WORLD,
+		    &ddcp->sendp_reqs[dir1 + p * N_DIR]);
+	}
       }
     }
   }
 
-  MPI_Waitall(N_DIR, ddcp->recv_reqs, MPI_STATUSES_IGNORE);
+  MPI_Waitall(psc.nr_patches * N_DIR, ddcp->recv_reqs, MPI_STATUSES_IGNORE);
 
   // calc total # of particles
-  int new_n_particles = patch->head; // particles which stayed on this proc
+  foreach_patch(p) {
+    struct ddcp_patch *patch = &ddcp->patches[p];
+    particles_base_t *pp = &particles->p[p];
+    int new_n_particles = patch->head; // particles which stayed on this proc
 
-  for (dir[2] = -1; dir[2] <= 1; dir[2]++) {
-    for (dir[1] = -1; dir[1] <= 1; dir[1]++) {
-      for (dir[0] = -1; dir[0] <= 1; dir[0]++) {
-	int dir1 = mrc_ddc_dir2idx(dir);
-	struct ddcp_nei *nei = &patch->nei[dir1];
-	if (nei->rank < 0) {
-	  continue;
+    for (dir[2] = -1; dir[2] <= 1; dir[2]++) {
+      for (dir[1] = -1; dir[1] <= 1; dir[1]++) {
+	for (dir[0] = -1; dir[0] <= 1; dir[0]++) {
+	  int dir1 = mrc_ddc_dir2idx(dir);
+	  struct ddcp_nei *nei = &patch->nei[dir1];
+	  if (nei->rank < 0) {
+	    continue;
+	  }
+	  // add the ones we're about to receive
+	  new_n_particles += nei->n_recv;
 	}
-	// add the ones we're about to receive
-	new_n_particles += nei->n_recv;
+      }
+    }
+    particles_base_realloc(pp, new_n_particles);
+  }
+
+  // post particle receives
+  foreach_patch(p) {
+    struct ddcp_patch *patch = &ddcp->patches[p];
+    particles_base_t *pp = &particles->p[p];
+
+    for (dir[2] = -1; dir[2] <= 1; dir[2]++) {
+      for (dir[1] = -1; dir[1] <= 1; dir[1]++) {
+	for (dir[0] = -1; dir[0] <= 1; dir[0]++) {
+	  int dir1 = mrc_ddc_dir2idx(dir);
+	  int dir1neg = mrc_ddc_dir2idx((int[3]) { -dir[0], -dir[1], -dir[2] });
+	  struct ddcp_nei *nei = &patch->nei[dir1];
+	  if (nei->rank < 0) {
+	    continue;
+	  }
+#if 0
+	  mprintf(":%d irecv P from %d:%d tag %d len %d\n", p, nei->rank, nei->patch,
+		  dir1neg + p * N_DIR, nei->n_recv);
+#endif
+	  MPI_Irecv(&pp->particles[patch->head], sz * nei->n_recv,
+		    MPI_PARTICLES_BASE_REAL,
+		    nei->rank, dir1neg + p * N_DIR, MPI_COMM_WORLD,
+		    &ddcp->recv_reqs[dir1 + p * N_DIR]);
+	  patch->head += nei->n_recv;
+	}
       }
     }
   }
-  particles_base_realloc(pp, new_n_particles);
 
-  for (dir[2] = -1; dir[2] <= 1; dir[2]++) {
-    for (dir[1] = -1; dir[1] <= 1; dir[1]++) {
-      for (dir[0] = -1; dir[0] <= 1; dir[0]++) {
-	int dir1 = mrc_ddc_dir2idx(dir);
-	int dir1neg = mrc_ddc_dir2idx((int[3]) { -dir[0], -dir[1], -dir[2] });
-	struct ddcp_nei *nei = &patch->nei[dir1];
-	if (nei->rank < 0) {
-	  continue;
-	}
-	MPI_Irecv(&pp->particles[patch->head], sz * nei->n_recv,
-		  MPI_PARTICLES_BASE_REAL,
-		  nei->rank, 2100 + dir1neg, MPI_COMM_WORLD, &ddcp->recv_reqs[dir1]);
-	patch->head += nei->n_recv;
-      }
-    }
-  }
-
-  MPI_Waitall(N_DIR, ddcp->recv_reqs, MPI_STATUSES_IGNORE);
-  MPI_Waitall(N_DIR, ddcp->send_reqs, MPI_STATUSES_IGNORE);
-  MPI_Waitall(N_DIR, ddcp->sendp_reqs, MPI_STATUSES_IGNORE);
+  MPI_Waitall(psc.nr_patches * N_DIR, ddcp->recv_reqs, MPI_STATUSES_IGNORE);
+  MPI_Waitall(psc.nr_patches * N_DIR, ddcp->send_reqs, MPI_STATUSES_IGNORE);
+  MPI_Waitall(psc.nr_patches * N_DIR, ddcp->sendp_reqs, MPI_STATUSES_IGNORE);
 }
 
 static void
