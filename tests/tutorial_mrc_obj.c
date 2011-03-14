4397ea80bceb483a7780897d889b2270fff937ad
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-03-14 16:43:24 -0400

    tutorial: make "nr_elements" a parameter, view()

--- tests/tutorial_mrc_obj.c
+++ tests/tutorial_mrc_obj.c
@@ -13,9 +13,17 @@ struct vector {
 
 MRC_CLASS_DECLARE(vector, struct vector);
 
+#define VAR(x) (void *)offsetof(struct vector, x)
+static struct param vector_descr[] = {
+  { "nr_elements"       , VAR(nr_elements)      , PARAM_INT(1)  },
+  {},
+};
+#undef VAR
+
 struct mrc_class_vector mrc_class_vector = {
   .name             = "vector",
   .size             = sizeof(struct vector),
+  .param_descr      = vector_descr,
 };
 
 // ======================================================================
@@ -27,8 +35,11 @@ main(int argc, char **argv)
   libmrc_params_init(argc, argv);
 
   struct vector *vec = vector_create(MPI_COMM_WORLD);
+
+  assert(vec->nr_elements == 1);
   vec->nr_elements = 3;
-  assert(vec->nr_elements == 3);
+  vector_view(vec);
+
   vector_destroy(vec);
 
   MPI_Finalize();
