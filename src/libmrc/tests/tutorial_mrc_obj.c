c2e1bdc64642aef524b93c8593d90ae2c937ee11
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-03-14 16:02:59 -0400

    tutorial: ::setup()

--- src/libmrc/tests/tutorial_mrc_obj.c
+++ src/libmrc/tests/tutorial_mrc_obj.c
@@ -2,6 +2,7 @@
 #include <mrc_obj.h>
 #include <mrc_params.h>
 #include <assert.h>
+#include <stdlib.h>
 
 // ======================================================================
 // vector class
@@ -9,9 +10,23 @@
 struct vector {
   struct mrc_obj obj;
   int nr_elements;
+  int *elements;
 };
 
 MRC_CLASS_DECLARE(vector, struct vector);
+
+// ======================================================================
+
+// ----------------------------------------------------------------------
+// vector_setup
+
+void
+_vector_setup(struct vector *vec)
+{
+  vec->elements = calloc(vec->nr_elements, sizeof(*vec->elements));
+}
+
+// ----------------------------------------------------------------------
 
 #define VAR(x) (void *)offsetof(struct vector, x)
 static struct param vector_descr[] = {
@@ -24,6 +39,7 @@ struct mrc_class_vector mrc_class_vector = {
   .name             = "vector",
   .size             = sizeof(struct vector),
   .param_descr      = vector_descr,
+  .setup            = _vector_setup,
 };
 
 // ======================================================================
@@ -36,6 +52,7 @@ main(int argc, char **argv)
 
   struct vector *vec = vector_create(MPI_COMM_WORLD);
   vector_set_from_options(vec);
+  vector_setup(vec);
   vector_view(vec);
   vector_destroy(vec);
 
