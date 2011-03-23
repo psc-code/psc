d0f4c3b5c1e43ee6301bdd095479c812aa4d0102
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-03-22 22:46:34 -0400

    psc/bnd: make wrappers for calling push_fields subclass methods
    lengthy wrappers, but the actual functions look cleaner.

--- src/psc_push_fields.c
+++ src/psc_push_fields.c
@@ -5,57 +5,91 @@
 // ======================================================================
 // forward to subclass
 
+static inline void
+psc_push_fields_push_a_e(struct psc_push_fields *push, mfields_base_t *flds)
+{
+  struct psc_push_fields_ops *ops = psc_push_fields_ops(push);
+  static int pr;
+  if (!pr) {
+    pr = prof_register("push_fields_a_e", 1., 0, 0);
+  }
+  assert(ops->push_a_e);
+  
+  prof_start(pr);
+  ops->push_a_e(push, flds);
+  prof_stop(pr);
+}
+
+static inline void
+psc_push_fields_push_a_h(struct psc_push_fields *push, mfields_base_t *flds)
+{
+  struct psc_push_fields_ops *ops = psc_push_fields_ops(push);
+  static int pr;
+  if (!pr) {
+    pr = prof_register("push_fields_a_h", 1., 0, 0);
+  }
+  assert(ops->push_a_h);
+  
+  prof_start(pr);
+  ops->push_a_h(push, flds);
+  prof_stop(pr);
+}
+
+static inline void
+psc_push_fields_push_b_e(struct psc_push_fields *push, mfields_base_t *flds)
+{
+  struct psc_push_fields_ops *ops = psc_push_fields_ops(push);
+  static int pr;
+  if (!pr) {
+    pr = prof_register("push_fields_b_e", 1., 0, 0);
+  }
+  assert(ops->push_b_e);
+  
+  prof_start(pr);
+  ops->push_b_e(push, flds);
+  prof_stop(pr);
+}
+
+static inline void
+psc_push_fields_push_b_h(struct psc_push_fields *push, mfields_base_t *flds)
+{
+  struct psc_push_fields_ops *ops = psc_push_fields_ops(push);
+  static int pr;
+  if (!pr) {
+    pr = prof_register("push_fields_b_h", 1., 0, 0);
+  }
+  assert(ops->push_b_h);
+  
+  prof_start(pr);
+  ops->push_b_h(push, flds);
+  prof_stop(pr);
+}
+
 void
 psc_push_fields_step_a(struct psc_push_fields *push, mfields_base_t *flds)
 {
-  struct psc_push_fields_ops *ops = psc_push_fields_ops(push);
   if (psc.domain.use_pml) {
     // FIXME, pml routines sehould be split into E, H push + ghost points, too
     // pml could become a separate push_fields subclass
+    struct psc_push_fields_ops *ops = psc_push_fields_ops(push);
     assert(ops->pml_a);
     ops->pml_a(push, flds);
   } else {
-    static int pr_e, pr_h;
-    if (!pr_e) {
-      pr_e = prof_register("push_fields_a_e", 1., 0, 0);
-      pr_h = prof_register("push_fields_a_h", 1., 0, 0);
-    }
-    assert(ops->push_a_e);
-    assert(ops->push_a_h);
-
-    prof_start(pr_e);
-    ops->push_a_e(push, flds);
-    prof_stop(pr_e);
-
-    prof_start(pr_h);
-    ops->push_a_h(push, flds);
-    prof_stop(pr_h);
+    psc_push_fields_push_a_e(push, flds);
+    psc_push_fields_push_a_h(push, flds);
   }
 }
 
 void
 psc_push_fields_step_b(struct psc_push_fields *push, mfields_base_t *flds)
 {
-  struct psc_push_fields_ops *ops = psc_push_fields_ops(push);
   if (psc.domain.use_pml) {
+    struct psc_push_fields_ops *ops = psc_push_fields_ops(push);
     assert(ops->pml_b);
     ops->pml_b(push, flds);
   } else {
-    static int pr_e, pr_h;
-    if (!pr_e) {
-      pr_e = prof_register("push_fields_b_e", 1., 0, 0);
-      pr_h = prof_register("push_fields_b_h", 1., 0, 0);
-    }
-    assert(ops->push_b_h);
-    assert(ops->push_b_e);
-
-    prof_start(pr_e);
-    ops->push_b_h(push, flds);
-    prof_stop(pr_e);
-
-    prof_start(pr_h);
-    ops->push_b_e(push, flds);
-    prof_stop(pr_h);
+    psc_push_fields_push_b_h(push, flds);
+    psc_push_fields_push_b_e(push, flds);
   }
 }
 
