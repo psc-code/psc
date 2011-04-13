bfa7236319516625a9e57cd23a18d5acd29bad23
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-04-12 23:48:02 -0400

    psc/reorg: fix new tests, lots of other bits

--- src/psc_checkpoint.c
+++ src/psc_checkpoint.c
@@ -13,7 +13,7 @@ psc_read_checkpoint(void)
   mpi_printf(MPI_COMM_WORLD, "INFO: Reading checkpoint.\n");
   
   int n_part;
-  SERV_read_1(&psc.timestep, &n_part);
+  //  SERV_read_1(&psc.timestep, &n_part);
   particles_base_realloc(&psc.particles.p[0], n_part);
   psc.particles.p[0].n_part = n_part;
   
@@ -22,7 +22,7 @@ psc_read_checkpoint(void)
   mfields_fortran_t flds;
   fields_fortran_get(&flds, 0, 0, &psc.flds);
 
-  SERV_read_2(&particles.p[0], &flds.f[0]);
+  //  SERV_read_2(&particles.p[0], &flds.f[0]);
 
   particles_fortran_put(&particles, &psc.particles);
   fields_fortran_put(&flds, JXI, HZ + 1, &psc.flds);
@@ -41,7 +41,7 @@ psc_write_checkpoint(void)
   mfields_fortran_t flds;
   fields_fortran_get(&flds, JXI, HZ + 1, &psc.flds);
 
-  SERV_write(&psc, &particles.p[0], &flds.f[0]);
+  //  SERV_write(&psc, &particles.p[0], &flds.f[0]);
 
   particles_fortran_put(&particles, &psc.particles);
   fields_fortran_put(&flds, 0, 0, &psc.flds);
--- src/test_sort.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-set -e
-./test_sort --mod_sort fortran
-./test_sort --mod_sort qsort
