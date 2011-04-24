538779938c7047f9a0ef75c1fc7c8748a0d84e0b
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-04-24 11:54:37 -0400

    libmrc/ts: mv rk2 step into subclass

---
+++ src/libmrc/src/mrc_ts_rk2.c
@@ -0,0 +1,46 @@
+
+#include <mrc_ts_private.h>
+
+struct mrc_ts_rk2 {
+  struct mrc_f1 *xm;
+};
+
+static void
+mrc_ts_rk2_setup(struct mrc_ts *ts)
+{
+  struct mrc_ts_rk2 *rk2 = mrc_to_subobj(ts, struct mrc_ts_rk2);
+  
+  rk2->xm = mrc_f1_duplicate(ts->x);
+}
+
+static void
+mrc_ts_rk2_destroy(struct mrc_ts *ts)
+{
+  struct mrc_ts_rk2 *rk2 = mrc_to_subobj(ts, struct mrc_ts_rk2);
+
+  mrc_f1_destroy(rk2->xm);
+}
+
+static void
+mrc_ts_rk2_step(struct mrc_ts *ts)
+{
+  struct mrc_ts_rk2 *rk2 = mrc_to_subobj(ts, struct mrc_ts_rk2);
+
+  struct mrc_f1 *x = ts->x;
+  struct mrc_f1 *rhs = ts->rhs;
+  struct mrc_f1 *xm = rk2->xm;
+
+  ts->rhsf(ts->ctx, rhs, x);
+  mrc_f1_waxpy(xm, .5 * ts->dt, rhs, x);
+  
+  ts->rhsf(ts->ctx, rhs, xm);
+  mrc_f1_axpy(x, ts->dt, rhs);
+}
+
+struct mrc_ts_ops mrc_ts_rk2_ops = {
+  .name             = "rk2",
+  .size             = sizeof(struct mrc_ts_rk2),
+  .setup            = mrc_ts_rk2_setup,
+  .destroy          = mrc_ts_rk2_destroy,
+  .step             = mrc_ts_rk2_step,
+};
