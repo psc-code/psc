90bb03afbf90c2bd64d4a2407f0121f249eaea94
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-05-09 09:55:58 -0400

    psc: make sub-objs children
    So we don't have to explicitly do standard stuff anymore.

--- src/libpsc/psc.c
+++ src/libpsc/psc.c
@@ -33,16 +33,27 @@ _psc_create(struct psc *psc)
   MPI_Comm comm = psc_comm(psc);
 
   psc->push_particles = psc_push_particles_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->push_particles);
   psc->push_fields = psc_push_fields_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->push_fields);
   psc->bnd = psc_bnd_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->bnd);
   psc->collision = psc_collision_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->collision);
   psc->randomize = psc_randomize_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->randomize);
   psc->sort = psc_sort_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->sort);
   psc->output_fields = psc_output_fields_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->output_fields);
   psc->output_particles = psc_output_particles_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->output_particles);
   psc->moments = psc_moments_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->moments);
   psc->event_generator = psc_event_generator_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->event_generator);
   psc->balance = psc_balance_create(comm);
+  psc_add_child(psc, (struct mrc_obj *) psc->balance);
 
   psc->time_start = MPI_Wtime();
 
@@ -56,18 +67,6 @@ _psc_create(struct psc *psc)
 static void
 _psc_set_from_options(struct psc *psc)
 {
-  psc_push_particles_set_from_options(psc->push_particles);
-  psc_push_fields_set_from_options(psc->push_fields);
-  psc_bnd_set_from_options(psc->bnd);
-  psc_collision_set_from_options(psc->collision);
-  psc_randomize_set_from_options(psc->randomize);
-  psc_sort_set_from_options(psc->sort);
-  psc_output_fields_set_from_options(psc->output_fields);
-  psc_output_particles_set_from_options(psc->output_particles);
-  psc_moments_set_from_options(psc->moments);
-  psc_event_generator_set_from_options(psc->event_generator);
-  psc_balance_set_from_options(psc->balance);
-
   psc_set_from_options_domain(psc);
   psc_set_from_options_psc(psc);
 }
@@ -80,18 +79,6 @@ _psc_setup(struct psc *psc)
 {
   psc_setup_coeff(psc);
   psc_setup_domain(psc); // needs to be done before setting up psc_bnd
-
-  psc_push_particles_setup(psc->push_particles);
-  psc_push_fields_setup(psc->push_fields);
-  psc_bnd_setup(psc->bnd);
-  psc_collision_setup(psc->collision);
-  psc_randomize_setup(psc->randomize);
-  psc_sort_setup(psc->sort);
-  psc_output_fields_setup(psc->output_fields);
-  psc_output_particles_setup(psc->output_particles);
-  psc_moments_setup(psc->moments);
-  psc_event_generator_setup(psc->event_generator);
-  psc_balance_setup(psc->balance);
 }
 
 // ----------------------------------------------------------------------
@@ -102,18 +89,6 @@ _psc_view(struct psc *psc)
 {
   psc_view_psc(psc);
   psc_view_domain(psc);
-
-  psc_push_particles_view(psc->push_particles);
-  psc_push_fields_view(psc->push_fields);
-  psc_bnd_view(psc->bnd);
-  psc_collision_view(psc->collision);
-  psc_randomize_view(psc->randomize);
-  psc_sort_view(psc->sort);
-  psc_output_fields_view(psc->output_fields);
-  psc_output_particles_view(psc->output_particles);
-  psc_moments_view(psc->moments);
-  psc_event_generator_view(psc->event_generator);
-  psc_balance_view(psc->balance);
 }
 
 // ----------------------------------------------------------------------
@@ -125,18 +100,6 @@ _psc_destroy(struct psc *psc)
   mfields_base_destroy(psc->flds);
   mparticles_base_destroy(&psc->particles);
   mphotons_destroy(&psc->mphotons);
-
-  psc_push_particles_destroy(psc->push_particles);
-  psc_push_fields_destroy(psc->push_fields);
-  psc_bnd_destroy(psc->bnd);
-  psc_collision_destroy(psc->collision);
-  psc_randomize_destroy(psc->randomize);
-  psc_sort_destroy(psc->sort);
-  psc_output_fields_destroy(psc->output_fields);
-  psc_output_particles_destroy(psc->output_particles);
-  psc_moments_destroy(psc->moments);
-  psc_event_generator_destroy(psc->event_generator);
-  psc_balance_destroy(psc->balance);
 
   psc_destroy_domain(psc);
 }
