97ae1e638f9baa0ff5958f1f9ed7abeb8d6c5256
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-05-19 16:55:12 -0400

    libmrc/tests: add mrctest_set_crds_multi_rectilinear_1()
    still more work tbd to do this better.

--- src/libmrc/include/mrctest.h
+++ src/libmrc/include/mrctest.h
@@ -41,6 +41,7 @@ struct mrc_domain *mrctest_create_domain_rectilinear(MPI_Comm comm,
 						     struct mrctest_domain_params *par);
 void mrctest_domain_init_values_0(struct mrc_f3 *f);
 void mrctest_domain(void (*mod_domain)(struct mrc_mod *mod, void *arg));
+void mrctest_set_crds_multi_rectilinear_1(struct mrc_domain *domain);
 struct mrc_f3 *mrctest_create_field_1(struct mrc_domain *domain);
 struct mrc_f3 *mrctest_create_field_2(struct mrc_domain *domain);
 struct mrc_m1 *mrctest_create_m1_1(struct mrc_domain *domain, int dim);
