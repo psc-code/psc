a1fbd5a10ff1bb059e152a091e44c059dd96caf5
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-05-30 23:29:09 -0400

    libmrc/ts: add rk4
    Really should go into its own file.

--- src/libmrc/src/mrc_ts_rk2.c
+++ src/libmrc/src/mrc_ts_rk2.c
@@ -47,3 +47,66 @@ struct mrc_ts_ops mrc_ts_rk2_ops = {
   .destroy          = mrc_ts_rk2_destroy,
   .step             = mrc_ts_rk2_step,
 };
+
+// ======================================================================
+
+struct mrc_ts_rk4 {
+  struct mrc_f1 *xt;
+  struct mrc_f1 *xk[4];
+};
+
+static void
+mrc_ts_rk4_setup(struct mrc_ts *ts)
+{
+  struct mrc_ts_rk4 *rk4 = mrc_to_subobj(ts, struct mrc_ts_rk4);
+  
+  rk4->xt = mrc_f1_duplicate(ts->x);
+  for (int k = 0; k < 4; k++) {
+    rk4->xk[k] = mrc_f1_duplicate(ts->x);
+  }
+}
+
+static void
+mrc_ts_rk4_destroy(struct mrc_ts *ts)
+{
+  struct mrc_ts_rk4 *rk4 = mrc_to_subobj(ts, struct mrc_ts_rk4);
+
+  mrc_f1_destroy(rk4->xt);
+  for (int k = 0; k < 4; k++) {
+    mrc_f1_destroy(rk4->xk[k]);
+  }
+}
+
+static void
+mrc_ts_rk4_step(struct mrc_ts *ts)
+{
+  struct mrc_ts_rk4 *rk4 = mrc_to_subobj(ts, struct mrc_ts_rk4);
+
+  struct mrc_f1 *x = ts->x;
+  struct mrc_f1 *xt = rk4->xt;
+  struct mrc_f1 **xk = rk4->xk;
+
+  mrc_ts_rhsf(ts, xk[0], ts->time, x);
+
+  mrc_f1_waxpy(xt, .5 * ts->dt, xk[0], x);
+  mrc_ts_rhsf(ts, xk[1], ts->time + .5 * ts->dt, xt);
+    
+  mrc_f1_waxpy(xt, .5 * ts->dt, xk[1], x);
+  mrc_ts_rhsf(ts, xk[2], ts->time + .5 * ts->dt, xt);
+  
+  mrc_f1_waxpy(xt, ts->dt, xk[2], x);
+  mrc_ts_rhsf(ts, xk[3], ts->time + ts->dt, xt);
+
+  mrc_f1_axpy(x, 1./6. * ts->dt, xk[0]);
+  mrc_f1_axpy(x, 1./3. * ts->dt, xk[1]);
+  mrc_f1_axpy(x, 1./3. * ts->dt, xk[2]);
+  mrc_f1_axpy(x, 1./6. * ts->dt, xk[3]);
+}
+
+struct mrc_ts_ops mrc_ts_rk4_ops = {
+  .name             = "rk4",
+  .size             = sizeof(struct mrc_ts_rk4),
+  .setup            = mrc_ts_rk4_setup,
+  .destroy          = mrc_ts_rk4_destroy,
+  .step             = mrc_ts_rk4_step,
+};
