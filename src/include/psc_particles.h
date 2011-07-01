11b26a4de3251c45a9ff5dc1471a45fd7388dd8b
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-07-01 09:33:47 -0400

    psc/mparticles: rm some unused defines

--- src/include/psc_particles.h
+++ src/include/psc_particles.h
@@ -53,9 +53,7 @@ typedef particle_fortran_t particle_base_t;
 typedef particle_fortran_real_t particle_base_real_t;
 #define MPI_PARTICLES_BASE_REAL MPI_PARTICLES_FORTRAN_REAL
 
-#define particles_base_alloc   particles_fortran_alloc
 #define particles_base_realloc particles_fortran_realloc
-#define particles_base_free    particles_fortran_free
 #define particles_base_get_one particles_fortran_get_one
 #define psc_mparticles_base_create  psc_mparticles_fortran_create
 #define psc_mparticles_base_set_domain_nr_particles psc_mparticles_fortran_set_domain_nr_particles
@@ -70,9 +68,7 @@ typedef particle_c_t particle_base_t;
 typedef particle_c_real_t particle_base_real_t;
 #define MPI_PARTICLES_BASE_REAL    MPI_PARTICLES_C_REAL
 
-#define particles_base_alloc   particles_c_alloc
 #define particles_base_realloc particles_c_realloc
-#define particles_base_free    particles_c_free
 #define particles_base_get_one particles_c_get_one
 #define psc_mparticles_base_create  psc_mparticles_c_create
 #define psc_mparticles_base_set_domain_nr_particles psc_mparticles_c_set_domain_nr_particles
@@ -87,9 +83,7 @@ typedef particle_sse2_t particle_base_t;
 typedef particle_sse2_real_t particle_base_real_t;
 #define MPI_PARTICLES_BASE_REAL    MPI_PARTICLES_SSE2_REAL
 
-#define particles_base_alloc   particles_sse2_alloc
 #define particles_base_realloc particles_sse2_realloc
-#define particles_base_free    particles_sse2_free
 #define particles_base_get_one particles_sse2_get_one
 #define psc_mparticles_base_create  psc_mparticles_sse2_create
 #define psc_mparticles_base_set_domain_nr_particles psc_mparticles_sse2_set_domain_nr_particles
@@ -104,9 +98,7 @@ typedef particle_cbe_t particle_base_t;
 typedef particle_cbe_real_t particle_base_real_t;
 #define MPI_PARTICLES_BASE_REAL    MPI_PARTICLES_CBE_REAL
 
-#define particles_base_alloc   particles_cbe_alloc
 #define particles_base_realloc particles_cbe_realloc
-#define particles_base_free    particles_cbe_free
 #define particles_base_get_one particles_cbe_get_one
 #define psc_mparticles_base_create  psc_mparticles_cbe_create
 #define psc_mparticles_base_set_domain_nr_particles psc_mparticles_cbe_set_domain_nr_particles
