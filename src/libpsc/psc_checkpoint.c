3d7f83db1a48a12373952a0d0f53b74454428eb4
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-07-03 05:25:19 -0400

    psc/checkpoint: use xdmf_serial for checkpointing
    That needs work, too, but in the end will be easier as each proc just
    dumps its own state independently.

--- src/libpsc/psc_checkpoint.c
+++ src/libpsc/psc_checkpoint.c
@@ -13,7 +13,7 @@ psc_read_checkpoint(MPI_Comm comm)
   mpi_printf(MPI_COMM_WORLD, "INFO: Reading checkpoint.\n");
 
   struct mrc_io *io = mrc_io_create(comm);
-  mrc_io_set_type(io, "xdmf_collective");
+  mrc_io_set_type(io, "xdmf_serial");
   mrc_io_set_name(io, "checkpoint");
   mrc_io_set_param_string(io, "basename", "checkpoint");
   mrc_io_set_from_options(io);
@@ -35,7 +35,7 @@ psc_write_checkpoint(struct psc *psc)
   mpi_printf(psc_comm(psc), "INFO: Writing checkpoint.\n");
 
   struct mrc_io *io = mrc_io_create(psc_comm(psc));
-  mrc_io_set_type(io, "xdmf_collective");
+  mrc_io_set_type(io, "xdmf_serial");
   mrc_io_set_name(io, "checkpoint");
   mrc_io_set_param_string(io, "basename", "checkpoint");
   mrc_io_set_from_options(io);
