126962572b174e82b6dc54ffba84aeaac95bdf66
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-08-28 16:33:23 -0400

    psc/patchmanager: C++ clean
    'this' is C++ style, but C++ really doesn't like it in C code..

--- src/include/psc_patchmanager.h
+++ src/include/psc_patchmanager.h
@@ -13,21 +13,21 @@ struct psc_patchmanager
   struct psc_case* currentcase;
 };
 
-void psc_patchmanager_set_from_options(struct psc_patchmanager *this);
+void psc_patchmanager_set_from_options(struct psc_patchmanager *self);
 
 ///This creates a patchmanager assuming \a np patches along each axis
 ///\note Make sure, \a np equals the \a np given to mrc_domain
-void psc_patchmanager_setup(struct psc_patchmanager *this, int np[3]);
+void psc_patchmanager_setup(struct psc_patchmanager *self, int np[3]);
 
 ///Use this to deallocate the patchmanager
-void psc_patchmanager_destroy(struct psc_patchmanager *this);
+void psc_patchmanager_destroy(struct psc_patchmanager *self);
 
 ///This function needs to be called every timestep to advance all windows
 ///and reset the domain if neccessary
-void psc_patchmanager_timestep(struct psc_patchmanager* this);
+void psc_patchmanager_timestep(struct psc_patchmanager* self);
 
 ///\brief Creates a new domain-window
 ///
 ///Use this function during your case::setup() or afterwards to create new domain-windows
 ///\param type The type of window to create. Currently, only "movingwindow_z" is supported
-struct psc_domainwindow* psc_patchmanager_create_window(struct psc_patchmanager *this, const char* type);
+struct psc_domainwindow* psc_patchmanager_create_window(struct psc_patchmanager *self, const char* type);
