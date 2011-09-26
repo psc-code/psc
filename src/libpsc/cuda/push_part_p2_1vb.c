b5bf45ec8161eb5e557e40261b7b94154a37fa31
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-09-25 20:45:20 -0400

    psc/cuda: more consolidation / get rid of shape_info for 1vb

--- src/libpsc/cuda/push_part_p2_1vb.c
+++ src/libpsc/cuda/push_part_p2_1vb.c
@@ -27,7 +27,7 @@ __shared__ volatile bool do_calc_jz;
 
 #define WARPS_PER_BLOCK (THREADS_PER_BLOCK / 32)
 
-__shared__ int _cell_end[WARPS_PER_BLOCK]; // last particle in current cell valid in p2x
+__shared__ int s_cell_end;
 
 #define xBLOCKSTRIDE ((((BLOCKSIZE_Y + 2*SW) * (BLOCKSIZE_Z + 2*SW) + 31) / 32) * 32)
 __shared__ real scurr1[WARPS_PER_BLOCK * xBLOCKSTRIDE];
@@ -38,96 +38,9 @@ __shared__ real scurr2[WARPS_PER_BLOCK * xBLOCKSTRIDE];
 
 #define w_scurr(scurr, jy, jz) w_scurr_(scurr, threadIdx.x >> 5, jy, jz)
 
-#define w_cell_end (_cell_end[threadIdx.x >> 5])
-
-#if CACHE_SHAPE_ARRAYS == 7
-
-struct shapeinfo_h {
-  real xm[2], xp[2];
-};
-
-struct shapeinfo_i {
-  char4 shiftyz; // x: y[0] y: y[1] z: z[0] w: z[1]
-};
-
-#define DECLARE_SHAPE_INFO			\
-  struct shapeinfo_h _si_h, *si_h = &_si_h;	\
-  struct shapeinfo_i _si_i, *si_i = &_si_i
-
-#define SHAPE_INFO_PARAMS si_h, si_i
-#define SHAPE_INFO_ARGS struct shapeinfo_h *si_h, struct shapeinfo_i *si_i
-
-#define D_SHAPEINFO_PARAMS d_si_h, d_si_i
-#define D_SHAPEINFO_ARGS struct shapeinfo_h *d_si_h, struct shapeinfo_i *d_si_i
-
-#define SI_SHIFT0Y si_i->shiftyz.x
-#define SI_SHIFT1Y si_i->shiftyz.y
-#define SI_SHIFT10Y (si_i->shiftyz.y - si_i->shiftyz.x)
-#define SI_SHIFT0Z si_i->shiftyz.z
-#define SI_SHIFT1Z si_i->shiftyz.w
-#define SI_SHIFT10Z (si_i->shiftyz.w - si_i->shiftyz.z)
-
-__device__ static void
-cache_shape_arrays(SHAPE_INFO_ARGS, real *xm, real *xp,
-		   short int shift0y, short int shift0z,
-		   short int shift1y, short int shift1z)
-{
-  si_h->xm[0] = xm[1];
-  si_h->xm[1] = xm[2];
-  si_h->xp[0] = xp[1];
-  si_h->xp[1] = xp[2];
-  SI_SHIFT0Y = shift0y;
-  SI_SHIFT1Y = shift1y;
-  SI_SHIFT0Z = shift0z;
-  SI_SHIFT1Z = shift1z;
-}
-
-// ======================================================================
-#else
-#error
-#endif
-
 __shared__ int ci0[3]; // cell index of lower-left cell in block
 
 // ======================================================================
-
-// ----------------------------------------------------------------------
-// calc_shape_info
-
-__device__ static void
-calc_shape_info(int i, particles_cuda_dev_t d_particles,
-		real *vxi, real *qni_wni, SHAPE_INFO_ARGS)
-{
-  short int shift0y;
-  short int shift0z;
-  short int shift1y;
-  short int shift1z;
-  real h0[3], h1[3];
-  real xm[3], xp[3];
-
-  struct d_particle p;
-  LOAD_PARTICLE(p, d_particles, i);
-  *qni_wni = p.qni_wni;
-  
-  int j[3], k[3];
-  calc_vxi(vxi, p);
-  
-  // x^(n+1.0), p^(n+1.0) -> x^(n+0.5), p^(n+1.0) 
-  push_xi(&p, vxi, -.5f * d_dt);
-  find_idx_off_pos_1st(p.xi, j, h0, xm, real(0.));
-  
-  // x^(n+0.5), p^(n+1.0) -> x^(n+1.5), p^(n+1.0) 
-  push_xi(&p, vxi, d_dt);
-  find_idx_off_pos_1st(p.xi, k, h1, xp, real(0.));
-  
-  shift0y = j[1] - ci0[1];
-  shift0z = j[2] - ci0[2];
-  shift1y = k[1] - ci0[1];
-  shift1z = k[2] - ci0[2];
-
-  cache_shape_arrays(SHAPE_INFO_PARAMS, xm, xp, shift0y, shift0z,
-		     shift1y, shift1z);
-}
 
 // ======================================================================
 
@@ -225,52 +138,74 @@ curr_2d_vb_cell(int i[2], real x[2], real dx[2], real qni_wni,
 }
 
 __device__ static void
-yz_calc_jyjz(real qni_wni, SHAPE_INFO_ARGS)
+yz_calc_jyjz(int i, particles_cuda_dev_t d_particles)
 {
-  int i[2] = { SI_SHIFT0Y + ci0[1], SI_SHIFT0Z + ci0[2] };
-  int idiff[2] = { SI_SHIFT1Y - SI_SHIFT0Y, SI_SHIFT1Z - SI_SHIFT0Z };
-  real *xp = si_h->xp, *xm = si_h->xm;
-  real dx[2] = { xp[0] - xm[0], xp[1] - xm[1] };
-  real x[2] = { xm[0] - (i[0] + real(.5)), xm[1] - (i[1] + real(.5)) };
+  struct d_particle p;
 
-  i[0] -= ci0[1]; i[1] -= ci0[2];
-
-  real dx1[2];
-  int off[2];
-  int first_dir, second_dir = -1;
-  // FIXME, make sure we never div-by-zero?
-  if (idiff[0] == 0 && idiff[1] == 0) {
-    first_dir = -1;
-  } else if (idiff[0] == 0) {
-    first_dir = 1;
-  } else if (idiff[1] == 0) {
-    first_dir = 0;
-  } else {
-    dx1[0] = .5 * idiff[0] - x[0];
-    dx1[1] = dx[1] / dx[0] * dx1[0];
-    if (fabsf(x[1] + dx1[1]) > .5f) {
-      first_dir = 1;
-    } else {
-      first_dir = 0;
-    }
-    second_dir = 1 - first_dir;
+  if (do_read) {
+    LOAD_PARTICLE(p, d_particles, i);
   }
 
-  if (first_dir >= 0) {
-    off[1-first_dir] = 0;
-    off[first_dir] = idiff[first_dir];
-    calc_dx1(dx1, x, dx, off);
-    curr_2d_vb_cell(i, x, dx1, qni_wni, dx, off);
-  }
-
-  if (second_dir >= 0) {
-    off[first_dir] = 0;
-    off[second_dir] = idiff[second_dir];
-    calc_dx1(dx1, x, dx, off);
-    curr_2d_vb_cell(i, x, dx1, qni_wni, dx, off);
-  }
+  if (do_calc_jy) {
+    real vxi[3];
+    real h0[3], h1[3];
+    real xm[3], xp[3];
     
-  curr_2d_vb_cell(i, x, dx, qni_wni, NULL, NULL);
+    int j[3], k[3];
+    calc_vxi(vxi, p);
+    
+    // x^(n+1.0), p^(n+1.0) -> x^(n+0.5), p^(n+1.0) 
+    push_xi(&p, vxi, -.5f * d_dt);
+    find_idx_off_pos_1st(p.xi, j, h0, xm, real(0.));
+    
+    // x^(n+0.5), p^(n+1.0) -> x^(n+1.5), p^(n+1.0) 
+    push_xi(&p, vxi, d_dt);
+    find_idx_off_pos_1st(p.xi, k, h1, xp, real(0.));
+    
+    int idiff[2] = { k[1] - j[1], k[2] - j[2] };
+    real dx[2] = { xp[1] - xm[1], xp[2] - xm[2] };
+    real x[2] = { xm[1] - (j[1] + real(.5)), xm[2] - (j[2] + real(.5)) };
+    int i[2] = { j[1] - ci0[1], j[2] - ci0[2] };
+  
+    int off[2];
+    int first_dir, second_dir = -1;
+    // FIXME, make sure we never div-by-zero?
+    if (idiff[0] == 0 && idiff[1] == 0) {
+      first_dir = -1;
+    } else if (idiff[0] == 0) {
+      first_dir = 1;
+    } else if (idiff[1] == 0) {
+      first_dir = 0;
+    } else {
+      real dx1[2];
+      dx1[0] = .5 * idiff[0] - x[0];
+      dx1[1] = dx[1] / dx[0] * dx1[0];
+      if (fabsf(x[1] + dx1[1]) > .5f) {
+	first_dir = 1;
+      } else {
+	first_dir = 0;
+      }
+      second_dir = 1 - first_dir;
+    }
+    
+    if (first_dir >= 0) {
+      real dx1[2];
+      off[1-first_dir] = 0;
+      off[first_dir] = idiff[first_dir];
+      calc_dx1(dx1, x, dx, off);
+      curr_2d_vb_cell(i, x, dx1, p.qni_wni, dx, off);
+    }
+    
+    if (second_dir >= 0) {
+      real dx1[2];
+      off[first_dir] = 0;
+      off[second_dir] = idiff[second_dir];
+      calc_dx1(dx1, x, dx, off);
+      curr_2d_vb_cell(i, x, dx1, p.qni_wni, dx, off);
+    }
+    
+    curr_2d_vb_cell(i, x, dx, p.qni_wni, NULL, NULL);
+  }
 }
 
 __device__ static void
@@ -328,9 +263,9 @@ add_scurr_to_flds1(real *scurr, real *d_flds, int m)
 
 #define push_part_p2_2							\
   int cell_begin = d_particles.c_offsets[bid * cells_per_block];	\
-  w_cell_end = d_particles.c_offsets[(bid + 1) * cells_per_block];	\
+  s_cell_end = d_particles.c_offsets[(bid + 1) * cells_per_block];	\
   									\
-  for (int i = cell_begin + tid; i < w_cell_end; i += THREADS_PER_BLOCK)
+  for (int i = cell_begin + tid; i < s_cell_end; i += THREADS_PER_BLOCK)
 
 // ======================================================================
 
@@ -371,15 +306,7 @@ push_part_p2y(int n_particles, particles_cuda_dev_t d_particles, real *d_flds,
 
   push_part_p2_1 {
     push_part_p2_2 {
-      DECLARE_SHAPE_INFO;
-      real vxi[3];
-      real qni_wni;
-      if (do_read) {
-	calc_shape_info(i, d_particles, vxi, &qni_wni, SHAPE_INFO_PARAMS);
-      }
-      if (do_calc_jy) {
-	yz_calc_jyjz(qni_wni, SHAPE_INFO_PARAMS);
-      }
+      yz_calc_jyjz(i, d_particles);
     }
   }
   if (do_write) {
