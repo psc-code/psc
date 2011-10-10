8e7924cb360e3a17ee165e8d69c129a19d7179cf
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-10-10 13:18:49 -0400

    psc/moments: add "1st" order density calculation
    Could be generally useful because it's faster, and in particular
    useful for figuring out whether the continuity equation holds for
    the 1st order currents.

--- src/libpsc/psc_moments/psc_moments.c
+++ src/libpsc/psc_moments/psc_moments.c
@@ -10,7 +10,7 @@ psc_moments_calc_densities(struct psc_moments *moments,
 			   mfields_c_t *res)
 {
   struct psc_moments_ops *ops = psc_moments_ops(moments);
-  assert(ops->calc_densities);
+  assert(ops && ops->calc_densities);
   ops->calc_densities(moments, flds, particles, res);
 }
 
@@ -20,7 +20,7 @@ psc_moments_calc_v(struct psc_moments *moments,
 		   mfields_c_t *res)
 {
   struct psc_moments_ops *ops = psc_moments_ops(moments);
-  assert(ops->calc_v);
+  assert(ops && ops->calc_v);
   ops->calc_v(moments, flds, particles, res);
 }
 
@@ -30,7 +30,7 @@ psc_moments_calc_vv(struct psc_moments *moments,
 		    mfields_c_t *res)
 {
   struct psc_moments_ops *ops = psc_moments_ops(moments);
-  assert(ops->calc_vv);
+  assert(ops && ops->calc_vv);
   ops->calc_vv(moments, flds, particles, res);
 }
 
@@ -39,7 +39,7 @@ psc_moments_calc_photon_n(struct psc_moments *moments,
 			  mphotons_t *photons, mfields_c_t *res)
 {
   struct psc_moments_ops *ops = psc_moments_ops(moments);
-  assert(ops->calc_photon_n);
+  assert(ops && ops->calc_photon_n);
   ops->calc_photon_n(moments, photons, res);
 }
 
@@ -50,6 +50,7 @@ static void
 psc_moments_init()
 {
   mrc_class_register_subclass(&mrc_class_psc_moments, &psc_moments_c_ops);
+  mrc_class_register_subclass(&mrc_class_psc_moments, &psc_moments_1st_ops);
   mrc_class_register_subclass(&mrc_class_psc_moments, &psc_moments_fortran_ops);
 }
 
