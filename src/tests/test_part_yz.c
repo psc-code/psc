677e0d41812da2a9f247d2cd24c1ecab226e9507
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-10-10 23:19:30 -0400

    psc/tests: test_part_yz cleanup
    Use more of the existing functionality.

--- src/tests/test_part_yz.c
+++ src/tests/test_part_yz.c
@@ -11,8 +11,6 @@
 #include <string.h>
 #include <mpi.h>
 
-static bool check_currents = true;
-
 static struct psc_case *
 create_test(const char *s_push_particles)
 {
@@ -24,33 +22,6 @@ create_test(const char *s_push_particles)
   psc_bnd_exchange_particles(ppsc->bnd, ppsc->particles);
   psc_sort_run(ppsc->sort, ppsc->particles);
   return _case;
-}
-
-static void
-run_test(bool is_ref, const char *s_push_particles, double eps_particles, double eps_fields)
-{
-  printf("=== testing push_part_yz() %s %s\n", s_push_particles,
-	 is_ref ? "(ref)" : "");
-
-  struct psc_case *_case = create_test(s_push_particles);
-  psc_testing_dump(ppsc, s_push_particles);
-  psc_push_particles_run(ppsc->push_particles, ppsc->particles, ppsc->flds);
-
-  psc_bnd_exchange_particles(ppsc->bnd, ppsc->particles);
-  psc_sort_run(ppsc->sort, ppsc->particles);
-  psc_testing_dump(ppsc, s_push_particles);
-
-  psc_check_continuity(ppsc, ppsc->particles, ppsc->flds, 1e-14);
-  if (is_ref) {
-    psc_save_particles_ref(ppsc, ppsc->particles);
-    psc_save_fields_ref(ppsc, ppsc->flds);
-  } else {
-    psc_check_particles_ref(ppsc, ppsc->particles, eps_particles, "push_part_yz()");
-    if (check_currents) { // only check currents for full pusher
-      psc_check_currents_ref(ppsc, ppsc->flds, eps_fields);
-    }
-  }
-  psc_case_destroy(_case);
 }
 
 // ----------------------------------------------------------------------
@@ -68,14 +39,19 @@ main(int argc, char **argv)
 {
   psc_testing_init(&argc, &argv);
 
-  mrc_params_get_option_bool("check_currents", &check_currents);
-
   mrc_params_get_option_string("type", &s_type);
   mrc_params_get_option_double("eps_particles", &eps_particles);
   mrc_params_get_option_double("eps_particles", &eps_fields);
 
-  run_test(true, "fortran", 0., 0.);
-  run_test(false, s_type, eps_particles, eps_fields);
+  struct psc_case *_case = create_test("fortran");
+  psc_testing_push_particles(ppsc, "fortran");
+  psc_testing_save_ref(ppsc);
+  psc_case_destroy(_case);
+
+  _case = create_test(s_type);
+  psc_testing_push_particles(ppsc, s_type);
+  psc_testing_push_particles_check(ppsc, eps_particles, eps_fields);
+  psc_case_destroy(_case);
 
   psc_testing_finalize();
 }
