73e4428c5611ffba147d5f189caedfa71a4e8214
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-10-11 11:36:11 -0400

    psc/tests: single-particle case in test_part_xy
    May help in figuring out why things fail..

--- src/tests/test_part_yz.c
+++ src/tests/test_part_yz.c
@@ -2,6 +2,37 @@
 #include "psc_testing.h"
 #include <mrc_params.h>
 
+static void
+psc_test_setup_particles(struct psc *psc, int *nr_particles_by_patch, bool count_only)
+{
+  assert(psc->nr_patches == 1);
+
+  if (count_only) {
+    nr_particles_by_patch[0] = 1;
+    return;
+  }
+
+  particles_c_t *pp = psc_mparticles_get_patch_c(psc->particles, 0);
+  pp->n_part = nr_particles_by_patch[0];
+  for (int i = 0; i < pp->n_part; i++) {
+    particle_c_t *prt = particles_c_get_one(pp, 0);
+    prt->qni = 1.; prt->mni = 1.; prt->wni = 1.;
+    prt->yi = prt->zi = .5;
+  }
+}
+
+// ======================================================================
+// psc_test_ops_1a
+
+static struct psc_ops psc_test_ops_1a = {
+  .name             = "test",
+  .size             = sizeof(struct psc_test),
+  .create           = psc_test_create,
+  .init_field       = psc_test_init_field_linear,
+  .setup_particles  = psc_test_setup_particles,
+  .step             = psc_test_step,
+};
+
 // ----------------------------------------------------------------------
 // check push_particles_push_yz against "fortran" ref
 
@@ -15,6 +46,8 @@ static double eps_particles = 1e-7;
 static double eps_fields = 1e-7;
 // which moment calculation to use for charge continuity
 static const char *s_moments = "c";
+// which test case to set up particles / fields
+static const char *s_case = "1";
 
 int
 main(int argc, char **argv)
@@ -26,8 +59,15 @@ main(int argc, char **argv)
   mrc_params_get_option_double("eps_particles", &eps_particles);
   mrc_params_get_option_double("eps_fields", &eps_fields);
   mrc_params_get_option_string("moments", &s_moments);
+  mrc_params_get_option_string("case", &s_case);
 
-  mrc_class_register_subclass(&mrc_class_psc, &psc_test_ops_1);
+  if (strcmp(s_case, "1") == 0) {
+    mrc_class_register_subclass(&mrc_class_psc, &psc_test_ops_1);
+  } else if (strcmp(s_case, "1a") == 0) {
+    mrc_class_register_subclass(&mrc_class_psc, &psc_test_ops_1a);
+  } else {
+    assert(0);
+  }
 
   struct psc *psc = psc_testing_create_test_yz(s_ref_type, 0, s_moments);
   psc_setup(psc);
