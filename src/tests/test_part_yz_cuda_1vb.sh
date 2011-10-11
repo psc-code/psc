fc34ae0b814cef6587878e831799f7efb76cde01
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-10-11 10:22:32 -0400

    psc/tests: update test_part_yz_cuda_1vb.sh

--- src/tests/test_part_yz_cuda_1vb.sh
+++ src/tests/test_part_yz_cuda_1vb.sh
@@ -1,5 +1,9 @@
 # test "cuda_1vb" against "1vb"
 
+# particle order is different, so we can't compare particles directly,
+# however comparing densities works out.
+
 ./test_part_yz \
     --ref_type 1vb --type cuda_1vb --moments 1st \
-    --eps_particles 1e-6 --eps_fields 2e-4
+    --eps_particles 1e-6 --check_particles false \
+    --eps_fields 2e-4
