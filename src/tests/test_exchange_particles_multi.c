1d61ed5d78d0b7d1fe966ef33fdbdb5fdcb520ed
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-10-16 18:59:50 -0400

    psc/tests: revamp test_exchange_particles_multi
    use new-style testing and two testing scripts

--- src/tests/test_exchange_particles_multi.c
+++ src/tests/test_exchange_particles_multi.c
@@ -10,58 +10,51 @@
 #include <mpi.h>
 #include <string.h>
 
-void
-setup_particles(mparticles_base_t *particles_base)
+static void
+psc_test_setup_particles(struct psc *psc, int *nr_particles_by_patch, bool count_only)
 {
-  mparticles_t *particles = psc_mparticles_get_cf(particles_base, MP_DONT_COPY);
-
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  // FIXME, realloc
-  // only particles on proc 0 / patch 0, but some are out of bounds.
-  // particles are right on nodes of the grid, but as far as
-  // particle -> cell mapping goes, that's right in the center of the
-  // particle ordering cell, for reasons that should be laid out more clearly.
-  // for the old ordering, nodes aren't good because they're indeterminate
-  // (could go either way), so let's shift them a bit so we get a unique answer
-  // we can check.
-
-  psc_foreach_patch(ppsc, p) {
-    particles_t *pp = psc_mparticles_get_patch(particles, p);
-    if (rank != 0 || p != 0) {
-      pp->n_part = 0;
-      continue;
-    }
-
+  for (int p = 0; p < psc->nr_patches; p++) {
     struct psc_patch *patch = &ppsc->patch[p];
-    int *ilo = patch->off;
-    int ihi[3] = { patch->off[0] + patch->ldims[0],
-		   patch->off[1] + patch->ldims[1],
-		   patch->off[2] + patch->ldims[2] };
+ 
     int i = 0;
-    
-    for (int iz = ilo[2]-1; iz < ihi[2]+1; iz++) {
-      for (int iy = ilo[1]; iy < ihi[1]; iy++) { // xz only !!!
-	for (int ix = ilo[0]-1; ix < ihi[0]+1; ix++) {
-	  particle_t *p;
-	  p = particles_get_one(pp, i++);
-	  memset(p, 0, sizeof(*p));
-	  p->xi = (ix + .51) * ppsc->dx[0];
-	  p->yi = (iy + .51) * ppsc->dx[1];
-	  p->zi = (iz + .51) * ppsc->dx[2];
-	  
-	  p = particles_get_one(pp, i++);
-	  memset(p, 0, sizeof(*p));
-	  p->xi = (ix + .49) * ppsc->dx[0];
-	  p->yi = (iy + .49) * ppsc->dx[1];
-	  p->zi = (iz + .49) * ppsc->dx[2];
+    if (p == 0) { // initially, only particles on first patch
+      int *ilo = patch->off;
+      int ihi[3] = { patch->off[0] + patch->ldims[0],
+		     patch->off[1] + patch->ldims[1],
+		     patch->off[2] + patch->ldims[2] };
+      for (int iz = ilo[2]-1; iz < ihi[2]+1; iz++) {
+	for (int iy = ilo[1]; iy < ihi[1]; iy++) { // xz only !!!
+	  for (int ix = ilo[0]-1; ix < ihi[0]+1; ix++) {
+	    if (!count_only) {
+	      particles_c_t *pp = psc_mparticles_get_patch_c(psc->particles, p);
+	      particle_t *p;
+	      p = particles_get_one(pp, i);
+	      memset(p, 0, sizeof(*p));
+	      p->qni = -1.; p->mni = 1.; p->wni = 1.;
+	      p->xi = (ix + .5) * ppsc->dx[0];
+	      p->yi = (iy + .5) * ppsc->dx[1];
+	      p->zi = (iz + .5) * ppsc->dx[2];
+	      
+	      p = particles_get_one(pp, i + 1);
+	      memset(p, 0, sizeof(*p));
+	      p->qni = 1.; p->mni = 100.; p->wni = 1.;
+	      p->xi = (ix + .5) * ppsc->dx[0];
+	      p->yi = (iy + .5) * ppsc->dx[1];
+	      p->zi = (iz + .5) * ppsc->dx[2];
+	    }
+	    i += 2;
+	  }
 	}
       }
     }
-    pp->n_part = i;
+
+    if (count_only) {
+      nr_particles_by_patch[p] = i;
+    } else {
+      particles_c_t *pp = psc_mparticles_get_patch_c(psc->particles, p);
+      pp->n_part = i;
+    }
   }
-  psc_mparticles_put_cf(particles, particles_base);
 }
 
 // FIXME, make generic
@@ -84,26 +77,37 @@ get_total_num_particles(mparticles_base_t *particles_base)
   return total_nr_part;
 }
 
+// ======================================================================
+// psc_test_ops_xz
+
+static struct psc_ops psc_test_ops_xz = {
+  .name             = "test",
+  .size             = sizeof(struct psc_test),
+  .create           = psc_test_create,
+  .setup_particles  = psc_test_setup_particles,
+};
+
 int
 main(int argc, char **argv)
 {
   psc_testing_init(&argc, &argv);
 
-  // test psc_exchange_particles()
+  mrc_class_register_subclass(&mrc_class_psc, &psc_test_ops_xz);
 
-  struct psc_case *_case = psc_create_test_xz();
-  psc_bnd_set_type(ppsc->bnd, "c");
-  psc_case_setup(_case);
-  mparticles_base_t *particles = ppsc->particles;
-  setup_particles(particles);
-  //  psc_dump_particles("part-0");
-  int total_num_particles_before = get_total_num_particles(particles);
-  psc_bnd_exchange_particles(ppsc->bnd, particles);
-  //  psc_dump_particles("part-1");
-  int total_num_particles_after = get_total_num_particles(particles);
-  psc_check_particles(particles);
+  struct psc *psc = psc_testing_create_test_xz();
+  psc_setup(psc);
+  if (opt_testing_dump) {
+    psc_dump_particles(psc->particles, "part-0");
+  }
+  int total_num_particles_before = get_total_num_particles(psc->particles);
+  psc_bnd_exchange_particles(psc->bnd, psc->particles);
+  if (opt_testing_dump) {
+    psc_dump_particles(psc->particles, "part-1");
+  }
+  int total_num_particles_after = get_total_num_particles(psc->particles);
+  psc_check_particles(psc->particles);
   assert(total_num_particles_before == total_num_particles_after);
-  psc_case_destroy(_case);
+  psc_destroy(psc);
 
   psc_testing_finalize();
 }
---
+++ src/tests/test_exchange_particles_multi_0.sh
@@ -0,0 +1,3 @@
+# test particle exchange single proc, single patch, psc_bnd "c"
+
+./test_exchange_particles_multi --npx 1 --psc_bnd_type c
