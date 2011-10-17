d69fcde3c744985b142419d3d696230399272167
Kai Germaschewski <kai.germaschewski@unh.edu>
2011-10-16 22:45:41 -0400

    psc/tests: add missing declaration

--- src/include/psc_testing.h
+++ src/include/psc_testing.h
@@ -17,6 +17,8 @@ void psc_check_fields_ref(struct psc *psc, mfields_base_t *flds, int *m_flds, do
 void psc_check_particles_ref(struct psc *psc, mparticles_base_t *particles,
 			     double thres, const char *test_str);
 void psc_check_particles_sorted(struct psc *psc, mparticles_base_t *particles);
+void psc_testing_check_densities_ref(struct psc *psc, struct psc_mparticles *particles,
+				     double eps);
 
 void psc_testing_push_particles(struct psc *psc, const char *s_push_particles);
 void psc_testing_save_ref(struct psc *psc);
