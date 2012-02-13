003af33498177ef7b53a7fc55871dd51ea62a51d
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-02-13 09:33:32 -0500

    psc/harris: base length units on d_i

--- src/psc_harris.c
+++ src/psc_harris.c
@@ -12,21 +12,21 @@
 
 // Plasma simulation parameters
 //   needed because they determine length scales for initial conditions
-// BB:  peak Harris magnetic field  (magnitude gives ratio w_ce/w_pe)
-// nnb: number of background particles (density max == 1)
-// TTe,TTi:  bulk temperature of electrons and ions (units m_e c^2)
-// MMi: ion mass / electron mass
-// LLL = reversal length scale (units of c/wpe)
-// LLz, LLx = simulation box size (units of c/wpe)
-// AA = perturbation (units of B * de)
+// B0:  peak Harris magnetic field  (magnitude gives ratio w_ce/w_pe)
+// nb:  background particle density
+// Te, Ti:  bulk temperature of electrons and ions (units m_e c^2)
+// mi_over_me: ion mass / electron mass
+// lambda = shear length scale (units of d_i)
+// ly, lz = simulation box size (units of d_i)
+// pert = perturbation (units of B * d_i)
 
 // FIXME (description), below parameters don't include scaling factors
 
 struct psc_harris {
-  double BB;
-  double nnb;
+  double B0;
+  double nb;
   double Te, Ti;
-  double MMi;
+  double mi_over_me;
   double lambda;
   double lz, ly;
   double pert;
@@ -36,9 +36,9 @@ struct psc_harris {
 
 #define VAR(x) (void *)offsetof(struct psc_harris, x)
 static struct param psc_harris_descr[] = {
-  { "BB"            , VAR(BB)              , PARAM_DOUBLE(1.)     },
-  { "MMi"           , VAR(MMi)             , PARAM_DOUBLE(25.)    },
-  { "nnb"           , VAR(nnb)             , PARAM_DOUBLE(.2)     },
+  { "B0"            , VAR(B0)              , PARAM_DOUBLE(1.)     },
+  { "mi_over_me"    , VAR(mi_over_me)      , PARAM_DOUBLE(25.)    },
+  { "nb"            , VAR(nb)              , PARAM_DOUBLE(.2)     },
   { "Te"            , VAR(Te)              , PARAM_DOUBLE(1./12.) },
   { "Ti"            , VAR(Ti)              , PARAM_DOUBLE(5./12.) },
   { "lambda"        , VAR(lambda)          , PARAM_DOUBLE(.5)     },
@@ -73,11 +73,9 @@ psc_harris_create(struct psc *psc)
 
   ppsc->prm.nicell = 50;
 
-  // FIXME, won't know if MMi is changed
-  real d_i = sqrt(harris->MMi); // in units of d_e
   ppsc->domain.length[0] = 1.; // no x dependence 
-  ppsc->domain.length[1] = 2. * harris->ly * d_i; // double tearing
-  ppsc->domain.length[2] = harris->lz * d_i;
+  ppsc->domain.length[1] = 2. * harris->ly; // double tearing
+  ppsc->domain.length[2] = harris->lz;
 
   ppsc->domain.gdims[0] = 1;
   ppsc->domain.gdims[1] = 640;
@@ -102,26 +100,25 @@ psc_harris_init_field(struct psc *psc, double x[3], int m)
 {
   struct psc_harris *harris = to_psc_harris(psc);
 
-  real d_i = sqrt(harris->MMi); // in units of d_e
-  double BB = harris->BB;
-  double LLz = harris->lz * d_i, LLy = harris->ly * d_i;
-  double LLL = harris->lambda * d_i;
-  double AA = harris->pert * BB * d_i;
+  double B0 = harris->B0;
+  double lz = harris->lz, ly = harris->ly;
+  double lambda = harris->lambda;
+  double AA = harris->pert * B0;
 
   switch (m) {
   case HZ:
     return
-      BB * (-1. + tanh((x[1] - 0.5*LLy) / LLL)- tanh((x[1] - 1.5*LLy) / LLL))
-      + AA * M_PI/LLy * sin(2.*M_PI * x[2] / LLz) * cos(M_PI * x[1] / LLy);
+      B0 * (-1. + tanh((x[1] - 0.5*ly) / lambda)- tanh((x[1] - 1.5*ly) / lambda))
+      + AA * M_PI/ly * sin(2.*M_PI * x[2] / lz) * cos(M_PI * x[1] / ly);
 
   case HY:
-    return - AA * 2.*M_PI / LLz * cos(2.*M_PI * x[2] / LLz) * sin(M_PI * x[1] / LLy);
+    return - AA * 2.*M_PI / lz * cos(2.*M_PI * x[2] / lz) * sin(M_PI * x[1] / ly);
 
   case JXI:
-    return BB / LLL *
-      (1./sqr(cosh((x[1] - 0.5*LLy) / LLL)) - 1./sqr(cosh((x[1] - 1.5*LLy) / LLL)))
-      - (AA*sqr(M_PI) * (1./sqr(LLy) + 4./sqr(LLz)) 
-	 * sin(2.*M_PI * x[2] / LLz) * sin(M_PI * x[1] / LLy));
+    return B0 / lambda *
+      (1./sqr(cosh((x[1] - 0.5*ly) / lambda)) - 1./sqr(cosh((x[1] - 1.5*ly) / lambda)))
+      - (AA*sqr(M_PI) * (1./sqr(ly) + 4./sqr(lz)) 
+	 * sin(2.*M_PI * x[2] / lz) * sin(M_PI * x[1] / ly));
 
   default: return 0.;
   }
@@ -136,30 +133,30 @@ psc_harris_init_npt(struct psc *psc, int kind, double x[3],
 {
   struct psc_harris *harris = to_psc_harris(psc);
 
-  real d_i = sqrt(harris->MMi); // in units of d_e
-  double BB = harris->BB;
-  double LLy = harris->ly * d_i;
-  double LLL = harris->lambda * d_i;
-  double nnb = harris->nnb;
-  double TTi = harris->Ti * sqr(BB);
-  double TTe = harris->Te * sqr(BB);
+  double B0 = harris->B0;
+  double ly = harris->ly;
+  double lambda = harris->lambda;
+  double nb = harris->nb;
+  double TTi = harris->Ti * sqr(B0) * harris->mi_over_me; // FIXME, why???
+  double TTe = harris->Te * sqr(B0) * harris->mi_over_me;
 
-  double jx0 = 1./sqr(cosh((x[1] - 0.5*LLy) / LLL)) - 1./sqr(cosh((x[1] - 1.5*LLy) / LLL));
+  double jx0 = B0 / lambda *
+    (1./sqr(cosh((x[1] - 0.5*ly) / lambda)) - 1./sqr(cosh((x[1] - 1.5*ly) / lambda)));
 
-  npt->n = nnb + 1./sqr(cosh((x[1] - 0.5*LLy) / LLL)) + 1./sqr(cosh((x[1] - 1.5*LLy) / LLL));
+  npt->n = nb + 1./sqr(cosh((x[1] - 0.5*ly) / lambda)) + 1./sqr(cosh((x[1] - 1.5*ly) / lambda));
   switch (kind) {
   case 0: // electrons
     npt->q = -1.;
-    npt->m = 1.;
-    npt->p[0] = - 2. * TTe / BB / LLL * jx0 / npt->n;
+    npt->m = 1. / harris->mi_over_me;
+    npt->p[0] = - 2. * TTe / B0 / lambda * (jx0 / (B0 / lambda)) / npt->n;
     npt->T[0] = TTe;
     npt->T[1] = TTe;
     npt->T[2] = TTe;
     break;
   case 1: // ions
     npt->q = 1.;
-    npt->m = harris->MMi;
-    npt->p[0] = 2. * TTi / BB / LLL * jx0 / npt->n;
+    npt->m = 1.;
+    npt->p[0] = 2. * TTi / B0 / lambda * (jx0 / B0 / lambda) / npt->n;
     npt->T[0] = TTi;
     npt->T[1] = TTi;
     npt->T[2] = TTi;
