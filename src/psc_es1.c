7ce2a0562d580bb70477868df8d5a0af76d60724
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-03-13 08:29:53 -0400

    es1: add two-stream instability

--- src/psc_es1.c
+++ src/psc_es1.c
@@ -13,13 +13,18 @@
 #include <math.h>
 #include <time.h>
 
+// plasma oscillation
 // src/psc_es1 --psc_output_particles_type ascii --psc_output_particles_every_step 1 --mrc_io_type ascii --pfield_step 1 --psc_diag_every_step 1
+
+// two-stream
+// src/psc_es1 --psc_output_particles_type ascii --psc_output_particles_every_step 10 --mrc_io_type ascii --write_tfield no --pfield_step 10 --psc_diag_every_step 10 --nr_kinds 2 --v0_1 1. --v0_2 -1. --cc 5. --nicell 32
 
 struct psc_es1 {
   // parameters
   double om_pe;
   double om_ce;
-  int nlg; // number of loading groups
+  // species 1
+  int nlg_1; // number of loading groups
   double q_1;
   double m_1;
   double mode_1;
@@ -28,6 +33,16 @@ struct psc_es1 {
   double v1_1;
   double thetax_1;
   double thetav_1;
+  // species 2
+  int nlg_2; // number of loading groups
+  double q_2;
+  double m_2;
+  double mode_2;
+  double v0_2;
+  double x1_2;
+  double v1_2;
+  double thetax_2;
+  double thetav_2;
 };
 
 #define to_psc_es1(psc) mrc_to_subobj(psc, struct psc_es1)
@@ -36,7 +51,8 @@ struct psc_es1 {
 static struct param psc_es1_descr[] = {
   { "om_pe"         , VAR(om_pe)           , PARAM_DOUBLE(1.)            },
   { "om_ce"         , VAR(om_ce)           , PARAM_DOUBLE(2.)            },
-  { "nlg"           , VAR(nlg)             , PARAM_INT(1)                },
+
+  { "nlg_1"         , VAR(nlg_1)           , PARAM_INT(1)                },
   { "q_1"           , VAR(q_1)             , PARAM_DOUBLE(-1.)           },
   { "m_1"           , VAR(m_1)             , PARAM_DOUBLE(1.)            },
   { "mode_1"        , VAR(mode_1)          , PARAM_DOUBLE(1.)            },
@@ -45,6 +61,16 @@ static struct param psc_es1_descr[] = {
   { "v1_1"          , VAR(v1_1)            , PARAM_DOUBLE(0.)            },
   { "thetax_1"      , VAR(thetax_1)        , PARAM_DOUBLE(0.)            },
   { "thetav_1"      , VAR(thetav_1)        , PARAM_DOUBLE(0.)            },
+
+  { "nlg_2"         , VAR(nlg_2)           , PARAM_INT(1)                },
+  { "q_2"           , VAR(q_2)             , PARAM_DOUBLE(-1.)           },
+  { "m_2"           , VAR(m_2)             , PARAM_DOUBLE(1.)            },
+  { "mode_2"        , VAR(mode_2)          , PARAM_DOUBLE(1.)            },
+  { "v0_2"          , VAR(v0_2)            , PARAM_DOUBLE(0.)            },
+  { "x1_2"          , VAR(x1_2)            , PARAM_DOUBLE(.001)          },
+  { "v1_2"          , VAR(v1_2)            , PARAM_DOUBLE(0.)            },
+  { "thetax_2"      , VAR(thetax_2)        , PARAM_DOUBLE(0.)            },
+  { "thetav_2"      , VAR(thetav_2)        , PARAM_DOUBLE(0.)            },
   {},
 };
 #undef VAR
@@ -95,20 +121,6 @@ psc_es1_create(struct psc *psc)
 }
 
 // ----------------------------------------------------------------------
-// psc_es1_setup
-//
-// the parameters are now set, calculate quantities to initialize fields,
-// particles
-
-static void
-psc_es1_setup(struct psc *psc)
-{
-  //  struct psc_es1 *es1 = to_psc_es1(psc);
-
-  psc_setup_default(psc);
-}
-
-// ----------------------------------------------------------------------
 // psc_es1_init_field
 
 static double
@@ -116,10 +128,30 @@ psc_es1_init_field(struct psc *psc, double x[3], int m)
 {
   struct psc_es1 *es1 = to_psc_es1(psc);
 
-  double theta = 2. * M_PI * es1->mode_1 / psc->domain.length[2] * x[2];
 
   switch (m) {
-  case EZ: return - es1->x1_1 * cos(theta);
+  case EZ: ;
+    double ez = 0;
+    for (int kind = 0; kind < psc->prm.nr_kinds; kind++) {
+      double q, mode, x1, thetax;
+      switch (kind) {
+      case 0:
+	q = es1->q_1;
+	mode = es1->mode_1; x1 = es1->x1_1; thetax = es1->thetax_1;
+	break;
+      case 1:
+	q = es1->q_2;
+	mode = es1->mode_2; x1 = es1->x1_2; thetax = es1->thetax_2;
+	break;
+      default:
+	assert(0);
+      }
+      // FIXME, charge needs to go in here
+      double theta = 2. * M_PI * es1->mode_1 / psc->domain.length[2] * x[2];
+      ez += q * x1 * cos(theta + thetax);
+    }
+    return ez;
+
   default: return 0.;
   }
 }
@@ -160,30 +192,47 @@ psc_es1_setup_particles(struct psc *psc, int *nr_particles_by_patch,
 
   psc_foreach_patch(psc, p) {
     particles_t *pp = psc_mparticles_get_patch(particles, p);
+
     int il1 = 0;
-
-    int *ldims = psc->patch[p].ldims;
-    int n = ldims[0] * ldims[1] * ldims[2] * psc->prm.nicell;
-    int ngr = n / es1->nlg;
-    //double lg = l / nlg;
-    double ddx = l / n;
-    for (int i = 0; i < ngr; i++) {
-      particle_t *p = particles_get_one(pp, il1 + i);
-      double x0 = (i + .5) * ddx;
-
-      p->zi = x0;
-      p->pzi = es1->v0_1;
-      p->qni = es1->q_1;
-      p->mni = es1->m_1;
-      p->wni = 1.;
+    for (int kind = 0; kind < psc->prm.nr_kinds; kind++) {
+      int nlg;
+      double q, m, mode, v0, x1, v1, thetax, thetav;
+      switch (kind) { // FIXME, the horror...
+      case 0:
+      nlg = es1->nlg_1; q = es1->q_1; m = es1->m_1; mode = es1->mode_1;
+      v0 = es1->v0_1; x1 = es1->x1_1; v1 = es1->v1_1;
+      thetax = es1->thetax_1; thetav = es1->thetav_1;
+      break;
+      case 1:
+	nlg = es1->nlg_2; q = es1->q_2; m = es1->m_2; mode = es1->mode_2;
+	v0 = es1->v0_2; x1 = es1->x1_2; v1 = es1->v1_2;
+	thetax = es1->thetax_2; thetav = es1->thetav_2;
+	break;
+      default: assert(0);
+      }
+      int *ldims = psc->patch[p].ldims;
+      int n = ldims[0] * ldims[1] * ldims[2] * psc->prm.nicell;
+      int ngr = n / nlg;
+      //double lg = l / nlg;
+      double ddx = l / n;
+      for (int i = 0; i < ngr; i++) {
+	particle_t *p = particles_get_one(pp, il1 + i);
+	double x0 = (i + .5) * ddx;
+	
+	p->zi = x0;
+	p->pzi = v0 / psc->prm.cc;
+	p->qni = q;
+	p->mni = m;
+	p->wni = 1.;
+      }
+      for (int i = 0; i < n; i++) {
+	particle_t *p = particles_get_one(pp, il1++);
+	double theta = 2. * M_PI * mode * p->zi / l;
+	p->zi  += x1 * cos(theta + thetax);
+	p->pzi += v1 * sin(theta + thetav);
+      }
     }
-    for (int i = 0; i < n; i++) {
-      particle_t *p = particles_get_one(pp, il1 + i);
-      double theta = 2. * M_PI * es1->mode_1 * p->zi / l;
-      p->zi  += es1->x1_1 * cos(theta + es1->thetax_1);
-      p->pzi += es1->v1_1 * sin(theta + es1->thetav_1);
-    }
-    pp->n_part = ngr;
+    pp->n_part = il1;
     assert(pp->n_part == nr_particles_by_patch[p]);
   }
   psc_mparticles_put_cf(particles, psc->particles);
@@ -197,7 +246,6 @@ struct psc_ops psc_es1_ops = {
   .size             = sizeof(struct psc_es1),
   .param_descr      = psc_es1_descr,
   .create           = psc_es1_create,
-  .setup            = psc_es1_setup,
   .init_field       = psc_es1_init_field,
   .setup_particles  = psc_es1_setup_particles,
 };
