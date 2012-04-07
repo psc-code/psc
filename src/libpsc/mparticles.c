2c7626f50d137609215ba7a59c63a2cd66002ac8
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-04-07 16:21:12 -0400

    mparticles: delete some old code
    That isn't working anymore, anyway.

--- src/libpsc/mparticles.c
+++ src/libpsc/mparticles.c
@@ -6,67 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAKE_MPARTICLES_METHODS(type)					\
-									\
-static void								\
-_psc_mparticles_##type##_set_domain_nr_particles(mparticles_##type##_t *mparticles, \
-						 struct mrc_domain *domain, \
-						 int *nr_particles_by_patch) \
-{									\
-  mparticles->domain = domain;						\
-  mrc_domain_get_patches(domain, &mparticles->nr_patches);		\
-									\
-  mparticles->data = calloc(mparticles->nr_patches,			\
-			    sizeof(particles_##type##_t));		\
-  for (int p = 0; p < mparticles->nr_patches; p++) {			\
-    particles_##type##_alloc(&mparticles->data[p],			\
-			     nr_particles_by_patch[p]);			\
-  }									\
-}									\
-									\
-static void								\
-_psc_mparticles_##type##_destroy(mparticles_##type##_t *mparticles)	\
-{									\
-  for (int p = 0; p < mparticles->nr_patches; p++) {			\
-    particles_##type##_free(&mparticles->data[p]);			\
-  }									\
-  free(mparticles->data);						\
-}									\
-									\
-static int									\
-_psc_mparticles_##type##_nr_particles_by_patch(mparticles_##type##_t *mparticles, \
-					      int p)			\
-{									\
-  return psc_mparticles_get_patch_##type(mparticles, p)->n_part;	\
-}									\
-									\
-struct psc_mparticles_##type##_ops psc_mparticles_##type##_ops = {	\
-  .name                    = #type,					\
-  .set_domain_nr_particles = _psc_mparticles_##type##_set_domain_nr_particles, \
-  .nr_particles_by_patch = _psc_mparticles_##type##_nr_particles_by_patch, \
-};									\
-									\
-static void								\
-psc_mparticles_##type##_init()						\
-{									\
-  mrc_class_register_subclass(&mrc_class_psc_mparticles_##type, &psc_mparticles_##type##_ops); \
-}									\
-									\
-struct mrc_class_psc_mparticles_##type mrc_class_psc_mparticles_##type = {	\
-  .name             = "psc_mparticles_" #type,				\
-  .size             = sizeof(struct psc_mparticles_##type),		\
-  .init		    = psc_mparticles_##type##_init,			\
-  .destroy          = _psc_mparticles_##type##_destroy,			\
-};									\
-
-
-#ifdef USE_SSE2
-MAKE_MPARTICLES_METHODS(sse2)
-#endif
-#ifdef USE_CBE
-MAKE_MPARTICLES_METHODS(cbe)
-#endif
-
 // ======================================================================
 
 void
