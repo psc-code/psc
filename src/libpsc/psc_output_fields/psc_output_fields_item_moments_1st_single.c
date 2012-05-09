5d37339c1d56f3086dfd13ffea8fb8f4496189b3
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-05-09 00:08:29 -0400

    psc/output: mv newer "1st" moments -> psc_output_fields/

--- src/libpsc/psc_output_fields/psc_output_fields_item_moments_1st_single.c
+++ src/libpsc/psc_output_fields/psc_output_fields_item_moments_1st_single.c
@@ -7,7 +7,7 @@
 // !!! These moments are shifted to (n+.5) * dt, rather than n * dt,
 // since the "single" particles are shifted that way.
 
-#include "psc_output_fields_item_n_1st.c"
+#include "psc_output_fields_item_moments_1st.c"
 
 // ======================================================================
 // psc_output_fields_item: subclass "n_1st_single"
