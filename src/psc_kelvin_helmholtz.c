6e422390e556b236ca7aaf8662193040660bdf9d
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-05-10 12:43:03 -0400

    psc/output: fix psc/kh to not default to the removed 1st_cc moment type

--- src/psc_kelvin_helmholtz.c
+++ src/psc_kelvin_helmholtz.c
@@ -114,7 +114,6 @@ psc_kh_create(struct psc *psc)
 
   // FIXME: can only use 1st order pushers with current conducting wall b.c.
   psc_push_particles_set_type(psc->push_particles, "1vb");
-  psc_moments_set_type(psc->moments, "c_1st_cc");
 }
 
 // ----------------------------------------------------------------------
