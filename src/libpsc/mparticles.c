8008d79cd4f344b326fd3337a10f92794108d567
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-05-18 22:01:58 -0400

    particles: allocate patches separately

--- src/libpsc/mparticles.c
+++ src/libpsc/mparticles.c
@@ -31,10 +31,9 @@ _psc_mparticles_setup(struct psc_mparticles *mparticles)
   struct psc_mparticles_ops *ops = psc_mparticles_ops(mparticles);
 
   if (ops->alloc_patch) {
-    char *data = calloc(mparticles->nr_patches, ops->size_of_particles_t);
     mparticles->patches = calloc(mparticles->nr_patches, sizeof(*mparticles->patches));
     for (int p = 0; p < mparticles->nr_patches; p++) {
-      mparticles->patches[p] = data + p * ops->size_of_particles_t;
+      mparticles->patches[p] = calloc(1, ops->size_of_particles_t);
       ops->alloc_patch(mparticles, p, mparticles->nr_particles_by_patch[p]);
     }
 
