3ec145b7be26c8d76d61a62f6442e78e43861690
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-05-28 13:35:54 -0400

    psc: add conversion to "single" for "cuda" particles

--- src/libpsc/cuda/psc_particles_cuda.c
+++ src/libpsc/cuda/psc_particles_cuda.c
@@ -3,6 +3,7 @@
 #include "psc_cuda.h"
 #include "psc_bnd_cuda.h"
 #include "psc_particles_cuda.h"
+#include "psc_particles_single.h"
 #include "psc_push_particles.h"
 
 EXTERN_C void cuda_init(int rank);
@@ -114,6 +115,9 @@ blockIdx_to_blockCrd(struct psc_patch *patch, struct cell_map *map,
     bi[d] /= blocksize[d];
   }
 }
+
+// ======================================================================
+// conversion to "c"
 
 static void
 psc_particles_cuda_copy_from_c(struct psc_particles *prts_cuda,
@@ -319,6 +323,152 @@ psc_particles_cuda_copy_to_c(struct psc_particles *prts_cuda,
 }
 
 // ======================================================================
+// conversion to "single"
+
+static void
+psc_particles_cuda_copy_from_single(struct psc_particles *prts_cuda,
+				    struct psc_particles *prts, unsigned int flags)
+{
+  int p = prts_cuda->p;
+  struct psc_patch *patch = &ppsc->patch[p];
+  struct psc_particles_cuda *cuda = psc_particles_cuda(prts_cuda);
+  prts_cuda->n_part = prts->n_part;
+  assert(prts_cuda->n_part <= cuda->n_alloced);
+  
+  float4 *xi4  = calloc(prts->n_part, sizeof(float4));
+  float4 *pxi4 = calloc(prts->n_part, sizeof(float4));
+  
+  for (int n = 0; n < prts->n_part; n++) {
+    particle_single_t *part = particles_single_get_one(prts, n);
+    
+    real qni_wni = part->qni_wni;
+    // FIXME!!! KH hack
+    if (part->kind == 1 || part->kind == 3) {
+      qni_wni *= (1 + 1e-6);
+    }
+    
+    xi4[n].x  = part->xi;
+    xi4[n].y  = part->yi;
+    xi4[n].z  = part->zi;
+    xi4[n].w  = ppsc->kinds[part->kind].q / ppsc->kinds[part->kind].m;
+    pxi4[n].x = part->pxi;
+    pxi4[n].y = part->pyi;
+    pxi4[n].z = part->pzi;
+    pxi4[n].w = qni_wni;
+    
+    float xi[3] = { xi4[n].x, xi4[n].y, xi4[n].z };
+    for (int d = 0; d < 3; d++) {
+      int bi = cuda_fint(xi[d] * cuda->b_dxi[d]);
+      if (bi < 0 || bi >= cuda->b_mx[d]) {
+	printf("XXX p %d xi %g %g %g\n", p, xi[0], xi[1], xi[2]);
+	printf("XXX p %d n %d d %d xi4[n] %g biy %d // %d\n",
+	       p, n, d, xi[d], bi, cuda->b_mx[d]);
+	if (bi < 0) {
+	  xi[d] = 0.f;
+	} else {
+	  xi[d] *= (1. - 1e-6);
+	}
+      }
+      bi = cuda_fint(xi[d] * cuda->b_dxi[d]);
+      assert(bi >= 0 && bi < cuda->b_mx[d]);
+    }
+    xi4[n].x = xi[0];
+    xi4[n].y = xi[1];
+    xi4[n].z = xi[2];
+  }
+  
+  int bs[3];
+  for (int d = 0; d < 3; d++) {
+    bs[d] = cuda->blocksize[d];
+    if (bs[d] != 1) {
+      bs[d] *= 2; // sort not only within blocks, but also on lowest block
+      // bit, so we can do the checkerboard passes
+    }
+  }
+  struct cell_map map;
+  cell_map_init(&map, patch->ldims, bs); // FIXME, already have it elsewhere
+  
+  // FIXME, could be computed on the cuda side
+  int *c_pos = calloc(map.N * 3, sizeof(*c_pos));
+  for (int cidx = 0; cidx < map.N; cidx++) {
+    int ci[3];
+    cell_map_1to3(&map, cidx, ci);
+    c_pos[3*cidx + 0] = ci[0];
+    c_pos[3*cidx + 1] = ci[1];
+    c_pos[3*cidx + 2] = ci[2];
+  }
+  
+  cell_map_free(&map);
+  
+  __particles_cuda_to_device(prts_cuda, xi4, pxi4, NULL, NULL, c_pos);
+  
+  if (prts_cuda->flags & MP_NEED_BLOCK_OFFSETS) {
+    cuda_sort_patch(p, prts_cuda);
+  }
+  if (prts_cuda->flags & MP_NEED_CELL_OFFSETS) {
+    cuda_sort_patch_by_cell(p, prts_cuda);
+  }
+  // FIXME, sorting twice because we need both would be suboptimal
+  if ((prts_cuda->flags & MP_NEED_CELL_OFFSETS) && 
+      (prts_cuda->flags & MP_NEED_BLOCK_OFFSETS)) {
+    MHERE;
+  }
+  
+  free(c_pos);
+  free(xi4);
+  free(pxi4);
+}
+
+static void
+psc_particles_cuda_copy_to_single(struct psc_particles *prts_cuda,
+				  struct psc_particles *prts, unsigned int flags)
+{
+  struct psc_particles_single *sngl = psc_particles_single(prts);
+  prts->n_part = prts_cuda->n_part;
+  assert(prts->n_part <= sngl->n_alloced);
+  
+  float4 *xi4  = calloc(prts_cuda->n_part, sizeof(float4));
+  float4 *pxi4 = calloc(prts_cuda->n_part, sizeof(float4));
+  
+  __particles_cuda_from_device(prts_cuda, xi4, pxi4);
+  
+  for (int n = 0; n < prts->n_part; n++) {
+    particle_c_real_t qni_div_mni = xi4[n].w;
+    particle_c_real_t qni_wni = pxi4[n].w;
+    particle_c_real_t qni, mni, wni;
+    unsigned int kind;
+    if (qni_div_mni == 0.) {
+      qni = 0.;
+      wni = qni_wni;
+      mni = -1.;
+      assert(0); // can't recover the mass of a neutral particle
+    } else {
+      qni = qni_div_mni > 0 ? 1. : -1.;
+      mni = qni / qni_div_mni;
+      wni = qni_wni / qni;
+      kind = (qni > 0.) ? 2 : 0;
+      if (wni > 1. + .5e-7) {
+	kind++;
+      }
+      wni = 1.f;
+    }
+    
+    particle_single_t *part_base = particles_single_get_one(prts, n);
+    part_base->xi  = xi4[n].x;
+    part_base->yi  = xi4[n].y;
+    part_base->zi  = xi4[n].z;
+    part_base->pxi = pxi4[n].x;
+    part_base->pyi = pxi4[n].y;
+    part_base->pzi = pxi4[n].z;
+    part_base->qni_wni = qni * wni;
+    part_base->kind = kind;
+  }
+
+  free(xi4);
+  free(pxi4);
+}
+
+// ======================================================================
 // psc_mparticles: subclass "cuda"
   
 struct psc_mparticles_ops psc_mparticles_cuda_ops = {
@@ -329,8 +479,10 @@ struct psc_mparticles_ops psc_mparticles_cuda_ops = {
 // psc_particles: subclass "cuda"
 
 static struct mrc_obj_method psc_particles_cuda_methods[] = {
-  MRC_OBJ_METHOD("copy_to_c",  psc_particles_cuda_copy_to_c),
-  MRC_OBJ_METHOD("copy_from_c",psc_particles_cuda_copy_from_c),
+  MRC_OBJ_METHOD("copy_to_c"       , psc_particles_cuda_copy_to_c),
+  MRC_OBJ_METHOD("copy_from_c"     , psc_particles_cuda_copy_from_c),
+  MRC_OBJ_METHOD("copy_to_single"  , psc_particles_cuda_copy_to_single),
+  MRC_OBJ_METHOD("copy_from_single", psc_particles_cuda_copy_from_single),
   {}
 };
 
