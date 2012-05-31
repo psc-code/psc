119698b3c54ee0a7c6af5c94ee54209148e498b7
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-05-31 01:26:58 -0400

    psc/push_fields: "auto" default to "single2"

--- src/libpsc/psc_push_fields/psc_push_fields_auto.c
+++ src/libpsc/psc_push_fields/psc_push_fields_auto.c
@@ -16,7 +16,11 @@ psc_push_fields_auto_setup(struct psc_push_fields *push)
 
   const char *mflds_type = psc_mfields_type(ppsc->flds);
   char s[10 + strlen(mflds_type)];
-  sprintf(s, "%s", mflds_type);
+  if (strcmp(mflds_type, "single") == 0) {
+    sprintf(s, "single2");
+  } else {
+    sprintf(s, "%s", mflds_type);
+  }
 
   MPI_Comm comm = psc_push_fields_comm(push);
   mpi_printf(comm, "INFO: using psc_push_fields '%s'\n", s);
@@ -30,43 +34,29 @@ psc_push_fields_auto_setup(struct psc_push_fields *push)
 }
 
 static void
-psc_push_fields_auto_push_a_E(struct psc_push_fields *push,
-			      struct psc_fields *flds_base)
+psc_push_fields_auto_step_a(struct psc_push_fields *push,
+			    struct psc_mfields *mflds)
 {
   struct sub *sub = sub(push);
   struct psc_push_fields_ops *ops = psc_push_fields_ops(sub->fwd);
-  assert(ops->push_a_E);
-  ops->push_a_E(sub->fwd, flds_base);
+  if (ops->step_a) {
+    ops->step_a(sub->fwd, mflds);
+  } else {
+    psc_push_fields_step_a(sub->fwd, mflds);
+  }
 }
 
 static void
-psc_push_fields_auto_push_a_H(struct psc_push_fields *push,
-			      struct psc_fields *flds_base)
+psc_push_fields_auto_step_b(struct psc_push_fields *push,
+			    struct psc_mfields *mflds)
 {
   struct sub *sub = sub(push);
   struct psc_push_fields_ops *ops = psc_push_fields_ops(sub->fwd);
-  assert(ops->push_a_H);
-  ops->push_a_H(sub->fwd, flds_base);
-}
-
-static void
-psc_push_fields_auto_push_b_H(struct psc_push_fields *push,
-			      struct psc_fields *flds_base)
-{
-  struct sub *sub = sub(push);
-  struct psc_push_fields_ops *ops = psc_push_fields_ops(sub->fwd);
-  assert(ops->push_b_H);
-  ops->push_b_H(sub->fwd, flds_base);
-}
-
-static void
-psc_push_fields_auto_push_b_E(struct psc_push_fields *push,
-			      struct psc_fields *flds_base)
-{
-  struct sub *sub = sub(push);
-  struct psc_push_fields_ops *ops = psc_push_fields_ops(sub->fwd);
-  assert(ops->push_b_E);
-  ops->push_b_E(sub->fwd, flds_base);
+  if (ops->step_b) {
+    ops->step_b(sub->fwd, mflds);
+  } else {
+    psc_push_fields_step_b(sub->fwd, mflds);
+  }
 }
 
 // ======================================================================
@@ -76,9 +66,7 @@ struct psc_push_fields_ops psc_push_fields_auto_ops = {
   .name                  = "auto",
   .size                  = sizeof(struct sub),
   .setup                 = psc_push_fields_auto_setup,
-  .push_a_E              = psc_push_fields_auto_push_a_E,
-  .push_a_H              = psc_push_fields_auto_push_a_H,
-  .push_b_H              = psc_push_fields_auto_push_b_H,
-  .push_b_E              = psc_push_fields_auto_push_b_E,
+  .step_a                = psc_push_fields_auto_step_a,
+  .step_b                = psc_push_fields_auto_step_b,
 };
 
