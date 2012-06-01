6858dfa3c056ab280e15dcfad4ede2413ea50373
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-06-01 11:59:55 -0400

    psc/checkpoint: fields "cuda" read/write

--- src/libpsc/cuda/psc_fields_cuda.c
+++ src/libpsc/cuda/psc_fields_cuda.c
@@ -49,6 +49,74 @@ psc_fields_cuda_destroy(struct psc_fields *pf)
 }
 
 // ======================================================================
+
+#ifdef HAVE_LIBHDF5_HL
+
+#include <mrc_io.h>
+
+// FIXME. This is a rather bad break of proper layering, HDF5 should be all
+// mrc_io business. OTOH, it could be called flexibility...
+
+#include <hdf5.h>
+#include <hdf5_hl.h>
+
+#define H5_CHK(ierr) assert(ierr >= 0)
+#define CE assert(ierr == 0)
+
+// ----------------------------------------------------------------------
+// psc_fields_cuda_write
+
+static void
+psc_fields_cuda_write(struct psc_fields *flds, struct mrc_io *io)
+{
+  int ierr;
+  long h5_file;
+  mrc_io_get_h5_file(io, &h5_file);
+  hid_t group = H5Gopen(h5_file, psc_fields_name(flds), H5P_DEFAULT); H5_CHK(group);
+  ierr = H5LTset_attribute_int(group, ".", "p", &flds->p, 1); CE;
+  ierr = H5LTset_attribute_int(group, ".", "ib", flds->ib, 3); CE;
+  ierr = H5LTset_attribute_int(group, ".", "im", flds->im, 3); CE;
+  ierr = H5LTset_attribute_int(group, ".", "nr_comp", &flds->nr_comp, 1); CE;
+  // write components separately instead?
+  hsize_t hdims[4] = { flds->nr_comp, flds->im[2], flds->im[1], flds->im[0] };
+  float *h_flds = malloc(flds->nr_comp * psc_fields_size(flds) * sizeof(*h_flds));
+  __fields_cuda_from_device(flds, h_flds, 0, flds->nr_comp);
+  ierr = H5LTmake_dataset_float(group, "fields_cuda", 4, hdims, h_flds); CE;
+  free(h_flds);
+  ierr = H5Gclose(group); CE;
+}
+
+// ----------------------------------------------------------------------
+// psc_fields_cuda_read
+
+static void
+psc_fields_cuda_read(struct psc_fields *flds, struct mrc_io *io)
+{
+  int ierr;
+  long h5_file;
+  mrc_io_get_h5_file(io, &h5_file);
+  hid_t group = H5Gopen(h5_file, psc_fields_name(flds), H5P_DEFAULT); H5_CHK(group);
+  int ib[3], im[3], nr_comp;
+  ierr = H5LTget_attribute_int(group, ".", "p", &flds->p); CE;
+  ierr = H5LTget_attribute_int(group, ".", "ib", ib); CE;
+  ierr = H5LTget_attribute_int(group, ".", "im", im); CE;
+  ierr = H5LTget_attribute_int(group, ".", "nr_comp", &nr_comp); CE;
+  for (int d = 0; d < 3; d++) {
+    assert(ib[d] == flds->ib[d]);
+    assert(im[d] == flds->im[d]);
+  }
+  assert(nr_comp == flds->nr_comp);
+  psc_fields_setup(flds);
+  float *h_flds = malloc(flds->nr_comp * psc_fields_size(flds) * sizeof(*h_flds));
+  ierr = H5LTread_dataset_float(group, "fields_cuda", h_flds); CE;
+  __fields_cuda_to_device(flds, h_flds, 0, flds->nr_comp);
+  free(h_flds);
+  ierr = H5Gclose(group); CE;
+}
+
+#endif
+
+// ======================================================================
 // convert from/to "c"
 
 static void
@@ -162,5 +230,9 @@ struct psc_fields_ops psc_fields_cuda_ops = {
   .methods               = psc_fields_cuda_methods,
   .setup                 = psc_fields_cuda_setup,
   .destroy               = psc_fields_cuda_destroy,
+#ifdef HAVE_LIBHDF5_HL
+  .read                  = psc_fields_cuda_read,
+  .write                 = psc_fields_cuda_write,
+#endif
 };
 
