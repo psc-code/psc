de28b534152b55040a5296b913aeaae5fb7f5ae8
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-06-08 15:00:35 -0400

    psc/harris: init like Will's fortran version

--- src/psc_harris.c
+++ src/psc_harris.c
@@ -24,24 +24,31 @@
 
 struct psc_harris {
   double B0;
+  double LLy, LLz; // in d_i
   double nb;
   double Te, Ti;
   double mi_over_me;
-  double lambda;
+  double lambda; // in d_i
   double pert;
+
+  // normalized quantities
+  double LLL; // lambda in d_e
+  double AA; // perturbation amplitude (from pert)
 };
 
 #define to_psc_harris(psc) mrc_to_subobj(psc, struct psc_harris)
 
 #define VAR(x) (void *)offsetof(struct psc_harris, x)
 static struct param psc_harris_descr[] = {
-  { "B0"            , VAR(B0)              , PARAM_DOUBLE(1.)     },
-  { "mi_over_me"    , VAR(mi_over_me)      , PARAM_DOUBLE(25.)    },
-  { "nb"            , VAR(nb)              , PARAM_DOUBLE(.2)     },
-  { "Te"            , VAR(Te)              , PARAM_DOUBLE(1./12.) },
-  { "Ti"            , VAR(Ti)              , PARAM_DOUBLE(5./12.) },
-  { "lambda"        , VAR(lambda)          , PARAM_DOUBLE(.5)     },
-  { "pert"          , VAR(pert)            , PARAM_DOUBLE(.1)     },
+  { "B0"            , VAR(B0)              , PARAM_DOUBLE(.5)     },
+  { "mi_over_me"    , VAR(mi_over_me)      , PARAM_DOUBLE(40.)    },
+  { "nb"            , VAR(nb)              , PARAM_DOUBLE(.3)     },
+  { "LLy"           , VAR(LLy)             , PARAM_DOUBLE(50.)    },
+  { "LLz"           , VAR(LLz)             , PARAM_DOUBLE(200.)   },
+  { "Te"            , VAR(Te)              , PARAM_DOUBLE(0.0625) },
+  { "Ti"            , VAR(Ti)              , PARAM_DOUBLE(0.0625) },
+  { "lambda"        , VAR(lambda)          , PARAM_DOUBLE(2.)     },
+  { "pert"          , VAR(pert)            , PARAM_DOUBLE(.025)   },
   {},
 };
 #undef VAR
@@ -56,14 +63,16 @@ psc_harris_create(struct psc *psc)
 
   psc->prm.nmax = 16000;
   psc->prm.nicell = 50;
+  psc->prm.cfl = 0.98;
 
+  // will be set to actual values in psc_harris_setup()
   psc->domain.length[0] = 1.; // no x dependence 
-  psc->domain.length[1] = 12.8;
-  psc->domain.length[2] = 25.6;
+  psc->domain.length[1] = 1.;
+  psc->domain.length[2] = 1.;
 
   psc->domain.gdims[0] = 1;
-  psc->domain.gdims[1] = 320;
-  psc->domain.gdims[2] = 640;
+  psc->domain.gdims[1] = 800;
+  psc->domain.gdims[2] = 3200;
 
   psc->domain.bnd_fld_lo[0] = BND_FLD_PERIODIC;
   psc->domain.bnd_fld_hi[0] = BND_FLD_PERIODIC;
@@ -84,6 +93,39 @@ psc_harris_create(struct psc *psc)
 }
 
 // ----------------------------------------------------------------------
+// psc_harris_setup
+
+static void
+psc_harris_setup(struct psc *psc)
+{
+  struct psc_harris *harris = to_psc_harris(psc);
+
+  double d_i = sqrt(harris->mi_over_me);
+  psc->domain.corner[1] = -.5 * harris->LLy * d_i;
+  psc->domain.length[1] = harris->LLy * d_i;
+  psc->domain.length[2] = harris->LLz * d_i;
+  harris->LLL = harris->lambda * d_i;
+  harris->AA = harris->pert * harris->B0 * psc->domain.length[2] / (2. * M_PI);
+
+  psc->kinds[KIND_ELECTRON].m = 1.;
+  psc->kinds[KIND_ION     ].m = harris->mi_over_me;
+  psc->kinds[KIND_ELECTRON].T = harris->Te;
+  psc->kinds[KIND_ELECTRON].T = harris->Ti;
+
+  // initializes fields, particles, etc.
+  psc_setup_super(psc);
+
+  MPI_Comm comm = psc_comm(psc);
+  mpi_printf(comm, "dt = %g, dy = %g dz = %g\n", psc->dt, psc->dx[1], psc->dx[2]);
+  mpi_printf(comm, "d_e = %g, d_i = %g\n", 1., d_i);
+  mpi_printf(comm, "v_A = %g\n", harris->B0 / sqrt(harris->mi_over_me));
+  mpi_printf(comm, "om_ci = %g om_ce = %g\n", harris->B0 / harris->mi_over_me,
+	     harris->B0);
+  mpi_printf(comm, "om_pi = %g om_pe = %g\n", 1. / sqrt(harris->mi_over_me), 1.);
+  mpi_printf(comm, "lambda_De = %g\n", sqrt(harris->Te));
+}
+
+// ----------------------------------------------------------------------
 // psc_harris_init_field
 
 static double
@@ -91,25 +133,24 @@ psc_harris_init_field(struct psc *psc, double x[3], int m)
 {
   struct psc_harris *harris = to_psc_harris(psc);
 
-  double B0 = harris->B0;
-  double lz = psc->domain.length[2], ly = psc->domain.length[1];
-  double lambda = harris->lambda;
-  double AA = harris->pert * B0;
+  double BB = harris->B0;
+  double LLz = psc->domain.length[2], LLy = psc->domain.length[1];
+  double LLL = harris->LLL;
+  double AA = harris->AA;
 
   switch (m) {
   case HZ:
-    return
-      B0 * tanh((x[1] - 0.5*ly) / lambda)
-      + AA * M_PI/ly * sin(2.*M_PI * x[2] / lz) * cos(M_PI * x[1] / ly);
+    return BB * tanh((x[1]) / LLL)
+      - AA * M_PI/LLy * cos(2.*M_PI * (x[2] - .5 * LLz) / LLz) * sin(M_PI * x[1] / LLy);
 
   case HY:
-    return - AA * 2.*M_PI / lz * cos(2.*M_PI * x[2] / lz) * sin(M_PI * x[1] / ly);
+    return AA * 2.*M_PI / LLz * sin(2.*M_PI * (x[2] - .5 * LLz) / LLz) * cos(M_PI * x[1] / LLy);
 
   case JXI:
     return
-      B0 / lambda * (1./sqr(cosh((x[1] - 0.5*ly) / lambda)))
-      - (AA*sqr(M_PI) * (1./sqr(ly) + 4./sqr(lz)) 
-	 * sin(2.*M_PI * x[2] / lz) * sin(M_PI * x[1] / ly));
+      BB / LLL * (1./sqr(cosh(x[1] / LLL)))
+      - AA * sqr(M_PI) * (1./sqr(LLy) + 4./sqr(LLz)) 
+      * cos(2.*M_PI * (x[2] - .5 *LLz) / LLz) * cos(M_PI * x[1] / LLy);
 
   default: return 0.;
   }
@@ -124,32 +165,19 @@ psc_harris_init_npt(struct psc *psc, int kind, double x[3],
 {
   struct psc_harris *harris = to_psc_harris(psc);
 
-  double B0 = harris->B0;
-  double ly = psc->domain.length[1];
-  double lambda = harris->lambda;
-  double nb = harris->nb;
-  double TTi = harris->Ti * sqr(B0) * harris->mi_over_me; // FIXME, why???
-  double TTe = harris->Te * sqr(B0) * harris->mi_over_me;
+  double BB = harris->B0;
+  double LLL = harris->LLL;
+  double nnb = harris->nb;
+  double TTi = harris->Ti;
+  double TTe = harris->Te;
 
-  double jx0 = B0 / lambda * 1./sqr(cosh((x[1] - 0.5*ly) / lambda));
-
-  npt->n = nb + 1./sqr(cosh((x[1] - 0.5*ly) / lambda));
+  npt->n = nnb + 1./sqr(cosh((x[1]) / LLL));
   switch (kind) {
-  case 0: // electrons
-    npt->q = -1.;
-    npt->m = 1. / harris->mi_over_me;
-    npt->p[0] = - 2. * TTe / B0 / lambda * (jx0 / (B0 / lambda)) / npt->n;
-    npt->T[0] = TTe;
-    npt->T[1] = TTe;
-    npt->T[2] = TTe;
+  case KIND_ELECTRON:
+    npt->p[0] = - 2. * TTe / BB / LLL * (npt->n - nnb) / npt->n;
     break;
-  case 1: // ions
-    npt->q = 1.;
-    npt->m = 1.;
-    npt->p[0] = 2. * TTi / B0 / lambda * (jx0 / B0 / lambda) / npt->n;
-    npt->T[0] = TTi;
-    npt->T[1] = TTi;
-    npt->T[2] = TTi;
+  case KIND_ION:
+    npt->p[0] = 2. * TTi / BB / LLL * (npt->n - nnb) / npt->n;
     break;
   default:
     assert(0);
@@ -164,6 +192,7 @@ struct psc_ops psc_harris_ops = {
   .size             = sizeof(struct psc_harris),
   .param_descr      = psc_harris_descr,
   .create           = psc_harris_create,
+  .setup            = psc_harris_setup,
   .init_field       = psc_harris_init_field,
   .init_npt         = psc_harris_init_npt,
 };
