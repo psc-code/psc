177a9f4052c5c6a0761976b3f7d3234abb725dd0
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-07-06 08:55:30 -0400

    libmrc/amr: mv test domain setup -> mrctest.c

--- src/mrctest.c
+++ src/mrctest.c
@@ -358,3 +358,117 @@ mrctest_crds_compare(struct mrc_crds *crds1, struct mrc_crds *crds2)
   }
 }
 
+// ======================================================================
+// mrc_domain "amr" sample setups
+
+void
+mrctest_set_amr_domain_0(struct mrc_domain *domain)
+{
+  mrc_domain_add_patch(domain, 1, (int [3]) { 0, 0, 0 });
+  mrc_domain_add_patch(domain, 1, (int [3]) { 0, 1, 0 });
+  mrc_domain_add_patch(domain, 1, (int [3]) { 1, 0, 0 });
+  mrc_domain_add_patch(domain, 1, (int [3]) { 1, 1, 0 });
+}
+
+void
+mrctest_set_amr_domain_1(struct mrc_domain *domain)
+{
+  mrc_domain_add_patch(domain, 1, (int [3]) { 0, 0, 0 });
+  mrc_domain_add_patch(domain, 1, (int [3]) { 0, 1, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 1, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 1, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 2, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 2, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 3, 0 });
+}
+
+void
+mrctest_set_amr_domain_2(struct mrc_domain *domain)
+{
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 1, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 0, 0 });
+
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 1, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 2, 2, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 3, 2, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 2, 3, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 3, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 1, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 1, 0 });
+
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 2, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 1, 2, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 2, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 2, 0 });
+
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 1, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 3, 0 });
+}
+
+void
+mrctest_set_amr_domain_3(struct mrc_domain *domain)
+{
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 1, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 0, 0 });
+
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 1, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 2, 2, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 3, 2, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 2, 3, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 3, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 1, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 1, 0 });
+
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 2, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 1, 2, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 4, 4, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 5, 4, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 4, 5, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 5, 5, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 2, 0 });
+
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 1, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 3, 0 });
+}
+
+void
+mrctest_set_amr_domain_4(struct mrc_domain *domain)
+{
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 1, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 0, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 0, 0 });
+
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 1, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 1, 1, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 4, 2, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 5, 2, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 4, 3, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 5, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 1, 0 });
+
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 2, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 2, 4, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 3, 4, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 2, 5, 0 });
+  mrc_domain_add_patch(domain, 3, (int [3]) { 3, 5, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 2, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 2, 0 });
+
+  mrc_domain_add_patch(domain, 2, (int [3]) { 0, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 1, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 2, 3, 0 });
+  mrc_domain_add_patch(domain, 2, (int [3]) { 3, 3, 0 });
+}
+
