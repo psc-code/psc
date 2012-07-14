add5ed9fbb409048c07313abca5a74d15a1ff00e
Kai Germaschewski <kai.germaschewski@unh.edu>
2012-07-14 18:29:18 -0400

    psc/cuda: do push_fields at-once

--- src/libpsc/cuda/psc_push_fields_cuda.c
+++ src/libpsc/cuda/psc_push_fields_cuda.c
@@ -9,33 +9,37 @@ EXTERN_C void cuda_push_fields_E_yz(int p, struct psc_fields *flds);
 EXTERN_C void cuda_push_fields_H_yz(int p, struct psc_fields *flds);
 
 // ----------------------------------------------------------------------
-// psc_push_fields_cuda_push_E
+// psc_push_fields_cuda_push_mflds_E
 
 static void
-psc_push_fields_cuda_push_E(struct psc_push_fields *push, struct psc_fields *flds_base)
+psc_push_fields_cuda_push_mflds_E(struct psc_push_fields *push, struct psc_mfields *mflds_base)
 {
-  struct psc_fields *flds = psc_fields_get_as(flds_base, "cuda", JXI, HX + 3);
+  struct psc_mfields *mflds = psc_mfields_get_cuda(mflds_base, JXI, HX + 3);
   if (ppsc->domain.gdims[0] == 1) {
-    cuda_push_fields_E_yz(flds->p, flds);
+    for (int p = 0; p < mflds->nr_patches; p++) {
+	cuda_push_fields_E_yz(p, psc_mfields_get_patch(mflds, p));
+    }
   } else {
     assert(0);
   }
-  psc_fields_put_as(flds, flds_base, EX, EX + 3);
+  psc_mfields_put_cuda(mflds, mflds_base, EX, EX + 3);
 }
 
 // ----------------------------------------------------------------------
-// psc_push_fields_cuda_push_H
+// psc_push_fields_cuda_push_mflds_H
 
 static void
-psc_push_fields_cuda_push_H(struct psc_push_fields *push, struct psc_fields *flds_base)
+psc_push_fields_cuda_push_mflds_H(struct psc_push_fields *push, struct psc_mfields *mflds_base)
 {
-  struct psc_fields *flds = psc_fields_get_as(flds_base, "cuda", EX, HX + 3);
+  struct psc_mfields *mflds = psc_mfields_get_cuda(mflds_base, EX, HX + 3);
   if (ppsc->domain.gdims[0] == 1) {
-    cuda_push_fields_H_yz(flds->p, flds);
+    for (int p = 0; p < mflds->nr_patches; p++) {
+      cuda_push_fields_H_yz(p, psc_mfields_get_patch(mflds, p));
+    }
   } else {
     assert(0);
   }
-  psc_fields_put_as(flds, flds_base, HX, HX + 3);
+  psc_mfields_put_cuda(mflds, mflds_base, HX, HX + 3);
 }
 
 // ======================================================================
@@ -43,6 +47,6 @@ psc_push_fields_cuda_push_H(struct psc_push_fields *push, struct psc_fields *fld
 
 struct psc_push_fields_ops psc_push_fields_cuda_ops = {
   .name                  = "cuda",
-  .push_E                = psc_push_fields_cuda_push_E,
-  .push_H                = psc_push_fields_cuda_push_H,
+  .push_mflds_E          = psc_push_fields_cuda_push_mflds_E,
+  .push_mflds_H          = psc_push_fields_cuda_push_mflds_H,
 };
