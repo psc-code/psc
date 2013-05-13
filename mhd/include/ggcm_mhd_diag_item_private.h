d114400fe680e0aac185924eb5967aaed06b9f77
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-05-12 23:14:47 -0400

    [ggcm] ggcm_mhd: mv ggcm_mhd_diag_item headers to libmrc

---
+++ mhd/include/ggcm_mhd_diag_item_private.h
@@ -0,0 +1,36 @@
+
+#ifndef GGCM_MHD_DIAG_ITEM_PRIVATE_H
+#define GGCM_MHD_DIAG_ITEM_PRIVATE_H
+
+#include "ggcm_mhd_diag_item.h"
+
+struct ggcm_mhd_diag_item {
+  struct mrc_obj obj;
+  struct ggcm_mhd_diag *diag;
+};
+
+struct ggcm_mhd_diag_item_ops {
+  MRC_SUBCLASS_OPS(struct ggcm_mhd_diag_item);
+  void (*run)(struct ggcm_mhd_diag_item *item,
+	      struct mrc_io *io, struct mrc_f3 *f,
+	      int diag_type, float plane);
+};
+
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_v;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_rr;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_pp;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_b;
+
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_rr1;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_uu1;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_rv1;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_b1;
+
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_e;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_j;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_xtra;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_divb;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_currcc;
+extern struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_zmask;
+
+#endif
