6fc3c9b6f808946bf9cb417aa2c65a3d3b6d2f1e
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-05-16 00:56:38 -0400

    [ggcm] ggcm_mhd: mv ic "fadeev" -> mhd_fadeev.c

--- mhd/tests/mhd2.c
+++ mhd/tests/mhd2.c
@@ -117,7 +117,6 @@ main(int argc, char **argv)
   mrc_class_register_subclass(&mrc_class_ggcm_mhd, &ggcm_mhd_cweno_ops);  
   mrc_class_register_subclass(&mrc_class_ggcm_mhd_diag, &ggcm_mhd_diag_c_ops);
 
-  mrc_class_register_subclass(&mrc_class_ggcm_mhd_ic, &ggcm_mhd_ic_fadeev_ops);  
   mrc_class_register_subclass(&mrc_class_ggcm_mhd_ic, &ggcm_mhd_ic_whistler_ops);
   mrc_class_register_subclass(&mrc_class_ggcm_mhd_ic, &ggcm_mhd_ic_bw_ops); 
   mrc_class_register_subclass(&mrc_class_ggcm_mhd_ic, &ggcm_mhd_ic_ot_ops);
