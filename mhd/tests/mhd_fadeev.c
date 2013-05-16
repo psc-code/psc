4f848a6be2dbe6a702f57e040c2f3be38c695eea
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-05-16 00:46:06 -0400

    [ggcm] ggcm_mhd: separate test "mhd_fadeev"

---
+++ mhd/tests/mhd_fadeev.c
@@ -0,0 +1,138 @@
+
+//#define BOUNDS_CHECK
+
+#include "ggcm_mhd_defs.h"
+#include "ggcm_mhd_private.h"
+#include "ggcm_mhd_step.h"
+#include "ggcm_mhd_crds_private.h"
+#include "ggcm_mhd_crds_gen.h"
+#include "ggcm_mhd_bnd.h"
+#include "ggcm_mhd_diag.h"
+#include "ggcm_mhd_ic.h"
+
+#include <mrc_ts.h>
+#include <mrc_ts_monitor.h>
+#include <mrc_fld.h>
+#include <mrc_domain.h>
+#include <mrc_params.h>
+#include <mrc_ddc.h>
+#include <mrctest.h>
+#include <mrc_io.h> 
+
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <assert.h>
+
+static void
+ggcm_mhd_cweno_create(struct ggcm_mhd *mhd)
+{
+  mhd->par.rrnorm = 1.f;
+  mhd->par.ppnorm = 1.f;
+  mhd->par.vvnorm = 1.f;
+  mhd->par.bbnorm = 1.f;
+  mhd->par.ccnorm = 1.f;
+  mhd->par.eenorm = 1.f;
+  mhd->par.resnorm = 1.f;
+  mhd->par.diffco = 0.f;
+
+  ggcm_mhd_bnd_set_type(mhd->bnd, "none");
+
+  mrc_domain_set_param_int(mhd->domain, "bcx", BC_PERIODIC);
+  mrc_domain_set_param_int(mhd->domain, "bcy", BC_PERIODIC);
+  mrc_domain_set_param_int(mhd->domain, "bcz", BC_PERIODIC);
+
+  /* set defaults for coord arrays */
+  struct mrc_crds *crds = mrc_domain_get_crds(mhd->domain);
+  mrc_crds_set_type(crds, "gaussian_2D");
+  mrc_crds_set_param_int(crds, "sw", SW_2);   // 'stencil width' 
+  mrc_crds_set_param_float3(crds, "l", (float[3]) {  0.0, 0.0, -1.0 });
+  mrc_crds_set_param_float3(crds, "h", (float[3]) {  2.*M_PI, 2.*M_PI,  1.0 });
+
+  /* set defaults for the ddc, this does the communication */
+  struct mrc_ddc *ddc = mrc_domain_get_ddc(mhd->domain);
+  mrc_ddc_set_param_int(ddc, "max_n_fields", 8);
+  mrc_ddc_set_param_int3(ddc, "ibn", (int[3]) { SW_2, SW_2, SW_2 });
+
+  // generate MHD solver grid from mrc_crds
+  ggcm_mhd_crds_gen_set_type(mhd->crds->crds_gen, "mrc");
+  ggcm_mhd_set_param_float(mhd, "isphere", 0.);
+  ggcm_mhd_set_param_float(mhd, "diffsphere", 0.);
+  ggcm_mhd_set_param_float(mhd, "speedlimit", 1e9);
+}
+
+static struct ggcm_mhd_ops ggcm_mhd_cweno_ops = {
+  .name             = "cweno",
+  .create           = ggcm_mhd_cweno_create,
+};
+
+
+// ======================================================================
+  
+static void
+calc_mhd_rhs(void *ctx, struct mrc_obj *_rhs, float time, struct mrc_obj *_fld)
+{
+  struct ggcm_mhd *mhd = ctx;
+  struct mrc_f3 *rhs = (struct mrc_f3 *) _rhs;
+  struct mrc_f3 *fld = (struct mrc_f3 *) _fld;
+  
+  mhd->time = time;
+  ggcm_mhd_step_calc_rhs(mhd->step, rhs, fld);
+}
+
+// ======================================================================
+
+extern struct ggcm_mhd_diag_ops ggcm_mhd_diag_c_ops;
+
+extern struct ggcm_mhd_ic_ops ggcm_mhd_ic_fadeev_ops;
+
+int
+main(int argc, char **argv)
+{
+  MPI_Init(&argc, &argv);
+  libmrc_params_init(argc, argv);
+  ggcm_mhd_register();
+
+  mrc_class_register_subclass(&mrc_class_ggcm_mhd, &ggcm_mhd_cweno_ops);  
+  mrc_class_register_subclass(&mrc_class_ggcm_mhd_diag, &ggcm_mhd_diag_c_ops);
+
+  mrc_class_register_subclass(&mrc_class_ggcm_mhd_ic, &ggcm_mhd_ic_fadeev_ops);  
+ 
+  struct ggcm_mhd *mhd = ggcm_mhd_create(MPI_COMM_WORLD);
+  ggcm_mhd_set_type(mhd, "cweno");
+  ggcm_mhd_step_set_type(mhd->step, "cweno");
+  ggcm_mhd_flds_set_type(mhd->flds_base, "c");
+  ggcm_mhd_set_from_options(mhd);
+  ggcm_mhd_setup(mhd);
+  ggcm_mhd_view(mhd);
+
+  // set up initial condition
+  ggcm_mhd_ic_run(mhd->ic);
+
+  // run time integration
+  struct mrc_ts *ts = mrc_ts_create(mrc_domain_comm(mhd->domain));
+  mrc_ts_set_type(ts, "rk2");
+  mrc_ts_set_context(ts, ggcm_mhd_to_mrc_obj(mhd));
+
+  struct mrc_ts_monitor *mon_output =
+    mrc_ts_monitor_create(mrc_ts_comm(ts));
+  mrc_ts_monitor_set_type(mon_output, "ggcm");
+  mrc_ts_monitor_set_name(mon_output, "mrc_ts_output");
+  mrc_ts_add_monitor(ts, mon_output);
+
+  mrc_ts_set_dt(ts, 1e-6);
+  mrc_ts_set_solution(ts, mrc_f3_to_mrc_obj(ggcm_mhd_flds_get_mrc_f3(mhd->flds_base)));
+  mrc_ts_set_rhs_function(ts, calc_mhd_rhs, mhd);
+  mrc_ts_set_from_options(ts);
+  mrc_ts_view(ts);
+  mrc_ts_setup(ts);
+  mrc_ts_solve(ts);
+  mrc_ts_view(ts);
+  mrc_ts_destroy(ts);  
+  ggcm_mhd_destroy(mhd);
+
+  MPI_Finalize();
+  return 0;
+}
+
