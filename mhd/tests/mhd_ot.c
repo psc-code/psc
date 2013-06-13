6f83b586d2560bc5d7c7d93906dba76bb2aad495
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-06-13 08:10:15 -0400

    [ggcm] mhd_ot: use init_from_primitive, some clean-up

--- mhd/tests/mhd_ot.c
+++ mhd/tests/mhd_ot.c
@@ -25,14 +25,9 @@
 #include <stdlib.h> 
 #include <assert.h>
 
-
-
 // ======================================================================
 // ggcm_mhd_ic subclass "ot"
 
-struct ggcm_mhd_ic_ot {
-  float mpermi;
-};
 // ----------------------------------------------------------------------
 // ggcm_mhd_ic_ot_run
 
@@ -42,53 +37,33 @@ ggcm_mhd_ic_ot_run(struct ggcm_mhd_ic *ic)
   //  struct ggcm_mhd_ic_ot *sub = mrc_to_subobj(ic, struct ggcm_mhd_ic_ot);
 
   struct ggcm_mhd *mhd = ic->mhd;
-  struct mrc_fld *f3 = mrc_fld_get_as(mhd->fld, "float");
   struct mrc_crds *crds = mrc_domain_get_crds(mhd->domain);  
+  struct mrc_fld *fld = mrc_fld_get_as(mhd->fld, "float");
 
-  float gamma = mhd->par.gamm;
-
-  mrc_fld_foreach(f3, ix,iy,iz, 0, 0) {
+  // FIXME, the "1" no of ghosts is ugly here, and caused by the use of
+  // the B1* macros which shift the index (due to staggering)...
+  mrc_fld_foreach(fld, ix,iy,iz, 1, 1) {
     float r[3];
     r[0] = MRC_CRD(crds, 0, ix);
     r[1] = MRC_CRD(crds, 1, iy);
-    r[2] = MRC_CRD(crds, 2, iz);
-    MRC_F3(f3, _RR1 , ix, iy, iz) = 25. / (36.*M_PI );
-    MRC_F3(f3, _RV1X, ix, iy, iz) = - sin(2. * M_PI * r[1] ) * MRC_F3(f3, _RR1 , ix, iy, iz);
-    MRC_F3(f3, _RV1Y, ix, iy, iz) = sin(2. * M_PI * r[0] )* MRC_F3(f3, _RR1 , ix, iy, iz);
-    MRC_F3(f3, _RV1Z, ix, iy, iz) = 0.0;
-    B1X(f3, ix+1, iy, iz) = - sqrt(1./(4.*M_PI)) * sin( 2. * M_PI * r[1] ); 
-    B1Y(f3, ix, iy+1, iz) =   sqrt(1./(4.*M_PI)) * sin( 4. * M_PI * r[0] );
-    B1Z(f3, ix, iy, iz+1) = 0.0;
-
-    MRC_F3(f3, _UU1 , ix, iy, iz) =  MRC_F3(f3, _RR1, ix, iy, iz) / (gamma -1.f) + 	
-      .5f * (sqr(MRC_F3(f3, _RV1X, ix, iy, iz)) +
-	     sqr(MRC_F3(f3, _RV1Y, ix, iy, iz)) +
-	     sqr(MRC_F3(f3, _RV1Z, ix, iy, iz))) / MRC_F3(f3, _RR1, ix, iy, iz) +
-      .5f * (sqr(.5*(B1X(f3, ix,iy,iz) + B1X(f3, ix+1,iy,iz))) +
-	     sqr(.5*(B1Y(f3, ix,iy,iz) + B1Y(f3, ix,iy+1,iz))) +
-	     sqr(.5*(B1Z(f3, ix,iy,iz) + B1Z(f3, ix,iy,iz+1))));
+    RR(fld, ix,iy,iz) = 25. / (36.*M_PI);
+    VX(fld, ix,iy,iz) = - sin(2. * M_PI * r[1]);
+    VY(fld, ix,iy,iz) =   sin(2. * M_PI * r[0] );
+    PP(fld, ix,iy,iz) = RR(fld, ix,iy,iz);
+    B1X(fld, ix,iy,iz) = - sqrt(1./(4.*M_PI)) * sin(2. * M_PI * r[1]); 
+    B1Y(fld, ix,iy,iz) =   sqrt(1./(4.*M_PI)) * sin(4. * M_PI * r[0]);
+    B1Z(fld, ix,iy,iz) = 0.0;
   } mrc_fld_foreach_end;
 
-  mrc_fld_put_as(f3, mhd->fld);
+  mrc_fld_put_as(fld, mhd->fld);
+  ggcm_mhd_init_from_primitive(mhd, fld);
 }
-
-// ----------------------------------------------------------------------
-// ggcm_mhd_ic_ot_descr
-
-#define VAR(x) (void *)offsetof(struct ggcm_mhd_ic_ot, x)
-static struct param ggcm_mhd_ic_ot_descr[] = {
-  //  { "mpermi"          , VAR(mpermi)             , PARAM_FLOAT(1.)         },
-  {},
-};
-#undef VAR
 
 // ----------------------------------------------------------------------
 // ggcm_mhd_ic_ot_ops
 
 struct ggcm_mhd_ic_ops ggcm_mhd_ic_ot_ops = {
   .name        = "ot",
-  .size        = sizeof(struct ggcm_mhd_ic_ot),
-  .param_descr = ggcm_mhd_ic_ot_descr,
   .run         = ggcm_mhd_ic_ot_run,
 };
 
