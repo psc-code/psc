bf39c95a364cf83968b5db80f52d9120a166ef60
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-06-22 16:33:06 -0400

    psc/output_fields: add "n_1st_nc_double" moment

--- src/libpsc/psc_output_fields/psc_output_fields_item_moments_1st_nc_double.c
+++ src/libpsc/psc_output_fields/psc_output_fields_item_moments_1st_nc_double.c
@@ -1,22 +1,22 @@
 
 #include "psc.h"
-#include "psc_particles_as_c.h"
+#include "psc_particles_as_double.h"
 #include "psc_fields_as_c.h"
 
 // ======================================================================
+// !!! These moments are shifted to (n+.5) * dt, rather than n * dt,
+// since the "double" particles are shifted that way.
+//
 // node-centered moments -- probably mostly useful for testing
 // charge continuity
-//
-// NOTE: This is at time t^n, which may not be what you want for checking
-// continuity / Gauss's Law, since that should hold at t^{n+.5}
 
 #include "psc_output_fields_item_moments_1st_nc.c"
 
 // ======================================================================
-// psc_output_fields_item: subclass "n_1st_nc_c"
+// psc_output_fields_item: subclass "n_1st_nc_double"
 
-struct psc_output_fields_item_ops psc_output_fields_item_n_1st_nc_c_ops = {
-  .name               = "n_1st_nc_c",
+struct psc_output_fields_item_ops psc_output_fields_item_n_1st_nc_double_ops = {
+  .name               = "n_1st_nc_double",
   .nr_comp            = 1,
   .fld_names          = { "n_nc" },
   .run                = n_run,
