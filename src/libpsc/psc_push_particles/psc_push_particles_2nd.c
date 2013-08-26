d96ab04faaadd38a7c2a3e96c6a506be431a0fce
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-08-25 21:45:05 -0400

    psc/push_part: add "2nd_double"
    so far it's actually a copy from generic_c, still
    working on "c" particles

---
+++ src/libpsc/psc_push_particles/psc_push_particles_2nd.c
@@ -0,0 +1,15 @@
+
+#include "psc_push_particles_private.h"
+#include "psc_push_particles_2nd.h"
+
+// ======================================================================
+// psc_push_particles: subclass "2nd_double"
+//
+// 2nd order, Esirkepov, like the old generic_c/fortran, but on "double"
+// particles
+
+struct psc_push_particles_ops psc_push_particles_2nd_double_ops = {
+  .name                  = "2nd_double",
+  .push_a_yz             = psc_push_particles_2nd_double_push_a_yz,
+};
+
---
+++ src/libpsc/psc_push_particles/psc_push_particles_2nd.h
@@ -0,0 +1,11 @@
+
+#ifndef PSC_PUSH_PARTICLES_2ND_H
+#define PSC_PUSH_PARTICLES_2ND_H
+
+#include "psc.h"
+
+void psc_push_particles_2nd_double_push_a_yz(struct psc_push_particles *push,
+					     struct psc_particles *prts_base,
+					     struct psc_fields *flds_base);
+
+#endif
