7d355c6873b96967159f67d3778b748af1d2d2ff
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-09-14 21:30:49 -0400

    psc_balance: mv subclass -> psc_balance_{double,common.c}

---
+++ src/libpsc/psc_balance/psc_balance_double.c
@@ -0,0 +1,16 @@
+
+#include "psc_balance_private.h"
+
+#include "psc_particles_as_double.h"
+#include "psc_fields_as_c.h"
+
+#include "psc_balance_common.c"
+
+// ======================================================================
+// psc_balance subclass "double"
+
+struct psc_balance_ops psc_balance_double_ops = {
+  .name                  = "double",
+  .communicate_particles = psc_balance_sub_communicate_particles,
+  .communicate_fields    = psc_balance_sub_communicate_fields,
+};
