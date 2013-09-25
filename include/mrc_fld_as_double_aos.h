f4d925e41e469545e13115a229deb73fb6c12231
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-09-25 15:22:31 -0400

    [ggcm] mrc_fld: introduce "mrc_fld_as_double_aos.h"

---
+++ include/mrc_fld_as_double_aos.h
@@ -0,0 +1,10 @@
+
+#ifndef MRC_FLD_AS_DOUBLE_AOS_H
+#define MRC_FLD_AS_DOUBLE_AOS_H
+
+typedef double mrc_fld_data_t;
+
+#define F3(f, m, i,j,k) MRC_D4(f, m, i,j,k)
+#define FLD_TYPE "double_aos"
+
+#endif
