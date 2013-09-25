94012996d90f6a9a7a6d78fc0f4d7a445d95ebcb
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-09-25 16:03:34 -0400

    [ggcm] mrc_ddc: rename the copy_to/from_buf files -> mrc_fld_*

--- src/mrc_fld_float.c
+++ src/mrc_fld_float.c
@@ -4,5 +4,5 @@
 #define mrc_fld_TYPE_ddc_copy_to_buf mrc_fld_float_ddc_copy_to_buf
 #define mrc_fld_TYPE_ddc_copy_from_buf mrc_fld_float_ddc_copy_from_buf
 
-#include "mrc_ddc_multi_common.c"
+#include "mrc_fld_common.c"
 
