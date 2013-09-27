74c39d4fb74df3b06f98bfd9ef97e1885cdcfa4d
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-09-27 18:42:08 -0400

    openmp: duplicate psc_bnd_particles double -> double_omp

---
+++ src/libpsc/psc_bnd_particles/psc_bnd_particles_common_omp.c
@@ -0,0 +1,245 @@
+
+#include <mrc_domain.h>
+#include <mrc_profile.h>
+#include <string.h>
+
+// ----------------------------------------------------------------------
+// ddcp_particles helpers
+
+static void
+ddcp_particles_realloc(void *_ctx, int p, int new_n_particles)
+{
+  mparticles_t *particles = _ctx;
+  struct psc_particles *prts = psc_mparticles_get_patch(particles, p);
+  particles_realloc(prts, new_n_particles);
+}
+
+static void *
+ddcp_particles_get_addr(void *_ctx, int p, int n)
+{
+  mparticles_t *particles = _ctx;
+  struct psc_particles *prts = psc_mparticles_get_patch(particles, p);
+  return particles_get_one(prts, n);
+}
+
+// ----------------------------------------------------------------------
+// psc_bnd_particles_sub_setup
+
+static void
+psc_bnd_particles_sub_setup(struct psc_bnd_particles *bnd)
+{
+  bnd->ddcp = ddc_particles_create(bnd->psc->mrc_domain, sizeof(particle_t),
+				   sizeof(particle_real_t),
+				   MPI_PARTICLES_REAL,
+				   ddcp_particles_realloc,
+				   ddcp_particles_get_addr);
+}
+
+// ----------------------------------------------------------------------
+// psc_bnd_particles_sub_unsetup
+
+static void
+psc_bnd_particles_sub_unsetup(struct psc_bnd_particles *bnd)
+{
+  ddc_particles_destroy(bnd->ddcp);
+}
+
+// ======================================================================
+//
+// ----------------------------------------------------------------------
+// find_block_position
+
+static inline void
+find_block_position(int b_pos[3], particle_real_t xi[3], particle_real_t b_dxi[3])
+{
+  for (int d = 0; d < 3; d++) {
+    b_pos[d] = particle_real_fint(xi[d] * b_dxi[d]);
+  }
+}
+
+// ----------------------------------------------------------------------
+// psc_bnd_particles_sub_exchange_particles_prep
+
+static void
+psc_bnd_particles_sub_exchange_particles_prep(struct psc_bnd_particles *bnd, struct psc_particles *prts)
+{
+  struct ddc_particles *ddcp = bnd->ddcp;
+  struct psc *psc = bnd->psc;
+
+  // New-style boundary requirements.
+  // These will need revisiting when it comes to non-periodic domains.
+
+  struct psc_patch *ppatch = &psc->patch[prts->p];
+  particle_real_t b_dxi[3] = { 1.f / ppatch->dx[0], 1.f / ppatch->dx[1], 1.f / ppatch->dx[2] };
+  particle_real_t xm[3];
+  int b_mx[3];
+  for (int d = 0; d < 3; d++ ) {
+    if (psc->domain.bnd_part_hi[d] == BND_PART_REFLECTING &&
+	!psc->prm.gdims_in_terms_of_cells &&
+	ppatch->off[d] + ppatch->ldims[d] == psc->domain.gdims[d]) {
+      b_mx[d] = ppatch->ldims[d] - 1;
+    } else {
+      b_mx[d] = ppatch->ldims[d];
+    }
+    xm[d] = b_mx[d] * ppatch->dx[d];
+  }
+  
+  struct ddcp_patch *patch = &ddcp->patches[prts->p];
+  patch->head = 0;
+  for (int dir1 = 0; dir1 < N_DIR; dir1++) {
+    patch->nei[dir1].n_send = 0;
+  }
+  for (int i = 0; i < prts->n_part; i++) {
+    particle_t *part = particles_get_one(prts, i);
+    particle_real_t *xi = &part->xi; // slightly hacky relies on xi, yi, zi to be contiguous in the struct. FIXME
+    
+    int b_pos[3];
+    find_block_position(b_pos, xi, b_dxi);
+    particle_real_t *pxi = &part->pxi;
+    if (b_pos[0] >= 0 && b_pos[0] < b_mx[0] &&
+	b_pos[1] >= 0 && b_pos[1] < b_mx[1] &&
+	b_pos[2] >= 0 && b_pos[2] < b_mx[2]) {
+      // fast path
+      // inside domain: move into right position
+      *particles_get_one(prts, patch->head++) = *part;
+    } else {
+      // slow path
+      bool drop = false;
+      int dir[3];
+      for (int d = 0; d < 3; d++) {
+	if (b_pos[d] < 0) {
+	  if (ppatch->off[d] > 0 ||
+	      psc->domain.bnd_part_lo[d] == BND_PART_PERIODIC) {
+	    xi[d] += xm[d];
+	    dir[d] = -1;
+	    int bi = particle_real_fint(xi[d] * b_dxi[d]);
+	    if (bi >= b_mx[d]) {
+	      xi[d] = 0.;
+	      dir[d] = 0;
+	    }
+	  } else {
+	    switch (psc->domain.bnd_part_lo[d]) {
+	    case BND_PART_REFLECTING:
+	      xi[d] =  -xi[d];
+	      pxi[d] = -pxi[d];
+	      dir[d] = 0;
+	      break;
+	    case BND_PART_ABSORBING:
+	      drop = true;
+	      break;
+	    default:
+	      assert(0);
+	    }
+	  }
+	} else if (b_pos[d] >= b_mx[d]) {
+	  if (ppatch->off[d] + ppatch->ldims[d] < psc->domain.gdims[d] ||
+	      psc->domain.bnd_part_hi[d] == BND_PART_PERIODIC) {
+	    xi[d] -= xm[d];
+	    dir[d] = +1;
+	    int bi = particle_real_fint(xi[d] * b_dxi[d]);
+	    if (bi < 0) {
+	      xi[d] = 0.;
+	    }
+	  } else {
+	    switch (psc->domain.bnd_part_hi[d]) {
+	    case BND_PART_REFLECTING:
+	      xi[d] = 2.f * xm[d] - xi[d];
+	      pxi[d] = -pxi[d];
+	      dir[d] = 0;
+	      int bi = particle_real_fint(xi[d] * b_dxi[d]);
+	      if (bi >= b_mx[d]) {
+		xi[d] *= (1. - 1e-6);
+	      }
+	      break;
+	    case BND_PART_ABSORBING:
+	      drop = true;
+	      break;
+	    default:
+	      assert(0);
+	    }
+	  }
+	} else {
+	  // computational bnd
+	  dir[d] = 0;
+	}
+	if (!drop) {
+	  if (xi[d] < 0.f && xi[d] > -1e-6f) {
+	    //	    mprintf("d %d xi %g\n", d, xi[d]);
+	    xi[d] = 0.f;
+	  }
+	  assert(xi[d] >= 0.f);
+	  assert(xi[d] <= xm[d]);
+	}
+      }
+      if (!drop) {
+	if (dir[0] == 0 && dir[1] == 0 && dir[2] == 0) {
+	  *particles_get_one(prts, patch->head++) = *part;
+	} else {
+	  ddc_particles_queue(ddcp, patch, dir, part);
+	}
+      }
+    }
+  }
+}
+
+// ----------------------------------------------------------------------
+// psc_bnd_particles_sub_exchange_particles_post
+
+static void
+psc_bnd_particles_sub_exchange_particles_post(struct psc_bnd_particles *bnd, struct psc_particles *prts)
+{
+  struct ddc_particles *ddcp = bnd->ddcp;
+  struct ddcp_patch *patch = &ddcp->patches[prts->p];
+  prts->n_part = patch->head;
+}
+
+// ----------------------------------------------------------------------
+// psc_bnd_particles_sub_exchange_particles
+
+extern double *psc_balance_comp_time_by_patch;
+extern int pr_time_step_no_comm;
+
+static void
+psc_bnd_particles_sub_exchange_particles(struct psc_bnd_particles *bnd, mparticles_base_t *particles_base)
+{
+  struct psc *psc = bnd->psc;
+
+  mparticles_t *particles = psc_mparticles_get_cf(particles_base, 0);
+
+  static int pr_A, pr_B, pr_C;
+  if (!pr_A) {
+    pr_A = prof_register("xchg_prep", 1., 0, 0);
+    pr_B = prof_register("xchg_comm", 1., 0, 0);
+    pr_C = prof_register("xchg_post", 1., 0, 0);
+  }
+  
+  prof_start(pr_A);
+
+  struct ddc_particles *ddcp = bnd->ddcp;
+
+  // FIXME we should make sure (assert) we don't quietly drop particle which left
+  // in the invariant direction
+
+  prof_restart(pr_time_step_no_comm);
+  psc_foreach_patch(psc, p) {
+    psc_balance_comp_time_by_patch[p] -= MPI_Wtime();
+    psc_bnd_particles_sub_exchange_particles_prep(bnd, psc_mparticles_get_patch(particles, p));
+    psc_balance_comp_time_by_patch[p] += MPI_Wtime();
+  }
+  prof_stop(pr_time_step_no_comm);
+
+  prof_stop(pr_A);
+
+  prof_start(pr_B);
+  ddc_particles_comm(ddcp, particles);
+  prof_stop(pr_B);
+
+  prof_start(pr_C);
+  for (int p = 0; p < particles->nr_patches; p++) {
+    psc_bnd_particles_sub_exchange_particles_post(bnd, psc_mparticles_get_patch(particles, p));
+  }
+  prof_stop(pr_C);
+
+  psc_mparticles_put_cf(particles, particles_base, 0);
+}
+
---
+++ src/libpsc/psc_bnd_particles/psc_bnd_particles_double_omp.c
@@ -0,0 +1,18 @@
+
+#include "psc_bnd_particles_private.h"
+#include "../psc_bnd/ddc_particles.h"
+#include "psc_particles_as_double.h"
+
+#include "psc_bnd_particles_common_omp.c"
+
+// ======================================================================
+// psc_bnd_particles: subclass "double_omp"
+
+struct psc_bnd_particles_ops psc_bnd_particles_double_omp_ops = {
+  .name                    = "double_omp",
+  .setup                   = psc_bnd_particles_sub_setup,
+  .unsetup                 = psc_bnd_particles_sub_unsetup,
+  .exchange_particles      = psc_bnd_particles_sub_exchange_particles,
+  .exchange_particles_prep = psc_bnd_particles_sub_exchange_particles_prep,
+  .exchange_particles_post = psc_bnd_particles_sub_exchange_particles_post,
+};
