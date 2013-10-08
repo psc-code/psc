8431cae90fc6da6a5b944a9d1124a0ff831ab2aa
Kai Germaschewski <kai.germaschewski@unh.edu>
2013-10-08 14:46:19 -0400

    psc_bnd_fields/auto: fix checkpointing

--- src/libpsc/psc_bnd_fields/psc_bnd_fields_auto.c
+++ src/libpsc/psc_bnd_fields/psc_bnd_fields_auto.c
@@ -1,18 +1,19 @@
 
 #include "psc_bnd_fields_private.h"
 
+#include <mrc_io.h>
 #include <string.h>
 
-struct sub {
+struct psc_bnd_fields_auto {
   struct psc_bnd_fields *fwd;
 };
 
-#define sub(bnd) mrc_to_subobj(bnd, struct sub)
+#define psc_bnd_fields_auto(bnd) mrc_to_subobj(bnd, struct psc_bnd_fields_auto)
 
 static void
 psc_bnd_fields_auto_setup(struct psc_bnd_fields *bnd)
 {
-  struct sub *sub = sub(bnd);
+  struct psc_bnd_fields_auto *sub = psc_bnd_fields_auto(bnd);
 
   const char *mflds_type = psc_mfields_type(ppsc->flds);
   char s[10 + strlen(mflds_type)];
@@ -20,20 +21,44 @@ psc_bnd_fields_auto_setup(struct psc_bnd_fields *bnd)
 
   MPI_Comm comm = psc_bnd_fields_comm(bnd);
   mpi_printf(comm, "INFO: using psc_bnd_fields '%s'\n", s);
-  
-  sub->fwd = psc_bnd_fields_create(comm);
-  psc_bnd_fields_set_type(sub->fwd, s);
-  psc_bnd_fields_setup(sub->fwd);
-  psc_bnd_fields_add_child(bnd, (struct mrc_obj *) sub->fwd);
 
+  sub->fwd = psc_bnd_fields_create(psc_bnd_fields_comm(bnd));
+  psc_bnd_fields_set_type(sub->fwd, s);
+  psc_bnd_fields_setup_member_objs_sub(bnd);
   psc_bnd_fields_setup_super(bnd);
+}
+
+static void
+psc_bnd_fields_auto_destroy(struct psc_bnd_fields *bnd)
+{
+  struct psc_bnd_fields_auto *sub = psc_bnd_fields_auto(bnd);
+
+  psc_bnd_fields_destroy(sub->fwd);
+}
+
+static void
+psc_bnd_fields_auto_write(struct psc_bnd_fields *bnd, struct mrc_io *io)
+{
+  struct psc_bnd_fields_auto *sub = psc_bnd_fields_auto(bnd);
+
+  psc_bnd_fields_write(bnd, io);
+  mrc_io_write_ref(io, bnd, "fwd", sub->fwd);
+}
+
+static void
+psc_bnd_fields_auto_read(struct psc_bnd_fields *bnd, struct mrc_io *io)
+{
+  struct psc_bnd_fields_auto *sub = psc_bnd_fields_auto(bnd);
+
+  psc_bnd_fields_read_super(bnd, io);
+  sub->fwd = mrc_io_read_ref(io, bnd, "fwd", psc_bnd_fields);
 }
 
 static void
 psc_bnd_fields_auto_fill_ghosts_a_E(struct psc_bnd_fields *bnd,
 				    struct psc_fields *flds_base)
 {
-  struct sub *sub = sub(bnd);
+  struct psc_bnd_fields_auto *sub = psc_bnd_fields_auto(bnd);
   struct psc_bnd_fields_ops *ops = psc_bnd_fields_ops(sub->fwd);
   assert(ops->fill_ghosts_a_E);
   ops->fill_ghosts_a_E(sub->fwd, flds_base);
@@ -43,7 +68,7 @@ static void
 psc_bnd_fields_auto_fill_ghosts_a_H(struct psc_bnd_fields *bnd,
 				    struct psc_fields *flds_base)
 {
-  struct sub *sub = sub(bnd);
+  struct psc_bnd_fields_auto *sub = psc_bnd_fields_auto(bnd);
   struct psc_bnd_fields_ops *ops = psc_bnd_fields_ops(sub->fwd);
   assert(ops->fill_ghosts_a_H);
   ops->fill_ghosts_a_H(sub->fwd, flds_base);
@@ -53,7 +78,7 @@ static void
 psc_bnd_fields_auto_fill_ghosts_b_E(struct psc_bnd_fields *bnd,
 				    struct psc_fields *flds_base)
 {
-  struct sub *sub = sub(bnd);
+  struct psc_bnd_fields_auto *sub = psc_bnd_fields_auto(bnd);
   struct psc_bnd_fields_ops *ops = psc_bnd_fields_ops(sub->fwd);
   assert(ops->fill_ghosts_b_E);
   ops->fill_ghosts_b_E(sub->fwd, flds_base);
@@ -63,7 +88,7 @@ static void
 psc_bnd_fields_auto_fill_ghosts_b_H(struct psc_bnd_fields *bnd,
 				    struct psc_fields *flds_base)
 {
-  struct sub *sub = sub(bnd);
+  struct psc_bnd_fields_auto *sub = psc_bnd_fields_auto(bnd);
   struct psc_bnd_fields_ops *ops = psc_bnd_fields_ops(sub->fwd);
   assert(ops->fill_ghosts_b_H);
   ops->fill_ghosts_b_H(sub->fwd, flds_base);
@@ -73,7 +98,7 @@ static void
 psc_bnd_fields_auto_add_ghosts_J(struct psc_bnd_fields *bnd,
 				 struct psc_fields *flds_base)
 {
-  struct sub *sub = sub(bnd);
+  struct psc_bnd_fields_auto *sub = psc_bnd_fields_auto(bnd);
   struct psc_bnd_fields_ops *ops = psc_bnd_fields_ops(sub->fwd);
   assert(ops->add_ghosts_J);
   ops->add_ghosts_J(sub->fwd, flds_base);
@@ -84,8 +109,11 @@ psc_bnd_fields_auto_add_ghosts_J(struct psc_bnd_fields *bnd,
 
 struct psc_bnd_fields_ops psc_bnd_fields_auto_ops = {
   .name                  = "auto",
-  .size                  = sizeof(struct sub),
+  .size                  = sizeof(struct psc_bnd_fields_auto),
   .setup                 = psc_bnd_fields_auto_setup,
+  .destroy               = psc_bnd_fields_auto_destroy,
+  .write                 = psc_bnd_fields_auto_write,
+  .read                  = psc_bnd_fields_auto_read,
   .fill_ghosts_a_E       = psc_bnd_fields_auto_fill_ghosts_a_E,
   .fill_ghosts_a_H       = psc_bnd_fields_auto_fill_ghosts_a_H,
   .fill_ghosts_b_E       = psc_bnd_fields_auto_fill_ghosts_b_E,
