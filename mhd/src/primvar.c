105e0cbdcb1a29b7fa219928b77277c269f930db
Kai Germaschewski <kai.germaschewski@unh.edu>
2014-01-10 17:43:19 -0500

    [ggcm] ggcm_mhd: add primvar_c() function
    which can be used for primvar1 or primvar2

--- mhd/src/primvar.c
+++ mhd/src/primvar.c
@@ -9,14 +9,14 @@
 #include <assert.h>
 
 // ----------------------------------------------------------------------
-// primvar1_c
+// primvar_c
 
 void
-primvar1_c(struct ggcm_mhd *mhd)
+primvar_c(struct ggcm_mhd *mhd, int m_curr)
 {
   static int PR;
   if (!PR) {
-    PR = prof_register("primvar1_c", 1., 0, 0);
+    PR = prof_register("primvar_c", 1., 0, 0);
   }
   prof_start(PR);
 
@@ -25,22 +25,31 @@ primvar1_c(struct ggcm_mhd *mhd)
   float s = gamm - 1.f;
 
   mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-    MRC_F3(f,_RR, ix,iy,iz) = MRC_F3(f,_RR1, ix,iy,iz);
-    float rri = 1.f / MRC_F3(f,_RR1, ix,iy,iz);
-    MRC_F3(f,_VX, ix,iy,iz) = rri * MRC_F3(f,_RV1X, ix,iy,iz);
-    MRC_F3(f,_VY, ix,iy,iz) = rri * MRC_F3(f,_RV1Y, ix,iy,iz);
-    MRC_F3(f,_VZ, ix,iy,iz) = rri * MRC_F3(f,_RV1Z, ix,iy,iz);
+    MRC_F3(f,_RR, ix,iy,iz) = MRC_F3(f, m_curr + _RR1, ix,iy,iz);
+    float rri = 1.f / MRC_F3(f, m_curr + _RR1, ix,iy,iz);
+    MRC_F3(f,_VX, ix,iy,iz) = rri * MRC_F3(f, m_curr + _RV1X, ix,iy,iz);
+    MRC_F3(f,_VY, ix,iy,iz) = rri * MRC_F3(f, m_curr + _RV1Y, ix,iy,iz);
+    MRC_F3(f,_VZ, ix,iy,iz) = rri * MRC_F3(f, m_curr + _RV1Z, ix,iy,iz);
     float rvv =
-      MRC_F3(f,_VX, ix,iy,iz) * MRC_F3(f,_RV1X, ix,iy,iz) +
-      MRC_F3(f,_VY, ix,iy,iz) * MRC_F3(f,_RV1Y, ix,iy,iz) +
-      MRC_F3(f,_VZ, ix,iy,iz) * MRC_F3(f,_RV1Z, ix,iy,iz);
-    MRC_F3(f,_PP, ix,iy,iz) = s * (MRC_F3(f,_UU1, ix,iy,iz) - .5f * rvv);
-    float cs2 = fmaxf(gamm*MRC_F3(f,_PP, ix,iy,iz)*rri, 0.f);
+      MRC_F3(f,_VX, ix,iy,iz) * MRC_F3(f, m_curr + _RV1X, ix,iy,iz) +
+      MRC_F3(f,_VY, ix,iy,iz) * MRC_F3(f, m_curr + _RV1Y, ix,iy,iz) +
+      MRC_F3(f,_VZ, ix,iy,iz) * MRC_F3(f, m_curr + _RV1Z, ix,iy,iz);
+    MRC_F3(f,_PP, ix,iy,iz) = s * (MRC_F3(f, m_curr + _UU1, ix,iy,iz) - .5f * rvv);
+    float cs2 = fmaxf(gamm * MRC_F3(f,_PP, ix,iy,iz) * rri, 0.f);
     MRC_F3(f,_CMSV, ix,iy,iz) = sqrtf(rvv * rri) + sqrtf(cs2);
   } mrc_fld_foreach_end;
 
   mrc_fld_put_as(f, mhd->fld);
 
   prof_stop(PR);
+}
+
+// ----------------------------------------------------------------------
+// primvar1_c
+
+void
+primvar1_c(struct ggcm_mhd *mhd)
+{
+  return primvar_c(mhd, _RR1);
 }
 
