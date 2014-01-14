e4270039139f2f5c2a504121e1bcedb89b9d3028
Kai Germaschewski <kai.germaschewski@unh.edu>
2014-01-14 14:34:36 -0500

    ggcm_mhd: calculate diag primitive variables directly
    rather than relying on primvar1(), which only works right for
    semi-conservative state.

--- mhd/src/ggcm_mhd_diag_item_primitive.c
+++ mhd/src/ggcm_mhd_diag_item_primitive.c
@@ -17,16 +17,33 @@
 
 static void
 ggcm_mhd_diag_item_v_run(struct ggcm_mhd_diag_item *item,
-			 struct mrc_io *io, struct mrc_fld *f,
+			 struct mrc_io *io, struct mrc_fld *fld,
 			 int diag_type, float plane)
 {
   struct ggcm_mhd *mhd = item->diag->mhd;
 
-  primvar1_c(mhd);
+  struct mrc_fld *fld_r = mrc_domain_fld_create(mhd->domain, SW_2, "vx:vy:vz");
+  mrc_fld_setup(fld_r);
+
+  struct mrc_fld *r = mrc_fld_get_as(fld_r, "float");
+  struct mrc_fld *f = mrc_fld_get_as(fld, "float");
+
+  mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+    float rri = 1.f / MRC_F3(f, _RR1, ix,iy,iz);
+    MRC_F3(r, 0, ix,iy,iz) = rri * MRC_F3(f, _RV1X, ix,iy,iz);
+    MRC_F3(r, 1, ix,iy,iz) = rri * MRC_F3(f, _RV1Y, ix,iy,iz);
+    MRC_F3(r, 2, ix,iy,iz) = rri * MRC_F3(f, _RV1Z, ix,iy,iz);
+  } mrc_fld_foreach_end;
+
+  mrc_fld_put_as(r, fld_r);
+  mrc_fld_put_as(f, fld);
+
   float scale_vv = mhd->par.vvnorm;
-  ggcm_mhd_diag_c_write_one_field(io, f, _VX, "vx", scale_vv, diag_type, plane);
-  ggcm_mhd_diag_c_write_one_field(io, f, _VY, "vy", scale_vv, diag_type, plane);
-  ggcm_mhd_diag_c_write_one_field(io, f, _VZ, "vz", scale_vv, diag_type, plane);
+  ggcm_mhd_diag_c_write_one_field(io, fld_r, 0, "vx", scale_vv, diag_type, plane);
+  ggcm_mhd_diag_c_write_one_field(io, fld_r, 1, "vy", scale_vv, diag_type, plane);
+  ggcm_mhd_diag_c_write_one_field(io, fld_r, 2, "vz", scale_vv, diag_type, plane);
+
+  mrc_fld_destroy(fld_r);
 }
 
 // ----------------------------------------------------------------------
@@ -50,9 +67,8 @@ ggcm_mhd_diag_item_rr_run(struct ggcm_mhd_diag_item *item,
 {
   struct ggcm_mhd *mhd = item->diag->mhd;
 
-  primvar1_c(mhd);
   float scale_rr = mhd->par.rrnorm;
-  ggcm_mhd_diag_c_write_one_field(io, f, _RR, "rr", scale_rr, diag_type, plane);
+  ggcm_mhd_diag_c_write_one_field(io, f, _RR1, "rr", scale_rr, diag_type, plane);
 }
 
 // ----------------------------------------------------------------------
@@ -69,12 +85,17 @@ struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_rr = {
 // ----------------------------------------------------------------------
 // ggcm_mhd_diag_item_pp_run
 
+#define F3 MRC_F3 // FIXME
+
 static void
 ggcm_mhd_diag_item_pp_run(struct ggcm_mhd_diag_item *item,
 			  struct mrc_io *io, struct mrc_fld *fld,
 			  int diag_type, float plane)
 {
   struct ggcm_mhd *mhd = item->diag->mhd;
+
+  int mhd_type;
+  mrc_fld_get_param_int(fld, "mhd_type", &mhd_type);
 
   struct mrc_fld *fld_r = mrc_domain_fld_create(mhd->domain, SW_2, "pp");
   mrc_fld_setup(fld_r);
@@ -84,12 +105,26 @@ ggcm_mhd_diag_item_pp_run(struct ggcm_mhd_diag_item *item,
   struct mrc_fld *r = mrc_fld_get_as(fld_r, "float");
   struct mrc_fld *f = mrc_fld_get_as(fld, "float");
 
-  mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-    float rvv = (sqr(MRC_F3(f, _RV1X, ix,iy,iz)) +
-		 sqr(MRC_F3(f, _RV1Y, ix,iy,iz)) +
-		 sqr(MRC_F3(f, _RV1Z, ix,iy,iz))) / MRC_F3(f, _RR1, ix,iy,iz);
-    MRC_F3(r,0, ix,iy,iz) = (gamm - 1.f) * (MRC_F3(f,_UU1, ix,iy,iz) - .5f * rvv);
-  } mrc_fld_foreach_end;
+  if (mhd_type == MT_SEMI_CONSERVATIVE) {
+    mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+      float rvv = (sqr(MRC_F3(f, _RV1X, ix,iy,iz)) +
+		   sqr(MRC_F3(f, _RV1Y, ix,iy,iz)) +
+		   sqr(MRC_F3(f, _RV1Z, ix,iy,iz))) / MRC_F3(f, _RR1, ix,iy,iz);
+      MRC_F3(r,0, ix,iy,iz) = (gamm - 1.f) * (MRC_F3(f,_UU1, ix,iy,iz) - .5f * rvv);
+    } mrc_fld_foreach_end;
+  } else if (mhd_type == MT_FULLY_CONSERVATIVE) {
+    mrc_fld_foreach(f, ix,iy,iz, 1, 1) {
+      float rvv = (sqr(MRC_F3(f, _RV1X, ix,iy,iz)) +
+		   sqr(MRC_F3(f, _RV1Y, ix,iy,iz)) +
+		   sqr(MRC_F3(f, _RV1Z, ix,iy,iz))) / MRC_F3(f, _RR1, ix,iy,iz);
+      float b2  = (sqr(.5*(B1X(f, ix,iy,iz) + B1X(f, ix+1,iy  ,iz  ))) +
+		   sqr(.5*(B1Y(f, ix,iy,iz) + B1Y(f, ix  ,iy+1,iz  ))) +
+		   sqr(.5*(B1Z(f, ix,iy,iz) + B1Z(f, ix  ,iy  ,iz+1))));
+      MRC_F3(r,0, ix,iy,iz) = (gamm - 1.f) * (MRC_F3(f,_UU1, ix,iy,iz) - .5f * rvv - .5f * b2);
+    } mrc_fld_foreach_end;
+  } else {
+    assert(0);
+  }
 
   mrc_fld_put_as(r, fld_r);
   mrc_fld_put_as(f, fld);
@@ -105,55 +140,6 @@ ggcm_mhd_diag_item_pp_run(struct ggcm_mhd_diag_item *item,
 struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_pp = {
   .name             = "pp",
   .run              = ggcm_mhd_diag_item_pp_run,
-};
-
-// ======================================================================
-// ggcm_mhd_diag_item subclass "pp_full"
-
-// ----------------------------------------------------------------------
-// ggcm_mhd_diag_item_pp_full_run
-
-#define F3 MRC_F3 // FIXME
-
-static void
-ggcm_mhd_diag_item_pp_full_run(struct ggcm_mhd_diag_item *item,
-			       struct mrc_io *io, struct mrc_fld *fld,
-			       int diag_type, float plane)
-{
-  struct ggcm_mhd *mhd = item->diag->mhd;
-
-  struct mrc_fld *fld_r = mrc_domain_fld_create(mhd->domain, SW_2, "pp_full");
-  mrc_fld_setup(fld_r);
-
-  float gamm = mhd->par.gamm;
-
-  struct mrc_fld *r = mrc_fld_get_as(fld_r, "float");
-  struct mrc_fld *f = mrc_fld_get_as(fld, "mhd_fc_float");
-
-  mrc_fld_foreach(f, ix,iy,iz, 1, 1) {
-    float rvv = (sqr(MRC_F3(f, _RV1X, ix,iy,iz)) +
-		 sqr(MRC_F3(f, _RV1Y, ix,iy,iz)) +
-		 sqr(MRC_F3(f, _RV1Z, ix,iy,iz))) / MRC_F3(f, _RR1, ix,iy,iz);
-    float b2  = (sqr(.5*(B1X(f, ix,iy,iz) + B1X(f, ix+1,iy  ,iz  ))) +
-		 sqr(.5*(B1Y(f, ix,iy,iz) + B1Y(f, ix  ,iy+1,iz  ))) +
-		 sqr(.5*(B1Z(f, ix,iy,iz) + B1Z(f, ix  ,iy  ,iz+1))));
-    MRC_F3(r,0, ix,iy,iz) = (gamm - 1.f) * (MRC_F3(f,_UU1, ix,iy,iz) - .5f * rvv - .5f * b2);
-  } mrc_fld_foreach_end;
-
-  mrc_fld_put_as(r, fld_r);
-  mrc_fld_put_as(f, fld);
-
-  ggcm_mhd_diag_c_write_one_field(io, fld_r, 0, "pp_full", mhd->par.ppnorm, diag_type, plane);
-
-  mrc_fld_destroy(fld_r);
-}
-
-// ----------------------------------------------------------------------
-// ggcm_mhd_diag_item subclass "pp_full"
-
-struct ggcm_mhd_diag_item_ops ggcm_mhd_diag_item_ops_pp_full = {
-  .name             = "pp_full",
-  .run              = ggcm_mhd_diag_item_pp_full_run,
 };
 
 // ======================================================================
