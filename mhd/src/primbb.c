2cf08ca1b0be26930623e1d9a06ef5fce1b2119e
Kai Germaschewski <kai.germaschewski@unh.edu>
2014-06-06 15:36:20 -0400

    [ggcm] ggcm_mhd: add "double" version of primbb()

--- mhd/src/primbb.c
+++ mhd/src/primbb.c
@@ -1,14 +1,32 @@
 
 #include "ggcm_mhd_private.h"
 
+#include <string.h>
+
 void
-primbb_c(struct ggcm_mhd *mhd, int m_curr)
+primbb_c(struct ggcm_mhd *mhd, int m)
 {
-  return primbb_float(mhd, m_curr);
+  const char *type = mrc_fld_type(mhd->fld);
+
+  if (strcmp(type, "float") == 0) {
+    return primbb_float(mhd, m);
+  } else if (strcmp(type, "double") == 0) {
+    return primbb_double(mhd, m);
+  } else {
+    assert(0);
+  }
 }
 
 void
-primbb_c2_c(struct ggcm_mhd *mhd, int m_curr)
+primbb_c2_c(struct ggcm_mhd *mhd, int m)
 {
-  return primbb_c2_float(mhd, m_curr);
+  const char *type = mrc_fld_type(mhd->fld);
+
+  if (strcmp(type, "float") == 0) {
+    return primbb_c2_float(mhd, m);
+  } else if (strcmp(type, "double") == 0) {
+    return primbb_c2_double(mhd, m);
+  } else {
+    assert(0);
+  }
 }
