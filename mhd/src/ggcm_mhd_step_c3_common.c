8079f0629ab03df5c64585789e5da316bb746e6e
Kai Germaschewski <kai.germaschewski@unh.edu>
2014-06-18 23:14:04 -0400

    [ggcm] ggcm_mhd: step "c3" mv newstep_sc(), zmask() -> mhd_sc.c
    Conflicts:
    	target-build/csrc/libmrc/mhd/src/ggcm_mhd_step_c3_common.c

--- mhd/src/ggcm_mhd_step_c3_common.c
+++ mhd/src/ggcm_mhd_step_c3_common.c
@@ -14,6 +14,7 @@
 
 #include "mhd_1d.c"
 #include "mhd_3d.c"
+#include "mhd_sc.c"
 
 // ======================================================================
 // ggcm_mhd_step subclass "c3"
@@ -692,66 +693,6 @@ pushstage_c(struct ggcm_mhd_step *step, mrc_fld_data_t dt,
   ggcm_mhd_step_put_3d_fld(step, fluxes[0]);
   ggcm_mhd_step_put_3d_fld(step, fluxes[1]);
   ggcm_mhd_step_put_3d_fld(step, fluxes[2]);
-}
-
-// ----------------------------------------------------------------------
-// newstep_sc
-
-static mrc_fld_data_t
-newstep_sc(struct ggcm_mhd *mhd, struct mrc_fld *x)
-{
-  float *fd1x = ggcm_mhd_crds_get_crd(mhd->crds, 0, FD1);
-  float *fd1y = ggcm_mhd_crds_get_crd(mhd->crds, 1, FD1);
-  float *fd1z = ggcm_mhd_crds_get_crd(mhd->crds, 2, FD1);
-
-  mrc_fld_data_t splim2 = sqr(mhd->par.speedlimit / mhd->par.vvnorm);
-  mrc_fld_data_t gamm   = mhd->par.gamm;
-  mrc_fld_data_t thx    = mhd->par.thx;
-  mrc_fld_data_t eps    = 1e-9f;
-  mrc_fld_data_t dt     = 1e10f;
-
-  mrc_fld_foreach(x, ix, iy, iz, 0, 0) {
-    mrc_fld_data_t hh = mrc_fld_max(mrc_fld_max(fd1x[ix], fd1y[iy]), fd1z[iz]);
-    mrc_fld_data_t rri = 1.f / mrc_fld_abs(RR(x, ix,iy,iz)); // FIXME abs necessary?
-    mrc_fld_data_t bb = (sqr(.5f * (BX(x, ix,iy,iz) + BX(x, ix-1,iy,iz))) + 
-			 sqr(.5f * (BY(x, ix,iy,iz) + BY(x, ix,iy-1,iz))) +
-			 sqr(.5f * (BZ(x, ix,iy,iz) + BZ(x, ix,iy,iz-1))));
-    mrc_fld_data_t vv1 = mrc_fld_min(bb * rri, splim2);
-    mrc_fld_data_t rrvv = (sqr(RVX(x, ix,iy,iz)) + 
-			   sqr(RVY(x, ix,iy,iz)) +
-			   sqr(RVZ(x, ix,iy,iz)));
-    mrc_fld_data_t pp = (gamm - 1.f) * (UU(x, ix,iy,iz) - .5f * rrvv * rri);
-    mrc_fld_data_t vv2 = gamm * pp * rri;
-    mrc_fld_data_t vv = mrc_fld_sqrt(vv1 + vv2) + mrc_fld_sqrt(rrvv) * rri;
-    vv = mrc_fld_max(eps, vv);
-
-    mrc_fld_data_t tt = thx / mrc_fld_max(eps, hh*vv*F3(x, _ZMASK, ix,iy,iz));
-    dt = mrc_fld_min(dt, tt);
-  } mrc_fld_foreach_end;
-
-  mrc_fld_data_t dtn;
-  MPI_Allreduce(&dt, &dtn, 1, MPI_MRC_FLD_DATA_T, MPI_MIN, ggcm_mhd_comm(mhd));
-
-  return dtn;
-}
-
-// ----------------------------------------------------------------------
-// zmaskn
-
-static void
-zmaskn(struct ggcm_mhd *mhd, struct mrc_fld *x)
-{
-  float va02i = 1.f / sqr(mhd->par.speedlimit / mhd->par.vvnorm);
-  float eps   = 1e-15f;
-
-  mrc_fld_foreach(x, ix,iy,iz, 1, 1) {
-    mrc_fld_data_t bb = (sqr(.5f * (BX(x, ix,iy,iz) + BX(x, ix-1,iy,iz))) +
-			 sqr(.5f * (BY(x, ix,iy,iz) + BY(x, ix,iy-1,iz))) +
-			 sqr(.5f * (BZ(x, ix,iy,iz) + BZ(x, ix,iy,iz-1))));
-    float rrm = fmaxf(eps, bb * va02i);
-    F3(x, _ZMASK, ix,iy,iz) = F3(x, _YMASK, ix,iy,iz) *
-      fminf(1.f, RR(x, ix,iy,iz) / rrm);
-  } mrc_fld_foreach_end;
 }
 
 // ----------------------------------------------------------------------
