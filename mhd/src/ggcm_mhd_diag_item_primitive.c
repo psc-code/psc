24a2eed83eb3b6b824501eec5c6224eaeef241cd
Kai Germaschewski <kai.germaschewski@unh.edu>
2014-06-23 23:04:50 -0400

    [ggcm] ggcm_mhd: use compute_B_cc() in "b" diag item

--- mhd/src/ggcm_mhd_diag_item_primitive.c
+++ mhd/src/ggcm_mhd_diag_item_primitive.c
@@ -10,6 +10,8 @@
 #include <stdio.h>
 #include <assert.h>
 
+#include "mhd_3d.c"
+
 // ======================================================================
 // ggcm_mhd_diag_item subclass "v"
 
@@ -174,11 +176,7 @@ ggcm_mhd_diag_item_b_run(struct ggcm_mhd_diag_item *item,
     } mrc_fld_foreach_end;
   } else if (mhd_type == MT_SEMI_CONSERVATIVE ||
 	     mhd_type == MT_FULLY_CONSERVATIVE) {
-    mrc_fld_foreach(f, ix,iy,iz, 0, 0) {
-      F3(fld_r, 0, ix,iy,iz) = .5f * (BX(f, ix,iy,iz) + BX(f, ix+1,iy,iz));
-      F3(fld_r, 1, ix,iy,iz) = .5f * (BY(f, ix,iy,iz) + BY(f, ix,iy+1,iz));
-      F3(fld_r, 2, ix,iy,iz) = .5f * (BZ(f, ix,iy,iz) + BZ(f, ix,iy,iz+1));
-    } mrc_fld_foreach_end;
+    compute_B_cc(fld_r, f, 0, 0);
   } else {
     assert(0);
   }
