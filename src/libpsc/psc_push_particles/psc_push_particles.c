bb230cbcc93a1320e0e621dc659dcd2b9c77fb93
Kai Germaschewski <kai.germaschewski@unh.edu>
2014-07-06 22:59:16 -0400

    push_particles: fix "mix": only if CUDA and SSE2

--- src/libpsc/psc_push_particles/psc_push_particles.c
+++ src/libpsc/psc_push_particles/psc_push_particles.c
@@ -200,7 +200,9 @@ psc_push_particles_init()
   mrc_class_register_subclass(&mrc_class_psc_push_particles, &psc_push_particles_1vb_2x2_cuda_ops);
   mrc_class_register_subclass(&mrc_class_psc_push_particles, &psc_push_particles_1vb_4x4_cuda_ops);
   mrc_class_register_subclass(&mrc_class_psc_push_particles, &psc_push_particles_1vb_8x8_cuda_ops);
+#ifdef USE_SSE2
   mrc_class_register_subclass(&mrc_class_psc_push_particles, &psc_push_particles_1vb_mix_ops);
+#endif
 #endif
 }
 
