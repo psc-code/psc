10fb27c411c7730934a8e56a79e1ed75cd05154c
Kai Germaschewski <kai.germaschewski@unh.edu>
2014-11-16 19:44:11 -0500

    [ggcm] ggcm_mhd_crds: mv the various cc/nc/fc/ec access functions here

--- mhd/include/ggcm_mhd_crds.h
+++ mhd/include/ggcm_mhd_crds.h
@@ -8,7 +8,15 @@
 
 MRC_CLASS_DECLARE(ggcm_mhd_crds, struct ggcm_mhd_crds);
 
+// cell-centered + other pre-calc coords as arrays
 float *ggcm_mhd_crds_get_crd(struct ggcm_mhd_crds *crds, int d, int m);
 float *ggcm_mhd_crds_get_global_crd(struct ggcm_mhd_crds *crds, int d);
+
+// direct access to coords for a given cell
+// (ideally avoided for performance critical parts, because it's slower)
+void ggcm_mhd_crds_get_cc(struct ggcm_mhd_crds *crds, int ix, int iy, int iz, float crd_cc[3]);
+void ggcm_mhd_crds_get_nc(struct ggcm_mhd_crds *crds, int ix, int iy, int iz, float crd_nc[3]);
+void ggcm_mhd_crds_get_fc(struct ggcm_mhd_crds *crds, int ix, int iy, int iz, int d, float crd_fc[3]);
+void ggcm_mhd_crds_get_ec(struct ggcm_mhd_crds *crds, int ix, int iy, int iz, int d, float crd_ec[3]);
 
 #endif
