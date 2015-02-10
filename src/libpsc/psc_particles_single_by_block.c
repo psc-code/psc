1314a71edfaef2d76e3552d8149c7cf9da0c986c
Kai Germaschewski <kai.germaschewski@unh.edu>
2015-02-10 10:25:47 -0500

    by_block: define new particle type "single_by_block"
    which is identical so far to "single", because that already incorporated
    a lot of sorted-by-block.

---
+++ src/libpsc/psc_particles_single_by_block.c
@@ -0,0 +1,263 @@
+
+#include "psc_particles_single_by_block.h"
+#include "psc.h"
+
+#include <mrc_io.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+
+// ======================================================================
+// psc_particles "single_by_block"
+
+static void
+psc_particles_single_by_block_setup(struct psc_particles *prts)
+{
+  struct psc_particles_single_by_block *sub = psc_particles_single_by_block(prts);
+
+  sub->n_alloced = prts->n_part * 1.2;
+  sub->particles = calloc(sub->n_alloced, sizeof(*sub->particles));
+  sub->particles_alt = calloc(sub->n_alloced, sizeof(*sub->particles_alt));
+  sub->b_idx = calloc(sub->n_alloced, sizeof(*sub->b_idx));
+  sub->b_ids = calloc(sub->n_alloced, sizeof(*sub->b_ids));
+
+  for (int d = 0; d < 3; d++) {
+    sub->b_mx[d] = ppsc->patch[prts->p].ldims[d];
+    sub->b_dxi[d] = 1.f / ppsc->patch[prts->p].dx[d];
+  }
+  sub->nr_blocks = sub->b_mx[0] * sub->b_mx[1] * sub->b_mx[2];
+  sub->b_cnt = calloc(sub->nr_blocks + 1, sizeof(*sub->b_cnt));
+}
+
+static void
+psc_particles_single_by_block_destroy(struct psc_particles *prts)
+{
+  struct psc_particles_single_by_block *sub = psc_particles_single_by_block(prts);
+
+  free(sub->particles);
+  free(sub->particles_alt);
+  free(sub->b_idx);
+  free(sub->b_ids);
+  free(sub->b_cnt);
+}
+
+static void
+psc_particles_single_by_block_reorder(struct psc_particles *prts)
+{
+  struct psc_particles_single_by_block *sub = psc_particles_single_by_block(prts);
+
+  if (!sub->need_reorder) {
+    return;
+  }
+
+  for (int n = 0; n < prts->n_part; n++) {
+    sub->particles_alt[n] = sub->particles[sub->b_ids[n]];
+  }
+  
+  // swap in alt array
+  particle_single_by_block_t *tmp = sub->particles;
+  sub->particles = sub->particles_alt;
+  sub->particles_alt = tmp;
+  sub->need_reorder = false;
+}
+
+void
+particles_single_realloc(struct psc_particles *prts, int new_n_part)
+{
+  struct psc_particles_single_by_block *sub = psc_particles_single_by_block(prts);
+
+  if (new_n_part <= sub->n_alloced)
+    return;
+
+  sub->n_alloced = new_n_part * 1.2;
+  sub->particles = realloc(sub->particles, sub->n_alloced * sizeof(*sub->particles));
+  sub->b_idx = realloc(sub->b_idx, sub->n_alloced * sizeof(*sub->b_idx));
+  sub->b_ids = realloc(sub->b_ids, sub->n_alloced * sizeof(*sub->b_ids));
+  free(sub->particles_alt);
+  sub->particles_alt = malloc(sub->n_alloced * sizeof(*sub->particles_alt));
+}
+
+// ======================================================================
+
+#ifdef HAVE_LIBHDF5_HL
+
+// FIXME. This is a rather bad break of proper layering, HDF5 should be all
+// mrc_io business. OTOH, it could be called flexibility...
+
+#include <hdf5.h>
+#include <hdf5_hl.h>
+
+#define H5_CHK(ierr) assert(ierr >= 0)
+#define CE assert(ierr == 0)
+
+// ----------------------------------------------------------------------
+// psc_particles_single_by_block_write
+
+static void
+psc_particles_single_by_block_write(struct psc_particles *prts, struct mrc_io *io)
+{
+  int ierr;
+  assert(sizeof(particle_single_by_block_t) / sizeof(particle_single_by_block_real_t) == 8);
+  assert(sizeof(particle_single_by_block_real_t) == sizeof(float));
+
+  long h5_file;
+  mrc_io_get_h5_file(io, &h5_file);
+
+  hid_t group = H5Gopen(h5_file, mrc_io_obj_path(io, prts), H5P_DEFAULT); H5_CHK(group);
+  // save/restore n_alloced, too?
+  ierr = H5LTset_attribute_int(group, ".", "p", &prts->p, 1); CE;
+  ierr = H5LTset_attribute_int(group, ".", "n_part", &prts->n_part, 1); CE;
+  ierr = H5LTset_attribute_uint(group, ".", "flags", &prts->flags, 1); CE;
+  if (prts->n_part > 0) {
+    // in a rather ugly way, we write the int "kind" member as a float
+    hsize_t hdims[2] = { prts->n_part, 8 };
+    ierr = H5LTmake_dataset_float(group, "particles_single_by_block", 2, hdims,
+				  (float *) particles_single_by_block_get_one(prts, 0)); CE;
+  }
+  ierr = H5Gclose(group); CE;
+}
+
+// ----------------------------------------------------------------------
+// psc_particles_single_by_block_by_block_read
+
+static void
+psc_particles_single_by_block_read(struct psc_particles *prts, struct mrc_io *io)
+{
+  int ierr;
+  long h5_file;
+  mrc_io_get_h5_file(io, &h5_file);
+
+  hid_t group = H5Gopen(h5_file, mrc_io_obj_path(io, prts), H5P_DEFAULT); H5_CHK(group);
+  ierr = H5LTget_attribute_int(group, ".", "p", &prts->p); CE;
+  ierr = H5LTget_attribute_int(group, ".", "n_part", &prts->n_part); CE;
+  ierr = H5LTget_attribute_uint(group, ".", "flags", &prts->flags); CE;
+  psc_particles_setup(prts);
+  if (prts->n_part > 0) {
+    ierr = H5LTread_dataset_float(group, "particles_single_by_block",
+				  (float *) particles_single_by_block_get_one(prts, 0)); CE;
+  }
+  ierr = H5Gclose(group); CE;
+}
+
+#endif
+
+// ======================================================================
+
+static inline void
+calc_vxi(particle_single_by_block_real_t vxi[3], particle_single_by_block_t *part)
+{
+  particle_single_by_block_real_t root =
+    1.f / sqrtf(1.f + sqr(part->pxi) + sqr(part->pyi) + sqr(part->pzi));
+  vxi[0] = part->pxi * root;
+  vxi[1] = part->pyi * root;
+  vxi[2] = part->pzi * root;
+}
+
+static void
+psc_particles_single_by_block_copy_to_c(struct psc_particles *prts_base,
+			       struct psc_particles *prts_c, unsigned int flags)
+{
+  particle_single_by_block_real_t dth[3] = { .5 * ppsc->dt, .5 * ppsc->dt, .5 * ppsc->dt };
+  // don't shift in invariant directions
+  for (int d = 0; d < 3; d++) {
+    if (ppsc->domain.gdims[d] == 1) {
+      dth[d] = 0.;
+    }
+  }
+
+  prts_c->n_part = prts_base->n_part;
+  assert(prts_c->n_part <= psc_particles_c(prts_c)->n_alloced);
+  for (int n = 0; n < prts_base->n_part; n++) {
+    particle_single_by_block_t *part_base = particles_single_by_block_get_one(prts_base, n);
+    particle_c_t *part = particles_c_get_one(prts_c, n);
+    
+    particle_c_real_t qni = ppsc->kinds[part_base->kind].q;
+    particle_c_real_t mni = ppsc->kinds[part_base->kind].m;
+    particle_c_real_t wni = part_base->qni_wni / qni;
+    
+    particle_single_by_block_real_t vxi[3];
+    calc_vxi(vxi, part_base);
+    part->xi  = part_base->xi - dth[0] * vxi[0];
+    part->yi  = part_base->yi - dth[1] * vxi[1];
+    part->zi  = part_base->zi - dth[2] * vxi[2];
+    part->pxi = part_base->pxi;
+    part->pyi = part_base->pyi;
+    part->pzi = part_base->pzi;
+    part->qni = qni;
+    part->mni = mni;
+    part->wni = wni;
+    part->kind = part_base->kind;
+  }
+}
+
+static void
+psc_particles_single_by_block_copy_from_c(struct psc_particles *prts_base,
+				 struct psc_particles *prts_c, unsigned int flags)
+{
+  particle_single_by_block_real_t dth[3] = { .5 * ppsc->dt, .5 * ppsc->dt, .5 * ppsc->dt };
+  // don't shift in invariant directions
+  for (int d = 0; d < 3; d++) {
+    if (ppsc->domain.gdims[d] == 1) {
+      dth[d] = 0.;
+    }
+  }
+
+  struct psc_particles_single_by_block *sub = psc_particles_single_by_block(prts_base);
+  prts_base->n_part = prts_c->n_part;
+  assert(prts_base->n_part <= sub->n_alloced);
+  for (int n = 0; n < prts_base->n_part; n++) {
+    particle_single_by_block_t *part_base = particles_single_by_block_get_one(prts_base, n);
+    particle_c_t *part = particles_c_get_one(prts_c, n);
+    
+    particle_single_by_block_real_t qni_wni;
+    if (part->qni != 0.) {
+      qni_wni = part->qni * part->wni;
+    } else {
+      qni_wni = part->wni;
+    }
+    
+    part_base->xi          = part->xi;
+    part_base->yi          = part->yi;
+    part_base->zi          = part->zi;
+    part_base->pxi         = part->pxi;
+    part_base->pyi         = part->pyi;
+    part_base->pzi         = part->pzi;
+    part_base->qni_wni     = qni_wni;
+    part_base->kind        = part->kind;
+
+    particle_single_by_block_real_t vxi[3];
+    calc_vxi(vxi, part_base);
+    part_base->xi += dth[0] * vxi[0];
+    part_base->yi += dth[1] * vxi[1];
+    part_base->zi += dth[2] * vxi[2];
+  }
+}
+
+// ======================================================================
+// psc_mparticles: subclass "single_by_block"
+  
+struct psc_mparticles_ops psc_mparticles_single_by_block_ops = {
+  .name                    = "single_by_block",
+};
+
+// ======================================================================
+// psc_particles: subclass "single"
+
+static struct mrc_obj_method psc_particles_single_by_block_methods[] = {
+  MRC_OBJ_METHOD("copy_to_c"       , psc_particles_single_by_block_copy_to_c),
+  MRC_OBJ_METHOD("copy_from_c"     , psc_particles_single_by_block_copy_from_c),
+  {}
+};
+
+struct psc_particles_ops psc_particles_single_by_block_ops = {
+  .name                    = "single_by_block",
+  .size                    = sizeof(struct psc_particles_single_by_block),
+  .methods                 = psc_particles_single_by_block_methods,
+  .setup                   = psc_particles_single_by_block_setup,
+  .destroy                 = psc_particles_single_by_block_destroy,
+#ifdef HAVE_LIBHDF5_HL
+  .read                    = psc_particles_single_by_block_read,
+  .write                   = psc_particles_single_by_block_write,
+#endif
+  .reorder                 = psc_particles_single_by_block_reorder,
+};
