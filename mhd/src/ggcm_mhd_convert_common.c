52c5f8bfe75eccffbac2fbf4b39af9072d2d6a3f
Kai Germaschewski <kai.germaschewski@unh.edu>
2015-04-10 00:58:04 -0400

    [ggcm] ggcm_mhd_convert: handle multi-patch domains
    (fixes some diag issues with AMR / multi-patch)

--- mhd/src/ggcm_mhd_convert_common.c
+++ mhd/src/ggcm_mhd_convert_common.c
@@ -10,23 +10,25 @@ copy_sc_ggcm_to_sc(struct mrc_fld *_ff, struct mrc_fld *_f, int mb, int me)
   struct mrc_fld *f = mrc_fld_get_as(_f, FLD_TYPE);
   struct mrc_fld *ff = mrc_fld_get_as(_ff, FLD_TYPE);
 
-  for (int m = mb; m < me; m++) {
-    if (m == _B1X || m == _B2X) {
-      mrc_fld_foreach(f, ix,iy,iz, 1, 2) {
-	F3(f, m, ix,iy,iz) = F3(ff, m, ix-1,iy,iz);
+  for (int p = 0; p < mrc_fld_nr_patches(f); p++) {
+    for (int m = mb; m < me; m++) {
+      if (m == _B1X || m == _B2X) {
+	mrc_fld_foreach(f, ix,iy,iz, 1, 2) {
+	  M3(f, m, ix,iy,iz, p) = M3(ff, m, ix-1,iy,iz, p);
+	} mrc_fld_foreach_end;
+      } else if (m == _B1Y || m == _B2Y) {
+	mrc_fld_foreach(f, ix,iy,iz, 1, 2) {
+	  M3(f, m, ix,iy,iz, p) = M3(ff, m, ix,iy-1,iz, p);
       } mrc_fld_foreach_end;
-    } else if (m == _B1Y || m == _B2Y) {
-      mrc_fld_foreach(f, ix,iy,iz, 1, 2) {
-	F3(f, m, ix,iy,iz) = F3(ff, m, ix,iy-1,iz);
-      } mrc_fld_foreach_end;
-    } else if (m == _B1Z || m == _B2Z) {
-      mrc_fld_foreach(f, ix,iy,iz, 1, 2) {
-	F3(f, m, ix,iy,iz) = F3(ff, m, ix,iy,iz-1);
-      } mrc_fld_foreach_end;
-    } else {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(f, m, ix,iy,iz) = F3(ff, m, ix,iy,iz);
-      } mrc_fld_foreach_end;
+      } else if (m == _B1Z || m == _B2Z) {
+	mrc_fld_foreach(f, ix,iy,iz, 1, 2) {
+	  M3(f, m, ix,iy,iz, p) = M3(ff, m, ix,iy,iz-1, p);
+	} mrc_fld_foreach_end;
+      } else {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(f, m, ix,iy,iz, p) = M3(ff, m, ix,iy,iz, p);
+	} mrc_fld_foreach_end;
+      }
     }
   }
 
@@ -40,30 +42,32 @@ copy_sc_ggcm_to_fc(struct mrc_fld *_ff, struct mrc_fld *_f, int mb, int me)
   struct mrc_fld *f = mrc_fld_get_as(_f, FLD_TYPE);
   struct mrc_fld *ff = mrc_fld_get_as(_ff, FLD_TYPE);
 
-  for (int m = mb; m < me; m++) {
-    if (m == _B1X || m == _B2X) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(f, m, ix,iy,iz) = F3(ff, m, ix-1,iy,iz);
-      } mrc_fld_foreach_end;
-    } else if (m == _B1Y || m == _B2Y) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(f, m, ix,iy,iz) = F3(ff, m, ix,iy-1,iz);
-      } mrc_fld_foreach_end;
-    } else if (m == _B1Z || m == _B2Z) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(f, m, ix,iy,iz) = F3(ff, m, ix,iy,iz-1);
-      } mrc_fld_foreach_end;
-    } else if (m == _UU1) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 1) {
-	float b2  = (sqr(.5f * (F3(ff, _B1X, ix,iy,iz) + F3(ff, _B1X, ix+1,iy  ,iz  ))) +
-		     sqr(.5f * (F3(ff, _B1Y, ix,iy,iz) + F3(ff, _B1Y, ix  ,iy+1,iz  ))) +
-		     sqr(.5f * (F3(ff, _B1Z, ix,iy,iz) + F3(ff, _B1Z, ix  ,iy  ,iz+1))));
-	F3(f, _UU1, ix,iy,iz) = F3(ff, _UU1, ix,iy,iz) + .5 * b2;
-      } mrc_fld_foreach_end;
-    } else {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(f, m, ix,iy,iz) = F3(ff, m, ix,iy,iz);
-      } mrc_fld_foreach_end;
+  for (int p = 0; p < mrc_fld_nr_patches(f); p++) {
+    for (int m = mb; m < me; m++) {
+      if (m == _B1X || m == _B2X) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(f, m, ix,iy,iz, p) = M3(ff, m, ix-1,iy,iz, p);
+	} mrc_fld_foreach_end;
+      } else if (m == _B1Y || m == _B2Y) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(f, m, ix,iy,iz, p) = M3(ff, m, ix,iy-1,iz, p);
+	} mrc_fld_foreach_end;
+      } else if (m == _B1Z || m == _B2Z) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(f, m, ix,iy,iz, p) = M3(ff, m, ix,iy,iz-1, p);
+	} mrc_fld_foreach_end;
+      } else if (m == _UU1) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 1) {
+	  float b2  = (sqr(.5f * (M3(ff, _B1X, ix,iy,iz, p) + M3(ff, _B1X, ix+1,iy  ,iz  , p))) +
+		       sqr(.5f * (M3(ff, _B1Y, ix,iy,iz, p) + M3(ff, _B1Y, ix  ,iy+1,iz  , p))) +
+		       sqr(.5f * (M3(ff, _B1Z, ix,iy,iz, p) + M3(ff, _B1Z, ix  ,iy  ,iz+1, p))));
+	  M3(f, _UU1, ix,iy,iz, p) = M3(ff, _UU1, ix,iy,iz, p) + .5 * b2;
+	} mrc_fld_foreach_end;
+      } else {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(f, m, ix,iy,iz, p) = M3(ff, m, ix,iy,iz, p);
+	} mrc_fld_foreach_end;
+      }
     }
   }
 
@@ -77,23 +81,25 @@ copy_sc_to_sc_ggcm(struct mrc_fld *_f, struct mrc_fld *_ff, int mb, int me)
   struct mrc_fld *f = mrc_fld_get_as(_f, FLD_TYPE);
   struct mrc_fld *ff = mrc_fld_get_as(_ff, FLD_TYPE);
 
-  for (int m = mb; m < me; m++) {
-    if (m == _B1X || m == _B2X) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 1) {
-	F3(ff, m, ix,iy,iz) = F3(f, m, ix+1,iy,iz);
-      } mrc_fld_foreach_end;
-    } else if (m == _B1Y || m == _B2Y) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 1) {
-	F3(ff, m, ix,iy,iz) = F3(f, m, ix,iy+1,iz);
-      } mrc_fld_foreach_end;
-    } else if (m == _B1Z || m == _B2Z) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 1) {
-	F3(ff, m, ix,iy,iz) = F3(f, m, ix,iy,iz+1);
-      } mrc_fld_foreach_end;
-    } else {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(ff, m, ix,iy,iz) = F3(f, m, ix,iy,iz);
-      } mrc_fld_foreach_end;
+  for (int p = 0; p < mrc_fld_nr_patches(f); p++) {
+    for (int m = mb; m < me; m++) {
+      if (m == _B1X || m == _B2X) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 1) {
+	  M3(ff, m, ix,iy,iz, p) = M3(f, m, ix+1,iy,iz, p);
+	} mrc_fld_foreach_end;
+      } else if (m == _B1Y || m == _B2Y) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 1) {
+	  M3(ff, m, ix,iy,iz, p) = M3(f, m, ix,iy+1,iz, p);
+	} mrc_fld_foreach_end;
+      } else if (m == _B1Z || m == _B2Z) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 1) {
+	  M3(ff, m, ix,iy,iz, p) = M3(f, m, ix,iy,iz+1, p);
+	} mrc_fld_foreach_end;
+      } else {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(ff, m, ix,iy,iz, p) = M3(f, m, ix,iy,iz, p);
+	} mrc_fld_foreach_end;
+      }
     }
   }
 
@@ -107,30 +113,32 @@ copy_fc_to_sc_ggcm(struct mrc_fld *_f, struct mrc_fld *_ff, int mb, int me)
   struct mrc_fld *f = mrc_fld_get_as(_f, FLD_TYPE);
   struct mrc_fld *ff = mrc_fld_get_as(_ff, FLD_TYPE);
 
-  for (int m = mb; m < me; m++) {
-    if (m == _B1X || m == _B2X) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(ff, m, ix,iy,iz) = F3(f, m, ix+1,iy,iz);
-      } mrc_fld_foreach_end;
-    } else if (m == _B1Y || m == _B2Y) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(ff, m, ix,iy,iz) = F3(f, m, ix,iy+1,iz);
-      } mrc_fld_foreach_end;
-    } else if (m == _B1Z || m == _B2Z) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(ff, m, ix,iy,iz) = F3(f, m, ix,iy,iz+1);
-      } mrc_fld_foreach_end;
-    } else if (m == _UU1) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	float b2  = (sqr(.5f * (F3(f, _B1X, ix,iy,iz) + F3(f, _B1X, ix+1,iy  ,iz  ))) +
-		     sqr(.5f * (F3(f, _B1Y, ix,iy,iz) + F3(f, _B1Y, ix  ,iy+1,iz  ))) +
-		     sqr(.5f * (F3(f, _B1Z, ix,iy,iz) + F3(f, _B1Z, ix  ,iy  ,iz+1))));
-	F3(ff, _UU1, ix,iy,iz) = F3(f, _UU1, ix,iy,iz) - .5 * b2;
-      } mrc_fld_foreach_end;
-    } else {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(ff, m, ix,iy,iz) = F3(f, m, ix,iy,iz);
-      } mrc_fld_foreach_end;
+  for (int p = 0; p < mrc_fld_nr_patches(f); p++) {
+    for (int m = mb; m < me; m++) {
+      if (m == _B1X || m == _B2X) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(ff, m, ix,iy,iz, p) = M3(f, m, ix+1,iy,iz, p);
+	} mrc_fld_foreach_end;
+      } else if (m == _B1Y || m == _B2Y) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(ff, m, ix,iy,iz, p) = M3(f, m, ix,iy+1,iz, p);
+	} mrc_fld_foreach_end;
+      } else if (m == _B1Z || m == _B2Z) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(ff, m, ix,iy,iz, p) = M3(f, m, ix,iy,iz+1, p);
+	} mrc_fld_foreach_end;
+      } else if (m == _UU1) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  float b2  = (sqr(.5f * (M3(f, _B1X, ix,iy,iz, p) + M3(f, _B1X, ix+1,iy  ,iz  , p))) +
+		       sqr(.5f * (M3(f, _B1Y, ix,iy,iz, p) + M3(f, _B1Y, ix  ,iy+1,iz  , p))) +
+		       sqr(.5f * (M3(f, _B1Z, ix,iy,iz, p) + M3(f, _B1Z, ix  ,iy  ,iz+1, p))));
+	  M3(ff, _UU1, ix,iy,iz, p) = M3(f, _UU1, ix,iy,iz, p) - .5 * b2;
+	} mrc_fld_foreach_end;
+      } else {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(ff, m, ix,iy,iz, p) = M3(f, m, ix,iy,iz, p);
+	} mrc_fld_foreach_end;
+      }
     }
   }
 
@@ -144,18 +152,20 @@ copy_fc_to_sc(struct mrc_fld *_f, struct mrc_fld *_ff, int mb, int me)
   struct mrc_fld *f = mrc_fld_get_as(_f, FLD_TYPE);
   struct mrc_fld *ff = mrc_fld_get_as(_ff, FLD_TYPE);
 
-  for (int m = mb; m < me; m++) {
-    if (m == _UU1) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	float b2  = (sqr(.5f * (F3(f, _B1X, ix,iy,iz) + F3(f, _B1X, ix+1,iy  ,iz  ))) +
-		     sqr(.5f * (F3(f, _B1Y, ix,iy,iz) + F3(f, _B1Y, ix  ,iy+1,iz  ))) +
-		     sqr(.5f * (F3(f, _B1Z, ix,iy,iz) + F3(f, _B1Z, ix  ,iy  ,iz+1))));
-	F3(ff, _UU1, ix,iy,iz) = F3(f, _UU1, ix,iy,iz) - .5 * b2;
-      } mrc_fld_foreach_end;
-    } else {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(ff, m, ix,iy,iz) = F3(f, m, ix,iy,iz);
-      } mrc_fld_foreach_end;
+  for (int p = 0; p < mrc_fld_nr_patches(f); p++) {
+    for (int m = mb; m < me; m++) {
+      if (m == _UU1) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  float b2  = (sqr(.5f * (M3(f, _B1X, ix,iy,iz, p) + M3(f, _B1X, ix+1,iy  ,iz  , p))) +
+		       sqr(.5f * (M3(f, _B1Y, ix,iy,iz, p) + M3(f, _B1Y, ix  ,iy+1,iz  , p))) +
+		       sqr(.5f * (M3(f, _B1Z, ix,iy,iz, p) + M3(f, _B1Z, ix  ,iy  ,iz+1, p))));
+	M3(ff, _UU1, ix,iy,iz, p) = M3(f, _UU1, ix,iy,iz, p) - .5 * b2;
+	} mrc_fld_foreach_end;
+      } else {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(ff, m, ix,iy,iz, p) = M3(f, m, ix,iy,iz, p);
+	} mrc_fld_foreach_end;
+      }
     }
   }
 
@@ -169,18 +179,20 @@ copy_sc_to_fc(struct mrc_fld *_f, struct mrc_fld *_ff, int mb, int me)
   struct mrc_fld *f = mrc_fld_get_as(_f, FLD_TYPE);
   struct mrc_fld *ff = mrc_fld_get_as(_ff, FLD_TYPE);
 
-  for (int m = mb; m < me; m++) {
-    if (m == _UU1) {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	float b2  = (sqr(.5f * (F3(f, _B1X, ix,iy,iz) + F3(f, _B1X, ix+1,iy  ,iz  ))) +
-		     sqr(.5f * (F3(f, _B1Y, ix,iy,iz) + F3(f, _B1Y, ix  ,iy+1,iz  ))) +
-		     sqr(.5f * (F3(f, _B1Z, ix,iy,iz) + F3(f, _B1Z, ix  ,iy  ,iz+1))));
-	F3(ff, _UU1, ix,iy,iz) = F3(f, _UU1, ix,iy,iz) + .5 * b2;
+  for (int p = 0; p < mrc_fld_nr_patches(f); p++) {
+    for (int m = mb; m < me; m++) {
+      if (m == _UU1) {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  float b2  = (sqr(.5f * (M3(f, _B1X, ix,iy,iz, p) + M3(f, _B1X, ix+1,iy  ,iz  , p))) +
+		       sqr(.5f * (M3(f, _B1Y, ix,iy,iz, p) + M3(f, _B1Y, ix  ,iy+1,iz  , p))) +
+		       sqr(.5f * (M3(f, _B1Z, ix,iy,iz, p) + M3(f, _B1Z, ix  ,iy  ,iz+1, p))));
+	  M3(ff, _UU1, ix,iy,iz, p) = M3(f, _UU1, ix,iy,iz, p) + .5 * b2;
       } mrc_fld_foreach_end;
-    } else {
-      mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-	F3(ff, m, ix,iy,iz) = F3(f, m, ix,iy,iz);
-      } mrc_fld_foreach_end;
+      } else {
+	mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
+	  M3(ff, m, ix,iy,iz, p) = M3(f, m, ix,iy,iz, p);
+	} mrc_fld_foreach_end;
+      }
     }
   }
 
