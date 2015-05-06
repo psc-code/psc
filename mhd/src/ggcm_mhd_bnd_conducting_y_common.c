28c07d1ff9564ff05617594e05c4d2834629963c
Kristofor Maynard <k.maynard@unh.edu>
2015-05-06 11:42:14 -0400

    [ggcm] ggcm_mhd_bnd: conducting y bugfixes (divB issues), still not 100% right
    for double/triple ghost points, it's not always possible to find By since
    it depends on Bx / Bz values that simply don't exist in the array

--- mhd/src/ggcm_mhd_bnd_conducting_y_common.c
+++ mhd/src/ggcm_mhd_bnd_conducting_y_common.c
@@ -1,4 +1,3 @@
-
 #include "ggcm_mhd_bnd_private.h"
 
 #include "ggcm_mhd_defs.h"
@@ -8,7 +7,6 @@
 #include <mrc_domain.h>
 #include <assert.h>
 
-
 void
 GGCM_MHD_BND_CONDUCTING_Y_FILL_GHOST(struct ggcm_mhd_bnd *bnd,
                           struct mrc_fld *fld_base,
@@ -17,7 +15,10 @@ GGCM_MHD_BND_CONDUCTING_Y_FILL_GHOST(struct ggcm_mhd_bnd *bnd,
   struct ggcm_mhd *mhd = bnd->mhd;
 
   struct mrc_fld *x = mrc_fld_get_as(fld_base, FLD_TYPE);
-  
+  int mhd_type;
+  mrc_fld_get_param_int(x, "mhd_type", &mhd_type);
+  assert(mhd_type != MT_SEMI_CONSERVATIVE_GGCM);  // written for C staggering only
+
   const int *dims = mrc_fld_spatial_dims(x);
   int nx = dims[0], ny = dims[1], nz = dims[2];
   int sw = x->_nr_ghosts;
@@ -35,18 +36,24 @@ GGCM_MHD_BND_CONDUCTING_Y_FILL_GHOST(struct ggcm_mhd_bnd *bnd,
   // struct mrc_patch_info info;
   // mrc_domain_get_local_patch_info(fld->_domain, 0, &info);
 
-   /* float *bd2x = ggcm_mhd_crds_get_crd(mhd->crds, 0, BD2); */
-   /* float *bd2y = ggcm_mhd_crds_get_crd(mhd->crds, 1, BD2); */
-   /* float *bd2z = ggcm_mhd_crds_get_crd(mhd->crds, 2, BD2); */
+  /* float *bd2x = ggcm_mhd_crds_get_crd(mhd->crds, 0, BD2); */
+  /* float *bd2y = ggcm_mhd_crds_get_crd(mhd->crds, 1, BD2); */
+  /* float *bd2z = ggcm_mhd_crds_get_crd(mhd->crds, 2, BD2); */
 
   double dx[3];
   mrc_crds_get_dx_base(mrc_domain_get_crds(mhd->domain), dx);
 
+  // used for extrapolation for double / triple ghosts
+  mrc_fld_data_t Bxp, Bzp;
+
   // lower boundary
   if (bc[1] != BC_PERIODIC && info.off[1] == 0) { // x lo
-    for (int iz = -sw; iz < nz + sw; iz++) {
-      for (int ix = -sw; ix < nx + sw; ix++) {
-        for (int ig = 0; ig < sw; ig++) {      
+    // either the ig loop is the outer loop, or the iz/ix loops have to go backward
+    for (int ig = 0; ig < sw; ig++) {      
+      int iyy = -ig;  // index of edge centered B_y's interior neighbors
+      for (int iz = -sw; iz < nz + sw; iz++) {
+        for (int ix = -sw; ix < nx + sw; ix++) {
+
           F3(x, m+RR , ix, -1 - ig, iz) =   F3(x, m+RR,  ix, ig, iz);
           F3(x, m+RVX, ix, -1 - ig, iz) =   F3(x, m+RVX, ix, ig, iz);
           F3(x, m+RVY, ix, -1 - ig, iz) = - F3(x, m+RVY, ix, ig, iz);
@@ -55,18 +62,35 @@ GGCM_MHD_BND_CONDUCTING_Y_FILL_GHOST(struct ggcm_mhd_bnd *bnd,
           F3(x, m+BX , ix, -1 - ig, iz) =   F3(x, m+BX,  ix, ig, iz);
           F3(x, m+BZ , ix, -1 - ig, iz) =   F3(x, m+BZ,  ix, ig, iz);
 
-          // // nothing special, B != 0, but Bz stays 0
-          // F3(x, m+BY , ix, -1 - ig, iz) =   F3(x, m+BY,  ix, ig, iz);
-
-          // to make div B = 0, but bz != 0 and it creeps in over time
+          // to make div B = 0
+          // for double / triple ghost points, we don't have the Bx/Bz we need
+          // to make divB=0, but these By points are used by Ohm's Law, so let's
+          // extrapolate
           if (ix + 1 == nx + sw || iz + 1 == nz + sw) {
-            // F3(x, m+BY, ix, iy + 1, iz) = 1.0 / bd2x[ix] + 1.0 / bd2z[iz];
-            continue;
+            if (ix + 1 == nx + sw) {
+              // extrapolate Bx value
+              Bxp = 2.0f * F3(x, m+BX, ix, iyy, iz) - F3(x, m+BX, ix-1, iyy, iz);
+            } else {
+              // use the Bx value we have
+              Bxp = F3(x, m+BX, ix+1, iyy, iz);
+            }
+  
+            if (iz + 1 == nz + sw) {
+              // extrapolate Bz value
+              Bzp = 2 * F3(x, m+BZ, ix, iyy, iz) - F3(x, m+BZ, ix, iyy, iz-1);
+            } else {
+              // use the Bz value we have
+              Bzp = F3(x, m+BZ, ix, iyy, iz+1);
+            }
+
+            F3(x, m+BY, ix, iyy, iz) = F3(x, m+BY, ix, iyy + 1, iz) + dx[1] * 
+                ((Bxp - F3(x, m+BX, ix, iyy, iz)) / dx[0] +
+                 (Bzp - F3(x, m+BZ, ix, iyy, iz)) / dx[2]);
+          } else {
+            F3(x, m+BY, ix, iyy, iz) = F3(x, m+BY, ix, iyy + 1, iz) + dx[1] * 
+                ((F3(x, m+BX, ix+1, iyy, iz  ) - F3(x, m+BX, ix, iyy, iz)) / dx[0] +
+                 (F3(x, m+BZ, ix  , iyy, iz+1) - F3(x, m+BZ, ix, iyy, iz)) / dx[2]);
           }
-          int iy = - ig;
-          F3(x, m+BY, ix, iy, iz) = F3(x, m+BY, ix, iy + 1, iz) +
-              ((F3(x, m+BX, ix+1, iy, iz    ) - F3(x, m+BX, ix, iy, iz)) / dx[0] +
-               (F3(x, m+BZ, ix  , iy, iz + 1) - F3(x, m+BZ, ix, iy, iz)) / dx[2]) * dx[1];
         }
       }
     }
@@ -74,9 +98,11 @@ GGCM_MHD_BND_CONDUCTING_Y_FILL_GHOST(struct ggcm_mhd_bnd *bnd,
 
   // upper boundary
   if (bc[1] != BC_PERIODIC && info.off[1] + info.ldims[1] == gdims[1]) { // x hi
-    for (int iz = -sw; iz < nz + sw; iz++) {
-      for (int ix = -sw; ix < nx + sw; ix++) {
-        for (int ig = 0; ig < sw; ig++) {      
+    for (int ig = 0; ig < sw; ig++) {      
+      int iyy = ny + ig - 1;  // index of edge centered B_y's interior neighbors
+      for (int iz = -sw; iz < nz + sw; iz++) {
+        for (int ix = -sw; ix < nx + sw; ix++) {
+
           F3(x, m+RR , ix, ny + ig, iz) =   F3(x, m+RR , ix, ny - 1 - ig, iz);
           F3(x, m+RVX, ix, ny + ig, iz) =   F3(x, m+RVX, ix, ny - 1 - ig, iz);
           F3(x, m+RVY, ix, ny + ig, iz) = - F3(x, m+RVY, ix, ny - 1 - ig, iz);
@@ -85,21 +111,34 @@ GGCM_MHD_BND_CONDUCTING_Y_FILL_GHOST(struct ggcm_mhd_bnd *bnd,
           F3(x, m+BX , ix, ny + ig, iz) =   F3(x, m+BX , ix, ny - 1 - ig, iz);
           F3(x, m+BZ , ix, ny + ig, iz) =   F3(x, m+BZ , ix, ny - 1 - ig, iz);
 
-          // nothing special, B != 0, but Bz stays 0
-          // F3(x, m+BY , ix, ny + ig, iz) =   F3(x, m+BY , ix, ny - 1 - ig, iz);
-
-          // to make div B = 0, but bz != 0 and it creeps in over time
+          // to make div B = 0
+          // for double / triple ghost points, we don't have the Bx/Bz we need
+          // to make divB=0, but these By points are used by Ohm's Law, so let's
+          // extrapolate
           if (ix + 1 == nx + sw || iz + 1 == nz + sw) {
-            // F3(x, m+BY, ix, iy + 1, iz) = 1.0 / bd2x[ix] + 1.0 / bd2z[iz];
-            continue;
+            if (ix + 1 == nx + sw) {
+              // extrapolate Bx value
+              Bxp = 2 * F3(x, m+BX, ix, iyy, iz) - F3(x, m+BX, ix-1, iyy, iz);
+            } else {
+              // use the Bx value we have
+              Bxp = F3(x, m+BX, ix+1, iyy, iz);
+            }
+  
+            if (iz + 1 == nz + sw) {
+              // extrapolate Bz value
+              Bzp = 2 * F3(x, m+BZ, ix, iyy, iz) - F3(x, m+BZ, ix, iyy, iz-1);
+            } else {
+              // use the Bz value we have
+              Bzp = F3(x, m+BZ, ix, iyy, iz+1);
+            }
+            F3(x, m+BY, ix, iyy + 1, iz) = F3(x, m+BY, ix, iyy, iz) - dx[1] * 
+                ((Bxp - F3(x, m+BX, ix, iyy, iz)) / dx[0] +
+                 (Bzp - F3(x, m+BZ, ix, iyy, iz)) / dx[2]);
+          } else{
+            F3(x, m+BY, ix, iyy + 1, iz) = F3(x, m+BY, ix, iyy, iz) - dx[1] * 
+                ((F3(x, m+BX, ix+1, iyy, iz    ) - F3(x, m+BX, ix, iyy, iz)) / dx[0] +
+                 (F3(x, m+BZ, ix  , iyy, iz + 1) - F3(x, m+BZ, ix, iyy, iz)) / dx[2]);
           }
-          int iy = ny - 1 + ig;
-          /* F3(x, m+BY, ix, iy + 1, iz) = F3(x, m+BY, ix, iy, iz) - */
-          /*     ((F3(x, m+BX, ix+1, iy, iz    ) - F3(x, m+BX, ix, iy, iz)) / bd2x[ix] + */
-          /*      (F3(x, m+BZ, ix  , iy, iz + 1) - F3(x, m+BZ, ix, iy, iz)) / bd2z[iz]) * bd2y[iy]; */
-          F3(x, m+BY, ix, iy + 1, iz) = F3(x, m+BY, ix, iy, iz) -
-              ((F3(x, m+BX, ix+1, iy, iz    ) - F3(x, m+BX, ix, iy, iz)) / dx[0] +
-               (F3(x, m+BZ, ix  , iy, iz + 1) - F3(x, m+BZ, ix, iy, iz)) / dx[2]) * dx[1];
         }
       }
     }
