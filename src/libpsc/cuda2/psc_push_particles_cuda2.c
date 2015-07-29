209782bbbe82be4eea9eec63155cfe53029be052
Kai Germaschewski <kai.germaschewski@unh.edu>
2015-07-29 15:48:10 -0400

    cuda2: call "cuda" push_particles (small diffs)
    seems to work fine, ie., small differences which I think is expected

--- src/libpsc/cuda2/psc_push_particles_cuda2.c
+++ src/libpsc/cuda2/psc_push_particles_cuda2.c
@@ -5,6 +5,9 @@
 
 #include <string.h>
 
+// FIXME
+EXTERN_C void yz4x4_1vbec3d_gmem_cuda_push_mprts(struct psc_mparticles *mprts, struct psc_mfields *mflds);
+
 // ======================================================================
 // psc_push_particles: subclass "1vb_cuda2"
 
@@ -19,15 +22,19 @@ psc_push_particles_1vbec_push_mprts_yz(struct psc_push_particles *push,
   struct psc_mfields *mflds =
     psc_mfields_get_as(mflds_base, "cuda2", EX, EX + 6);
 
+#if 1
   struct psc_mparticles *mprts_cuda =
     psc_mparticles_get_as(mprts, "cuda", 0);
   struct psc_mfields *mflds_cuda =
     psc_mfields_get_as(mflds, "cuda", EX, EX + 6);
 
+  yz4x4_1vbec3d_gmem_cuda_push_mprts(mprts_cuda, mflds_cuda);
+
   psc_mparticles_put_as(mprts_cuda, mprts, 0);
   psc_mfields_put_as(mflds_cuda, mflds, JXI, JXI + 3);
-
+#else
   cuda2_1vbec_push_mprts_yz(mprts, mflds);
+#endif
 
   psc_mparticles_put_as(mprts, mprts_base, 0);
   psc_mfields_put_as(mflds, mflds_base, JXI, JXI + 3);	
