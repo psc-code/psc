897f148643b9e251e5dbc5e19d687d76483865ad
Kai Germaschewski <kai.germaschewski@unh.edu>
2015-07-29 14:49:13 -0400

    cuda2: push_H in new CUDA FIXME!!!
    vastly different results (corner problem?)

--- src/libpsc/cuda2/cuda2_push_fields.cu
+++ src/libpsc/cuda2/cuda2_push_fields.cu
@@ -24,10 +24,6 @@
 #define F3_DEV(fldnr,ix,jy,jz)			\
   (d_flds)[X3_DEV_OFF_YZ(fldnr, jy,jz)]
 
-#define F3_DDEV(d_flds, fldnr,ix,jy,jz)		\
-  (d_flds)[X3_DEV_OFF_YZ(fldnr, jy,jz)]
-
-EXTERN_C void cuda_push_fields_H_yz(struct psc_mfields *mflds);
 // FIXME end
 // ----------------------------------------------------------------------
 
@@ -61,6 +57,35 @@ push_fields_E_yz(real *d_flds0, real dt, real cny, real cnz, int my, int mz,
     0.f -
     cny * (F3_DEV(HX, 0,iy,iz) - F3_DEV(HX, 0,iy-1,iz)) -
     .5f * dt * F3_DEV(JZI, 0,iy,iz);
+}
+
+__global__ static void
+push_fields_H_yz(real *d_flds0, real cny, real cnz, int my, int mz,
+		 unsigned int size, int gridy)
+{
+  int bidx_y = blockIdx.y % gridy;
+  int p = blockIdx.y / gridy;
+  int iy = blockIdx.x * blockDim.x + threadIdx.x;
+  int iz = bidx_y * blockDim.y + threadIdx.y;
+
+  if (!(iy < my - 2 * (2-BND) && iz < mz - 2 * (2-BND)))
+    return;
+  iy -= BND;
+  iz -= BND;
+
+  real *d_flds = d_flds0 + p * size;
+
+  F3_DEV(HX, 0,iy,iz) -=
+    cny * (F3_DEV(EZ, 0,iy+1,iz) - F3_DEV(EZ, 0,iy,iz)) -
+    cnz * (F3_DEV(EY, 0,iy,iz+1) - F3_DEV(EY, 0,iy,iz));
+  
+  F3_DEV(HY, 0,iy,iz) -=
+    cnz * (F3_DEV(EX, 0,iy,iz+1) - F3_DEV(EX, 0,iy,iz)) -
+    0.f;
+  
+  F3_DEV(HZ, 0,iy,iz) -=
+    0.f -
+    cny * (F3_DEV(EX, 0,iy+1,iz) - F3_DEV(EX, 0,iy,iz));
 }
 
 // ----------------------------------------------------------------------
@@ -97,6 +122,44 @@ cuda2_push_mflds_E_yz(struct psc_mfields *mflds)
 
   RUN_KERNEL(dimGrid, dimBlock,
 	     push_fields_E_yz, (sub->d_flds, dt, cny, cnz, my, mz,
+				size, grid[1]));
+
+  cuda_memcpy_host_from_device(sub->h_flds, sub->d_flds, total_size * sizeof(*sub->h_flds));
+}
+
+// ----------------------------------------------------------------------
+// cuda2_push_mflds_H_yz
+
+void
+cuda2_push_mflds_H_yz(struct psc_mfields *mflds)
+{
+  struct psc_mfields_cuda2 *sub = psc_mfields_cuda2(mflds);
+
+  if (mflds->nr_patches == 0) {
+    return;
+  }
+
+  size_t total_size = sub->im[0] * sub->im[1] * sub->im[2] * mflds->nr_patches * mflds->nr_fields;
+  cuda_memcpy_device_from_host(sub->d_flds, sub->h_flds, total_size * sizeof(*sub->d_flds));
+
+  struct psc_patch *patch = &ppsc->patch[0];
+
+  real cny = .5f * ppsc->dt / patch->dx[1];
+  real cnz = .5f * ppsc->dt / patch->dx[2];
+  assert(patch->ldims[0] == 1);
+
+  unsigned int size = mflds->nr_fields *
+    sub->im[0] * sub->im[1] * sub->im[2];
+  int my = sub->im[1];
+  int mz = sub->im[2];
+
+  int dimBlock[2] = { BLOCKSIZE_Y, BLOCKSIZE_Z };
+  int grid[2]  = { (patch->ldims[1] + 2*BND + BLOCKSIZE_Y - 1) / BLOCKSIZE_Y,
+		   (patch->ldims[2] + 2*BND + BLOCKSIZE_Z - 1) / BLOCKSIZE_Z };
+  int dimGrid[2] = { grid[0], grid[1] * mflds->nr_patches };
+
+  RUN_KERNEL(dimGrid, dimBlock,
+	     push_fields_H_yz, (sub->d_flds, cny, cnz, my, mz,
 				size, grid[1]));
 
   cuda_memcpy_host_from_device(sub->h_flds, sub->d_flds, total_size * sizeof(*sub->h_flds));
