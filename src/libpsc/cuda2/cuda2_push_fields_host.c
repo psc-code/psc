d39eb971c44088d322c0aa9fbce88b6248ec4564
Kai Germaschewski <kai.germaschewski@unh.edu>
2015-07-29 13:33:09 -0400

    cuda2: use cuda field pusher FIXME?
    still, cuda2 (host) and cuda pusher gives significantly different results, though
    not as bad as when the loop limits where mismatching?

--- src/libpsc/cuda2/cuda2_push_fields_host.c
+++ src/libpsc/cuda2/cuda2_push_fields_host.c
@@ -45,10 +45,10 @@ void
 cuda2_push_mflds_E_yz(struct psc_mfields *mflds)
 {
   struct psc_mfields *mflds_cuda = psc_mfields_get_as(mflds, "cuda", JXI, HX + 3);
-  //  cuda_push_fields_E_yz(mflds_cuda);
+  cuda_push_fields_E_yz(mflds_cuda);
   psc_mfields_put_as(mflds_cuda, mflds, EX, EX + 3);
 
-#if 1
+#if 0
   for (int p = 0; p < mflds->nr_patches; p++) {
     cuda2_push_flds_E_yz(psc_mfields_get_patch(mflds, p));
   }
