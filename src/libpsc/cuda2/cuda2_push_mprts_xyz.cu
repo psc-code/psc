62103b50c95c0590d9c6dd5df56d686ae0b91eac
Kai Germaschewski <kai.germaschewski@unh.edu>
2015-08-13 17:08:03 -0400

    cuda2: 4x4x4

--- src/libpsc/cuda2/cuda2_push_mprts_xyz.cu
+++ src/libpsc/cuda2/cuda2_push_mprts_xyz.cu
@@ -2,9 +2,9 @@
 #include "../psc_push_particles/inc_defs.h"
 
 #define DIM DIM_XYZ
-#define BLOCKSIZE_X 8
-#define BLOCKSIZE_Y 8
-#define BLOCKSIZE_Z 8
+#define BLOCKSIZE_X 4
+#define BLOCKSIZE_Y 4
+#define BLOCKSIZE_Z 4
 #define INTERPOLATE_1ST INTERPOLATE_1ST_EC
 #define EM_CACHE EM_CACHE_CUDA
 #define CURR_CACHE CURR_CACHE_NONE
