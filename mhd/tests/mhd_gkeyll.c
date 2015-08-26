06802162d98dfe49b91fa27b28c5208c5b9b7ecb
Liang Wang <liang.wang@unh.edu>
2015-08-26 16:14:38 -0400

    [ggcm] gkeyll: add an ic bowshock3d to mhd_gkeyll

--- mhd/tests/mhd_gkeyll.c
+++ mhd/tests/mhd_gkeyll.c
@@ -27,10 +27,142 @@ extern struct ggcm_mhd_diag_ops ggcm_mhd_diag_item_ops_gkeyll_i;
 extern struct ggcm_mhd_diag_ops ggcm_mhd_diag_item_ops_gkeyll_em;
 
 // ======================================================================
-// ggcm_mhd subclass "gkeyll"
+// ggcm_mhd_ic subclass "bowshock3d"
+
+struct ggcm_mhd_ic_bowshock3d {
+  float rho_obstacle; // initial density in obstacle
+  float r_obstacle;
+  float x_obstacle;
+  float rho0; // initial density outside
+  float p0; // initial pressure
+  float v0; // initial velocity
+  float Bx0;
+  float By0;
+  float Bz0;
+};
+
+// ----------------------------------------------------------------------
+// ggcm_mhd_ic_bowshock3d_run
+//
+// assuming domain [-1.5,-1.5]x[-1.5,1.5]x[-0.75,0.75]
+
+static void
+ggcm_mhd_ic_bowshock3d_run(struct ggcm_mhd_ic *ic)
+{
+  struct ggcm_mhd_ic_bowshock3d *sub = mrc_to_subobj(ic, struct ggcm_mhd_ic_bowshock3d);
+  struct ggcm_mhd *mhd = ic->mhd;
+  struct mrc_fld *fld = mrc_fld_get_as(mhd->fld, FLD_TYPE);
+
+  struct mrc_crds *crds = mrc_domain_get_crds(mhd->domain);
+  struct mrc_patch_info pinfo;
+  double dx0[3], dx[3];
+  mrc_crds_get_dx_base(crds, dx0);
+
+  int gdims[3], p1x, p1y, p1z;
+  mrc_domain_get_global_dims(mhd->domain, gdims);
+  p1x = (gdims[0] > 1);
+  p1y = (gdims[1] > 1);
+  p1z = (gdims[2] > 1);
+
+  struct mrc_fld *A = mrc_domain_fld_create(mhd->domain, SW_2, NULL);
+  mrc_fld_set_type(A, FLD_TYPE);
+  mrc_fld_set_param_int(A, "nr_comps", 3);
+  mrc_fld_setup(A);
+  mrc_fld_view(A);
+
+  for (int p = 0; p < mrc_fld_nr_patches(fld); p++) {
+    /* get dx for this patch */
+    mrc_domain_get_local_patch_info(mhd->domain, p, &pinfo);
+    for (int d = 0; d < 3; d++){
+      float refine = 1.0;
+      if (pinfo.ldims[d] > 1) {
+        refine = 1.0 / (1 << pinfo.level);
+      }
+      dx[d] = dx0[d] * refine;
+    }
+
+    /* Initialize vector potential */
+    mrc_fld_foreach(fld, ix,iy,iz, 1, 2) {
+      mrc_fld_data_t xx = MRC_MCRDX(crds, ix, p) - .5 * dx[0];
+      mrc_fld_data_t yy = MRC_MCRDY(crds, iy, p) - .5 * dx[1];
+      M3(A, 0, ix,iy,iz, p) = - sub->Bz0 * yy;
+      M3(A, 2, ix,iy,iz, p) = + sub->Bx0 * yy - sub->By0 * xx;
+    } mrc_fld_foreach_end;
+
+    /* Initialize face-centered fields B = curl(A) */
+    mrc_fld_foreach(fld, ix,iy,iz, 1, 1) {
+      BX_(fld, ix,iy,iz, p) =
+        + (M3(A, 2, ix    , iy+p1y, iz, p) - M3(A, 2, ix,iy,iz, p)) / dx[1]
+        - (M3(A, 1, ix    , iy, iz+p1z, p) - M3(A, 1, ix,iy,iz, p)) / dx[2];
+      BY_(fld, ix,iy,iz, p) =
+        + (M3(A, 0, ix, iy    , iz+p1z, p) - M3(A, 0, ix,iy,iz, p)) / dx[2]
+        - (M3(A, 2, ix+p1x, iy    , iz, p) - M3(A, 2, ix,iy,iz, p)) / dx[0];
+      BZ_(fld, ix,iy,iz, p) =
+        + (M3(A, 1, ix+p1x, iy    , iz, p) - M3(A, 1, ix,iy,iz, p)) / dx[0]
+        - (M3(A, 0, ix, iy    , iz+p1y, p) - M3(A, 0, ix,iy,iz, p)) / dx[1];
+    } mrc_fld_foreach_end;
+
+    /* Initialize density, momentum, total energy */
+    mrc_fld_foreach(fld, ix, iy, iz, 1, 1) {
+      mrc_fld_data_t xx = MRC_CRD(crds, 0, ix) - sub->x_obstacle;
+      mrc_fld_data_t yy = MRC_CRD(crds, 1, iy);
+      mrc_fld_data_t zz = MRC_CRD(crds, 2, iz);
+
+      PP(fld, ix, iy, iz) = sub->p0;
+      if( sqrt((xx*xx) + (yy*yy) + (zz*zz)) <= sub->r_obstacle ){
+        RR(fld, ix, iy, iz) = sub->rho_obstacle;
+        VX(fld, ix, iy, iz) = 0.;
+      } else{
+        RR(fld, ix, iy, iz) = sub->rho0;
+        VX(fld, ix, iy, iz) = sub->v0;
+        if( xx > 0. && sqrt((yy*yy) + (zz*zz)) <= sub->r_obstacle ){
+          VX(fld, ix, iy, iz) = 0.;
+        }
+      }
+    } mrc_fld_foreach_end;
+  }
+
+  mrc_fld_destroy(A);
+  mrc_fld_put_as(fld, mhd->fld);
+
+  ggcm_mhd_convert_from_primitive(mhd, mhd->fld);
+}
+
+// ----------------------------------------------------------------------
+// ggcm_mhd_ic_bowshock3d_descr
+
+#define VAR(x) (void *)offsetof(struct ggcm_mhd_ic_bowshock3d, x)
+static struct param ggcm_mhd_ic_bowshock3d_descr[] = {
+  { "rho_obstacle", VAR(rho_obstacle), PARAM_FLOAT(100.f)   },
+  { "r_obstacle"  , VAR(r_obstacle)  , PARAM_FLOAT(.0625f)  },
+  { "x_obstacle"  , VAR(x_obstacle)  , PARAM_FLOAT(-.75)    },
+  { "rho0"        , VAR(rho0)        , PARAM_FLOAT(.01f)    },
+  { "p0"          , VAR(p0)          , PARAM_FLOAT(.0015f)  },
+  { "v0"          , VAR(v0)          , PARAM_FLOAT(1.f)     },
+  { "Bx0"         , VAR(Bx0)         , PARAM_FLOAT(0.f)     },
+  { "By0"         , VAR(By0)         , PARAM_FLOAT(0.001f)  },
+  { "Bz0"         , VAR(Bz0)         , PARAM_FLOAT(0.f)     },
+  {},
+};
+#undef VAR
+
+// ----------------------------------------------------------------------
+// ggcm_mhd_ic_bowshock3d_ops
+
+struct ggcm_mhd_ic_ops ggcm_mhd_ic_bowshock3d_ops = {
+  .name        = "bowshock3d",
+  .size        = sizeof(struct ggcm_mhd_ic_bowshock3d),
+  .param_descr = ggcm_mhd_ic_bowshock3d_descr,
+  .run         = ggcm_mhd_ic_bowshock3d_run,
+};
+
+// ======================================================================
+// ggcm_mhd_ic subclass "ot"
 
 // ----------------------------------------------------------------------
 // ggcm_mhd_ic_ot_run
+//
+// assuming 2d domain [-1,1]x[-1,1]
 
 static void
 ggcm_mhd_ic_ot_run(struct ggcm_mhd_ic *ic)
@@ -140,6 +272,9 @@ ggcm_mhd_gkeyll_create(struct ggcm_mhd *mhd)
   mrc_crds_set_param_double3(crds, "h", (double[3]) {  1.0, 1.0, 0.1 });
 }
 
+// ======================================================================
+// ggcm_mhd subclass "gkeyll"
+
 // ----------------------------------------------------------------------
 // ggcm_mhd_gkeyll_ops
 
@@ -169,6 +304,7 @@ main(int argc, char **argv)
 {
   mrc_class_register_subclass(&mrc_class_ggcm_mhd_step, &ggcm_mhd_step_gkeyll_ops);
   mrc_class_register_subclass(&mrc_class_ggcm_mhd_diag, &ggcm_mhd_diag_c_ops);
+  mrc_class_register_subclass(&mrc_class_ggcm_mhd_ic, &ggcm_mhd_ic_bowshock3d_ops);  
   mrc_class_register_subclass(&mrc_class_ggcm_mhd_ic, &ggcm_mhd_ic_ot_ops);  
   mrc_class_register_subclass(&mrc_class_ggcm_mhd_ic, &ggcm_mhd_ic_gkeyll_ops);  
   mrc_class_register_subclass(&mrc_class_ggcm_mhd_diag_item, &ggcm_mhd_diag_item_ops_gkeyll_e);  
