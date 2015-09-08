7de0ba3b6611d6e5f5b9974643b72b7b2cf2ed1c
Kai Germaschewski <kai.germaschewski@unh.edu>
2015-09-08 15:00:41 -0400

    [ggcm] mhd_ot: make test script work again

--- mhd/tests/mhd_ot.sh
+++ mhd/tests/mhd_ot.sh
@@ -1,13 +1,15 @@
 #!/bin/sh 
 
-openmpirun -n 2 mhd_ot \
+mpirun -n 1 mhd_ot \
     --mrc_crds_lx 0.0 --mrc_crds_ly 0.0 \
     --mrc_crds_hx 1.0 --mrc_crds_hy 1.0 \
     --mrc_crds_lz 0.0 --mrc_crds_hz 0.1 \
-    --ggcm_mhd_diag_fields rr1:uu1:rv1:j:b1:divb:rr:pp_full:v:b \
+    --ggcm_mhd_diag_fields rr1:uu1:rv1:j:b1:divb:rr:pp:v:b \
     --lmx 0 --lmy 0 --lmz 0 --d_i 0.00 \
-    --mx 128 --my 128 --mz 2 --npx 1 --npy 2 \
+    --mx 64 --my 64 --mz 1 --npx 1 --npy 1 \
     --mrc_ts_output_every_time 0.01  \
-    --mrc_ts_max_time 0.0025 --mrc_ts_type rk2 --mrc_ts_dt 0.00125
+    --mrc_ts_max_time 1.0 \
+    --do_nwst \
+    2>&1 | tee log
 
 exit 0
