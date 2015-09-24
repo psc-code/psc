c101a02a3aae760b5d95beeab1ccc564f036f80e
Kristofor Maynard <kristofor.maynard@gmail.com>
2015-09-24 14:01:13 -0400

    [ggcm] libmrc: support using python scripts + Viscid in tests
    test becomes XFAIL if Viscid is not available

--- mhd/tests/test_mhd_wave_cpaw.sh
+++ mhd/tests/test_mhd_wave_cpaw.sh
@@ -2,14 +2,18 @@
 
 has_viscid=$(python -c 'import viscid' &> /dev/null; echo ${?})
 
-./mhd_wave_cpaw.sh x 1 64 -1.0 &> /dev/null
+sh ./mhd_wave_cpaw.sh x 1 64 -1.0 &> /dev/null
 errcode=$?
 
 if [ ${has_viscid} -eq 0 ]; then
   python check_wave_cpaw.py cpaw_x_64.3d.xdmf
   errcode=$?
 else
-  echo "Viscid python module not found, not checking result." >&2
+  echo "Warning: Viscid python module not found, not checking result." >&2
+  if [ $errcode -eq 0 ]; then
+    echo "         But for what it's worth, the test exited cleanly." >&2
+  fi  
+  errcode=100
 fi
 
 if [ $errcode -eq 0 ]; then
