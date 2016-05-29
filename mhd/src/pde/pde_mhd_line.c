27ac1ac88cd42caf3197c76356244672c0080d47
Kai Germaschewski <kai.germaschewski@unh.edu>
2016-05-29 01:43:38 -0400

    [ggcm] pde: remaining put_line_* -> pde_mhd_line.c

--- mhd/src/pde/pde_mhd_line.c
+++ mhd/src/pde/pde_mhd_line.c
@@ -30,6 +30,39 @@ pick_line_fc_cc(fld1d_state_t u, struct mrc_fld *U,
 }
 
 // ----------------------------------------------------------------------
+// put_line_fc_cc
+
+static void _mrc_unused
+put_line_fc_cc(struct mrc_fld *flux, struct mrc_fld *F_1d,
+	       int ldim, int l, int r, int j, int k, int dir, int p)
+{
+#define PUT_LINE(X, Y, Z, I, J, K) do {				\
+    for (int i = -l; i < ldim + r; i++) {			\
+      M3(flux, RR     , I,J,K, p) = F1(F_1d, RR , i);		\
+      M3(flux, RVX + X, I,J,K, p) = F1(F_1d, RVX, i);		\
+      M3(flux, RVX + Y, I,J,K, p) = F1(F_1d, RVY, i);		\
+      M3(flux, RVX + Z, I,J,K, p) = F1(F_1d, RVZ, i);		\
+      M3(flux, EE     , I,J,K, p) = F1(F_1d, EE , i);		\
+      M3(flux, BX + X , I,J,K, p) = F1(F_1d, BX , i);		\
+      M3(flux, BX + Y , I,J,K, p) = F1(F_1d, BY , i);		\
+      M3(flux, BX + Z , I,J,K, p) = F1(F_1d, BZ , i);		\
+    }								\
+} while (0)
+
+  if (dir == 0) {
+    PUT_LINE(0, 1, 2, i, j, k);
+  } else if (dir == 1) {
+    PUT_LINE(1, 2, 0, k, i, j);
+  } else if (dir == 2) {
+    PUT_LINE(2, 0, 1, j, k, i);
+  } else {
+    assert(0);
+  }
+#undef PUT_LINE
+}
+
+
+// ----------------------------------------------------------------------
 // pick_line_fc
 
 static void _mrc_unused
@@ -65,6 +98,38 @@ pick_line_fc(fld1d_state_t u
 }
 
 // ----------------------------------------------------------------------
+// put_line_fc
+
+static void _mrc_unused
+put_line_fc(struct mrc_fld *flux, struct mrc_fld *F_1d,
+	    int ldim, int l, int r, int j, int k, int dir, int p)
+{
+#define PUT_LINE(X, Y, Z, I, J, K) do {				\
+    for (int i = -l; i < ldim + r; i++) {			\
+      M3(flux, RR     , I,J,K, p) = F1(F_1d, RR , i);		\
+      M3(flux, RVX + X, I,J,K, p) = F1(F_1d, RVX, i);		\
+      M3(flux, RVX + Y, I,J,K, p) = F1(F_1d, RVY, i);		\
+      M3(flux, RVX + Z, I,J,K, p) = F1(F_1d, RVZ, i);		\
+      M3(flux, EE     , I,J,K, p) = F1(F_1d, EE , i);		\
+      M3(flux, BX + Y , I,J,K, p) = F1(F_1d, BY , i);		\
+      M3(flux, BX + Z , I,J,K, p) = F1(F_1d, BZ , i);		\
+    }								\
+} while (0)
+
+  if (dir == 0) {
+    PUT_LINE(0, 1, 2, i, j, k);
+  } else if (dir == 1) {
+    PUT_LINE(1, 2, 0, k, i, j);
+  } else if (dir == 2) {
+    PUT_LINE(2, 0, 1, j, k, i);
+  } else {
+    assert(0);
+  }
+#undef PUT_LINE
+}
+
+
+// ----------------------------------------------------------------------
 // pick_line_sc
 
 static void _mrc_unused
@@ -89,5 +154,32 @@ pick_line_sc(fld1d_state_t u, struct mrc_fld *U,
     PICK_LINE(2,0,1, j,k,i);
   }
 #undef PICK_LINE
+}
+
+// ----------------------------------------------------------------------
+// put_line_sc
+
+static void _mrc_unused
+put_line_sc(struct mrc_fld *flux, struct mrc_fld *F,
+	    int ldim, int l, int r, int j, int k, int dir, int p)
+{
+#define PUT_LINE(X,Y,Z, I,J,K) do {					\
+    for (int i = -l; i < ldim + r; i++) {				\
+      M3(flux, RR   , I,J,K, p) = F1(F, RR , i);			\
+      M3(flux, RVX+X, I,J,K, p) = F1(F, RVX, i);			\
+      M3(flux, RVX+Y, I,J,K, p) = F1(F, RVY, i);			\
+      M3(flux, RVX+Z, I,J,K, p) = F1(F, RVZ, i);			\
+      M3(flux, UU   , I,J,K, p) = F1(F, UU , i);			\
+    }									\
+  } while (0)
+
+  if (dir == 0) {
+    PUT_LINE(0,1,2, i,j,k);
+  } else if (dir == 1) {
+    PUT_LINE(1,2,0, k,i,j);
+  } else if (dir == 2) {
+    PUT_LINE(2,0,1, j,k,i);
+  }
+#undef PUT_LINE
 }
 
