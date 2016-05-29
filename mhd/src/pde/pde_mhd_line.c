050d15ea0b7070ea1034d5b939c14c12b73bf3d3
Kai Germaschewski <kai.germaschewski@unh.edu>
2016-05-29 01:21:11 -0400

    [ggcm] pde: pick_line_sc() -> pde_mhd_line.c

--- mhd/src/pde/pde_mhd_line.c
+++ mhd/src/pde/pde_mhd_line.c
@@ -64,3 +64,30 @@ pick_line_fc(fld1d_state_t u
 #undef PICK_LINE
 }
 
+// ----------------------------------------------------------------------
+// pick_line_sc
+
+static void _mrc_unused
+pick_line_sc(struct mrc_fld *x1, struct mrc_fld *x,
+	     int ldim, int l, int r, int j, int k, int dim, int p)
+{
+#define PICK_LINE(X,Y,Z,I,J,K) do {			\
+    for (int i = -l; i < ldim + r; i++) {		\
+      F1(x1, RR , i) = M3(x, RR   , I,J,K, p);		\
+      F1(x1, RVX, i) = M3(x, RVX+X, I,J,K, p);		\
+      F1(x1, RVY, i) = M3(x, RVX+Y, I,J,K, p);		\
+      F1(x1, RVZ, i) = M3(x, RVX+Z, I,J,K, p);		\
+      F1(x1, UU , i) = M3(x, UU   , I,J,K, p);		\
+    }							\
+  } while (0)
+
+  if (dim == 0) {
+    PICK_LINE(0,1,2, i,j,k);
+  } else if (dim == 1) {
+    PICK_LINE(1,2,0, k,i,j);
+  } else if (dim == 2) {
+    PICK_LINE(2,0,1, j,k,i);
+  }
+#undef PICK_LINE
+}
+
