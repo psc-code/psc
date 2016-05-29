fed3dd59dff7b4a8f02b26766f8040ef3f3ba257
Kai Germaschewski <kai.germaschewski@unh.edu>
2016-05-29 00:33:23 -0400

    [ggcm] pde: convert pick_line_fc() to fld1d_state_t

--- mhd/src/ggcm_mhd_step_vlct.c
+++ mhd/src/ggcm_mhd_step_vlct.c
@@ -197,7 +197,7 @@ flux_pred(struct ggcm_mhd_step *step, struct mrc_fld *flux[3], struct mrc_fld *x
   fld1d_state_t F = sub->F;
   struct mrc_fld *Bxi = sub->Bxi;
 
-  pick_line_fc(U.mrc_fld, Bxi, x, B_cc, ldim, nghost, nghost, j, k, dir, p);
+  pick_line_fc(U, Bxi, x, B_cc, ldim, nghost, nghost, j, k, dir, p);
   mhd_prim_from_fc(step->mhd, W.mrc_fld, U.mrc_fld, ldim, nghost, nghost);
   mhd_reconstruct_run(sub->reconstruct_pred, U_l.mrc_fld, U_r.mrc_fld, W_l.mrc_fld, W_r.mrc_fld, W.mrc_fld, Bxi,
 		      ldim, nghost - 1, nghost, dir);
@@ -215,7 +215,7 @@ flux_corr(struct ggcm_mhd_step *step, struct mrc_fld *flux[3], struct mrc_fld *x
   fld1d_state_t F = sub->F;
   struct mrc_fld *Bxi = sub->Bxi;
 
-  pick_line_fc(U.mrc_fld, Bxi, x, B_cc, ldim, nghost - 1, nghost - 1, j, k, dir, p);
+  pick_line_fc(U, Bxi, x, B_cc, ldim, nghost - 1, nghost - 1, j, k, dir, p);
   mhd_prim_from_fc(step->mhd, W.mrc_fld, U.mrc_fld, ldim, nghost - 1, nghost - 1);
   mhd_reconstruct_run(sub->reconstruct_corr, U_l.mrc_fld, U_r.mrc_fld, W_l.mrc_fld, W_r.mrc_fld, W.mrc_fld, Bxi,
 		      ldim, 1, 1, dir);
--- mhd/src/pde/pde_mhd_line.c
+++ mhd/src/pde/pde_mhd_line.c
@@ -33,22 +33,23 @@ pick_line_fc_cc(fld1d_state_t u, struct mrc_fld *U,
 // pick_line_fc
 
 static void _mrc_unused
-pick_line_fc(struct mrc_fld *U_1d, struct mrc_fld *Bxi,
+pick_line_fc(fld1d_state_t u
+, struct mrc_fld *Bxi,
 	     struct mrc_fld *U, struct mrc_fld *Bcc,
 	     int ldim, int l, int r, int j, int k, int dir, int p)
 {
 #define PICK_LINE(X, Y, Z, I, J, K)			       \
   do {							       \
     for (int i = - l; i < ldim + r; i++) {		       \
-      F1(U_1d, RR , i) = M3(U, RR     , I,J,K, p);	       \
-      F1(U_1d, RVX, i) = M3(U, RVX + X, I,J,K, p);	       \
-      F1(U_1d, RVY, i) = M3(U, RVX + Y, I,J,K, p);	       \
-      F1(U_1d, RVZ, i) = M3(U, RVX + Z, I,J,K, p);	       \
-      F1(U_1d, EE , i) = M3(U, EE     , I,J,K, p);	       \
-      F1(U_1d, BX , i) = M3(Bcc, X    , I,J,K, p);	       \
-      F1(U_1d, BY , i) = M3(Bcc, Y    , I,J,K, p);	       \
-      F1(U_1d, BZ , i) = M3(Bcc, Z    , I,J,K, p);	       \
-      F1(Bxi, 0, i)    = M3(U, BX+X   , I,J,K, p);	       \
+      F1S(u, RR , i) = M3(U, RR     , I,J,K, p);	       \
+      F1S(u, RVX, i) = M3(U, RVX + X, I,J,K, p);	       \
+      F1S(u, RVY, i) = M3(U, RVX + Y, I,J,K, p);	       \
+      F1S(u, RVZ, i) = M3(U, RVX + Z, I,J,K, p);	       \
+      F1S(u, EE , i) = M3(U, EE     , I,J,K, p);	       \
+      F1S(u, BX , i) = M3(Bcc, X    , I,J,K, p);	       \
+      F1S(u, BY , i) = M3(Bcc, Y    , I,J,K, p);	       \
+      F1S(u, BZ , i) = M3(Bcc, Z    , I,J,K, p);	       \
+      F1(Bxi, 0, i)  = M3(U, BX+X   , I,J,K, p);	       \
     }							       \
   } while (0)
   if (dir == 0) {
