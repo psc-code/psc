8e16f6e015208147b80debbe00471dfa58266d4b
Kai Germaschewski <kai.germaschewski@unh.edu>
2016-06-01 17:14:12 -0400

    [ggcm] tests: add "t011_sod_c3" and "t012_sod_mhdcc"

---
+++ mhd/tests/t011_sod_c3/t011_sod_c3.sh
@@ -0,0 +1,20 @@
+#!/bin/sh 
+
+mpirun -n 1 ../mhd_sod \
+    \
+    --mrc_domain_mx 128 \
+    --mrc_domain_npx 1 \
+    \
+    --mrc_ts_output_every_time 0.1  \
+    --ggcm_mhd_diag_fields rr1:uu1:rv1:j:b1:divb:rr:pp:v:b \
+    \
+    --mrc_ts_max_time .3 \
+    --ggcm_mhd_step_type c3_double \
+    --xggcm_mhd_step_debug_dump \
+    --ggcm_mhd_step_do_nwst \
+    \
+    --xtimelo 1000. \
+    \
+    2>&1 | tee log
+
+./plot.py
