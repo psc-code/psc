364c55d2b4580845051d247734b65310ade38f9c
Kai Germaschewski <kai.germaschewski@unh.edu>
2016-06-03 14:22:49 -0400

    [ggcm] pde: vlct newstep_fc -> pde_mhd_get_dt.c

--- mhd/src/ggcm_mhd_step_vlct.c
+++ mhd/src/ggcm_mhd_step_vlct.c
@@ -24,6 +24,7 @@
 #include "pde/pde_mhd_reconstruct.c"
 #include "pde/pde_mhd_riemann.c"
 #include "pde/pde_mhd_stage.c"
+#include "pde/pde_mhd_get_dt.c"
 
 #include "mhd_3d.c"
 
@@ -393,106 +394,6 @@ compute_Ediffu_const(struct ggcm_mhd_step *step, struct mrc_fld *E_ec,
 }
 
 // ----------------------------------------------------------------------
-// newstep_fc
-
-// FIXME, take into account resistivity
-// FIXME, rework
-
-static mrc_fld_data_t
-newstep_fc(struct ggcm_mhd *mhd, struct mrc_fld *x, struct mrc_fld *Bcc)
-{
-  mrc_fld_data_t qsq,asq,cf1sq,cf2sq,cf3sq;
-  mrc_fld_data_t b1,b2,b3,bsq,tsum,tdif;
-  mrc_fld_data_t max_v1=0.0,max_v2=0.0,max_v3=0.0,max_dti = 0.0;
-
-  mrc_fld_data_t gamma = mhd->par.gamm;
-  mrc_fld_data_t gamma_minus_1 = gamma - 1.;
-  
-  mrc_fld_data_t d_i = mhd->par.d_i;
-
-  struct mrc_crds *crds = mrc_domain_get_crds(mhd->domain);
-
-  for (int p = 0; p < mrc_fld_nr_patches(x); p++) {
-    double dx[3]; mrc_crds_get_dx(crds, p, dx);
-
-    mrc_fld_foreach(x, i,j,k, 0, 0) {
-      mrc_fld_data_t rri = 1.f / RR_(x, i,j,k, p);
-      mrc_fld_data_t vx = RVX_(x, i,j,k, p) * rri;
-      mrc_fld_data_t vy = RVY_(x, i,j,k, p) * rri;
-      mrc_fld_data_t vz = RVZ_(x, i,j,k, p) * rri;
-      qsq = sqr(vx) + sqr(vy) + sqr(vz);
-      
-      /* Use maximum of face-centered fields (always larger than cell-centered B) */
-      b1 = M3(Bcc, 0, i,j,k, p) + fabs(BX_(x, i,j,k, p) - M3(Bcc, 0, i,j,k, p));
-      b2 = M3(Bcc, 1, i,j,k, p) + fabs(BY_(x, i,j,k, p) - M3(Bcc, 1, i,j,k, p));
-      b3 = M3(Bcc, 2, i,j,k, p) + fabs(BZ_(x, i,j,k, p) - M3(Bcc, 2, i,j,k, p));
-      bsq = sqr(b1) + sqr(b2) + sqr(b3);
-      /* compute sound speed squared */
-      mrc_fld_data_t pp = fmax(gamma_minus_1*(EE_(x, i,j,k, p) - .5f*RR_(x, i,j,k, p)*qsq
-					      - .5f*bsq), TINY_NUMBER);
-      asq = gamma * pp * rri;
-      
-      /* compute fast magnetosonic speed squared in each direction */
-      tsum = bsq * rri + asq;
-      tdif = bsq * rri - asq;
-      cf1sq = 0.5*(tsum + sqrt(tdif*tdif + 4.0*asq*(b2*b2+b3*b3) * rri));
-      cf2sq = 0.5*(tsum + sqrt(tdif*tdif + 4.0*asq*(b1*b1+b3*b3) * rri));
-      cf3sq = 0.5*(tsum + sqrt(tdif*tdif + 4.0*asq*(b1*b1+b2*b2) * rri));
-      
-      /* compute maximum inverse dt (corresponding to minimum dt) */
-      max_v1 = fmax(max_v1, (fabs(vx) + sqrt(cf1sq)) / dx[0]);
-      max_v2 = fmax(max_v2, (fabs(vy) + sqrt(cf2sq)) / dx[1]);
-      max_v3 = fmax(max_v3, (fabs(vz) + sqrt(cf3sq)) / dx[2]);
-    } mrc_fld_foreach_end;
-  }
-
-  int gdims[3];
-  mrc_domain_get_global_dims(x->_domain, gdims);
-
-  if (gdims[0] > 1) max_dti = fmax(max_dti, max_v1);
-  if (gdims[1] > 1) max_dti = fmax(max_dti, max_v2);
-  if (gdims[2] > 1) max_dti = fmax(max_dti, max_v3);
-
-  mrc_fld_data_t cfl = mhd->par.thx;
-  mrc_fld_data_t local_dt = cfl / max_dti;
-  mrc_fld_data_t global_dt;
-  MPI_Allreduce(&local_dt, &global_dt, 1, MPI_MRC_FLD_DATA_T, MPI_MIN, ggcm_mhd_comm(mhd));
-
-  // FOR diffusive portion of everything
-  if (d_i > 0.0) {
-    mrc_fld_data_t local_diff_dt, global_diff_dt;
-    mrc_fld_data_t max_dti_diff = 0.0;
-
-    for (int p = 0; p < mrc_fld_nr_patches(x); p++) {
-      double dx[3]; mrc_crds_get_dx(crds, p, dx);
-
-      mrc_fld_foreach(x, i,j,k, 0, 0) {
-	mrc_fld_data_t dxmin, dti_hall;
-
-	dxmin = mrc_fld_min(dx[0], mrc_fld_min(dx[1], dx[2]));
-	
-	// FIXME: this could be streamlined by using the loop above, but
-	//        keeping them separate is exactly analogous to athena
-	/* Use maximum of face-centered fields (always larger than cell-centered B) */
-	b1 = M3(Bcc, 0, i,j,k, p) + fabs(BX_(x, i,j,k, p) - M3(Bcc, 0, i,j,k, p));
-	b2 = M3(Bcc, 1, i,j,k, p) + fabs(BY_(x, i,j,k, p) - M3(Bcc, 1, i,j,k, p));
-	b3 = M3(Bcc, 2, i,j,k, p) + fabs(BZ_(x, i,j,k, p) - M3(Bcc, 2, i,j,k, p));
-	bsq = sqr(b1) + sqr(b2) + sqr(b3);
-	
-	// FIXME? in athena it was (dxmin**2 / 6), but i don't think that's right?
-	dti_hall = (d_i * bsq / RR_(x, i,j,k, p)) / (sqr(dxmin) / 16.0);
-	max_dti_diff = mrc_fld_max(max_dti_diff, dti_hall);
-      } mrc_fld_foreach_end;
-    }
-    local_diff_dt = cfl / max_dti_diff;
-    MPI_Allreduce(&local_diff_dt, &global_diff_dt, 1, MPI_MRC_FLD_DATA_T, MPI_MIN, ggcm_mhd_comm(mhd));
-    global_dt = mrc_fld_min(global_dt, global_diff_dt);
-  }
-
-  return global_dt;
-}
-
-// ----------------------------------------------------------------------
 // ggcm_mhd_step_vlct_run
 
 static void
@@ -519,7 +420,7 @@ ggcm_mhd_step_vlct_run(struct ggcm_mhd_step *step, struct mrc_fld *x)
   compute_B_cc(B_cc, x, 3, 3);
   if (step->do_nwst) {
     double old_dt = mhd->dt;
-    mhd->dt = newstep_fc(mhd, x, B_cc);
+    mhd->dt = pde_mhd_get_dt_fcons_ct(mhd, x, B_cc);
     if (mhd->dt != old_dt) {
       mpi_printf(ggcm_mhd_comm(mhd), "switched dt %g <- %g\n", mhd->dt, old_dt);
       
--- mhd/src/pde/pde_mhd_get_dt.c
+++ mhd/src/pde/pde_mhd_get_dt.c
@@ -1,4 +1,6 @@
 
+#include "ggcm_mhd_crds.h" // FIXME
+
 // ----------------------------------------------------------------------
 // pde_mhd_get_dt_scons
 
@@ -115,5 +117,107 @@ pde_mhd_get_dt_fcons(struct ggcm_mhd *mhd, struct mrc_fld *x)
   MPI_Allreduce(&dt, &dtn, 1, MPI_MRC_FLD_DATA_T, MPI_MIN, ggcm_mhd_comm(mhd));
 
   return dtn;
+}
+
+// ----------------------------------------------------------------------
+// pde_mhd_get_dt_fcons_ct
+
+// FIXME, take into account resistivity
+// FIXME, rework
+
+static mrc_fld_data_t _mrc_unused
+pde_mhd_get_dt_fcons_ct(struct ggcm_mhd *mhd, struct mrc_fld *x, struct mrc_fld *Bcc)
+{
+  static const mrc_fld_data_t eps = 1.e-10; // FIXME
+
+  mrc_fld_data_t qsq,asq,cf1sq,cf2sq,cf3sq;
+  mrc_fld_data_t b1,b2,b3,bsq,tsum,tdif;
+  mrc_fld_data_t max_v1=0.0,max_v2=0.0,max_v3=0.0,max_dti = 0.0;
+
+  mrc_fld_data_t gamma = mhd->par.gamm;
+  mrc_fld_data_t gamma_minus_1 = gamma - 1.;
+  
+  mrc_fld_data_t d_i = mhd->par.d_i;
+
+  struct mrc_crds *crds = mrc_domain_get_crds(mhd->domain);
+
+  for (int p = 0; p < mrc_fld_nr_patches(x); p++) {
+    double dx[3]; mrc_crds_get_dx(crds, p, dx);
+
+    mrc_fld_foreach(x, i,j,k, 0, 0) {
+      mrc_fld_data_t rri = 1.f / RR_(x, i,j,k, p);
+      mrc_fld_data_t vx = RVX_(x, i,j,k, p) * rri;
+      mrc_fld_data_t vy = RVY_(x, i,j,k, p) * rri;
+      mrc_fld_data_t vz = RVZ_(x, i,j,k, p) * rri;
+      qsq = sqr(vx) + sqr(vy) + sqr(vz);
+      
+      /* Use maximum of face-centered fields (always larger than cell-centered B) */
+      b1 = M3(Bcc, 0, i,j,k, p) + fabs(BX_(x, i,j,k, p) - M3(Bcc, 0, i,j,k, p));
+      b2 = M3(Bcc, 1, i,j,k, p) + fabs(BY_(x, i,j,k, p) - M3(Bcc, 1, i,j,k, p));
+      b3 = M3(Bcc, 2, i,j,k, p) + fabs(BZ_(x, i,j,k, p) - M3(Bcc, 2, i,j,k, p));
+      bsq = sqr(b1) + sqr(b2) + sqr(b3);
+      /* compute sound speed squared */
+      mrc_fld_data_t pp = fmax(gamma_minus_1*(EE_(x, i,j,k, p) - .5f*RR_(x, i,j,k, p)*qsq
+					      - .5f*bsq), eps);
+      asq = gamma * pp * rri;
+      
+      /* compute fast magnetosonic speed squared in each direction */
+      tsum = bsq * rri + asq;
+      tdif = bsq * rri - asq;
+      cf1sq = 0.5*(tsum + sqrt(tdif*tdif + 4.0*asq*(b2*b2+b3*b3) * rri));
+      cf2sq = 0.5*(tsum + sqrt(tdif*tdif + 4.0*asq*(b1*b1+b3*b3) * rri));
+      cf3sq = 0.5*(tsum + sqrt(tdif*tdif + 4.0*asq*(b1*b1+b2*b2) * rri));
+      
+      /* compute maximum inverse dt (corresponding to minimum dt) */
+      max_v1 = fmax(max_v1, (fabs(vx) + sqrt(cf1sq)) / dx[0]);
+      max_v2 = fmax(max_v2, (fabs(vy) + sqrt(cf2sq)) / dx[1]);
+      max_v3 = fmax(max_v3, (fabs(vz) + sqrt(cf3sq)) / dx[2]);
+    } mrc_fld_foreach_end;
+  }
+
+  int gdims[3];
+  mrc_domain_get_global_dims(x->_domain, gdims);
+
+  if (gdims[0] > 1) max_dti = fmax(max_dti, max_v1);
+  if (gdims[1] > 1) max_dti = fmax(max_dti, max_v2);
+  if (gdims[2] > 1) max_dti = fmax(max_dti, max_v3);
+
+  mrc_fld_data_t cfl = mhd->par.thx;
+  mrc_fld_data_t local_dt = cfl / max_dti;
+  mrc_fld_data_t global_dt;
+  MPI_Allreduce(&local_dt, &global_dt, 1, MPI_MRC_FLD_DATA_T, MPI_MIN, ggcm_mhd_comm(mhd));
+
+  // FOR diffusive portion of everything
+  if (d_i > 0.0) {
+    mrc_fld_data_t local_diff_dt, global_diff_dt;
+    mrc_fld_data_t max_dti_diff = 0.0;
+
+    for (int p = 0; p < mrc_fld_nr_patches(x); p++) {
+      double dx[3]; mrc_crds_get_dx(crds, p, dx);
+
+      mrc_fld_foreach(x, i,j,k, 0, 0) {
+	mrc_fld_data_t dxmin, dti_hall;
+
+	dxmin = mrc_fld_min(dx[0], mrc_fld_min(dx[1], dx[2]));
+	
+	// FIXME: this could be streamlined by using the loop above, but
+	//        keeping them separate is exactly analogous to athena
+	/* Use maximum of face-centered fields (always larger than cell-centered B) */
+	b1 = M3(Bcc, 0, i,j,k, p) + fabs(BX_(x, i,j,k, p) - M3(Bcc, 0, i,j,k, p));
+	b2 = M3(Bcc, 1, i,j,k, p) + fabs(BY_(x, i,j,k, p) - M3(Bcc, 1, i,j,k, p));
+	b3 = M3(Bcc, 2, i,j,k, p) + fabs(BZ_(x, i,j,k, p) - M3(Bcc, 2, i,j,k, p));
+	bsq = sqr(b1) + sqr(b2) + sqr(b3);
+	
+	// FIXME? in athena it was (dxmin**2 / 6), but i don't think that's right?
+	dti_hall = (d_i * bsq / RR_(x, i,j,k, p)) / (sqr(dxmin) / 16.0);
+	max_dti_diff = mrc_fld_max(max_dti_diff, dti_hall);
+      } mrc_fld_foreach_end;
+    }
+    local_diff_dt = cfl / max_dti_diff;
+    MPI_Allreduce(&local_diff_dt, &global_diff_dt, 1, MPI_MRC_FLD_DATA_T, MPI_MIN, ggcm_mhd_comm(mhd));
+    global_dt = mrc_fld_min(global_dt, global_diff_dt);
+  }
+
+  return global_dt;
 }
 
