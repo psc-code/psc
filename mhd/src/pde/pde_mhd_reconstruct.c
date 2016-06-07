33ffaa663546984f5b2f2d0db54b7ee514771a2e
Kai Germaschewski <kai.germaschewski@unh.edu>
2016-06-07 15:58:12 -0400

    [ggcm] pde: streamline gminmod reconstruction

--- mhd/src/pde/pde_mhd_reconstruct.c
+++ mhd/src/pde/pde_mhd_reconstruct.c
@@ -31,52 +31,6 @@ mhd_reconstruct_pcm(fld1d_state_t U_l, fld1d_state_t U_r,
 }
 
 // ----------------------------------------------------------------------
-// mhd_reconstruct_plm_gminmod
-
-static void _mrc_unused
-mhd_reconstruct_plm_gminmod(fld1d_state_t U_l, fld1d_state_t U_r,
-			    fld1d_state_t W_l, fld1d_state_t W_r,
-			    fld1d_state_t W, fld1d_t bx, int ib, int ie)
-{
-  mrc_fld_data_t dWc[s_n_comps], dWl[s_n_comps];
-  mrc_fld_data_t dWr[s_n_comps], dWg[s_n_comps];
-  mrc_fld_data_t dWm[s_n_comps];
-
-  // These limits are such that Wl[ib..ie) and Wr[ib..ie) both get set,
-  for (int i = ib - 1; i < ie; i++) {
-    for (int n = 0; n < s_n_comps; n++) {
-      dWc[n] = F1S(W, n, i+1) - F1S(W, n, i-1);
-      dWl[n] = F1S(W, n, i  ) - F1S(W, n, i-1);
-      dWr[n] = F1S(W, n, i+1) - F1S(W, n, i  );
-      dWg[n] = (dWl[n] * dWr[n] > 0.) ? 2. * dWl[n] * dWr[n] / (dWl[n] + dWr[n]) : 0.;
-    }
-
-    for (int n = 0; n < s_n_comps; n++) {
-      if (dWl[n] * dWr[n] > 0.0) {
-        mrc_fld_data_t min_lr = fmin(    fabs(dWl[n]), fabs(dWr[n]));
-        mrc_fld_data_t min_cg = fmin(0.5*fabs(dWc[n]), fabs(dWg[n]));
-        dWm[n] = copysign(fmin(2.f * min_lr, min_cg), dWc[n]);
-      } else {
-	dWm[n] = 0.0;
-      }
-    }
-
-    for (int n = 0; n < s_n_comps; n++) {
-      F1S(W_l, n, i+1) = F1S(W, n, i) + .5 * dWm[n];
-      F1S(W_r, n, i  ) = F1S(W, n, i) - .5 * dWm[n];
-    }
-
-    if (fld1d_is_setup(bx)) {
-      F1S(W_l, BX, i) = F1(bx, i);
-      F1S(W_r, BX, i) = F1(bx, i);
-    }
-  }
-
-  mhd_cons_from_prim(U_l, W_l, ib, ie);
-  mhd_cons_from_prim(U_r, W_r, ib, ie);
-}
-
-// ----------------------------------------------------------------------
 // minmod
 
 static inline mrc_fld_data_t
@@ -95,9 +49,9 @@ minmod(mrc_fld_data_t a, mrc_fld_data_t b)
 // minmod limiter
 
 static inline mrc_fld_data_t
-limit_minmod(mrc_fld_data_t dvm, mrc_fld_data_t dvp)
+limit_minmod(mrc_fld_data_t dWm, mrc_fld_data_t dWp)
 {
-  return minmod(dvm, dvp);
+  return minmod(dWm, dWp);
 }
 
 // ----------------------------------------------------------------------
@@ -106,10 +60,23 @@ limit_minmod(mrc_fld_data_t dvm, mrc_fld_data_t dvp)
 // monotonized central limiter
 
 static inline mrc_fld_data_t
-limit_mc(mrc_fld_data_t dvm, mrc_fld_data_t dvp)
+limit_mc(mrc_fld_data_t dWm, mrc_fld_data_t dWp)
 {
-  mrc_fld_data_t dvc = .5 * (dvm + dvp);
-  return minmod(dvc, s_limiter_mc_beta * minmod(dvm, dvp));
+  mrc_fld_data_t dWc = .5f * (dWm + dWp);
+  return minmod(dWc, s_limiter_mc_beta * minmod(dWm, dWp));
+}
+
+// ----------------------------------------------------------------------
+// limit_gminmod
+//
+// generalized minmod (athena)
+
+static inline mrc_fld_data_t
+limit_gminmod(mrc_fld_data_t dWm, mrc_fld_data_t dWp)
+{
+  mrc_fld_data_t dWc = .5f * (dWm + dWp);
+  mrc_fld_data_t dWg = 2. * dWm * dWp / (dWm + dWp);
+  return minmod(minmod(dWc, dWg), 2.f * minmod(dWm, dWp));
 }
 
 // ----------------------------------------------------------------------
@@ -125,6 +92,10 @@ limit_slope(mrc_fld_data_t dW[], mrc_fld_data_t dWm[], mrc_fld_data_t dWp[])
   } else if (s_opt_limiter == OPT_LIMITER_MC) {
     for (int m = 0; m < s_n_comps; m++) {
       dW[m] = limit_mc(dWm[m], dWp[m]);
+    }
+  } else if (s_opt_limiter == OPT_LIMITER_GMINMOD) {
+    for (int m = 0; m < s_n_comps; m++) {
+      dW[m] = limit_gminmod(dWm[m], dWp[m]);
     }
   } else {
     assert(0);
@@ -147,7 +118,7 @@ limit_slope(mrc_fld_data_t dW[], mrc_fld_data_t dWm[], mrc_fld_data_t dWp[])
 static void
 mhd_reconstruct_plm_prim(fld1d_state_t Ul, fld1d_state_t Ur,
 			 fld1d_state_t Wl, fld1d_state_t Wr,
-			 fld1d_state_t W, int ib, int ie)
+			 fld1d_state_t W, fld1d_t bx, int ib, int ie)
 {
   for (int i = ib - 1; i < ie; i++) {
     // one-sided differences after geometric correction
@@ -166,6 +137,11 @@ mhd_reconstruct_plm_prim(fld1d_state_t Ul, fld1d_state_t Ur,
       F1S(Wl, m, i+1) = F1S(W, m, i) + .5f * dW[m];
       F1S(Wr, m, i  ) = F1S(W, m, i) - .5f * dW[m];
     }
+
+    if (fld1d_is_setup(bx)) {
+      F1S(Wl, BX, i) = F1(bx, i);
+      F1S(Wr, BX, i) = F1(bx, i);
+    }
   }
 
   // set conservative states, too
@@ -183,9 +159,7 @@ mhd_reconstruct(fld1d_state_t U_l, fld1d_state_t U_r,
 {
   if (s_opt_limiter == OPT_LIMITER_FLAT) {
     mhd_reconstruct_pcm(U_l, U_r, W_l, W_r, W, bx, ib, ie);
-  } else if (s_opt_limiter == OPT_LIMITER_GMINMOD) {
-    mhd_reconstruct_plm_gminmod(U_l, U_r, W_l, W_r, W, bx, ib, ie);
   } else {
-    mhd_reconstruct_plm_prim(U_l, U_r, W_l, W_r, W, ib, ie);
+    mhd_reconstruct_plm_prim(U_l, U_r, W_l, W_r, W, bx, ib, ie);
   }
 }
