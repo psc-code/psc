8cad79313c2876ce26fbc0a9bdea7ded2534c188
Kai Germaschewski <kai.germaschewski@unh.edu>
2016-06-10 02:26:11 -0400

    [ggcm] pde: upd_fv use s_n_comps

--- mhd/src/pde/pde_mhd_stage.c
+++ mhd/src/pde/pde_mhd_stage.c
@@ -10,11 +10,9 @@ mhd_update_finite_volume(struct ggcm_mhd *mhd,
 			 struct mrc_fld *ymask, mrc_fld_data_t dt, int p,
 			 int l, int r)
 {
-  int nr_comps = mrc_fld_nr_comps(fluxes[0]);
-
   mrc_fld_foreach(ymask, i,j,k, l, r) {
     mrc_fld_data_t ym = ymask ? M3(ymask, 0, i,j,k, p) : 1.f;
-    for (int m = 0; m < nr_comps; m++) {
+    for (int m = 0; m < s_n_comps; m++) {
       F3S(x, m, i,j,k) -= dt * ym *
 	(PDE_INV_DX(i) * (M3(fluxes[0], m, i+di,j,k, p) - M3(fluxes[0], m, i,j,k, p)) +
 	 PDE_INV_DY(j) * (M3(fluxes[1], m, i,j+dj,k, p) - M3(fluxes[1], m, i,j,k, p)) +
