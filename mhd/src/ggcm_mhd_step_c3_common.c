dd014ce3bdacbb78d5ecb222875af4fef15dfcab
Kai Germaschewski <kai.germaschewski@unh.edu>
2016-06-13 16:52:28 -0400

    [ggcm] pde: badval bug fix

--- mhd/src/ggcm_mhd_step_c3_common.c
+++ mhd/src/ggcm_mhd_step_c3_common.c
@@ -756,7 +756,7 @@ patch_badval_checks_sc(struct ggcm_mhd *mhd, fld3d_t p_U, fld3d_t p_W, int p)
   
   fld3d_foreach(i,j,k, 0, 0) {
     // check for negative pressure
-    if (F3S(p_U, PP, i,j,k) < ppmin) {
+    if (F3S(p_W, PP, i,j,k) < ppmin) {
       has_badval = 5;
       mprintf("pressure @ (x=%g y=%g z=%g) = %lg < %lg\n",
 	      crdx[i], crdy[j], crdz[k], F3S(p_W, PP, i,j,k), ppmin);
