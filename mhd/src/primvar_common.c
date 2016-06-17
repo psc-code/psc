67c7c2d7f4b8fcbb85f9e151a3ad209724a2c193
Kai Germaschewski <kai.germaschewski@unh.edu>
2016-06-17 01:01:36 -0400

    [ggcm] primvar: start converting -> fld3d_t

--- mhd/src/primvar_common.c
+++ mhd/src/primvar_common.c
@@ -8,6 +8,15 @@
 #include <math.h>
 #include <assert.h>
 
+#include "pde/pde_defs.h"
+
+// mhd options
+
+#define OPT_EQN OPT_EQN_MHD_SCONS
+
+#include "pde/pde_setup.c"
+#include "pde/pde_mhd_setup.c"
+
 // ----------------------------------------------------------------------
 // primvar_c
 
@@ -29,21 +38,31 @@ primvar_c(struct ggcm_mhd *mhd, int m_curr)
   mrc_fld_data_t gamm = mhd->par.gamm;
   mrc_fld_data_t s = gamm - 1.f;
 
+  pde_setup(f);
+  pde_mhd_setup(mhd);
+
+  fld3d_t p_f;
+  fld3d_setup(&p_f, f);
+
   for (int p = 0; p < mrc_fld_nr_patches(f); p++) {
-    mrc_fld_foreach(f, ix,iy,iz, 2, 2) {
-      M3(f,_RR, ix,iy,iz, p) = M3(f, m_curr + _RR1, ix,iy,iz, p);
-      mrc_fld_data_t rri = 1.f / M3(f, m_curr + _RR1, ix,iy,iz, p);
-      M3(f,_VX, ix,iy,iz, p) = rri * M3(f, m_curr + _RV1X, ix,iy,iz, p);
-      M3(f,_VY, ix,iy,iz, p) = rri * M3(f, m_curr + _RV1Y, ix,iy,iz, p);
-      M3(f,_VZ, ix,iy,iz, p) = rri * M3(f, m_curr + _RV1Z, ix,iy,iz, p);
+    fld3d_get(&p_f, p);
+
+    fld3d_foreach(ix,iy,iz, 2, 2) {
+      F3S(p_f,_RR, ix,iy,iz) = F3S(p_f, m_curr + _RR1, ix,iy,iz);
+      mrc_fld_data_t rri = 1.f / F3S(p_f, m_curr + _RR1, ix,iy,iz);
+      F3S(p_f,_VX, ix,iy,iz) = rri * F3S(p_f, m_curr + _RV1X, ix,iy,iz);
+      F3S(p_f,_VY, ix,iy,iz) = rri * F3S(p_f, m_curr + _RV1Y, ix,iy,iz);
+      F3S(p_f,_VZ, ix,iy,iz) = rri * F3S(p_f, m_curr + _RV1Z, ix,iy,iz);
       mrc_fld_data_t rvv =
-	M3(f,_VX, ix,iy,iz, p) * M3(f, m_curr + _RV1X, ix,iy,iz, p) +
-	M3(f,_VY, ix,iy,iz, p) * M3(f, m_curr + _RV1Y, ix,iy,iz, p) +
-	M3(f,_VZ, ix,iy,iz, p) * M3(f, m_curr + _RV1Z, ix,iy,iz, p);
-      M3(f,_PP, ix,iy,iz, p) = s * (M3(f, m_curr + _UU1, ix,iy,iz, p) - .5f * rvv);
-      mrc_fld_data_t cs2 = fmaxf(gamm * M3(f,_PP, ix,iy,iz, p) * rri, 0.f);
-      M3(f,_CMSV, ix,iy,iz, p) = sqrtf(rvv * rri) + sqrtf(cs2);
-    } mrc_fld_foreach_end;
+	F3S(p_f,_VX, ix,iy,iz) * F3S(p_f, m_curr + _RV1X, ix,iy,iz) +
+	F3S(p_f,_VY, ix,iy,iz) * F3S(p_f, m_curr + _RV1Y, ix,iy,iz) +
+	F3S(p_f,_VZ, ix,iy,iz) * F3S(p_f, m_curr + _RV1Z, ix,iy,iz);
+      F3S(p_f,_PP, ix,iy,iz) = s * (F3S(p_f, m_curr + _UU1, ix,iy,iz) - .5f * rvv);
+      mrc_fld_data_t cs2 = fmaxf(gamm * F3S(p_f,_PP, ix,iy,iz) * rri, 0.f);
+      F3S(p_f,_CMSV, ix,iy,iz) = sqrtf(rvv * rri) + sqrtf(cs2);
+    } fld3d_foreach_end;
+
+    fld3d_put(&p_f, p);
   }   
   mrc_fld_put_as(f, mhd->fld);
     
