d9a8bb46645a2a1e9540cd1eb3bee39a0c8d97d1
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-02-04 22:48:56 -0500

    [ggcm] mrc_ndarray: add default for "perm" param

--- tests/test_mrc_ndarray.c
+++ tests/test_mrc_ndarray.c
@@ -1,6 +1,7 @@
 
 #include <mrc_ndarray.h>
 
+#include <stdio.h>
 #include <assert.h>
 #include <string.h>
 #include <math.h>
@@ -43,12 +44,47 @@ test_0()
 }
 
 // ----------------------------------------------------------------------
+// test_1
+//
+// like test_0, but don't set perm (tests the default)
+
+static void
+test_1()
+{
+  struct mrc_ndarray *nd = mrc_ndarray_create(MPI_COMM_WORLD);
+  mrc_ndarray_set_param_int3(nd, "offs", (int [3]) { 1, 2, 3 });
+  mrc_ndarray_set_param_int3(nd, "dims", (int [3]) { 2, 3, 4 });
+  mrc_ndarray_set_from_options(nd);
+  mrc_ndarray_setup(nd);
+  mrc_ndarray_view(nd);
+
+  for (int k = 3; k < 3 + 4; k++) {
+    for (int j = 2; j < 2 + 3; j++) {
+      for (int i = 1; i < 1 + 2; i++) {
+	S3(nd, i,j,k) = i * 10000 + j * 100 + k;
+      }
+    }
+  }
+
+  for (int k = 3; k < 3 + 4; k++) {
+    for (int j = 2; j < 2 + 3; j++) {
+      for (int i = 1; i < 1 + 2; i++) {
+	assert(S3(nd, i,j,k) == i * 10000 + j * 100 + k);
+      }
+    }
+  }
+
+  mrc_ndarray_destroy(nd);
+}
+
+// ----------------------------------------------------------------------
 // main
 
 typedef void (*test_func)(void);
 
 static test_func tests[] = {
   [0] = test_0,
+  [1] = test_1,
 };
 
 static int n_tests = sizeof(tests)/sizeof(tests[0]);
