68b855c46f93eff04d4ec3c017124fbf62c4305d
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-03-15 16:56:31 -0400

    [ggcm] fill_ghosts: add a FIXME comment to mrc_ts_monitor_ggcm.c

--- mhd/src/mrc_ts_monitor_ggcm.c
+++ mhd/src/mrc_ts_monitor_ggcm.c
@@ -27,6 +27,10 @@ mrc_ts_monitor_ggcm_run(struct mrc_ts_monitor *mon, struct mrc_ts *ts)
   struct ggcm_mhd *mhd = (struct ggcm_mhd *) ts->ctx_obj;
 
   mhd->time_code = ts->time;
+
+  // FIXME? should we do this?
+  // we may not even know which components to use (mhd->fld may be all 44 Fortran components)
+  // OTOH, some I/O items may need ghost points (j, divB, even pp on Yee grid in fcons case)
   ggcm_mhd_fill_ghosts(mhd, mhd->fld, 0, mhd->time_code);
   ggcm_mhd_diag_run_now(mhd->diag, mhd->fld, DIAG_TYPE_3D, out->nr++);
 }
