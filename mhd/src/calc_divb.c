83b0be1317fad2a26d6ec0081b44615b769d66fa
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-03-17 11:51:48 -0400

    [ggcm] mt: start pde_mhd_divb_bgrid_cc()

--- mhd/src/calc_divb.c
+++ mhd/src/calc_divb.c
@@ -12,12 +12,52 @@
 
 #include <mrc_fld_as_double.h>
 
+#include "pde/pde_defs.h"
+
+#define OPT_FLD1D OPT_FLD1D_C_ARRAY
+
+#include "pde/pde_mhd_divb.c"
+
+static void
+ggcm_mhd_calc_divb_cc(struct ggcm_mhd *mhd, struct mrc_fld *f, struct mrc_fld *divB)
+{
+  fld3d_t p_U, p_divB;
+  fld3d_setup(&p_U, f);
+  fld3d_setup(&p_divB, divB);
+
+  for (int p = 0; p < mrc_fld_nr_patches(f); p++) {
+    pde_patch_set(p);
+    fld3d_get(&p_U, p);
+    fld3d_get(&p_divB, p);
+
+    patch_calc_divb_bgrid_cc(p_divB, fld3d_make_view(p_U, BX));
+  }
+}
+
 // ----------------------------------------------------------------------
 // ggcm_mhd_calc_divb
 
 void
 ggcm_mhd_calc_divb(struct ggcm_mhd *mhd, struct mrc_fld *fld, struct mrc_fld *divb)
 {
+  static bool is_setup = false;
+  if (!is_setup) {
+    pde_setup(fld);
+    pde_mhd_setup(mhd);
+    is_setup = true;
+  }
+
+  int mhd_type;
+  mrc_fld_get_param_int(fld, "mhd_type", &mhd_type);
+
+  struct mrc_fld *f = mrc_fld_get_as(fld, FLD_TYPE);
+  struct mrc_fld *d = mrc_fld_get_as(divb, FLD_TYPE);
+
+  if (MT_BGRID(mhd_type) == MT_BGRID_CC) {
+    ggcm_mhd_calc_divb_cc(mhd, f, d);
+    goto out;
+  }
+  
   mrc_fld_data_t hx = 1., hy = 1., hz = 1.;
   int gdims[3];
   mrc_domain_get_global_dims(fld->_domain, gdims);
@@ -27,17 +67,12 @@ ggcm_mhd_calc_divb(struct ggcm_mhd *mhd, struct mrc_fld *fld, struct mrc_fld *di
   int dx = (gdims[0] > 1), dy = (gdims[1] > 1), dz = (gdims[2] > 1);
 
 
-  struct mrc_fld *f = mrc_fld_get_as(fld, FLD_TYPE);
-  struct mrc_fld *d = mrc_fld_get_as(divb, FLD_TYPE);
   struct mrc_fld *ymask = NULL;
   if (mhd->ymask) {
     ymask = mrc_fld_get_as(mhd->ymask, FLD_TYPE);
   }
 
   mrc_fld_data_t max = 0.;
-
-  int mhd_type;
-  mrc_fld_get_param_int(fld, "mhd_type", &mhd_type);
 
   if (MT_BGRID(mhd_type) == MT_BGRID_FC_GGCM) {
     for (int p = 0; p < mrc_fld_nr_patches(divb); p++) {
@@ -77,29 +112,10 @@ ggcm_mhd_calc_divb(struct ggcm_mhd *mhd, struct mrc_fld *fld, struct mrc_fld *di
 	max = mrc_fld_max(max, mrc_fld_abs(M3(d,0, ix,iy,iz, p)));
       } mrc_fld_foreach_end;
     }
-  } else if (MT_BGRID(mhd_type) == MT_BGRID_CC) {
-    for (int p = 0; p < mrc_fld_nr_patches(divb); p++) {
-      float *fd1x = ggcm_mhd_crds_get_crd_p(mhd->crds, 0, FD1, p);
-      float *fd1y = ggcm_mhd_crds_get_crd_p(mhd->crds, 1, FD1, p);
-      float *fd1z = ggcm_mhd_crds_get_crd_p(mhd->crds, 2, FD1, p);
-
-      mrc_fld_foreach(divb, ix,iy,iz, 0, 0) {
-	M3(d,0, ix,iy,iz, p) =
-	  (BX_(f, ix+dx,iy,iz, p) - BX_(f, ix-dx,iy,iz, p)) * hx * .5f*fd1x[ix] +
-	  (BY_(f, ix,iy+dy,iz, p) - BY_(f, ix,iy-dy,iz, p)) * hy * .5f*fd1y[iy] +
-	  (BZ_(f, ix,iy,iz+dz, p) - BZ_(f, ix,iy,iz-dz, p)) * hz * .5f*fd1z[iz];
-	
-	if (M3(ymask, 0, ix,iy,iz, p) == 0)
-	  continue;
-	max = mrc_fld_max(max, mrc_fld_abs(M3(d,0, ix,iy,iz, p)));
-      } mrc_fld_foreach_end;
-    }
   } else {
     assert(0);
   }
 
-  mrc_fld_put_as(f, fld);
-  mrc_fld_put_as(d, divb);
   if (mhd->ymask) {
     mrc_fld_put_as(ymask, mhd->ymask);
   }
@@ -107,5 +123,13 @@ ggcm_mhd_calc_divb(struct ggcm_mhd *mhd, struct mrc_fld *fld, struct mrc_fld *di
   mrc_fld_data_t max_divb;
   MPI_Allreduce(&max, &max_divb, 1, MPI_MRC_FLD_DATA_T, MPI_MAX, ggcm_mhd_comm(mhd));
   mpi_printf(ggcm_mhd_comm(mhd), "max divb = %g\n", max_divb);
+
+ out:
+  mrc_fld_put_as(f, fld);
+  mrc_fld_put_as(d, divb);
+
+  if (0) {
+    pde_free();
+  }
 }
 
---
+++ mhd/src/pde/pde_mhd_divb.c
@@ -0,0 +1,19 @@
+
+#ifndef PDE_MHD_DIVB_C
+#define PDE_MHD_DIVB_C
+
+#include "pde/pde_mhd_setup.c"
+
+static void _mrc_unused
+patch_calc_divb_bgrid_cc(fld3d_t p_divB, fld3d_t p_B)
+{
+  fld3d_foreach(i,j,k, 0, 0) {
+    F3S(p_divB, 0, i,j,k) = .5f * ((F3S(p_B, 0, i+di,j,k) - F3S(p_B, 0, i-di,j,k)) * PDE_INV_DX(i) +
+				   (F3S(p_B, 1, i,j+dj,k) - F3S(p_B, 1, i,j-dj,k)) * PDE_INV_DY(j) +
+				   (F3S(p_B, 2, i,j,k+dk) - F3S(p_B, 2, i,j,k-dk)) * PDE_INV_DZ(k));
+  } fld3d_foreach_end;
+}
+
+
+
+#endif
