63541352962b96fb76891283977eb8e56f8808db
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-03-28 23:41:54 -0400

    [ggcm] si/iono: use some physics and other constants

--- include/mrc_physics.h
+++ include/mrc_physics.h
@@ -8,5 +8,6 @@
 // specific to earth (these shouldn't be used directly, but they provide defaults)
 #define C_RE (6371040.)
 #define C_DIPOLESTRENGTH (30574.E-9)
-
+#define C_BOLTZMANN (1.3806485279E-23)
+#define C_ELEMENTARY_CHARGE (1.60217662E-19)
 #endif
