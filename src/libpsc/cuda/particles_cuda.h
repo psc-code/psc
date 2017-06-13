bb427da7cdd3071dd0ff306998c51c72cd45ed29
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-06-13 00:05:17 -0400

    cmprts: remove cuda_mprts_find_block_indices_ids_total()

--- src/libpsc/cuda/particles_cuda.h
+++ src/libpsc/cuda/particles_cuda.h
@@ -30,7 +30,6 @@ EXTERN_C void free_params(struct cuda_params *prm);
 EXTERN_C void cuda_mprts_sort_initial(struct psc_mparticles *mprts);
 EXTERN_C void cuda_mprts_reorder_and_offsets(struct psc_mparticles *mprts);
 EXTERN_C void cuda_mprts_find_block_indices_2(struct psc_mparticles *mprts);
-EXTERN_C void cuda_mprts_find_block_indices_2_total(struct psc_mparticles *mprts);
 EXTERN_C void cuda_mprts_find_block_keys(struct psc_mparticles *mprts);
 EXTERN_C void cuda_mprts_find_block_indices_ids_total(struct psc_mparticles *mprts);
 EXTERN_C void cuda_mprts_scan_send_buf(struct psc_mparticles *mprts);
