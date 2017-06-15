df18110668525a1991d0c92346e71188fccd3c1d
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-06-15 00:32:58 -0400

    cmprts/iface: test sort_initial, get_particles

--- src/libpsc/cuda/tests/test_cuda_mparticles_1.c
+++ src/libpsc/cuda/tests/test_cuda_mparticles_1.c
@@ -24,7 +24,7 @@ cuda_domain_info_set_test_1(struct cuda_domain_info *info)
 // into Fortran order, a.k.a., this will exercise the initial sorting
 
 static void
-get_particle_test_1(struct cuda_mparticles_prt *prt, int n, void *ctx)
+set_particle_test_1(struct cuda_mparticles_prt *prt, int n, void *ctx)
 {
   struct cuda_domain_info *info = ctx;
 
@@ -58,69 +58,38 @@ cuda_mparticles_add_particles_test_1(struct cuda_mparticles *cmprts,
   unsigned int off = 0;
   for (int p = 0; p < info->n_patches; p++) {
     cuda_mparticles_set_particles(cmprts, n_prts_by_patch[p], off,
-				  get_particle_test_1, info);
+				  set_particle_test_1, info);
     off += n_prts_by_patch[p];
   }
 }
 
-#if 0
-static int
-get_block_idx(struct cuda_mparticles *cmprts, int n, int p)
-{
-  thrust::device_ptr<float4> d_xi4(cmprts->d_xi4);
-  float *b_dxi = cmprts->b_dxi;
-  int *b_mx = cmprts->b_mx;
-  
-  float4 xi4 = d_xi4[n];
-  unsigned int block_pos_y = (int) floor(xi4.y * b_dxi[1]);
-  unsigned int block_pos_z = (int) floor(xi4.z * b_dxi[2]);
-
-  int bidx;
-  if (block_pos_y >= b_mx[1] || block_pos_z >= b_mx[2]) {
-    bidx = -1;
-  } else {
-    bidx = (p * b_mx[2] + block_pos_z) * b_mx[1] + block_pos_y;
-  }
-
-  return bidx;
-}
+// ----------------------------------------------------------------------
+// get_particles_test
 
 static void
-cuda_mparticles_check_in_patch_unordered(struct cuda_mparticles *cmprts,
-					 unsigned int *nr_prts_by_patch)
+get_particle(struct cuda_mparticles_prt *prt, int n, void *ctx)
 {
-  unsigned int off = 0;
-  for (int p = 0; p < cmprts->n_patches; p++) {
-    for (int n = 0; n < nr_prts_by_patch[p]; n++) {
-      int bidx = get_block_idx(cmprts, off + n, p);
-      assert(bidx >= 0 && bidx <= cmprts->n_blocks);
-    }
-    off += nr_prts_by_patch[p];
-  }
-
-  assert(off == cmprts->n_prts);
+  printf("prt[%d] xi %g %g %g // pxi %g %g %g // kind %d // qni_wni %g\n",
+	 n, prt->xi[0], prt->xi[1], prt->xi[2],
+	 prt->pxi[0], prt->pxi[1], prt->pxi[2],
+	 prt->kind, prt->qni_wni);
 }
 
-static void
-cuda_mparticles_check_bidx_id_unordered(struct cuda_mparticles *cmprts,
-					unsigned int *n_prts_by_patch)
+void
+get_particles_test(struct cuda_mparticles *cmprts,
+		   struct cuda_domain_info *info,
+		   unsigned int *n_prts_by_patch)
 {
-  thrust::device_ptr<unsigned int> d_bidx(cmprts->d_bidx);
-  thrust::device_ptr<unsigned int> d_id(cmprts->d_id);
-
   unsigned int off = 0;
-  for (int p = 0; p < cmprts->n_patches; p++) {
-    for (int n = 0; n < n_prts_by_patch[p]; n++) {
-      int bidx = get_block_idx(cmprts, off + n, p);
-      assert(bidx == d_bidx[off+n]);
-      assert(off+n == d_id[off+n]);
-    }
+  for (int p = 0; p < info->n_patches; p++) {
+    cuda_mparticles_get_particles(cmprts, n_prts_by_patch[p], off,
+				  get_particle, info);
     off += n_prts_by_patch[p];
   }
-
-  assert(off == cmprts->n_prts);
 }
-#endif
+
+// ----------------------------------------------------------------------
+// main
 
 int
 main(void)
@@ -135,23 +104,13 @@ main(void)
   cuda_mparticles_add_particles_test_1(cmprts, &info, n_prts_by_patch);
   printf("added particles\n");
   cuda_mparticles_dump(cmprts);
-#if 0
-  cuda_mparticles_check_in_patch_unordered(cmprts, n_prts_by_patch);
 
-  cuda_mparticles_find_block_indices_ids(cmprts, n_prts_by_patch);
-  printf("find bidx, id\n");
-  cuda_mparticles_dump(cmprts);
-  cuda_mparticles_check_bidx_id_unordered(cmprts, n_prts_by_patch);
-
-  thrust::device_ptr<unsigned int> d_bidx(cmprts->d_bidx);
-  thrust::device_ptr<unsigned int> d_id(cmprts->d_id);
-  thrust::stable_sort_by_key(d_bidx, d_bidx + cmprts->n_prts, d_id);
+  cuda_mparticles_sort_initial(cmprts, n_prts_by_patch);
+  printf("sorted initially\n");
   cuda_mparticles_dump(cmprts);
 
-  cuda_mparticles_reorder_and_offsets(cmprts);
-  cuda_mparticles_dump(cmprts);
-  
-  cuda_mparticles_dealloc(cmprts);
-#endif
+  printf("get_particles_test\n");
+  get_particles_test(cmprts, &info, n_prts_by_patch);
+
   cuda_mparticles_destroy(cmprts);
 }
