82f9f6b7fd22e8b488df2c5a2681e7347946a982
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-06-17 00:14:13 -0400

    cuda: make cuda_bits.h

---
+++ src/libpsc/cuda/cuda_bits.h
@@ -0,0 +1,36 @@
+
+#ifndef CUDA_BITS_H
+#define CUDA_BITS_H
+
+#define cudaCheck(ierr) do {						\
+    if (ierr != cudaSuccess)						\
+      fprintf(stderr, "IERR = %d (%s)\n", ierr, cudaGetErrorName(ierr)); \
+    assert(ierr == cudaSuccess);					\
+  } while(0)
+
+static bool CUDA_SYNC = true;
+
+#define cuda_sync_if_enabled() do {					\
+    cudaError ierr = cudaGetLastError(); cudaCheck(ierr);		\
+    if (CUDA_SYNC) {							\
+      cudaError_t ierr = cudaDeviceSynchronize(); cudaCheck(ierr);	\
+    }									\
+  } while(0)
+
+static inline float
+cuda_int_as_float(int i)
+{
+  union { int i; float f; } u;
+  u.i = i;
+  return u.f;
+};
+
+static inline int
+cuda_float_as_int(float f)
+{
+  union { int i; float f; } u;
+  u.f = f;
+  return u.i;
+};
+
+#endif
--- src/libpsc/cuda/cuda_mparticles.cu
+++ src/libpsc/cuda/cuda_mparticles.cu
@@ -1,5 +1,6 @@
 
 #include "cuda_mparticles.h"
+#include "cuda_bits.h"
 
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
@@ -7,37 +8,6 @@
 
 #include <cstdio>
 #include <cassert>
-
-#define cudaCheck(ierr) do {						\
-    if (ierr != cudaSuccess)						\
-      fprintf(stderr, "IERR = %d (%s)\n", ierr, cudaGetErrorName(ierr)); \
-    assert(ierr == cudaSuccess);					\
-  } while(0)
-
-static bool CUDA_SYNC = true;
-
-#define cuda_sync_if_enabled() do {					\
-    cudaError ierr = cudaGetLastError(); cudaCheck(ierr);		\
-    if (CUDA_SYNC) {							\
-      cudaError_t ierr = cudaDeviceSynchronize(); cudaCheck(ierr);	\
-    }									\
-  } while(0)
-
-static inline float
-cuda_int_as_float(int i)
-{
-  union { int i; float f; } u;
-  u.i = i;
-  return u.f;
-};
-
-static inline int
-cuda_float_as_int(float f)
-{
-  union { int i; float f; } u;
-  u.f = f;
-  return u.i;
-};
 
 // ----------------------------------------------------------------------
 // cuda_mparticles_create
