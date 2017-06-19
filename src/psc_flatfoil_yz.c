a49f1666ca0caeb6b42e0d7cc920b4cc0e282acf
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-06-19 15:15:19 -0400

    flatfoil: start psc_flatfoil_yz
    just a copy of psc_bubble_yz for now

---
+++ src/psc_flatfoil_yz.c
@@ -0,0 +1,259 @@
+
+#include <psc.h>
+#include <psc_push_fields.h>
+#include <psc_bnd_fields.h>
+#include <psc_sort.h>
+#include <psc_balance.h>
+
+#include <mrc_params.h>
+
+#include <math.h>
+
+struct psc_flatfoil {
+  double BB;
+  double nnb;
+  double nn0;
+  double MMach;
+  double LLn;
+  double LLB;
+  double LLz;
+  double LLy;
+};
+
+#define to_psc_flatfoil(psc) mrc_to_subobj(psc, struct psc_flatfoil)
+
+#define VAR(x) (void *)offsetof(struct psc_flatfoil, x)
+static struct param psc_flatfoil_descr[] = {
+  { "BB"            , VAR(BB)              , PARAM_DOUBLE(.07)    },
+  { "nnb"           , VAR(nnb)             , PARAM_DOUBLE(.1)     },
+  { "nn0"           , VAR(nn0)             , PARAM_DOUBLE(1.)     },
+  { "MMach"         , VAR(MMach)           , PARAM_DOUBLE(3.)     },
+  { "LLn"           , VAR(LLn)             , PARAM_DOUBLE(200.)   },
+  { "LLB"           , VAR(LLB)             , PARAM_DOUBLE(200./6.)},
+  { "LLz"           , VAR(LLz)             , PARAM_DOUBLE(0.)     },
+  { "LLy"           , VAR(LLy)             , PARAM_DOUBLE(0.)     },
+  {},
+};
+#undef VAR
+
+// ----------------------------------------------------------------------
+// psc_flatfoil_create
+
+static void
+psc_flatfoil_create(struct psc *psc)
+{
+  psc_default_dimensionless(psc);
+
+  psc->prm.nmax = 32000;
+  psc->prm.nicell = 50;
+
+  psc->kinds[KIND_ELECTRON].T = .02;
+  psc->kinds[KIND_ION].m = 100.;
+  psc->kinds[KIND_ION].T = .02;
+
+  psc->domain.gdims[0] = 1;
+  psc->domain.gdims[1] = 160;
+  psc->domain.gdims[2] = 240;
+
+  psc->domain.bnd_fld_lo[0] = BND_FLD_PERIODIC;
+  psc->domain.bnd_fld_hi[0] = BND_FLD_PERIODIC;
+  psc->domain.bnd_fld_lo[1] = BND_FLD_PERIODIC;
+  psc->domain.bnd_fld_hi[1] = BND_FLD_PERIODIC;
+  psc->domain.bnd_fld_lo[2] = BND_FLD_PERIODIC;
+  psc->domain.bnd_fld_hi[2] = BND_FLD_PERIODIC;
+  psc->domain.bnd_part_lo[0] = BND_PART_PERIODIC;
+  psc->domain.bnd_part_hi[0] = BND_PART_PERIODIC;
+  psc->domain.bnd_part_lo[1] = BND_PART_PERIODIC;
+  psc->domain.bnd_part_hi[1] = BND_PART_PERIODIC;
+  psc->domain.bnd_part_lo[2] = BND_PART_PERIODIC;
+  psc->domain.bnd_part_hi[2] = BND_PART_PERIODIC;
+
+  struct psc_bnd_fields *bnd_fields = 
+    psc_push_fields_get_bnd_fields(psc->push_fields);
+  psc_bnd_fields_set_type(bnd_fields, "none");
+}
+
+// ----------------------------------------------------------------------
+// psc_flatfoil_setup
+
+static void
+psc_flatfoil_setup(struct psc *psc)
+{
+  struct psc_flatfoil *flatfoil = to_psc_flatfoil(psc);
+  
+  if (flatfoil->LLy == 0.) {
+    flatfoil->LLy = 2. * flatfoil->LLn;
+  }
+  if (flatfoil->LLz == 0.) {
+    flatfoil->LLz = 3. * flatfoil->LLn;
+  }
+  psc->domain.length[0] = flatfoil->LLn; // no x-dependence
+  psc->domain.length[1] = flatfoil->LLy;
+  psc->domain.length[2] = flatfoil->LLz;
+
+  // center around origin
+  for (int d = 0; d < 3; d++) {
+    psc->domain.corner[d] = -.5 * psc->domain.length[d];
+  }
+
+  psc_setup_super(psc);
+
+  MPI_Comm comm = psc_comm(psc);
+  double TTe = psc->kinds[KIND_ELECTRON].T;
+  mpi_printf(comm, "lambda_D = %g\n", sqrt(TTe));
+}
+
+// ----------------------------------------------------------------------
+// psc_flatfoil_read
+
+static void
+psc_flatfoil_read(struct psc *psc, struct mrc_io *io)
+{
+  psc_read_super(psc, io);
+}
+
+// ----------------------------------------------------------------------
+// psc_flatfoil_init_field
+
+static double
+psc_flatfoil_init_field(struct psc *psc, double x[3], int m)
+{
+  struct psc_flatfoil *flatfoil = to_psc_flatfoil(psc);
+
+  double BB = flatfoil->BB;
+  double LLn = flatfoil->LLn;
+  double LLy = flatfoil->LLy;
+  double LLB = flatfoil->LLB;
+  double MMi = psc->kinds[KIND_ION].m;
+  double MMach = flatfoil->MMach;
+  double TTe = psc->kinds[KIND_ELECTRON].T;
+
+  double z1 = x[2];
+  double y1 = x[1] + .5 * LLy;
+  double r1 = sqrt(sqr(z1) + sqr(y1));
+  double z2 = x[2];
+  double y2 = x[1] - .5 * LLy;
+  double r2 = sqrt(sqr(z2) + sqr(y2));
+
+  double rv = 0.;
+  switch (m) {
+  case HZ:
+    if ( (r1 < LLn) && (r1 > LLn - 2*LLB) ) {
+      rv += - BB * sin(M_PI * (LLn - r1)/(2.*LLB)) * y1 / r1;
+    }
+    if ( (r2 < LLn) && (r2 > LLn - 2*LLB) ) {
+      rv += - BB * sin(M_PI * (LLn - r2)/(2.*LLB)) * y2 / r2;
+    }
+    return rv;
+
+  case HY:
+    if ( (r1 < LLn) && (r1 > LLn - 2*LLB) ) {
+      rv += BB * sin(M_PI * (LLn - r1)/(2.*LLB)) * z1 / r1;
+    }
+    if ( (r2 < LLn) && (r2 > LLn - 2*LLB) ) {
+      rv += BB * sin(M_PI * (LLn - r2)/(2.*LLB)) * z2 / r2;
+    }
+    return rv;
+
+  case EX:
+    if ( (r1 < LLn) && (r1 > LLn - 2*LLB) ) {
+      rv += MMach * sqrt(TTe/MMi) * BB *
+	sin(M_PI * (LLn - r1)/(2.*LLB)) * sin(M_PI * r1 / LLn);
+    }
+    if ( (r2 < LLn) && (r2 > LLn - 2*LLB) ) {
+      rv += MMach * sqrt(TTe/MMi) * BB *
+	sin(M_PI * (LLn - r2)/(2.*LLB)) * sin(M_PI * r2 / LLn);
+    }
+    return rv;
+
+  case JXI:
+    if ( (r1 < LLn) && (r1 > LLn - 2*LLB) ) {
+      rv += BB * M_PI/(2.*LLB) * cos(M_PI * (LLn - r1)/(2.*LLB));
+    }
+    if ( (r2 < LLn) && (r2 > LLn - 2*LLB) ) {
+      rv += BB * M_PI/(2.*LLB) * cos(M_PI * (LLn - r2)/(2.*LLB));
+    }
+    return rv;
+
+  default:
+    return 0.;
+  }
+}
+
+// ----------------------------------------------------------------------
+// psc_flatfoil_init_npt
+
+static void
+psc_flatfoil_init_npt(struct psc *psc, int kind, double x[3],
+		    struct psc_particle_npt *npt)
+{
+  struct psc_flatfoil *flatfoil = to_psc_flatfoil(psc);
+
+  double BB = flatfoil->BB;
+  double LLy = flatfoil->LLy;
+  double LLn = flatfoil->LLn;
+  double LLB = flatfoil->LLB;
+  double V0 = flatfoil->MMach * sqrt(psc->kinds[KIND_ELECTRON].T / psc->kinds[KIND_ION].m);
+
+  double nnb = flatfoil->nnb;
+  double nn0 = flatfoil->nn0;
+
+  double r1 = sqrt(sqr(x[2]) + sqr(x[1] + .5 * LLy));
+  double r2 = sqrt(sqr(x[2]) + sqr(x[1] - .5 * LLy));
+
+  npt->n = nnb;
+  if (r1 < LLn) {
+    npt->n += (nn0 - nnb) * sqr(cos(M_PI / 2. * r1 / LLn));
+    if (r1 > 0.0) {
+      npt->p[2] += V0 * sin(M_PI * r1 / LLn) * x[2] / r1;
+      npt->p[1] += V0 * sin(M_PI * r1 / LLn) * (x[1] + .5 * LLy) / r1;
+    }
+  }
+  if (r2 < LLn) {
+    npt->n += (nn0 - nnb) * sqr(cos(M_PI / 2. * r2 / LLn));
+    if (r2 > 0.0) {
+      npt->p[2] += V0 * sin(M_PI * r2 / LLn) * x[2] / r2;
+      npt->p[1] += V0 * sin(M_PI * r2 / LLn) * (x[1] - .5 * LLy) / r2;
+    }
+  }
+
+  switch (kind) {
+  case 0: // electrons
+    // electron drift consistent with initial current
+    if ((r1 <= LLn) && (r1 >= LLn - 2.*LLB)) {
+      npt->p[0] = - BB * M_PI/(2.*LLB) * cos(M_PI * (LLn-r1)/(2.*LLB)) / npt->n;
+    }
+    if ((r2 <= LLn) && (r2 >= LLn - 2.*LLB)) {
+      npt->p[0] = - BB * M_PI/(2.*LLB) * cos(M_PI * (LLn-r2)/(2.*LLB)) / npt->n;
+    }
+
+    break;
+  case 1: // ions
+    break;
+  default:
+    assert(0);
+  }
+}
+
+// ======================================================================
+// psc_flatfoil_ops
+
+struct psc_ops psc_flatfoil_ops = {
+  .name             = "flatfoil",
+  .size             = sizeof(struct psc_flatfoil),
+  .param_descr      = psc_flatfoil_descr,
+  .create           = psc_flatfoil_create,
+  .setup            = psc_flatfoil_setup,
+  .read             = psc_flatfoil_read,
+  .init_field       = psc_flatfoil_init_field,
+  .init_npt         = psc_flatfoil_init_npt,
+};
+
+// ======================================================================
+// main
+
+int
+main(int argc, char **argv)
+{
+  return psc_main(&argc, &argv, &psc_flatfoil_ops);
+}
