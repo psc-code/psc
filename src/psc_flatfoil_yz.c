27b6c2cce18a907545ca059782cf18861e8423e8
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-06-19 22:35:35 -0400

    flatfoil: set up background species

--- src/psc_flatfoil_yz.c
+++ src/psc_flatfoil_yz.c
@@ -11,30 +11,45 @@
 
 struct psc_flatfoil {
   double BB;
-  double nnb;
-  double nn0;
-  double MMach;
-  double LLn;
-  double LLB;
+  /* double TTe; */
+  /* double TTi; */
+  double n_bg;
+  double Te_bg;
+  double Ti_bg;
+  double Zi;
+  double LLf;
   double LLz;
   double LLy;
+
+  // state
+  double LLs;
+  double LLn;
 };
 
 #define to_psc_flatfoil(psc) mrc_to_subobj(psc, struct psc_flatfoil)
 
 #define VAR(x) (void *)offsetof(struct psc_flatfoil, x)
 static struct param psc_flatfoil_descr[] = {
-  { "BB"            , VAR(BB)              , PARAM_DOUBLE(.07)    },
-  { "nnb"           , VAR(nnb)             , PARAM_DOUBLE(.1)     },
-  { "nn0"           , VAR(nn0)             , PARAM_DOUBLE(1.)     },
-  { "MMach"         , VAR(MMach)           , PARAM_DOUBLE(3.)     },
-  { "LLn"           , VAR(LLn)             , PARAM_DOUBLE(200.)   },
-  { "LLB"           , VAR(LLB)             , PARAM_DOUBLE(200./6.)},
-  { "LLz"           , VAR(LLz)             , PARAM_DOUBLE(0.)     },
-  { "LLy"           , VAR(LLy)             , PARAM_DOUBLE(0.)     },
+  { "BB"              , VAR(BB)              , PARAM_DOUBLE(.0)     },
+  { "n_bg"            , VAR(n_bg)            , PARAM_DOUBLE(.002)   },
+  { "Te_bg"           , VAR(Te_bg)           , PARAM_DOUBLE(.001)   },
+  { "Ti_bg"           , VAR(Ti_bg)           , PARAM_DOUBLE(.001)   },
+  { "Zi"              , VAR(Zi)              , PARAM_DOUBLE(1.)     },
+  { "LLf"             , VAR(LLf)             , PARAM_DOUBLE(25.)    },
+  { "LLz"             , VAR(LLz)             , PARAM_DOUBLE(400.*4) },
+  { "LLy"             , VAR(LLy)             , PARAM_DOUBLE(400.)   },
+
+  { "LLs"             , VAR(LLs)             , MRC_VAR_DOUBLE       },
+  { "LLn"             , VAR(LLn)             , MRC_VAR_DOUBLE       },
   {},
 };
 #undef VAR
+
+enum {
+  MY_ION,
+  MY_ELECTRON,
+  N_MY_KINDS,
+};
 
 // ----------------------------------------------------------------------
 // psc_flatfoil_create
@@ -44,16 +59,16 @@ psc_flatfoil_create(struct psc *psc)
 {
   psc_default_dimensionless(psc);
 
-  psc->prm.nmax = 32000;
-  psc->prm.nicell = 50;
-
-  psc->kinds[KIND_ELECTRON].T = .02;
-  psc->kinds[KIND_ION].m = 100.;
-  psc->kinds[KIND_ION].T = .02;
+  psc->prm.nmax = 210001;
+  psc->prm.nicell = 100;
+  psc->prm.gdims_in_terms_of_cells = true;
+  psc->prm.nr_populations = N_MY_KINDS;
+  psc->prm.fractional_n_particles_per_cell = true;
+  psc->prm.cfl = 0.75;
 
   psc->domain.gdims[0] = 1;
-  psc->domain.gdims[1] = 160;
-  psc->domain.gdims[2] = 240;
+  psc->domain.gdims[1] = 1600;
+  psc->domain.gdims[2] = 1600*4;
 
   psc->domain.bnd_fld_lo[0] = BND_FLD_PERIODIC;
   psc->domain.bnd_fld_hi[0] = BND_FLD_PERIODIC;
@@ -79,28 +94,31 @@ psc_flatfoil_create(struct psc *psc)
 static void
 psc_flatfoil_setup(struct psc *psc)
 {
-  struct psc_flatfoil *flatfoil = to_psc_flatfoil(psc);
+  struct psc_flatfoil *sub = to_psc_flatfoil(psc);
+
+  sub->LLs = 4. * sub->LLf;
+  sub->LLn = .5 * sub->LLf;
   
-  if (flatfoil->LLy == 0.) {
-    flatfoil->LLy = 2. * flatfoil->LLn;
-  }
-  if (flatfoil->LLz == 0.) {
-    flatfoil->LLz = 3. * flatfoil->LLn;
-  }
-  psc->domain.length[0] = flatfoil->LLn; // no x-dependence
-  psc->domain.length[1] = flatfoil->LLy;
-  psc->domain.length[2] = flatfoil->LLz;
+  psc->domain.length[0] = 1.;
+  psc->domain.length[1] = sub->LLy;
+  psc->domain.length[2] = sub->LLz;
 
   // center around origin
   for (int d = 0; d < 3; d++) {
     psc->domain.corner[d] = -.5 * psc->domain.length[d];
   }
 
+  // last population is neutralizing
+  psc->kinds[MY_ELECTRON].q = -1.;
+  psc->kinds[MY_ELECTRON].m = 1.;
+
+  psc->kinds[MY_ION     ].q = sub->Zi;
+  psc->kinds[MY_ION     ].m = 100. * sub->Zi;  // FIXME, hardcoded mass ratio 100
+
   psc_setup_super(psc);
 
   MPI_Comm comm = psc_comm(psc);
-  double TTe = psc->kinds[KIND_ELECTRON].T;
-  mpi_printf(comm, "lambda_D = %g\n", sqrt(TTe));
+  mpi_printf(comm, "lambda_De_bg = %g\n", sqrt(sub->Te_bg));
 }
 
 // ----------------------------------------------------------------------
@@ -118,62 +136,13 @@ psc_flatfoil_read(struct psc *psc, struct mrc_io *io)
 static double
 psc_flatfoil_init_field(struct psc *psc, double x[3], int m)
 {
-  struct psc_flatfoil *flatfoil = to_psc_flatfoil(psc);
+  struct psc_flatfoil *sub = to_psc_flatfoil(psc);
 
-  double BB = flatfoil->BB;
-  double LLn = flatfoil->LLn;
-  double LLy = flatfoil->LLy;
-  double LLB = flatfoil->LLB;
-  double MMi = psc->kinds[KIND_ION].m;
-  double MMach = flatfoil->MMach;
-  double TTe = psc->kinds[KIND_ELECTRON].T;
+  double BB = sub->BB;
 
-  double z1 = x[2];
-  double y1 = x[1] + .5 * LLy;
-  double r1 = sqrt(sqr(z1) + sqr(y1));
-  double z2 = x[2];
-  double y2 = x[1] - .5 * LLy;
-  double r2 = sqrt(sqr(z2) + sqr(y2));
-
-  double rv = 0.;
   switch (m) {
-  case HZ:
-    if ( (r1 < LLn) && (r1 > LLn - 2*LLB) ) {
-      rv += - BB * sin(M_PI * (LLn - r1)/(2.*LLB)) * y1 / r1;
-    }
-    if ( (r2 < LLn) && (r2 > LLn - 2*LLB) ) {
-      rv += - BB * sin(M_PI * (LLn - r2)/(2.*LLB)) * y2 / r2;
-    }
-    return rv;
-
   case HY:
-    if ( (r1 < LLn) && (r1 > LLn - 2*LLB) ) {
-      rv += BB * sin(M_PI * (LLn - r1)/(2.*LLB)) * z1 / r1;
-    }
-    if ( (r2 < LLn) && (r2 > LLn - 2*LLB) ) {
-      rv += BB * sin(M_PI * (LLn - r2)/(2.*LLB)) * z2 / r2;
-    }
-    return rv;
-
-  case EX:
-    if ( (r1 < LLn) && (r1 > LLn - 2*LLB) ) {
-      rv += MMach * sqrt(TTe/MMi) * BB *
-	sin(M_PI * (LLn - r1)/(2.*LLB)) * sin(M_PI * r1 / LLn);
-    }
-    if ( (r2 < LLn) && (r2 > LLn - 2*LLB) ) {
-      rv += MMach * sqrt(TTe/MMi) * BB *
-	sin(M_PI * (LLn - r2)/(2.*LLB)) * sin(M_PI * r2 / LLn);
-    }
-    return rv;
-
-  case JXI:
-    if ( (r1 < LLn) && (r1 > LLn - 2*LLB) ) {
-      rv += BB * M_PI/(2.*LLB) * cos(M_PI * (LLn - r1)/(2.*LLB));
-    }
-    if ( (r2 < LLn) && (r2 > LLn - 2*LLB) ) {
-      rv += BB * M_PI/(2.*LLB) * cos(M_PI * (LLn - r2)/(2.*LLB));
-    }
-    return rv;
+    return BB;
 
   default:
     return 0.;
@@ -184,51 +153,23 @@ psc_flatfoil_init_field(struct psc *psc, double x[3], int m)
 // psc_flatfoil_init_npt
 
 static void
-psc_flatfoil_init_npt(struct psc *psc, int kind, double x[3],
+psc_flatfoil_init_npt(struct psc *psc, int pop, double x[3],
 		    struct psc_particle_npt *npt)
 {
-  struct psc_flatfoil *flatfoil = to_psc_flatfoil(psc);
+  struct psc_flatfoil *sub = to_psc_flatfoil(psc);
 
-  double BB = flatfoil->BB;
-  double LLy = flatfoil->LLy;
-  double LLn = flatfoil->LLn;
-  double LLB = flatfoil->LLB;
-  double V0 = flatfoil->MMach * sqrt(psc->kinds[KIND_ELECTRON].T / psc->kinds[KIND_ION].m);
-
-  double nnb = flatfoil->nnb;
-  double nn0 = flatfoil->nn0;
-
-  double r1 = sqrt(sqr(x[2]) + sqr(x[1] + .5 * LLy));
-  double r2 = sqrt(sqr(x[2]) + sqr(x[1] - .5 * LLy));
-
-  npt->n = nnb;
-  if (r1 < LLn) {
-    npt->n += (nn0 - nnb) * sqr(cos(M_PI / 2. * r1 / LLn));
-    if (r1 > 0.0) {
-      npt->p[2] += V0 * sin(M_PI * r1 / LLn) * x[2] / r1;
-      npt->p[1] += V0 * sin(M_PI * r1 / LLn) * (x[1] + .5 * LLy) / r1;
-    }
-  }
-  if (r2 < LLn) {
-    npt->n += (nn0 - nnb) * sqr(cos(M_PI / 2. * r2 / LLn));
-    if (r2 > 0.0) {
-      npt->p[2] += V0 * sin(M_PI * r2 / LLn) * x[2] / r2;
-      npt->p[1] += V0 * sin(M_PI * r2 / LLn) * (x[1] - .5 * LLy) / r2;
-    }
-  }
-
-  switch (kind) {
-  case 0: // electrons
-    // electron drift consistent with initial current
-    if ((r1 <= LLn) && (r1 >= LLn - 2.*LLB)) {
-      npt->p[0] = - BB * M_PI/(2.*LLB) * cos(M_PI * (LLn-r1)/(2.*LLB)) / npt->n;
-    }
-    if ((r2 <= LLn) && (r2 >= LLn - 2.*LLB)) {
-      npt->p[0] = - BB * M_PI/(2.*LLB) * cos(M_PI * (LLn-r2)/(2.*LLB)) / npt->n;
-    }
-
+  switch (pop) {
+  case MY_ION:
+    npt->n    = sub->n_bg;
+    npt->T[0] = sub->Ti_bg;
+    npt->T[1] = sub->Ti_bg;
+    npt->T[2] = sub->Ti_bg;
     break;
-  case 1: // ions
+  case MY_ELECTRON:
+    npt->n    = sub->n_bg;
+    npt->T[0] = sub->Te_bg;
+    npt->T[1] = sub->Te_bg;
+    npt->T[2] = sub->Te_bg;
     break;
   default:
     assert(0);
