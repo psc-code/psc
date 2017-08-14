fea7687182fc949c8fc83616a1f83207fe917da8
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-08-13 23:14:09 -0400

    ddcp: next step

--- src/libpsc/psc_bnd_particles/ddc_particles_inc.c
+++ src/libpsc/psc_bnd_particles/ddc_particles_inc.c
@@ -914,21 +914,23 @@ psc_bnd_particles_sub_exchange_particles_prep(struct psc_bnd_particles *bnd,
   
   struct ddcp_patch *dpatch = &ddcp->patches[p];
   ddcp_buf_ctor(&dpatch->buf, mprts, p);
-#if DDCP_TYPE == DDCP_TYPE_COMMON2 || DDCP_TYPE == DDCP_TYPE_CUDA
-  ddcp_buf_resize(&dpatch->buf, get_head(mprts, p));
-#elif DDCP_TYPE == DDCP_TYPE_COMMON || DDCP_TYPE == DDCP_TYPE_COMMON_OMP
-  ddcp_buf_resize(&dpatch->buf, 0);
-#endif
 
   for (int dir1 = 0; dir1 < N_DIR; dir1++) {
     particle_buf_resize(&dpatch->nei[dir1].send_buf, 0);
   }
 
 #if DDCP_TYPE == DDCP_TYPE_COMMON2 || DDCP_TYPE == DDCP_TYPE_CUDA
+  ddcp_buf_resize(&dpatch->buf, get_head(mprts, p));
+  unsigned int n_send = get_n_send(mprts, p);
+#elif DDCP_TYPE == DDCP_TYPE_COMMON || DDCP_TYPE == DDCP_TYPE_COMMON_OMP
+  ddcp_buf_resize(&dpatch->buf, 0);
+  unsigned int n_send = mparticles_get_n_prts(mprts, p);
+#endif
 
-  int n_send = get_n_send(mprts, p);
   int n_end = ddcp_buf_size(&dpatch->buf) + n_send;
+
   for (int n = ddcp_buf_size(&dpatch->buf); n < n_end; n++) {
+#if DDCP_TYPE == DDCP_TYPE_COMMON2 || DDCP_TYPE == DDCP_TYPE_CUDA
     particle_t *prt = ddcp_buf_at(&dpatch->buf, n);
     particle_real_t *xi = &prt->xi;
     particle_real_t *pxi = &prt->pxi;
@@ -1007,14 +1009,12 @@ psc_bnd_particles_sub_exchange_particles_prep(struct psc_bnd_particles *bnd,
 	ddc_particles_queue(ddcp, dpatch, dir, prt);
       }
     }
-  }
 
 #elif DDCP_TYPE == DDCP_TYPE_COMMON || DDCP_TYPE == DDCP_TYPE_COMMON_OMP
-  unsigned int n_send = mparticles_get_n_prts(mprts, p);
-  for (int i = 0; i < n_send; i++) {
-    particle_t *part = mparticles_get_one(mprts, p, i);
-    particle_real_t *xi = &part->xi; // slightly hacky relies on xi, yi, zi to be contiguous in the struct. FIXME
-    particle_real_t *pxi = &part->pxi;
+
+    particle_t *prt = mparticles_get_one(mprts, p, n);
+    particle_real_t *xi = &prt->xi; // slightly hacky relies on xi, yi, zi to be contiguous in the struct. FIXME
+    particle_real_t *pxi = &prt->pxi;
     
     int b_pos[3];
     particle_xi_get_block_pos(xi, b_dxi, b_pos);
@@ -1023,7 +1023,7 @@ psc_bnd_particles_sub_exchange_particles_prep(struct psc_bnd_particles *bnd,
 	b_pos[2] >= 0 && b_pos[2] < b_mx[2]) {
       // fast path
       // inside domain: move into right position
-      ddcp_buf_push_back(&dpatch->buf, part);
+      ddcp_buf_push_back(&dpatch->buf, prt);
     } else {
       // slow path
       bool drop = false;
@@ -1094,15 +1094,15 @@ psc_bnd_particles_sub_exchange_particles_prep(struct psc_bnd_particles *bnd,
       }
       if (!drop) {
 	if (dir[0] == 0 && dir[1] == 0 && dir[2] == 0) {
-	  ddcp_buf_push_back(&dpatch->buf, part);
+	  ddcp_buf_push_back(&dpatch->buf, prt);
 	} else {
-	  ddc_particles_queue(ddcp, dpatch, dir, part);
+	  ddc_particles_queue(ddcp, dpatch, dir, prt);
 	}
       }
 
     }
-  }
 #endif
+  }
 }
 
 #if DDCP_TYPE == DDCP_TYPE_COMMON || DDCP_TYPE == DDCP_TYPE_COMMON_OMP || DDCP_TYPE == DDCP_TYPE_COMMON2
