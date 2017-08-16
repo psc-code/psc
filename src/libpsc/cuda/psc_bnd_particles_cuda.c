8165b40c71e8e5d2ce2ea0b1a51129f7ff06d810
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-08-15 22:25:07 -0400

    ddcp: fix a warning

--- src/libpsc/cuda/psc_bnd_particles_cuda.c
+++ src/libpsc/cuda/psc_bnd_particles_cuda.c
@@ -191,14 +191,14 @@ psc_bnd_particles_sub_exchange_mprts_post_cuda(struct psc_bnd_particles *bnd,
   cuda_mprts_update_offsets(mprts);
   prof_stop(pr_D1);
   
-#if 0
   prof_start(pr_E);
+#if 0
   cuda_mparticles_reorder(cmprts);
-  prof_stop(pr_E);
   //  cuda_mprts_check_ordered_total(mprts);
 #else
   cmprts->need_reorder = true;
 #endif
+  prof_stop(pr_E);
 
   struct ddc_particles *ddcp = bnd->ddcp;
   for (int p = 0; p < ddcp->nr_patches; p++) {
