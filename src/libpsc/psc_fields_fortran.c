b9d1a983f166259b8bd58526916545413d41c466
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-08-22 22:43:19 -0400

    mflds: remove unused "fortran" functions

--- src/libpsc/psc_fields_fortran.c
+++ src/libpsc/psc_fields_fortran.c
@@ -50,68 +50,6 @@ static struct mrc_obj_method psc_fields_fortran_methods[] = {
 
 #include "psc_fields_common.c"
 
-void
-fields_fortran_zero(struct psc_fields *pf, int m)
-{
-  fields_fortran_real_t **flds = pf->data;
-  memset(flds[m], 0, 
-	 pf->im[0] * pf->im[1] * pf->im[2] * sizeof(fields_fortran_real_t));
-}
-
-void
-fields_fortran_set(struct psc_fields *pf, int m, fields_fortran_real_t val)
-{
-  for (int jz = pf->ib[2]; jz < pf->ib[2] + pf->im[2]; jz++) {
-    for (int jy = pf->ib[1]; jy < pf->ib[1] + pf->im[1]; jy++) {
-      for (int jx = pf->ib[0]; jx < pf->ib[0] + pf->im[0]; jx++) {
-	F3_FORTRAN(pf, m, jx, jy, jz) = val;
-      }
-    }
-  }
-}
-
-void
-fields_fortran_copy(struct psc_fields *pf, int m_to, int m_from)
-{
-  for (int jz = pf->ib[2]; jz < pf->ib[2] + pf->im[2]; jz++) {
-    for (int jy = pf->ib[1]; jy < pf->ib[1] + pf->im[1]; jy++) {
-      for (int jx = pf->ib[0]; jx < pf->ib[0] + pf->im[0]; jx++) {
-	F3_FORTRAN(pf, m_to, jx,jy,jz) = F3_FORTRAN(pf, m_from, jx,jy,jz);
-      }
-    }
-  }
-}
-
-void
-fields_fortran_axpy(struct psc_fields *y, fields_fortran_real_t a,
-		    struct psc_fields *x)
-{
-  assert(y->nr_comp == x->nr_comp);
-  for (int m = 0; m < y->nr_comp; m++) {
-    for (int jz = y->ib[2]; jz < y->ib[2] + y->im[2]; jz++) {
-      for (int jy = y->ib[1]; jy < y->ib[1] + y->im[1]; jy++) {
-	for (int jx = y->ib[0]; jx < y->ib[0] + y->im[0]; jx++) {
-	  F3_FORTRAN(y, m, jx, jy, jz) += a * F3_FORTRAN(x, m, jx, jy, jz);
-	}
-      }
-    }
-  }
-}
-
-void
-fields_fortran_scale(struct psc_fields *pf, fields_fortran_real_t val)
-{
-  for (int m = 0; m < pf->nr_comp; m++) {
-    for (int jz = pf->ib[2]; jz < pf->ib[2] + pf->im[2]; jz++) {
-      for (int jy = pf->ib[1]; jy < pf->ib[1] + pf->im[1]; jy++) {
-	for (int jx = pf->ib[0]; jx < pf->ib[0] + pf->im[0]; jx++) {
-	  F3_FORTRAN(pf, m, jx, jy, jz) *= val;
-	}
-      }
-    }
-  }
-}
-
 // ======================================================================
 // psc_mfields: subclass "fortran"
   
