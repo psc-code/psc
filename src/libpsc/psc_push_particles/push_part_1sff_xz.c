b877f8d4a1c37e646e607bf351344f4b696ca988
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-08-25 22:13:57 -0400

    mflds: use existing common infrastructur for sff

--- src/libpsc/psc_push_particles/push_part_1sff_xz.c
+++ src/libpsc/psc_push_particles/push_part_1sff_xz.c
@@ -14,27 +14,13 @@
 static void
 do_push_part(int p, fields_t flds, particle_range_t prts)
 {
-#define S0X(off) s0x[off+1]
-#define S0Z(off) s0z[off+1]
-#define S1X(off) s1x[off+1]
-#define S1Z(off) s1z[off+1]
-
-  particle_real_t s0x[4] = {}, s0z[4] = {}, s1x[4], s1z[4];
-
-  particle_real_t dt = ppsc->dt;
-  particle_real_t xl = .5f * dt;
-  particle_real_t zl = .5f * dt;
-  particle_real_t dqs = .5f * ppsc->coeff.eta * dt;
-  particle_real_t fnqs = sqr(ppsc->coeff.alpha) * ppsc->coeff.cori / ppsc->coeff.eta;
-  particle_real_t fnqxs = ppsc->patch[p].dx[0] * fnqs / dt;
-  particle_real_t fnqzs = ppsc->patch[p].dx[2] * fnqs / dt;
-  particle_real_t dxi = 1.f / ppsc->patch[p].dx[0];
-  particle_real_t dzi = 1.f / ppsc->patch[p].dx[2];
+#include "push_part_common_vars.c"
 
   struct psc_patch *patch = &ppsc->patch[p];
 
-  fields_t flds_avg = fields_t_ctor((int[3]) { -2, 0, -2 },
-				    (int[3]) { patch->ldims[0] + 4, 1, patch->ldims[2] + 4 },
+  // FIXME, eventually no ghost points should be needed (?)
+  fields_t flds_avg = fields_t_ctor((int[3]) { -1, 0, -1 },
+				    (int[3]) { patch->ldims[0] + 2, 1, patch->ldims[2] + 1 },
 				    6);
   
   for (int iz = -1; iz < patch->ldims[2] + 1; iz++) {
@@ -51,166 +37,53 @@ do_push_part(int p, fields_t flds, particle_range_t prts)
 
   PARTICLE_ITER_LOOP(prt_iter, prts.begin, prts.end) {
     particle_t *part = particle_iter_deref(prt_iter);
+    particle_real_t *x = &part->xi;
 
     // x^n, p^n -> x^(n+.5), p^n
-
-    particle_real_t root = 1.f / particle_real_sqrt(1.f + sqr(part->pxi) + sqr(part->pyi) + sqr(part->pzi));
-    particle_real_t vxi = part->pxi * root;
-    particle_real_t vyi = part->pyi * root;
-    particle_real_t vzi = part->pzi * root;
-
-    part->xi += vxi * xl;
-    part->zi += vzi * zl;
-
-    particle_real_t u = part->xi * dxi;
-    particle_real_t w = part->zi * dzi;
-    int lg1 = particle_real_fint(u);
-    int lg3 = particle_real_fint(w);
-    particle_real_t h1 = u - lg1;
-    particle_real_t h3 = w - lg3;
-
-    particle_real_t g0x = 1.f - h1;
-    particle_real_t g0z = 1.f - h3;
-    particle_real_t g1x = h1;
-    particle_real_t g1z = h3;
+    particle_real_t vv[3];
+    calc_v(vv, &part->pxi);
+    push_x(x, vv);
 
     // CHARGE DENSITY FORM FACTOR AT (n+.5)*dt 
 
-    S0X(+0) = g0x;
-    S0X(+1) = g1x;
-    S0Z(+0) = g0z;
-    S0Z(+1) = g1z;
+    IF_DIM_X( DEPOSIT_AND_IP_COEFFS(lg1, lh1, gx, hx, 0, dxi, s0x); );
+    IF_DIM_Y( DEPOSIT_AND_IP_COEFFS(lg2, lh2, gy, hy, 0, dyi, s0y); );
+    IF_DIM_Z( DEPOSIT_AND_IP_COEFFS(lg3, lh3, gz, hz, 0, dzi, s0z); );
 
     // FIELD INTERPOLATION
 
-#define INTERPOLATE_FIELD(m, gx, gz)					\
-    (gz##0z*(gx##0x*_F3(flds_avg, m-EX, l##gx##1  ,0,l##gz##3  ) +	\
-	     gx##1x*_F3(flds_avg, m-EX, l##gx##1+1,0,l##gz##3  )) +	\
-     gz##1z*(gx##0x*_F3(flds_avg, m-EX, l##gx##1  ,0,l##gz##3+1) +	\
-	     gx##1x*_F3(flds_avg, m-EX, l##gx##1+1,0,l##gz##3+1)))	\
-      
-    particle_real_t exq = INTERPOLATE_FIELD(EX, g, g);
-    particle_real_t eyq = INTERPOLATE_FIELD(EY, g, g);
-    particle_real_t ezq = INTERPOLATE_FIELD(EZ, g, g);
+    particle_real_t E[3] = { IP_FIELD(flds_avg, EX-EX, g, g, g),
+			     IP_FIELD(flds_avg, EY-EX, g, g, g),
+			     IP_FIELD(flds_avg, EZ-EX, g, g, g), };
+    particle_real_t H[3] = { IP_FIELD(flds_avg, HX-EX, g, g, g),
+			     IP_FIELD(flds_avg, HY-EX, g, g, g),
+			     IP_FIELD(flds_avg, HZ-EX, g, g, g), };
 
-    particle_real_t hxq = INTERPOLATE_FIELD(HX, g, g);
-    particle_real_t hyq = INTERPOLATE_FIELD(HY, g, g);
-    particle_real_t hzq = INTERPOLATE_FIELD(HZ, g, g);
+    // x^(n+0.5), p^n -> x^(n+0.5), p^(n+1.0) 
+    particle_real_t dq = dqs * particle_qni_div_mni(part);
+    push_p(&part->pxi, E, H, dq);
 
-     // c x^(n+.5), p^n -> x^(n+1.0), p^(n+1.0) 
-
-    particle_real_t dq = dqs * part->qni / part->mni;
-    particle_real_t pxm = part->pxi + dq*exq;
-    particle_real_t pym = part->pyi + dq*eyq;
-    particle_real_t pzm = part->pzi + dq*ezq;
-
-    root = dq / particle_real_sqrt(1.f + pxm*pxm + pym*pym + pzm*pzm);
-    particle_real_t taux = hxq*root;
-    particle_real_t tauy = hyq*root;
-    particle_real_t tauz = hzq*root;
-
-    particle_real_t tau = 1.f / (1.f + taux*taux + tauy*tauy + tauz*tauz);
-    particle_real_t pxp = ((1.f+taux*taux-tauy*tauy-tauz*tauz)*pxm + 
-		(2.f*taux*tauy+2.f*tauz)*pym + 
-		(2.f*taux*tauz-2.f*tauy)*pzm)*tau;
-    particle_real_t pyp = ((2.f*taux*tauy-2.f*tauz)*pxm +
-		(1.f-taux*taux+tauy*tauy-tauz*tauz)*pym +
-		(2.f*tauy*tauz+2.f*taux)*pzm)*tau;
-    particle_real_t pzp = ((2.f*taux*tauz+2.f*tauy)*pxm +
-		(2.f*tauy*tauz-2.f*taux)*pym +
-		(1.f-taux*taux-tauy*tauy+tauz*tauz)*pzm)*tau;
-    
-    part->pxi = pxp + dq * exq;
-    part->pyi = pyp + dq * eyq;
-    part->pzi = pzp + dq * ezq;
-
-    root = 1.f / particle_real_sqrt(1.f + sqr(part->pxi) + sqr(part->pyi) + sqr(part->pzi));
-    vxi = part->pxi * root;
-    vyi = part->pyi * root;
-    vzi = part->pzi * root;
-
-    part->xi += vxi * xl;
-    part->zi += vzi * zl;
+    // x^(n+0.5), p^(n+1.0) -> x^(n+1.0), p^(n+1.0) 
+    calc_v(vv, &part->pxi);
+    push_x(x, vv);
 
     // CHARGE DENSITY FORM FACTOR AT (n+1.5)*dt 
     // x^(n+1), p^(n+1) -> x^(n+1.5f), p^(n+1)
 
-    particle_real_t xi = part->xi + vxi * xl;
-    particle_real_t zi = part->zi + vzi * zl;
+    // x^(n+1), p^(n+1) -> x^(n+1.5f), p^(n+1)
+    particle_real_t xn[3] = { x[0], x[1], x[2] };
+    push_x(xn, vv);
 
-    u = xi * dxi;
-    w = zi * dzi;
-    int k1 = particle_real_fint(u);
-    int k3 = particle_real_fint(w);
-    h1 = u - k1;
-    h3 = w - k3;
-
-    for (int i = -1; i <= 2; i++) {
-      S1X(i) = 0.f;
-      S1Z(i) = 0.f;
-    }
-
-    S1X(k1-lg1+0) = 1.f - h1;
-    S1X(k1-lg1+1) = h1;
-    S1Z(k3-lg3+0) = 1.f - h3;
-    S1Z(k3-lg3+1) = h3;
+    ZERO_S1;
+    IF_DIM_X( DEPOSIT(k1, gx, 0, dxi, s1x, lg1); );
+    IF_DIM_Y( DEPOSIT(k2, gy, 1, dyi, s1y, lg2); );
+    IF_DIM_Z( DEPOSIT(k3, gz, 2, dzi, s1z, lg3); );
 
     // CURRENT DENSITY AT (n+1.0)*dt
 
-    for (int i = 0; i <= 1; i++) {
-      S1X(i) -= S0X(i);
-      S1Z(i) -= S0Z(i);
-    }
-
-    int l1min, l3min, l1max, l3max;
-    
-    if (k1 == lg1) {
-      l1min = 0; l1max = +1;
-    } else if (k1 == lg1 - 1) {
-      l1min = -1; l1max = +1;
-    } else { // (k1 == lg1 + 1)
-      l1min = 0; l1max = +2;
-    }
-
-    if (k3 == lg3) {
-      l3min = 0; l3max = +1;
-    } else if (k3 == lg3 - 1) {
-      l3min = -1; l3max = +1;
-    } else { // (k3 == lg3 + 1)
-      l3min = 0; l3max = +2;
-    }
-
-    particle_real_t fnqx = part->qni * part->wni * fnqxs;
-    for (int l3 = l3min; l3 <= l3max; l3++) {
-      particle_real_t jxh = 0.f;
-      for (int l1 = l1min; l1 < l1max; l1++) {
-	particle_real_t wx = S1X(l1) * (S0Z(l3) + .5f*S1Z(l3));
-	jxh -= fnqx*wx;
-	_F3(flds, JXI, lg1+l1,0,lg3+l3) += jxh;
-      }
-    }
-
-    particle_real_t fnqy = vyi * part->qni * part->wni * fnqs;
-    for (int l3 = l3min; l3 <= l3max; l3++) {
-      for (int l1 = l1min; l1 <= l1max; l1++) {
-	particle_real_t wy = S0X(l1) * S0Z(l3)
-	  + .5f * S1X(l1) * S0Z(l3)
-	  + .5f * S0X(l1) * S1Z(l3)
-	  + (1.f/3.f) * S1X(l1) * S1Z(l3);
-	particle_real_t jyh = fnqy*wy;
-	_F3(flds, JYI, lg1+l1,0,lg3+l3) += jyh;
-      }
-    }
-
-    particle_real_t fnqz = part->qni * part->wni * fnqzs;
-    for (int l1 = l1min; l1 <= l1max; l1++) {
-      particle_real_t jzh = 0.f;
-      for (int l3 = l3min; l3 < l3max; l3++) {
-	particle_real_t wz = S1Z(l3) * (S0X(l1) + .5f*S1X(l1));
-	jzh -= fnqz*wz;
-	_F3(flds, JZI, lg1+l1,0,lg3+l3) += jzh;
-      }
-    }
+    SUBTR_S1_S0;
+    CURRENT_PREP;
+    CURRENT;
   }
 
   fields_t_dtor(&flds_avg);
