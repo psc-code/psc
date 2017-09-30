219f15bce7b763c76487ecba29037a1321fcc173
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-09-29 23:38:20 -0400

    mflds/cuda: mv cuda_mfields_{ctor,dtor} -> cuda_mfields.cu

--- src/libpsc/cuda/cuda_mfields.cu
+++ src/libpsc/cuda/cuda_mfields.cu
@@ -1,5 +1,6 @@
 
 #include "cuda_mfields.h"
+#include "cuda_bits.h"
 
 #include <cstdio>
 #include <cassert>
@@ -23,5 +24,47 @@ void
 cuda_mfields_destroy(struct cuda_mfields *cmflds)
 {
   free(cmflds);
+}
+
+// ----------------------------------------------------------------------
+// cuda_mfields_ctor
+
+void
+cuda_mfields_ctor(struct cuda_mfields *cmflds, int ib[3], int im[3],
+		  int n_fields, int n_patches)
+{
+  cudaError_t ierr;
+  
+  cmflds->n_patches = n_patches;
+  cmflds->n_fields = n_fields;
+  
+  for (int d = 0; d < 3; d++) {
+    cmflds->im[d] = im[d];
+    cmflds->ib[d] = ib[d];
+  }
+
+  cmflds->n_cells_per_patch = im[0] * im[1] * im[2];
+  cmflds->n_cells = n_patches * cmflds->n_cells_per_patch;
+
+  ierr = cudaMalloc((void **) &cmflds->d_flds,
+		    n_fields * cmflds->n_cells * sizeof(*cmflds->d_flds)); cudaCheck(ierr);
+
+  cmflds->d_flds_by_patch = new fields_cuda_real_t *[cmflds->n_patches];
+  for (int p = 0; p < cmflds->n_patches; p++) {
+    cmflds->d_flds_by_patch[p] = cmflds->d_flds + p * cmflds->n_fields * cmflds->n_cells_per_patch;
+  }
+}
+
+// ----------------------------------------------------------------------
+// cuda_mfields_dtor
+
+void
+cuda_mfields_dtor(struct cuda_mfields *cmflds)
+{
+  cudaError_t ierr;
+
+  ierr = cudaFree(cmflds->d_flds); cudaCheck(ierr);
+  
+  delete[] cmflds->d_flds_by_patch;
 }
 
--- src/libpsc/cuda/cuda_transfer.cu
+++ src/libpsc/cuda/cuda_transfer.cu
@@ -98,43 +98,6 @@ cuda_mfields_bnd_dtor(struct cuda_mfields_bnd *cbnd)
 
 // ======================================================================
 
-// ----------------------------------------------------------------------
-// cuda_mfields_ctor
-
-void
-cuda_mfields_ctor(struct cuda_mfields *cmflds, int ib[3], int im[3],
-		  int n_fields, int n_patches)
-{
-  cmflds->n_patches = n_patches;
-  cmflds->n_fields = n_fields;
-  
-  for (int d = 0; d < 3; d++) {
-    cmflds->im[d] = im[d];
-    cmflds->ib[d] = ib[d];
-  }
-
-  cmflds->n_cells_per_patch = im[0] * im[1] * im[2];
-  cmflds->n_cells = n_patches * cmflds->n_cells_per_patch;
-
-  check(cudaMalloc((void **) &cmflds->d_flds,
-		   n_fields * cmflds->n_cells * sizeof(*cmflds->d_flds)));
-
-  cmflds->d_flds_by_patch = new fields_cuda_real_t *[cmflds->n_patches];
-  for (int p = 0; p < cmflds->n_patches; p++) {
-    cmflds->d_flds_by_patch[p] = cmflds->d_flds + p * cmflds->n_fields * cmflds->n_cells_per_patch;
-  }
-}
-
-// ----------------------------------------------------------------------
-// cuda_mfields_dtor
-
-void
-cuda_mfields_dtor(struct cuda_mfields *cmflds)
-{
-  check(cudaFree(cmflds->d_flds));
-  delete[] cmflds->d_flds_by_patch;
-}
-
 EXTERN_C void
 __fields_cuda_to_device(struct psc_mfields *mflds, int p, real *h_flds, int mb, int me)
 {
