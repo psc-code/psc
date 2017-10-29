91d65f4ce2d3da70b0d494660576bce496cb9bc0
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-10-28 22:36:01 -0400

    cuda: get rid of constants.c

--- src/libpsc/cuda/cuda_exchange_particles.cu
+++ src/libpsc/cuda/cuda_exchange_particles.cu
@@ -8,69 +8,6 @@
 
 #define THREADS_PER_BLOCK 256
 
-#if 0
-#define PFX(x) xchg_##x
-#include "constants.c"
-
-// FIXME const mem for dims?
-// FIXME probably should do our own loop rather than use blockIdx
-
-__global__ static void
-exchange_particles(int n_part, particles_cuda_dev_t h_dev,
-		   int ldimsx, int ldimsy, int ldimsz)
-{
-  int ldims[3] = { ldimsx, ldimsy, ldimsz };
-  int xm[3];
-
-  for (int d = 0; d < 3; d++) {
-    xm[d] = ldims[d] / d_consts.dxi[d];
-  }
-
-  int i = threadIdx.x + THREADS_PER_BLOCK * blockIdx.x;
-  if (i < n_part) {
-    particle_cuda_real_t xi[3] = {
-      h_dev.xi4[i].x * d_consts.dxi[0],
-      h_dev.xi4[i].y * d_consts.dxi[1],
-      h_dev.xi4[i].z * d_consts.dxi[2] };
-    int pos[3];
-    for (int d = 0; d < 3; d++) {
-      pos[d] = __float2int_rd(xi[d]);
-    }
-    if (pos[1] < 0) {
-      h_dev.xi4[i].y += xm[1];
-      if (h_dev.xi4[i].y >= xm[1])
-	h_dev.xi4[i].y = 0.f;
-    }
-    if (pos[2] < 0) {
-      h_dev.xi4[i].z += xm[2];
-      if (h_dev.xi4[i].z >= xm[2])
-	h_dev.xi4[i].z = 0.f;
-    }
-    if (pos[1] >= ldims[1]) {
-      h_dev.xi4[i].y -= xm[1];
-    }
-    if (pos[2] >= ldims[2]) {
-      h_dev.xi4[i].z -= xm[2];
-    }
-  }
-}
-
-EXTERN_C void
-cuda_exchange_particles(int p, struct psc_particles *prts)
-{
-  struct psc_particles_cuda *cuda = psc_particles_cuda(prts);
-  struct psc_patch *patch = &ppsc->patch[p];
-
-  xchg_set_constants(prts, NULL);
-
-  int dimBlock[2] = { THREADS_PER_BLOCK, 1 };
-  int dimGrid[2]  = { (prts->n_part + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK, 1 };
-  RUN_KERNEL(dimGrid, dimBlock,
-	     exchange_particles, (prts->n_part, *cuda->h_dev,
-				  patch->ldims[0], patch->ldims[1], patch->ldims[2]));
-}
-#endif
-
 // ----------------------------------------------------------------------
 // cuda_mprts_find_block_indices_2_total
 //
