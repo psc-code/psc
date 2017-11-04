d5fc95426b06838b8ae45620d4d169edd225e370
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-04 19:33:51 -0400

    push_part: 1vbec_single do own dispatch

--- src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_single.c
+++ src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_single.c
@@ -15,13 +15,28 @@ void psc_push_particles_1vbec_single_stagger_mprts_yz(struct psc_push_particles 
 						      struct psc_mparticles *mprts,
 						      struct psc_mfields *mflds);
 
+static void
+psc_push_particles_1vbec_single_push_mprts(struct psc_push_particles *push,
+					   struct psc_mparticles *mprts,
+					   struct psc_mfields *mflds)
+{
+  int *gdims = ppsc->domain.gdims;
+
+  if (gdims[0] > 1 && gdims[1] > 1 && gdims[2] > 1) {
+    psc_push_particles_1vbec_single_push_mprts_xyz(push, mprts, mflds);
+  } else if (gdims[0] == 1 && gdims[1] > 1 && gdims[2] > 1) {
+    psc_push_particles_1vbec_single_push_mprts_yz(push, mprts, mflds);
+  } else {
+    assert(0);
+  }
+}
+
 // ======================================================================
 // psc_push_particles: subclass "1vbec_single"
 
 struct psc_push_particles_ops psc_push_particles_1vbec_single_ops = {
   .name                  = "1vbec_single",
-  .push_mprts_yz         = psc_push_particles_1vbec_single_push_mprts_yz,
-  .push_mprts_xyz        = psc_push_particles_1vbec_single_push_mprts_xyz,
+  .push_mprts            = psc_push_particles_1vbec_single_push_mprts,
   .stagger_mprts_yz      = psc_push_particles_1vbec_single_stagger_mprts_yz,
   .particles_type        = PARTICLE_TYPE,
   .fields_type           = FIELDS_TYPE,
