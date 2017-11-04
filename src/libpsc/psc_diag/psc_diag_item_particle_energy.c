cf7e930de171195cad08dc400ed0482cff599350
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-04 01:56:39 -0400

    diag_item: "energy" now based on "double" particles

--- src/libpsc/psc_diag/psc_diag_item_particle_energy.c
+++ src/libpsc/psc_diag/psc_diag_item_particle_energy.c
@@ -1,6 +1,6 @@
 
 #include "psc_diag_item_private.h"
-#include "psc_particles_as_c.h"
+#include "psc_particles_as_double.h"
 
 #include <math.h>
 
@@ -13,13 +13,13 @@ do_particle_energy(struct psc *psc, struct psc_mparticles *mprts, int p, double 
   struct psc_patch *patch = &psc->patch[p];
   double fac = patch->dx[0] * patch->dx[1] * patch->dx[2];
   for (int n = 0; n < particle_range_size(prts); n++) {
-    particle_c_t *part = particle_iter_at(prts.begin, n);
+    particle_t *part = particle_iter_at(prts.begin, n);
       
     double gamma = sqrt(1.f + sqr(part->pxi) + sqr(part->pyi) + sqr(part->pzi));
-    double Ekin = (gamma - 1.) * part->mni * part->wni * fnqs;
-    if (part->qni < 0.) {
+    double Ekin = (gamma - 1.) * particle_mni(part) * particle_wni(part) * fnqs;
+    if (particle_qni(part) < 0.) {
       result[0] += Ekin * fac;
-    } else if (part->qni > 0.) {
+    } else if (particle_qni(part) > 0.) {
       result[1] += Ekin * fac;
     } else {
       assert(0);
@@ -31,7 +31,7 @@ static void
 psc_diag_item_particle_energy_run(struct psc_diag_item *item,
 				  struct psc *psc, double *result)
 {
-  struct psc_mparticles *mprts = psc_mparticles_get_as(psc->particles, "c", 0);
+  struct psc_mparticles *mprts = psc_mparticles_get_as(psc->particles, PARTICLE_TYPE, 0);
 
   for (int p = 0; p < mprts->nr_patches; p++) {
     do_particle_energy(psc, mprts, p, result);
