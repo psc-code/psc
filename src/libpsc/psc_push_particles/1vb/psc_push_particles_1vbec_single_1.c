0be010945755c9c5e3d0a1df84117ed1ce74ae38
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-04 19:46:51 -0400

    push_part: use proper 1dvbec single _1 version
    since we already got one

--- src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_single_1.c
+++ src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_single_1.c
@@ -1,11 +1,10 @@
-#define BOUNDS_CHECK
 
 #include "psc_push_particles_private.h"
 
 #include "psc_particles_as_single.h"
 #include "psc_fields_as_single.h"
 
-#define DIM DIM_XYZ
+#define DIM DIM_1
 
 #include "../inc_defs.h"
 
@@ -13,10 +12,10 @@
 #define CURR_CACHE_DIM DIM_1
 #define ORDER ORDER_1ST
 #define IP_VARIANT IP_VARIANT_EC
-#define CALC_J CALC_J_1VB_SPLIT
+#define CALC_J CALC_J_1VB_VAR1
 
-#define psc_push_particles_push_mprts_xyz psc_push_particles_1vbec_single_push_mprts_1
-#define psc_push_particles_stagger_mprts_xyz psc_push_particles_1vbec_single_stagger_mprts_1
+#define psc_push_particles_push_mprts_1 psc_push_particles_1vbec_single_push_mprts_1
+#define psc_push_particles_stagger_mprts_1 psc_push_particles_1vbec_single_stagger_mprts_1
 
 #include "1vb.c"
 
