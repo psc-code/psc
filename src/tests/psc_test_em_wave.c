74dbade93f86f4be3aaa052c4724eb200b48a70b
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-08 22:33:40 -0500

    psc_test_em_wave: precalc k, B, E

--- src/tests/psc_test_em_wave.c
+++ src/tests/psc_test_em_wave.c
@@ -14,7 +14,12 @@
 struct psc_test_em_wave {
   // params
   double k[3];
+  double B[3];
   double tol;
+
+  // state
+  double om;
+  double E[3];
 };
 
 #define psc_test_em_wave(psc) mrc_to_subobj(psc, struct psc_test_em_wave)
@@ -22,6 +27,7 @@ struct psc_test_em_wave {
 #define VAR(x) (void *)offsetof(struct psc_test_em_wave, x)
 static struct param psc_test_em_wave_descr[] = {
   { "k"               , VAR(k)                 , PARAM_DOUBLE3(2., 0., 1.)  },
+  { "B"               , VAR(B)                 , PARAM_DOUBLE3(0., 1., 0.)  },
   {},
 };
 #undef VAR
@@ -63,27 +69,43 @@ psc_test_em_wave_create(struct psc *psc)
 }
 
 // ----------------------------------------------------------------------
+// psc_test_em_wave_setup
+
+static void
+psc_test_em_wave_setup(struct psc *psc)
+{
+  struct psc_test_em_wave *sub = psc_test_em_wave(psc);
+
+  double *k = sub->k, *B = sub->B, *E = sub->E;
+
+  assert(k[0]*B[0] + k[1]*B[1] + k[2]*B[2] == 0);
+  double k_norm = sqrt(sqr(k[0]) + sqr(k[1]) + sqr(k[2]));
+  sub->om = /* c * */ k_norm;
+  E[0] = 1./sub->om * (k[1] * B[2] - k[2] * B[1]);
+  E[1] = 1./sub->om * (k[2] * B[0] - k[0] * B[2]);
+  E[2] = 1./sub->om * (k[0] * B[1] - k[1] * B[0]);
+
+  psc_setup_super(psc);
+
+}
+
+// ----------------------------------------------------------------------
 // psc_test_em_wave_init_field
 
 static double
 psc_test_em_wave_init_field(struct psc *psc, double crd[3], int m)
 {
   struct psc_test_em_wave *sub = psc_test_em_wave(psc);
-  double *k = sub->k;
-  double k_norm_inv = -1. / sqrt(sqr(k[0]) + sqr(k[1]) + sqr(k[2]));
-  double b[3] = { 0., 1., 0. };
-  double e[3] = { k_norm_inv * (k[1] * b[2] - k[2] * b[1]),
-		  k_norm_inv * (k[2] * b[0] - k[0] * b[2]),
-		  k_norm_inv * (k[0] * b[1] - k[1] * b[0]) };
+  double *k = sub->k, *B = sub->B, *E = sub->E;
   double x = crd[0], y = crd[1], z = crd[2];
 
   switch (m) {
-  case EX: return e[0] * sin(k[0]*x + k[1]*y + k[2]*z);
-  case EY: return e[1] * sin(k[0]*x + k[1]*y + k[2]*z);
-  case EZ: return e[2] * sin(k[0]*x + k[1]*y + k[2]*z);
-  case HX: return b[0] * sin(k[0]*x + k[1]*y + k[2]*z);
-  case HY: return b[1] * sin(k[0]*x + k[1]*y + k[2]*z);
-  case HZ: return b[2] * sin(k[0]*x + k[1]*y + k[2]*z);
+  case EX: return E[0] * sin(k[0]*x + k[1]*y + k[2]*z);
+  case EY: return E[1] * sin(k[0]*x + k[1]*y + k[2]*z);
+  case EZ: return E[2] * sin(k[0]*x + k[1]*y + k[2]*z);
+  case HX: return B[0] * sin(k[0]*x + k[1]*y + k[2]*z);
+  case HY: return B[1] * sin(k[0]*x + k[1]*y + k[2]*z);
+  case HZ: return B[2] * sin(k[0]*x + k[1]*y + k[2]*z);
   default: return 0.;
   }
 }
@@ -100,6 +122,9 @@ psc_test_em_wave_step(struct psc *psc)
 
   struct psc_mfields *mflds = psc_mfields_get_as(psc->flds, FIELDS_TYPE, EX, EX + 6);
 
+  for (int p = 0; p < mflds->nr_patches; p++) {
+  }
+
   psc_mfields_put_as(mflds, psc->flds, 0, 0);
   
   psc_push_fields_push_H(psc->push_fields, psc->flds, .5);
@@ -115,6 +140,7 @@ struct psc_ops psc_test_em_wave_ops = {
   .size             = sizeof(struct psc_test_em_wave),
   .param_descr      = psc_test_em_wave_descr,
   .create           = psc_test_em_wave_create,
+  .setup            = psc_test_em_wave_setup,
   .init_field       = psc_test_em_wave_init_field,
   .step             = psc_test_em_wave_step,
 };
