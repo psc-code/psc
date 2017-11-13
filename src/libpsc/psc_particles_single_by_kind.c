1b55c502ce0eeb60bcae85471301ae9d1dd5618a
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-13 10:25:22 -0500

    vpic: get rid of psc_particles_as_single_by_kind.h

--- src/libpsc/psc_particles_single_by_kind.c
+++ src/libpsc/psc_particles_single_by_kind.c
@@ -1,7 +1,11 @@
 
 #include "psc.h"
-#include "psc_particles_as_single_by_kind.h"
-#include "psc_particles_inc.h"
+#include "psc_particles_single_by_kind.h"
+
+#define PARTICLE_TYPE "single_by_kind"
+#define PFX(x) psc_mparticles_single_by_kind_ ## x
+#define psc_mparticles_sub psc_mparticles_single_by_kind
+#define PARTICLE_BUF(x) psc_particle_single_by_kind_buf_ ## x
 
 // ======================================================================
 // psc_mparticles: subclass "single_by_kind"
