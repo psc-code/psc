fffd36a52aa0fcef2a8c664ea9a38c5c75c5b07a
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-13 21:31:47 -0500

    vpic: mv mparticles implementation into header
    after all it's templated now...

--- src/libpsc/bk_mparticles.h
+++ src/libpsc/bk_mparticles.h
@@ -15,13 +15,63 @@ struct mparticles {
   int n_patches;
   std::vector<particle_buf_type> buf;
 
-  mparticles(int _n_patches);
+  // ----------------------------------------------------------------------
+  // mparticles::ctor
 
-  void reserve_all(const int n_prts_by_patch[]);
-  void resize_all(const int n_prts_by_patch[]);
-  void size_all(int n_prts_by_patch[]) const;
-  int n_prts() const;
-  particle_type& at(int p, int n);
+  mparticles(int _n_patches)
+    : n_patches(_n_patches), buf(_n_patches)
+  {
+  }
+
+  // ----------------------------------------------------------------------
+  // mparticles::reserve_all
+
+  void reserve_all(const int n_prts_by_patch[])
+  {
+    for (int p = 0; p < n_patches; p++) {
+      buf[p].reserve(n_prts_by_patch[p]);
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  // mparticles::resize_all
+  
+  void resize_all(const int n_prts_by_patch[])
+  {
+    for (int p = 0; p < n_patches; p++) {
+      buf[p].resize(n_prts_by_patch[p]);
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  // mparticles::size_all
+  
+  void size_all(int n_prts_by_patch[]) const
+  {
+    for (int p = 0; p < n_patches; p++) {
+      n_prts_by_patch[p] = buf[p].size();
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  // mparticles::n_prts
+  
+  int n_prts() const
+  {
+    int n_prts = 0;
+    for (int p = 0; p < n_patches; p++) {
+      n_prts += buf[p].size();
+    }
+    return n_prts;
+  }
+
+  // ----------------------------------------------------------------------
+  // mparticles::at
+  
+  particle_type& at(int p, int n)
+  {
+    return buf[p][n];
+  }
 };
 
 #endif
