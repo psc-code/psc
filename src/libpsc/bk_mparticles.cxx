e8767ab9953e51e45998119a8b6f37eca9537756
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-13 19:17:27 -0500

    vpic: c++ify particle_buf_t

--- src/libpsc/bk_mparticles.cxx
+++ src/libpsc/bk_mparticles.cxx
@@ -5,12 +5,43 @@
 
 #define PARTICLE_BUF(x) psc_particle_single_by_kind_buf_ ## x
 
+struct particle_buf_t {
+  typedef particle_single_by_kind_t particle_t;
+
+  particle_buf_t() {
+    PARTICLE_BUF(ctor)(&z);
+  }
+
+  ~particle_buf_t() {
+    PARTICLE_BUF(dtor)(&z);
+  }
+
+  void reserve(int n) {
+    PARTICLE_BUF(reserve)(&z, n);
+  }
+  
+  void resize(int n) {
+    PARTICLE_BUF(resize)(&z, n);
+  }
+  
+  int size() {
+    return PARTICLE_BUF(size)(&z);
+  }
+
+  particle_t& operator[](int n) {
+    return *PARTICLE_BUF(at_ptr)(&z, n);
+  }
+
+  psc_particle_single_by_kind_buf_t z;
+
+};
+
 // ======================================================================
 // bk_mparticles
 
 struct bk_mparticles {
   typedef particle_single_by_kind_t particle_t;
-  typedef psc_particle_single_by_kind_buf_t particle_buf_t;
+  //typedef psc_particle_single_by_kind_buf_t particle_buf_t;
 
   int n_patches;
   std::vector<particle_buf_t> buf;
@@ -38,11 +69,7 @@ void bk_mparticles_destroy(struct bk_mparticles *bkmprts)
 void bk_mparticles_ctor(struct bk_mparticles *bkmprts, int n_patches)
 {
   bkmprts->n_patches = n_patches;
-
   bkmprts->buf.resize(n_patches);
-  for (int p = 0; p < n_patches; p++) {
-    PARTICLE_BUF(ctor)(&bkmprts->buf[p]);
-  }
 }
 
 // ----------------------------------------------------------------------
@@ -50,9 +77,6 @@ void bk_mparticles_ctor(struct bk_mparticles *bkmprts, int n_patches)
 
 void bk_mparticles_dtor(struct bk_mparticles *bkmprts)
 {
-  for (int p = 0; p < bkmprts->n_patches; p++) {
-    PARTICLE_BUF(dtor)(&bkmprts->buf[p]);
-  }
 }
 
 // ----------------------------------------------------------------------
@@ -61,7 +85,7 @@ void bk_mparticles_dtor(struct bk_mparticles *bkmprts)
 void bk_mparticles_reserve_all(struct bk_mparticles *bkmprts, int n_prts_by_patch[])
 {
   for (int p = 0; p < bkmprts->n_patches; p++) {
-    PARTICLE_BUF(reserve)(&bkmprts->buf[p], n_prts_by_patch[p]);
+    bkmprts->buf[p].reserve(n_prts_by_patch[p]);
   }
 }
 
@@ -71,7 +95,7 @@ void bk_mparticles_reserve_all(struct bk_mparticles *bkmprts, int n_prts_by_patc
 void bk_mparticles_resize_all(struct bk_mparticles *bkmprts, int n_prts_by_patch[])
 {
   for (int p = 0; p < bkmprts->n_patches; p++) {
-    PARTICLE_BUF(resize)(&bkmprts->buf[p], n_prts_by_patch[p]);
+    bkmprts->buf[p].resize(n_prts_by_patch[p]);
   }
 }
 
@@ -81,7 +105,7 @@ void bk_mparticles_resize_all(struct bk_mparticles *bkmprts, int n_prts_by_patch
 void bk_mparticles_get_size_all(struct bk_mparticles *bkmprts, int n_prts_by_patch[])
 {
   for (int p = 0; p < bkmprts->n_patches; p++) {
-    n_prts_by_patch[p] = PARTICLE_BUF(size)(&bkmprts->buf[p]);
+    n_prts_by_patch[p] = bkmprts->buf[p].size();
   }
 }
 
@@ -92,7 +116,7 @@ int bk_mparticles_get_n_prts(struct bk_mparticles *bkmprts)
 {
   int n_prts = 0;
   for (int p = 0; p < bkmprts->n_patches; p++) {
-    n_prts += PARTICLE_BUF(size)(&bkmprts->buf[p]);
+    n_prts += bkmprts->buf[p].size();
   }
   return n_prts;
 }
@@ -103,7 +127,7 @@ int bk_mparticles_get_n_prts(struct bk_mparticles *bkmprts)
 particle_single_by_kind_t *
 bk_mparticles_at_ptr(struct bk_mparticles *bkmprts, int p, int n)
 {
-  return PARTICLE_BUF(at_ptr)(&bkmprts->buf[p], n);
+  return &bkmprts->buf[p][n];
 }
 
 
