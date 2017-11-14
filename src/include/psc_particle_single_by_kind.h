49ade531e165cd942ecf14572cac295ca913d48c
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-13 21:45:43 -0500

    vpic: no macro mess needed for "single_by_kind"

--- src/include/psc_particle_single_by_kind.h
+++ src/include/psc_particle_single_by_kind.h
@@ -2,8 +2,18 @@
 #ifndef PSC_PARTICLE_SINGLE_BY_KIND_H
 #define PSC_PARTICLE_SINGLE_BY_KIND_H
 
-#define PTYPE PTYPE_SINGLE_BY_KIND
-#include "psc_particle_common.h"
-#undef PTYPE
+typedef float particle_single_by_kind_real_t;
+typedef struct {
+  particle_single_by_kind_real_t dx[3];
+  int i;
+  particle_single_by_kind_real_t ux[3];
+  particle_single_by_kind_real_t w;
+  int kind;
+} particle_single_by_kind_t;
+
+#define psc_mparticles_single_by_kind(mprts)({				\
+      assert((struct psc_mparticles_ops *) mprts->obj.ops == &psc_mparticles_single_by_kind_ops); \
+      mrc_to_subobj(mprts, struct psc_mparticles_single_by_kind);	\
+})
 
 #endif
