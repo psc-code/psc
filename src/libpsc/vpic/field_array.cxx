068f56b382900fb7b6017b6a1178ac3e08a97d7f
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-18 12:10:33 -0500

    vpic: advance_b in more natural code
    but how much slower?

--- src/libpsc/vpic/field_array.cxx
+++ src/libpsc/vpic/field_array.cxx
@@ -13,46 +13,25 @@
                                                                 \
   const float px   = (nx>1) ? frac*g->cvac*g->dt*g->rdx : 0;    \
   const float py   = (ny>1) ? frac*g->cvac*g->dt*g->rdy : 0;    \
-  const float pz   = (nz>1) ? frac*g->cvac*g->dt*g->rdz : 0;    \
-                                                                \
-  field_t * ALIGNED(16) f0;                                     \
-  field_t * ALIGNED(16) fx, * ALIGNED(16) fy, * ALIGNED(16) fz; \
-  int x, y, z
+  const float pz   = (nz>1) ? frac*g->cvac*g->dt*g->rdz : 0
 
 #define f(x,y,z) f[ VOXEL(x,y,z, nx,ny,nz) ]
 
-#define INIT_STENCIL()  \
-  f0 = &f(x,  y,  z  ); \
-  fx = &f(x+1,y,  z  ); \
-  fy = &f(x,  y+1,z  ); \
-  fz = &f(x,  y,  z+1)
-
-#define NEXT_STENCIL()                \
-  f0++; fx++; fy++; fz++; x++;        \
-  if( x>nx ) {                        \
-    /**/       y++;            x = 1; \
-    if( y>ny ) z++; if( y>ny ) y = 1; \
-    INIT_STENCIL();                   \
-  }
- 
-#define UPDATE_CBX() f0->cbx -= ( py*( fy->ez-f0->ez ) - pz*( fz->ey-f0->ey ) )
-#define UPDATE_CBY() f0->cby -= ( pz*( fz->ex-f0->ex ) - px*( fx->ez-f0->ez ) )
-#define UPDATE_CBZ() f0->cbz -= ( px*( fx->ey-f0->ey ) - py*( fy->ex-f0->ex ) )
+#define UPDATE_CBX() f(x,y,z).cbx -= (py*(f(x,y+1,z).ez - f(x,y,z).ez) - pz*(f(x,y,z+1).ey - f(x,y,z).ey))
+#define UPDATE_CBY() f(x,y,z).cby -= (pz*(f(x,y,z+1).ex - f(x,y,z).ex) - px*(f(x+1,y,z).ez - f(x,y,z).ez))
+#define UPDATE_CBZ() f(x,y,z).cbz -= (px*(f(x+1,y,z).ey - f(x,y,z).ey) - py*(f(x,y+1,z).ex - f(x,y,z).ex))
 
 static void
-advance_b_pipeline(field_array_t *fa,
-		   float frac,
-                   int pipeline_rank,
-                   int n_pipeline) {
+advance_b_interior(field_array_t *fa, float frac)
+{
   DECLARE_STENCIL();
 
-  int n_voxel = nx * ny * nz;
-  x = 1, y = 1, z = 1;
-
-  INIT_STENCIL();
-  for( ; n_voxel; n_voxel-- ) {
-    UPDATE_CBX(); UPDATE_CBY(); UPDATE_CBZ();
-    NEXT_STENCIL();
+  for (int z = 1; z <= nz; z++) {
+    for (int y = 1; y <= ny; y++) {
+      for (int x = 1; x <= nx; x++) {
+	UPDATE_CBX(); UPDATE_CBY(); UPDATE_CBZ();
+      }
+    }
   }
 }
 
@@ -60,45 +39,32 @@ advance_b_pipeline(field_array_t *fa,
 static void
 advance_b_(field_array_t* fa, float frac)
 {
-  advance_b_pipeline(fa, frac, 0, 1);
-
-  // While the pipelines are busy, do surface fields
+  advance_b_interior(fa, frac);
 
   DECLARE_STENCIL();
+  int x, y, z;
   
   // Do left over bx
+  x = nx+1;
   for( z=1; z<=nz; z++ ) {
     for( y=1; y<=ny; y++ ) {
-      f0 = &f(nx+1,y,  z);
-      fy = &f(nx+1,y+1,z);
-      fz = &f(nx+1,y,  z+1);
       UPDATE_CBX();
     }
   }
 
   // Do left over by
+  y = ny+1;
   for( z=1; z<=nz; z++ ) {
-    f0 = &f(1,ny+1,z);
-    fx = &f(2,ny+1,z);
-    fz = &f(1,ny+1,z+1);
     for( x=1; x<=nx; x++ ) {
       UPDATE_CBY();
-      f0++;
-      fx++;
-      fz++;
     }
   }
 
   // Do left over bz
+  z = nz+1;
   for( y=1; y<=ny; y++ ) {
-    f0 = &f(1,y,  nz+1);
-    fx = &f(2,y,  nz+1);
-    fy = &f(1,y+1,nz+1);
     for( x=1; x<=nx; x++ ) {
       UPDATE_CBZ();
-      f0++;
-      fx++;
-      fy++;
     }
   }
 
