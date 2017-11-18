cb8480a5eb271f3cd5d27abf00777034539f4aad
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-17 21:06:21 -0500

    vpic: rng.h, too

--- src/libpsc/vpic/simulation.h
+++ src/libpsc/vpic/simulation.h
@@ -5,80 +5,11 @@
 #include "vpic_iface.h"
 
 #include "vpic_init.h" // FIXME, bad name for _diag
-#include "util/rng/rng.h"
 
 #include "field_array.h"
+#include "rng.h"
 
 #include <psc.h> // FIXME, only need the BND_* constants
-
-// ======================================================================
-// class Rng
-
-#define IN_rng
-#include "util/rng/rng_private.h"
-
-struct Rng : rng {
-  double drand();
-  double drandn();
-  double uniform(double lo, double hi);
-  double normal(double mu, double sigma);
-};
-
-// ----------------------------------------------------------------------
-// Rng implementation
-
-inline double Rng::drand()
-{
-  return ::drand(this);
-}
-
-inline double Rng::drandn()
-{
-  return ::drandn(this);
-}
-
-inline double Rng::uniform(double lo, double hi)
-{
-  double dx = drand();
-  return lo*(1.-dx) + hi*dx;
-}
-
-inline double Rng::normal(double mu, double sigma)
-{
-  return mu + sigma*drandn();
-}
-
-// ======================================================================
-// class RngPool
-
-struct RngPool {
-  RngPool();
-  
-  void seed(int base, int which);
-  Rng *operator[](int n);
-
-  rng_pool *rng_pool_;
-};
-
-// ----------------------------------------------------------------------
-// RngPool implementation
-
-inline RngPool::RngPool()
-{
-  //rng_pool_ = simulation->entropy;
-  int new_rng = 2;
-  rng_pool_ = new_rng_pool(new_rng, 0, 0);
-}
-
-inline void RngPool::seed(int base, int which)
-{
-  seed_rng_pool(rng_pool_, base, which);
-}
-
-inline Rng* RngPool::operator[](int n)
-{
-  return reinterpret_cast<Rng *>(rng_pool_->rng[n]);
-}
 
 // ======================================================================
 // class Simulation
