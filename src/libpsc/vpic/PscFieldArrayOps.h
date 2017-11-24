d536f5daf7b5029d29b226d6e77636c334ad7170
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-24 09:30:18 -0500

    vpic: dispose of v4 compute_div_b_err
    It's slower in the first place (memory layout is not friendly to
    SSE4), and let's keep it simple for now.

--- src/libpsc/vpic/PscFieldArrayOps.h
+++ src/libpsc/vpic/PscFieldArrayOps.h
@@ -222,7 +222,7 @@ struct PscFieldArrayOps {
   // ----------------------------------------------------------------------
   // compute_div_b_err
   
-  static void compute_div_b_err_pipeline(FieldArray& fa)
+  static void compute_div_b_err(FieldArray& fa)
   {
     Field3D<FieldArray> F(fa);
     const grid_t *              g = fa.g;
@@ -240,103 +240,6 @@ struct PscFieldArrayOps {
 	}
       }
     }
-  }
-
-#define f(x,y,z) f[ VOXEL(x,y,z, nx,ny,nz) ]
-
-  //#define HAS_V4_PIPELINE
-#if defined(V4_ACCELERATION) && defined(HAS_V4_PIPELINE)
-
-  static void compute_div_b_err_pipeline_v4(FieldArray &fa)
-  {
-    using namespace v4;
-
-    field_t      * ALIGNED(128) f = fa.f;
-    const grid_t *              g = fa.g;
-
-    field_t * ALIGNED(16) f0;
-    field_t * ALIGNED(16) fx, * ALIGNED(16) fy, * ALIGNED(16) fz;
-    int x, y, z, n_voxel;
-
-    const int nx = g->nx;
-    const int ny = g->ny;
-    const int nz = g->nz;
-
-    const float px = (nx>1) ? g->rdx : 0;
-    const float py = (ny>1) ? g->rdy : 0;
-    const float pz = (nz>1) ? g->rdz : 0;
-
-    const v4float vpx(px);
-    const v4float vpy(py);
-    const v4float vpz(pz);
-
-    v4float f0_cbx, f0_cby, f0_cbz; // Voxel quad magnetic fields
-    v4float f0_div_b_err;           // Voxel quad div b errs
-    v4float fx_cbx;                 // Voxel quad +x neighbor x magnetic fields
-    v4float fy_cby;                 // Voxel quad +y neighbor y magnetic fields
-    v4float fz_cbz;                 // Voxel quad +z neighbor z magnetic fields
-
-    field_t * ALIGNED(16) f00, * ALIGNED(16) f01, * ALIGNED(16) f02, * ALIGNED(16) f03; // Voxel quad
-    field_t * ALIGNED(16) fx0, * ALIGNED(16) fx1, * ALIGNED(16) fx2, * ALIGNED(16) fx3; // Voxel quad +x neighbors
-    field_t * ALIGNED(16) fy0, * ALIGNED(16) fy1, * ALIGNED(16) fy2, * ALIGNED(16) fy3; // Voxel quad +x neighbors
-    field_t * ALIGNED(16) fz0, * ALIGNED(16) fz1, * ALIGNED(16) fz2, * ALIGNED(16) fz3; // Voxel quad +x neighbors
-
-    // Process the voxels assigned to this pipeline 
-  
-    DISTRIBUTE_VOXELS(1,nx, 1,ny, 1,nz, 16,
-		      0, 1,
-		      x, y, z, n_voxel );
-
-    // Process bulk of voxels 4 at a time
-
-# define LOAD_STENCIL()				\
-    f0 = &f(x,  y,  z  );			\
-    fx = &f(x+1,y,  z  );			\
-    fy = &f(x,  y+1,z  );			\
-    fz = &f(x,  y,  z+1)
-
-# define NEXT_STENCIL(n)			\
-    f0##n = f0++;				\
-    fx##n = fx++;				\
-    fy##n = fy++;				\
-    fz##n = fz++;				\
-    x++;					\
-    if( x>nx ) {				\
-      x=1, y++;					\
-      if( y>ny ) y=1, z++;			\
-      LOAD_STENCIL();				\
-    }
-
-    LOAD_STENCIL();
-
-    for( ; n_voxel>3; n_voxel-=4 ) {
-      NEXT_STENCIL(0); NEXT_STENCIL(1); NEXT_STENCIL(2); NEXT_STENCIL(3);
-
-      load_4x3_tr( &f00->cbx, &f01->cbx, &f02->cbx, &f03->cbx, f0_cbx, f0_cby, f0_cbz );
-
-      fx_cbx = v4float( fx0->cbx, fx1->cbx, fx2->cbx, fx3->cbx );
-      fy_cby = v4float( fy0->cby, fy1->cby, fy2->cby, fy3->cby );
-      fz_cbz = v4float( fz0->cbz, fz1->cbz, fz2->cbz, fz3->cbz );
-
-      f0_div_b_err = fma( vpx,fx_cbx-f0_cbx, fma( vpy,fy_cby-f0_cby, vpz*(fz_cbz-f0_cbz) ) );
-
-      store_4x1_tr( f0_div_b_err, &f00->div_b_err, &f01->div_b_err, &f02->div_b_err, &f03->div_b_err );
-    }
-
-# undef NEXT_STENCIL
-# undef LOAD_STENCIL
-
-  }
-
-#endif
-
-  void compute_div_b_err(FieldArray &fa) {
-#if defined(V4_ACCELERATION) && defined(HAS_V4_PIPELINE)
-    assert(fa.g->nx * fa.g->ny * fa.g->nz % 16 == 0);
-    compute_div_b_err_pipeline_v4(fa);
-#else
-    compute_div_b_err_pipeline(fa);
-#endif
   }
 
 };
