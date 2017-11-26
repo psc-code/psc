884266262d36bcddc1a395f08d727afd17cf0cc5
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-11-26 15:54:01 -0500

    vpic: accumulate_rho_p() -> PSC

--- src/libpsc/vpic/PscParticlesOps.h
+++ src/libpsc/vpic/PscParticlesOps.h
@@ -1183,11 +1183,67 @@ struct PscParticlesOps {
 
   // ----------------------------------------------------------------------
   // accumulate_rho_p
-  
+
+  void accumulate_rho_p(FieldArray& fa, SpeciesIter sp)
+  {
+    const particle_t * RESTRICT ALIGNED(128) p = sp->p;
+
+    const float q_8V = sp->q*sp->g->r8V;
+    const int np = sp->np;
+    const int sy = sp->g->sy;
+    const int sz = sp->g->sz;
+
+    float w0, w1, w2, w3, w4, w5, w6, w7, dz;
+
+    int n, v;
+
+    // Load the grid data
+
+    for( n=0; n<np; n++ ) {
+
+      // After detailed experiments and studying of assembly dumps, it was
+      // determined that if the platform does not support efficient 4-vector
+      // SIMD memory gather/scatter operations, the savings from using
+      // "trilinear" are slightly outweighed by the overhead of the
+      // gather/scatters.
+ 
+      // Load the particle data
+
+      w0 = p[n].dx;
+      w1 = p[n].dy;
+      dz = p[n].dz;
+      v  = p[n].i;
+      w7 = p[n].w*q_8V;
+
+      // Compute the trilinear weights
+      // Though the PPE should have hardware fma/fmaf support, it was
+      // measured to be more efficient _not_ to use it here.  (Maybe the
+      // compiler isn't actually generating the assembly for it.
+
+#   define FMA( x,y,z) ((z)+(x)*(y))
+#   define FNMS(x,y,z) ((z)-(x)*(y))
+      w6=FNMS(w0,w7,w7);                    // q(1-dx)
+      w7=FMA( w0,w7,w7);                    // q(1+dx)
+      w4=FNMS(w1,w6,w6); w5=FNMS(w1,w7,w7); // q(1-dx)(1-dy), q(1+dx)(1-dy)
+      w6=FMA( w1,w6,w6); w7=FMA( w1,w7,w7); // q(1-dx)(1+dy), q(1+dx)(1+dy)
+      w0=FNMS(dz,w4,w4); w1=FNMS(dz,w5,w5); w2=FNMS(dz,w6,w6); w3=FNMS(dz,w7,w7);
+      w4=FMA( dz,w4,w4); w5=FMA( dz,w5,w5); w6=FMA( dz,w6,w6); w7=FMA( dz,w7,w7);
+#   undef FNMS
+#   undef FMA
+
+      // Reduce the particle charge to rhof
+
+      fa[v      ].rhof += w0; fa[v      +1].rhof += w1;
+      fa[v   +sy].rhof += w2; fa[v   +sy+1].rhof += w3;
+      fa[v+sz   ].rhof += w4; fa[v+sz   +1].rhof += w5;
+      fa[v+sz+sy].rhof += w6; fa[v+sz+sy+1].rhof += w7;
+    }
+  }
+
   void accumulate_rho_p(Particles& vmprts, FieldArray &vmflds)
   {
     for (typename Particles::Iter sp = vmprts.begin(); sp != vmprts.end(); ++sp) {
-      TIC ::accumulate_rho_p(&vmflds, &*sp); TOC(accumulate_rho_p, 1);
+      TIC accumulate_rho_p(vmflds, sp); TOC(accumulate_rho_p, 1);
     }
   }
 
