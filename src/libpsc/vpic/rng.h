ed805ea13e2069223fe294fa05839321cade8b88
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-12-03 21:53:46 -0500

    vpic: rng inline methods

--- src/libpsc/vpic/rng.h
+++ src/libpsc/vpic/rng.h
@@ -9,66 +9,42 @@
 #include "util/rng/rng_private.h"
 
 struct Rng : rng {
-  double drand();
-  double drandn();
-  double uniform(double lo, double hi);
-  double normal(double mu, double sigma);
+  double drand() { return ::drand(this); }
+  double drandn() { return ::drandn(this); }
+
+  double uniform(double lo, double hi)
+  {
+    double dx = drand();
+    return lo * (1.-dx) + hi * dx;
+  }
+
+  double normal(double mu, double sigma)
+  {
+    return mu + sigma * drandn();
+  }
 };
-
-// ----------------------------------------------------------------------
-// Rng implementation
-
-inline double Rng::drand()
-{
-  return ::drand(this);
-}
-
-inline double Rng::drandn()
-{
-  return ::drandn(this);
-}
-
-inline double Rng::uniform(double lo, double hi)
-{
-  double dx = drand();
-  return lo*(1.-dx) + hi*dx;
-}
-
-inline double Rng::normal(double mu, double sigma)
-{
-  return mu + sigma*drandn();
-}
 
 // ======================================================================
 // class RngPool
 
 struct RngPool {
-  RngPool();
+  RngPool()
+  {
+    int new_rng = 2;
+    rng_pool_ = new_rng_pool(new_rng, 0, 0);
+  }
   
-  void seed(int base, int which);
-  Rng *operator[](int n);
+  void seed(int base, int which)
+  {
+    seed_rng_pool(rng_pool_, base, which);
+  }
+  
+  Rng *operator[](int n)
+  {
+    return reinterpret_cast<Rng *>(rng_pool_->rng[n]);
+  }
 
   rng_pool *rng_pool_;
 };
-
-// ----------------------------------------------------------------------
-// RngPool implementation
-
-inline RngPool::RngPool()
-{
-  //rng_pool_ = simulation->entropy;
-  int new_rng = 2;
-  rng_pool_ = new_rng_pool(new_rng, 0, 0);
-}
-
-inline void RngPool::seed(int base, int which)
-{
-  seed_rng_pool(rng_pool_, base, which);
-}
-
-inline Rng* RngPool::operator[](int n)
-{
-  return reinterpret_cast<Rng *>(rng_pool_->rng[n]);
-}
 
 #endif
