f65981f68ddce1b918b6153396b4a1a88d8d39c2
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-12-26 23:52:03 -0500

    vpic: incorporate ctor / dtor into PscMaterial

--- src/libpsc/vpic/PscMaterial.h
+++ src/libpsc/vpic/PscMaterial.h
@@ -7,15 +7,7 @@
 #include "material/material.h"
 
 #include <mrc_common.h>
-
 #include <cassert>
-
-static void material_ctor(material_t *m, const char *name,
-			  float epsx, float epsy, float epsz,
-			  float mux, float muy, float muz,
-			  float sigmax, float sigmay, float sigmaz,
-			  float zetax, float zetay, float zetaz);
-static void material_dtor(material_t *m);
 
 // ======================================================================
 // PscMaterial
@@ -25,16 +17,21 @@ struct PscMaterial : material_t
   PscMaterial(const char *name,
 	      float epsx, float epsy, float epsz,
 	      float mux, float muy, float muz,
-	       float sigmax, float sigmay, float sigmaz,
+	      float sigmax, float sigmay, float sigmaz,
 	      float zetax, float zetay, float zetaz)
   {
-    material_ctor(this, name, epsx, epsy, epsz, mux, muy, muz,
-		  sigmax, sigmay, sigmaz, zetax, zetay, zetaz);
+    int len = name ? strlen(name) : 0;
+    if (!len) LOG_ERROR("Cannot create a nameless material");
+    this->name = strdup(name);
+    this->epsx   = epsx,   this->epsy   = epsy,   this->epsz   = epsz;
+    this->mux    = mux,    this->muy    = muy,    this->muz    = muz;
+    this->sigmax = sigmax, this->sigmay = sigmay, this->sigmaz = sigmaz;
+    this->zetax  = zetax,  this->zetay  = zetay,  this->zetaz  = zetaz;
   }
   
   ~PscMaterial()
   {
-    material_dtor(this);
+    free(name);
   }
 };
 
@@ -77,30 +74,5 @@ struct PscMaterialList : public VpicListBase<PscMaterial>
     return m;
   }
 };
-
-// ======================================================================
-
-static void material_ctor(material_t *m, const char *name,
-	       float epsx, float epsy, float epsz,
-	       float mux, float muy, float muz,
-	       float sigmax, float sigmay, float sigmaz,
-	       float zetax, float zetay, float zetaz)
-{
-  // copy&paste from material.c, but as ctor not new
-  CLEAR(m, 1);
-
-  int len = name ? strlen(name) : 0;
-  if (!len) LOG_ERROR("Cannot create a nameless material");
-  m->name = strdup(name);
-  m->epsx   = epsx,   m->epsy   = epsy,   m->epsz   = epsz;
-  m->mux    = mux,    m->muy    = muy,    m->muz    = muz;
-  m->sigmax = sigmax, m->sigmay = sigmay, m->sigmaz = sigmaz;
-  m->zetax  = zetax,  m->zetay  = zetay,  m->zetaz  = zetaz;
-}
-
-static void material_dtor(material_t *m)
-{
-  FREE(m->name);
-}
 
 #endif
