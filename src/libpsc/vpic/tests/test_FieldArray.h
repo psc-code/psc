ca8b45ba1792e8eb42c62603cb0cfa1b4309f890
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-12-27 13:19:57 -0500

    vpic: use test_GridBase in test_FieldArray

--- src/libpsc/vpic/tests/test_FieldArray.h
+++ src/libpsc/vpic/tests/test_FieldArray.h
@@ -2,26 +2,11 @@
 #ifndef TEST_FIELD_ARRAY_H
 #define TEST_FIELD_ARRAY_H
 
+#include "test_GridBase.h"
+
 template<typename FieldArray>
-FieldArray* test_FieldArray_create()
+FieldArray* test_FieldArray_create(typename FieldArray::Grid *g)
 {
-  int gdims[3] = { 16, 32, 1 };
-  double xl[3] = { -1., -2., -4. };
-  double xh[3] = {  1. , 2. , 4. };
-  int np[3] = { 1, 1, 1 };
-  double dt = .05;
-  double cvac = 1.;
-  double eps0 = 1.;
-
-  double dx[3];
-  for (int d = 0; d < 3; d++) {
-    dx[d] = (xh[d] - xl[d]) / gdims[d];
-  }
-
-  auto *grid = FieldArray::Grid::create();
-  grid->setup(dx, dt, cvac, eps0);
-  grid->partition_periodic_box(xl, xh, gdims, np);
-  
   typename FieldArray::MaterialList material_list;
   material_list.append(material_list.create("vacuum",
 					    1., 1., 1.,
@@ -29,7 +14,7 @@ FieldArray* test_FieldArray_create()
 					    0., 0., 0.,
 					    0., 0., 0.));
 
-  return FieldArray::create(grid, material_list, 0.);
+  return FieldArray::create(g, material_list, 0.);
 }
 
 template<typename FieldArray>
@@ -69,9 +54,13 @@ void test_FieldArray_destroy(FieldArray* fa)
 template<typename FieldArray>
 void test_FieldArray()
 {
-  FieldArray *fa = test_FieldArray_create<FieldArray>();
+  auto *g = test_GridBase_create<typename FieldArray::Grid>();
+  
+  FieldArray *fa = test_FieldArray_create<FieldArray>(g);
   test_FieldArray_methods(*fa);
   test_FieldArray_destroy(fa);
+
+  test_GridBase_destroy(g);
 }
 
 #endif
