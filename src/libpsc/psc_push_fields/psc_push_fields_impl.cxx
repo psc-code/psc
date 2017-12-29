42ad9675fbb582a00924a26543ddcc3059c5695d
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-12-29 15:40:56 -0500

    push_fields: "c" and "single" now in a single file

--- src/libpsc/psc_push_fields/psc_push_fields_impl.cxx
+++ src/libpsc/psc_push_fields/psc_push_fields_impl.cxx
@@ -11,3 +11,12 @@ struct psc_push_fields_ops psc_push_fields_single_ops = {
   .push_mflds_E          = psc_push_fields_sub_push_mflds_E<fields_single_t>,
   .push_mflds_H          = psc_push_fields_sub_push_mflds_H<fields_single_t>,
 };
+
+// ======================================================================
+// psc_push_fields: subclass "c"
+
+struct psc_push_fields_ops psc_push_fields_c_ops = {
+  .name                  = "c",
+  .push_mflds_E          = psc_push_fields_sub_push_mflds_E<fields_c_t>,
+  .push_mflds_H          = psc_push_fields_sub_push_mflds_H<fields_c_t>,
+};
