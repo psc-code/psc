e48a3734579d9e47dc3157b943240bd0659f7bdb
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-12-29 00:00:44 -0500

    vpic: marder / diag_item "rho_nc" needs get_as / put_as

--- src/libpsc/psc_output_fields/psc_output_fields_item_moments_1st_nc.c
+++ src/libpsc/psc_output_fields/psc_output_fields_item_moments_1st_nc.c
@@ -7,17 +7,20 @@
 
 static void
 run_all(struct psc_output_fields_item *item, struct psc_mfields *mflds_base,
-	struct psc_mparticles *mprts_base, struct psc_mfields *mres,
+	struct psc_mparticles *mprts_base, struct psc_mfields *mres_base,
 	void (*do_run)(int p, fields_t flds, particle_range_t prts))
 {
   struct psc_mparticles *mprts = psc_mparticles_get_as(mprts_base, PARTICLE_TYPE, 0);
+  struct psc_mfields *mres = psc_mfields_get_as(mres_base, FIELDS_TYPE, 0, 0);
 
+  psc_mfields_view(mres);
   for (int p = 0; p < mprts->nr_patches; p++) {
     fields_t res = fields_t_mflds(mres, p);
     fields_t_zero_range(res, 0, mres->nr_fields);
     do_run(p, res, particle_range_mprts(mprts, p));
   }
 
+  psc_mfields_put_as(mres, mres_base, 0, mres->nr_fields);
   psc_mparticles_put_as(mprts, mprts_base, MP_DONT_COPY);
 }
 
