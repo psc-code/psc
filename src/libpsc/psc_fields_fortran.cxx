dc44bb0ab8bd36146bb61aeeb5dc655d76a9a54e
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-12-30 00:55:56 -0500

    _F3: psc_fields_{fortran, single}

--- src/libpsc/psc_fields_fortran.cxx
+++ src/libpsc/psc_fields_fortran.cxx
@@ -2,12 +2,16 @@
 #include "psc.h"
 #include "psc_fields_as_fortran.h"
 #include "psc_fields_c.h"
+#include "fields.hxx"
 
 #include <mrc_params.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "psc_fields_inc.h"
+
+using Fields = Fields3d<fields_t>;
+using FieldsC = Fields3d<fields_c_t>;
 
 // ======================================================================
 // convert to/from "c"
@@ -19,11 +23,13 @@ psc_mfields_fortran_copy_to_c(struct psc_mfields *mflds, struct psc_mfields *mfl
   for (int p = 0; p < mflds->nr_patches; p++) {
     fields_t flds = fields_t_mflds(mflds, p);
     fields_c_t flds_c = fields_c_t_mflds(mflds_c, p);
+    Fields F(flds);
+    FieldsC F_c(flds_c);
     for (int m = mb; m < me; m++) {
       for (int jz = flds_c.ib[2]; jz < flds_c.ib[2] + flds_c.im[2]; jz++) {
 	for (int jy = flds_c.ib[1]; jy < flds_c.ib[1] + flds_c.im[1]; jy++) {
 	  for (int jx = flds_c.ib[0]; jx < flds_c.ib[0] + flds_c.im[0]; jx++) {
-	    _F3_C(flds_c, m, jx,jy,jz) = _F3(flds, m, jx,jy,jz);
+	    F_c(m, jx,jy,jz) = F(m, jx,jy,jz);
 	  }
 	}
       }
@@ -38,11 +44,13 @@ psc_mfields_fortran_copy_from_c(struct psc_mfields *mflds, struct psc_mfields *m
   for (int p = 0; p < mflds->nr_patches; p++) {
     fields_t flds = fields_t_mflds(mflds, p);
     fields_c_t flds_c = fields_c_t_mflds(mflds_c, p);
+    Fields F(flds);
+    FieldsC F_c(flds_c);
     for (int m = mb; m < me; m++) {
       for (int jz = flds_c.ib[2]; jz < flds_c.ib[2] + flds_c.im[2]; jz++) {
 	for (int jy = flds_c.ib[1]; jy < flds_c.ib[1] + flds_c.im[1]; jy++) {
 	  for (int jx = flds_c.ib[0]; jx < flds_c.ib[0] + flds_c.im[0]; jx++) {
-	    _F3(flds, m, jx,jy,jz) = _F3_C(flds_c, m, jx,jy,jz);
+	    F(m, jx,jy,jz) = F_c(m, jx,jy,jz);
 	  }
 	}
       }
--- src/libpsc/psc_fields_single.cxx
+++ src/libpsc/psc_fields_single.cxx
@@ -2,6 +2,7 @@
 #include "psc.h"
 #include "psc_fields_as_single.h"
 #include "psc_fields_c.h"
+#include "fields.hxx"
 
 #include <mrc_params.h>
 #include <stdlib.h>
@@ -9,6 +10,9 @@
 #include <assert.h>
 
 #include "psc_fields_inc.h"
+
+using Fields = Fields3d<fields_t>;
+using FieldsC = Fields3d<fields_c_t>;
 
 // ======================================================================
 // convert to c
@@ -20,11 +24,13 @@ psc_mfields_single_copy_from_c(struct psc_mfields *mflds, struct psc_mfields *mf
   for (int p = 0; p < mflds->nr_patches; p++) {
     fields_t flds = fields_t_mflds(mflds, p);
     fields_c_t flds_c = fields_c_t_mflds(mflds_c, p);
+    Fields F(flds);
+    FieldsC F_c(flds_c);
     for (int m = mb; m < me; m++) {
       for (int jz = flds.ib[2]; jz < flds.ib[2] + flds.im[2]; jz++) {
 	for (int jy = flds.ib[1]; jy < flds.ib[1] + flds.im[1]; jy++) {
 	  for (int jx = flds.ib[0]; jx < flds.ib[0] + flds.im[0]; jx++) {
-	    _F3(flds, m, jx,jy,jz) = _F3_C(flds_c, m, jx,jy,jz);
+	    F(m, jx,jy,jz) = F_c(m, jx,jy,jz);
 	  }
 	}
       }
@@ -39,11 +45,13 @@ psc_mfields_single_copy_to_c(struct psc_mfields *mflds, struct psc_mfields *mfld
   for (int p = 0; p < mflds->nr_patches; p++) {
     fields_t flds = fields_t_mflds(mflds, p);
     fields_c_t flds_c = fields_c_t_mflds(mflds_c, p);
+    Fields F(flds);
+    FieldsC F_c(flds_c);
     for (int m = mb; m < me; m++) {
       for (int jz = flds.ib[2]; jz < flds.ib[2] + flds.im[2]; jz++) {
 	for (int jy = flds.ib[1]; jy < flds.ib[1] + flds.im[1]; jy++) {
 	  for (int jx = flds.ib[0]; jx < flds.ib[0] + flds.im[0]; jx++) {
-	    _F3_C(flds_c, m, jx,jy,jz) = _F3(flds, m, jx,jy,jz);
+	    F_c(m, jx,jy,jz) = F(m, jx,jy,jz);
 	  }
 	}
       }
