a5bfb2d02ae9c4b7b872a7ad33ef8355f5bf2744
Kai Germaschewski <kai.germaschewski@unh.edu>
2017-12-30 01:38:21 -0500

    _F3: psc_output_fields_item_fld_*

--- src/libpsc/psc_output_fields/psc_output_fields_item_flds_c.cxx
+++ src/libpsc/psc_output_fields/psc_output_fields_item_flds_c.cxx
@@ -2,4 +2,4 @@
 #include "psc_fields_as_c.h"
 #define psc_output_fields_item_dive_ops psc_output_fields_item_dive_c_ops
 
-#include "psc_output_fields_item_flds_common.c"
+#include "psc_output_fields_item_flds_common.cxx"
