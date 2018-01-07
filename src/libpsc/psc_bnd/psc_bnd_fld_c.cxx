d710f8dbcf6d9b6681a3bf30be16564411c760a4
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-07 02:25:55 -0500

    psc_bnd: two versions generated in one file

--- src/libpsc/psc_bnd/psc_bnd_fld_c.cxx
+++ src/libpsc/psc_bnd/psc_bnd_fld_c.cxx
@@ -1,7 +1,8 @@
 
 #include "psc_bnd_fld.h"
 #include "psc_bnd_private.h"
-#include "psc_fields_as_c.h"
+#include "psc_fields_c.h"
+#include "psc_fields_single.h"
 
 #include "psc_bnd_fld.cxx"
 
@@ -17,4 +18,15 @@ struct psc_bnd_ops psc_bnd_c_ops = {
   .fill_ghosts             = psc_bnd_fld_ops_c::fill_ghosts,
 };
 
+// ======================================================================
+// psc_bnd: subclass "single"
+
+using psc_bnd_fld_ops_single = psc_bnd_fld_ops<mfields_single_t>;
+
+struct psc_bnd_ops psc_bnd_single_ops = {
+  .name                    = "single",
+  .create_ddc              = psc_bnd_fld_ops_single::create,
+  .add_ghosts              = psc_bnd_fld_ops_single::add_ghosts,
+  .fill_ghosts             = psc_bnd_fld_ops_single::fill_ghosts,
+};
 
