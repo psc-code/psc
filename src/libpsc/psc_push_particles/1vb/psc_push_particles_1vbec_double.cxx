2dd66337f2abaca032e6f9213c2989fbe79d9b33
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-07 01:24:01 -0500

    push_particles: use existing dim dispatch mechanism

--- src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_double.cxx
+++ src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_double.cxx
@@ -18,30 +18,14 @@ void psc_push_particles_1vbec_double_stagger_mprts_1(struct psc_push_particles *
 						     struct psc_mparticles *mprts,
 						     struct psc_mfields *mflds);
 
-static void
-psc_push_particles_1vbec_double_push_mprts(struct psc_push_particles *push,
-					   struct psc_mparticles *mprts,
-					   struct psc_mfields *mflds)
-{
-  int *gdims = ppsc->domain.gdims;
-
-  if (gdims[0] > 1 && gdims[1] > 1 && gdims[2] > 1) {
-    psc_push_particles_1vbec_double_push_mprts_xyz(push, mprts, mflds);
-  } else if (gdims[0] == 1 && gdims[1] > 1 && gdims[2] > 1) {
-    psc_push_particles_1vbec_double_push_mprts_yz(push, mprts, mflds);
-  } else if (gdims[0] == 1 && gdims[1] == 1 && gdims[2] == 1) {
-    psc_push_particles_1vbec_double_push_mprts_1(push, mprts, mflds);
-  } else {
-    assert(0);
-  }
-}
-
 // ======================================================================
 // psc_push_particles: subclass "1vbec_double"
 
 struct psc_push_particles_ops psc_push_particles_1vbec_double_ops = {
   .name                  = "1vbec_double",
-  .push_mprts            = psc_push_particles_1vbec_double_push_mprts,
+  .push_mprts_xyz        = psc_push_particles_1vbec_double_push_mprts_xyz,
+  .push_mprts_yz         = psc_push_particles_1vbec_double_push_mprts_yz,
+  .push_mprts_1          = psc_push_particles_1vbec_double_push_mprts_1,
   //  .stagger_mprts_1       = psc_push_particles_1vbec_double_stagger_mprts_1,
   .particles_type        = PARTICLE_TYPE,
   .fields_type           = FIELDS_TYPE,
--- src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_single.cxx
+++ src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_single.cxx
@@ -5,32 +5,15 @@
 #include "psc_fields_as_single.h"
 #include "1vb/psc_push_particles_1vb.h"
 
-static void
-psc_push_particles_1vbec_single_push_mprts(struct psc_push_particles *push,
-					   struct psc_mparticles *mprts,
-					   struct psc_mfields *mflds)
-{
-  int *gdims = ppsc->domain.gdims;
-
-  if (gdims[0] > 1 && gdims[1] > 1 && gdims[2] > 1) {
-    psc_push_particles_1vbec_single_push_mprts_xyz(push, mprts, mflds);
-  } else if (gdims[0] > 1 && gdims[1] == 1 && gdims[2] > 1) {
-    psc_push_particles_1vbec_single_push_mprts_xyz_xz(push, mprts, mflds);
-  } else if (gdims[0] == 1 && gdims[1] > 1 && gdims[2] > 1) {
-    psc_push_particles_1vbec_single_push_mprts_yz(push, mprts, mflds);
-  } else if (gdims[0] == 1 && gdims[1] == 1 && gdims[2] == 1) {
-    psc_push_particles_1vbec_single_push_mprts_1(push, mprts, mflds);
-  } else {
-    assert(0);
-  }
-}
-
 // ======================================================================
 // psc_push_particles: subclass "1vbec_single"
 
 struct psc_push_particles_ops psc_push_particles_1vbec_single_ops = {
   .name                  = "1vbec_single",
-  .push_mprts            = psc_push_particles_1vbec_single_push_mprts,
+  .push_mprts_xyz        = psc_push_particles_1vbec_single_push_mprts_xyz,
+  .push_mprts_xz         = psc_push_particles_1vbec_single_push_mprts_xyz_xz,
+  .push_mprts_yz         = psc_push_particles_1vbec_single_push_mprts_yz,
+  .push_mprts_1          = psc_push_particles_1vbec_single_push_mprts_1,
   .stagger_mprts_yz      = psc_push_particles_1vbec_single_stagger_mprts_yz,
   .particles_type        = PARTICLE_TYPE,
   .fields_type           = FIELDS_TYPE,
