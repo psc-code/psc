7181bb01d5960c4ee72076ddcbc4fa232843e208
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-07 10:10:37 -0500

    fields: partially templateify psc_marder_common.cxx

--- src/libpsc/psc_push_fields/psc_marder_c.cxx
+++ src/libpsc/psc_push_fields/psc_marder_c.cxx
@@ -1,6 +1,18 @@
 
-#include "psc_fields_as_c.h"
+#include "psc_fields_c.h"
 #include "psc_particles_as_double.h"
 
-#define psc_marder_sub_ops psc_marder_c_ops
 #include "psc_marder_common.cxx"
+
+// ======================================================================
+// psc_marder: c
+
+using marder_ops_c = marder_ops<mfields_c_t>;
+
+struct psc_marder_ops psc_marder_c_ops = {
+  .name                  = "c",
+  .setup                 = marder_ops_c::setup,
+  .destroy               = marder_ops_c::destroy,
+  .correct               = marder_ops_c::correct,
+};
+
--- src/libpsc/psc_push_fields/psc_marder_common.cxx
+++ src/libpsc/psc_push_fields/psc_marder_common.cxx
@@ -6,209 +6,207 @@
 
 #include <mrc_io.h>
 
-using Fields = Fields3d<fields_t>;
+template<typename MF>
+struct marder_ops {
+  using mfields_t = MF;
+  using fields_t = typename mfields_t::fields_t;
+  using real_t = typename mfields_t::real_t;
+  using Fields = Fields3d<fields_t>;
 
-// FIXME: checkpointing won't properly restore state
-// FIXME: if the subclass creates objects, it'd be cleaner to have them
-// be part of the subclass
+  // FIXME: checkpointing won't properly restore state
+  // FIXME: if the subclass creates objects, it'd be cleaner to have them
+  // be part of the subclass
 
-// ----------------------------------------------------------------------
-// fld_create
-//
-// FIXME, should be consolidated with psc_checks.c, and probably other places
+  // ----------------------------------------------------------------------
+  // fld_create
+  //
+  // FIXME, should be consolidated with psc_checks.c, and probably other places
 
-static struct psc_mfields *
-fld_create(struct psc *psc, const char *name)
-{
-  struct psc_mfields *fld = psc_mfields_create(psc_comm(psc));
-  psc_mfields_set_type(fld, FIELDS_TYPE);
-  psc_mfields_set_param_obj(fld, "domain", psc->mrc_domain);
-  psc_mfields_set_param_int3(fld, "ibn", psc->ibn);
-  psc_mfields_set_param_int(fld, "nr_fields", 1);
-  psc_mfields_setup(fld);
-  psc_mfields_set_comp_name(fld, 0, name);
+  static struct psc_mfields *
+  fld_create(struct psc *psc, const char *name)
+  {
+    struct psc_mfields *fld = psc_mfields_create(psc_comm(psc));
+    psc_mfields_set_type(fld, fields_traits<fields_t>::name);
+    psc_mfields_set_param_obj(fld, "domain", psc->mrc_domain);
+    psc_mfields_set_param_int3(fld, "ibn", psc->ibn);
+    psc_mfields_set_param_int(fld, "nr_fields", 1);
+    psc_mfields_setup(fld);
+    psc_mfields_set_comp_name(fld, 0, name);
 
-  return fld;
-}
-
-// ----------------------------------------------------------------------
-// psc_marder_sub_setup
-
-static void
-psc_marder_sub_setup(struct psc_marder *marder)
-{
-  marder->div_e = fld_create(ppsc, "div_E");
-  marder->rho = fld_create(ppsc, "rho");
-
-  marder->bnd = psc_bnd_create(psc_marder_comm(marder));
-  psc_bnd_set_name(marder->bnd, "marder_bnd");
-  psc_bnd_set_type(marder->bnd, FIELDS_TYPE);
-  psc_bnd_set_psc(marder->bnd, ppsc);
-  psc_bnd_setup(marder->bnd);
-
-  // FIXME, output_fields should be taking care of their own psc_bnd?
-  marder->item_div_e = psc_output_fields_item_create(psc_comm(ppsc));
-  psc_output_fields_item_set_type(marder->item_div_e, "dive_" FIELDS_TYPE);
-  psc_output_fields_item_set_psc_bnd(marder->item_div_e, marder->bnd);
-  psc_output_fields_item_setup(marder->item_div_e);
-
-  marder->item_rho = psc_output_fields_item_create(psc_comm(ppsc));
-  psc_output_fields_item_set_type(marder->item_rho, "rho_1st_nc_" PARTICLE_TYPE);
-  psc_output_fields_item_set_psc_bnd(marder->item_rho, marder->bnd);
-  psc_output_fields_item_setup(marder->item_rho);
-
-  if (marder->dump) {
-    struct mrc_io *io = mrc_io_create(psc_comm(ppsc));
-    mrc_io_set_type(io, "xdmf_collective");
-    mrc_io_set_name(io, "mrc_io_marder");
-    mrc_io_set_param_string(io, "basename", "marder");
-    mrc_io_set_from_options(io);
-    mrc_io_setup(io);
-
-    marder->io = io;
+    return fld;
   }
-}
 
-// ----------------------------------------------------------------------
-// psc_marder_sub_destroy
+  // ----------------------------------------------------------------------
+  // setup
 
-static void
-psc_marder_sub_destroy(struct psc_marder *marder)
-{
-  psc_mfields_destroy(marder->div_e);
-  psc_mfields_destroy(marder->rho);
+  static void
+  setup(struct psc_marder *marder)
+  {
+    marder->div_e = fld_create(ppsc, "div_E");
+    marder->rho = fld_create(ppsc, "rho");
 
-  psc_output_fields_item_destroy(marder->item_div_e);
-  psc_output_fields_item_destroy(marder->item_rho);
+    marder->bnd = psc_bnd_create(psc_marder_comm(marder));
+    psc_bnd_set_name(marder->bnd, "marder_bnd");
+    psc_bnd_set_type(marder->bnd, fields_traits<fields_t>::name);
+    psc_bnd_set_psc(marder->bnd, ppsc);
+    psc_bnd_setup(marder->bnd);
 
-  psc_bnd_destroy(marder->bnd);
+    // FIXME, output_fields should be taking care of their own psc_bnd?
+    marder->item_div_e = psc_output_fields_item_create(psc_comm(ppsc));
+    char name[20];
+    snprintf(name, 20, "dive_%s", fields_traits<fields_t>::name);
+    psc_output_fields_item_set_type(marder->item_div_e, name);
+    psc_output_fields_item_set_psc_bnd(marder->item_div_e, marder->bnd);
+    psc_output_fields_item_setup(marder->item_div_e);
 
-  if (marder->dump) {
-    mrc_io_destroy(marder->io);
-  }
-}
+    marder->item_rho = psc_output_fields_item_create(psc_comm(ppsc));
+    psc_output_fields_item_set_type(marder->item_rho, "rho_1st_nc_" PARTICLE_TYPE);
+    psc_output_fields_item_set_psc_bnd(marder->item_rho, marder->bnd);
+    psc_output_fields_item_setup(marder->item_rho);
 
-// ----------------------------------------------------------------------
-// psc_marder_sub_correct_patch
-//
-// Do the modified marder correction (See eq.(5, 7, 9, 10) in Mardahl and Verboncoeur, CPC, 1997)
+    if (marder->dump) {
+      struct mrc_io *io = mrc_io_create(psc_comm(ppsc));
+      mrc_io_set_type(io, "xdmf_collective");
+      mrc_io_set_name(io, "mrc_io_marder");
+      mrc_io_set_param_string(io, "basename", "marder");
+      mrc_io_set_from_options(io);
+      mrc_io_setup(io);
 
-#define define_dxdydz(dx, dy, dz)					\
-  int dx _mrc_unused = (ppsc->domain.gdims[0] == 1) ? 0 : 1;		\
-  int dy _mrc_unused = (ppsc->domain.gdims[1] == 1) ? 0 : 1;		\
-  int dz _mrc_unused = (ppsc->domain.gdims[2] == 1) ? 0 : 1
-
-#define psc_foreach_3d_more(psc, p, ix, iy, iz, l, r) {			\
-  int __ilo[3] = { -l[0], -l[1], -l[2] };					\
-  int __ihi[3] = { psc->patch[p].ldims[0] + r[0],				\
-		   psc->patch[p].ldims[1] + r[1],				\
-		   psc->patch[p].ldims[2] + r[2] };				\
-  for (int iz = __ilo[2]; iz < __ihi[2]; iz++) {			\
-    for (int iy = __ilo[1]; iy < __ihi[1]; iy++) {			\
-      for (int ix = __ilo[0]; ix < __ihi[0]; ix++)
-
-#define psc_foreach_3d_more_end				\
-  } } }
-
-static void
-psc_marder_sub_correct_patch(struct psc_marder *marder, fields_t flds, fields_t f, int p)
-{
-  Fields F(flds), FF(f);
-  define_dxdydz(dx, dy, dz);
-
-  // FIXME: how to choose diffusion parameter properly?
-  //double deltax = ppsc->patch[p].dx[0];
-  double deltay = ppsc->patch[p].dx[1]; // FIXME double/float
-  double deltaz = ppsc->patch[p].dx[2];
-  double inv_sum = 0.;
-  int nr_levels;
-  mrc_domain_get_nr_levels(ppsc->mrc_domain, &nr_levels);
-  for (int d=0;d<3;d++) {
-    if (ppsc->domain.gdims[d] > 1) {
-      inv_sum += 1. / sqr(ppsc->patch[p].dx[d] / (1 << (nr_levels - 1)));
+      marder->io = io;
     }
   }
-  double diffusion_max = 1. / 2. / (.5 * ppsc->dt) / inv_sum;
-  double diffusion     = diffusion_max * marder->diffusion;
 
-  int l_cc[3] = {0, 0, 0}, r_cc[3] = {0, 0, 0};
-  int l_nc[3] = {0, 0, 0}, r_nc[3] = {0, 0, 0};
-  for (int d = 0; d < 3; d++) {
-   if (ppsc->domain.bnd_fld_lo[d] == BND_FLD_CONDUCTING_WALL && ppsc->patch[p].off[d] == 0) {
-    l_cc[d] = -1;
-    l_nc[d] = -1;
-   }
-   if (ppsc->domain.bnd_fld_hi[d] == BND_FLD_CONDUCTING_WALL && ppsc->patch[p].off[d] + ppsc->patch[p].ldims[d] == ppsc->domain.gdims[d]) {
-    r_cc[d] = -1;
-    r_nc[d] = 0;
-   }
+  // ----------------------------------------------------------------------
+  // destroy
+
+  static void
+  destroy(struct psc_marder *marder)
+  {
+    psc_mfields_destroy(marder->div_e);
+    psc_mfields_destroy(marder->rho);
+
+    psc_output_fields_item_destroy(marder->item_div_e);
+    psc_output_fields_item_destroy(marder->item_rho);
+
+    psc_bnd_destroy(marder->bnd);
+
+    if (marder->dump) {
+      mrc_io_destroy(marder->io);
+    }
   }
+
+  // ----------------------------------------------------------------------
+  // correct_patch
+  //
+  // Do the modified marder correction (See eq.(5, 7, 9, 10) in Mardahl and Verboncoeur, CPC, 1997)
+
+#define define_dxdydz(dx, dy, dz)				\
+  int dx _mrc_unused = (ppsc->domain.gdims[0] == 1) ? 0 : 1;	\
+  int dy _mrc_unused = (ppsc->domain.gdims[1] == 1) ? 0 : 1;	\
+  int dz _mrc_unused = (ppsc->domain.gdims[2] == 1) ? 0 : 1
+
+#define psc_foreach_3d_more(psc, p, ix, iy, iz, l, r) {	\
+  int __ilo[3] = { -l[0], -l[1], -l[2] };		\
+  int __ihi[3] = { psc->patch[p].ldims[0] + r[0],	\
+		   psc->patch[p].ldims[1] + r[1],	\
+		   psc->patch[p].ldims[2] + r[2] };	\
+  for (int iz = __ilo[2]; iz < __ihi[2]; iz++) {	\
+  for (int iy = __ilo[1]; iy < __ihi[1]; iy++) {	\
+  for (int ix = __ilo[0]; ix < __ihi[0]; ix++)
+
+#define psc_foreach_3d_more_end			\
+  } } }
+
+    static void
+    correct_patch(struct psc_marder *marder, fields_t flds, fields_t f, int p)
+  {
+    Fields F(flds), FF(f);
+    define_dxdydz(dx, dy, dz);
+
+    // FIXME: how to choose diffusion parameter properly?
+    //double deltax = ppsc->patch[p].dx[0];
+    double deltay = ppsc->patch[p].dx[1]; // FIXME double/float
+    double deltaz = ppsc->patch[p].dx[2];
+    double inv_sum = 0.;
+    int nr_levels;
+    mrc_domain_get_nr_levels(ppsc->mrc_domain, &nr_levels);
+    for (int d=0;d<3;d++) {
+      if (ppsc->domain.gdims[d] > 1) {
+	inv_sum += 1. / sqr(ppsc->patch[p].dx[d] / (1 << (nr_levels - 1)));
+      }
+    }
+    double diffusion_max = 1. / 2. / (.5 * ppsc->dt) / inv_sum;
+    double diffusion     = diffusion_max * marder->diffusion;
+
+    int l_cc[3] = {0, 0, 0}, r_cc[3] = {0, 0, 0};
+    int l_nc[3] = {0, 0, 0}, r_nc[3] = {0, 0, 0};
+    for (int d = 0; d < 3; d++) {
+      if (ppsc->domain.bnd_fld_lo[d] == BND_FLD_CONDUCTING_WALL && ppsc->patch[p].off[d] == 0) {
+	l_cc[d] = -1;
+	l_nc[d] = -1;
+      }
+      if (ppsc->domain.bnd_fld_hi[d] == BND_FLD_CONDUCTING_WALL && ppsc->patch[p].off[d] + ppsc->patch[p].ldims[d] == ppsc->domain.gdims[d]) {
+	r_cc[d] = -1;
+	r_nc[d] = 0;
+      }
+    }
 
 #if 0
-  psc_foreach_3d_more(ppsc, p, ix, iy, iz, l, r) {
-    // FIXME: F3 correct?
-    F(EX, ix,iy,iz) += 
-      (FF(DIVE_MARDER, ix+dx,iy,iz) - FF(DIVE_MARDER, ix,iy,iz))
-      * .5 * ppsc->dt * diffusion / deltax;
-    F(EY, ix,iy,iz) += 
-      (FF(DIVE_MARDER, ix,iy+dy,iz) - FF(DIVE_MARDER, ix,iy,iz))
-      * .5 * ppsc->dt * diffusion / deltay;
-    F(EZ, ix,iy,iz) += 
-      (FF(DIVE_MARDER, ix,iy,iz+dz) - FF(DIVE_MARDER, ix,iy,iz))
-      * .5 * ppsc->dt * diffusion / deltaz;
-  } psc_foreach_3d_more_end;
-#endif
-
-  assert(ppsc->domain.gdims[0] == 1);
-
-  {
-    int l[3] = { l_nc[0], l_cc[1], l_nc[2] };
-    int r[3] = { r_nc[0], r_cc[1], r_nc[2] };
     psc_foreach_3d_more(ppsc, p, ix, iy, iz, l, r) {
+      // FIXME: F3 correct?
+      F(EX, ix,iy,iz) += 
+	(FF(DIVE_MARDER, ix+dx,iy,iz) - FF(DIVE_MARDER, ix,iy,iz))
+	* .5 * ppsc->dt * diffusion / deltax;
       F(EY, ix,iy,iz) += 
-	(FF(0, ix,iy+dy,iz) - FF(0, ix,iy,iz))
+	(FF(DIVE_MARDER, ix,iy+dy,iz) - FF(DIVE_MARDER, ix,iy,iz))
 	* .5 * ppsc->dt * diffusion / deltay;
-    } psc_foreach_3d_more_end;
-  }
-
-  {
-    int l[3] = { l_nc[0], l_nc[1], l_cc[2] };
-    int r[3] = { r_nc[0], r_nc[1], r_cc[2] };
-    psc_foreach_3d_more(ppsc, p, ix, iy, iz, l, r) {
       F(EZ, ix,iy,iz) += 
-	(FF(0, ix,iy,iz+dz) - FF(0, ix,iy,iz))
+	(FF(DIVE_MARDER, ix,iy,iz+dz) - FF(DIVE_MARDER, ix,iy,iz))
 	* .5 * ppsc->dt * diffusion / deltaz;
     } psc_foreach_3d_more_end;
+#endif
+
+    assert(ppsc->domain.gdims[0] == 1);
+
+    {
+      int l[3] = { l_nc[0], l_cc[1], l_nc[2] };
+      int r[3] = { r_nc[0], r_cc[1], r_nc[2] };
+      psc_foreach_3d_more(ppsc, p, ix, iy, iz, l, r) {
+	F(EY, ix,iy,iz) += 
+	  (FF(0, ix,iy+dy,iz) - FF(0, ix,iy,iz))
+	  * .5 * ppsc->dt * diffusion / deltay;
+      } psc_foreach_3d_more_end;
+    }
+
+    {
+      int l[3] = { l_nc[0], l_nc[1], l_cc[2] };
+      int r[3] = { r_nc[0], r_nc[1], r_cc[2] };
+      psc_foreach_3d_more(ppsc, p, ix, iy, iz, l, r) {
+	F(EZ, ix,iy,iz) += 
+	  (FF(0, ix,iy,iz+dz) - FF(0, ix,iy,iz))
+	  * .5 * ppsc->dt * diffusion / deltaz;
+      } psc_foreach_3d_more_end;
+    }
   }
-}
 
 #undef psc_foreach_3d_more
 #undef psc_foreach_3d_more_end
 
-// ----------------------------------------------------------------------
-// psc_marder_sub_correct
+  // ----------------------------------------------------------------------
+  // correct
 
-static void
-psc_marder_sub_correct(struct psc_marder *marder, struct psc_mfields *mflds_base,
-		       struct psc_mfields *div_e)
-{
-  mfields_t mf = mflds_base->get_as<mfields_t>(EX, EX + 3);
-  mfields_t mf_div_e(div_e);
+  static void
+  correct(struct psc_marder *marder, struct psc_mfields *mflds_base,
+	  struct psc_mfields *div_e)
+  {
+    mfields_t mf = mflds_base->get_as<mfields_t>(EX, EX + 3);
+    mfields_t mf_div_e(div_e);
   
-  for (int p = 0; p < div_e->nr_patches; p++) {
-    psc_marder_sub_correct_patch(marder, mf[p], mf_div_e[p], p);
+    for (int p = 0; p < div_e->nr_patches; p++) {
+      correct_patch(marder, mf[p], mf_div_e[p], p);
+    }
+
+    mf.put_as(mflds_base, EX, EX + 3);
   }
-
-  mf.put_as(mflds_base, EX, EX + 3);
-}
-
-// ======================================================================
-// psc_marder: subclass FIELDS_TYPE
-
-struct psc_marder_ops psc_marder_sub_ops = {
-  .name                  = FIELDS_TYPE,
-  .setup                 = psc_marder_sub_setup,
-  .destroy               = psc_marder_sub_destroy,
-  .correct               = psc_marder_sub_correct,
 };
 
