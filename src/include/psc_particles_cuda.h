05baa6667bc6606e8f0c2867f938a3ee3b02e47b
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-08 22:13:35 -0500

    prts: missed cuda traits

--- src/include/psc_particles_cuda.h
+++ src/include/psc_particles_cuda.h
@@ -5,10 +5,21 @@
 #include "psc_particles_private.h"
 #include "psc_particles_single.h"
 
+#include "particles_traits.hxx"
+
 #include "psc_particle_buf_cuda.h"
 
 #define PTYPE PTYPE_CUDA
 #include "psc_particles_common.h"
 #undef PTYPE
+
+template<>
+struct mparticles_traits<particle_cuda_t>
+{
+  static constexpr const char* name = "cuda";
+  static MPI_Datatype mpi_dtype() { return MPI_FLOAT; }
+};
+
+
 
 #endif
