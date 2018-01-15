9adec965544bcb947ea01675c2329bc7178da77d
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-15 10:57:12 -0500

    cuda: general grid.hxx, vec3.hxx

---
+++ src/include/grid.hxx
@@ -0,0 +1,64 @@
+
+#ifndef GRID_HXX
+#define GRID_HXX
+
+#include "vec3.hxx"
+#include <vector>
+
+// ======================================================================
+// Grid
+
+template<class T>
+struct Grid
+{
+  using real_t = T;
+  using Real3 = Vec3<real_t>;
+  
+  struct Patch
+  {
+    Real3 xb;
+  };
+
+  struct Kind
+  {
+    Kind(real_t q_, real_t m_, const char *name_)
+      : q(q_), m(m_), name(strdup(name_))
+    {
+    };
+
+    Kind(const Kind& k)
+      : q(k.q), m(k.m), name(strdup(k.name))
+    {
+    }
+
+    Kind(Kind&& k)
+      : q(k.q), m(k.m), name(k.name)
+    {
+      k.name = nullptr;
+    }
+
+    ~Kind()
+    {
+      free((void*) name);
+    }
+    
+    real_t q;
+    real_t m;
+    const char *name;
+  };
+  
+  Grid() = default;
+  Grid(const Grid& grid) = delete;
+
+  Int3 gdims;
+  Int3 ldims;
+  Real3 dx;
+  real_t fnqs;
+  real_t eta;
+  real_t dt;
+  std::vector<Patch> patches;
+  std::vector<Kind> kinds;
+};
+
+#endif
+
---
+++ src/include/vec3.hxx
@@ -0,0 +1,48 @@
+
+#ifndef VEC3_HXX
+#define VEC3_HXX
+
+#include <array>
+#include <cassert>
+
+// ======================================================================
+// Vec3
+
+template<typename T>
+struct Vec3 : std::array<T, 3>
+{
+  using Base = std::array<T, 3>;
+
+  using Base::data;
+  
+  Vec3() = default;
+  Vec3(const T *p)
+  {
+    for (int i = 0; i < 3; i++) {
+      new (&(*this)[i])	T(p[i]); // placement new -- not really necessary
+    }
+  }
+
+  Vec3(std::initializer_list<T> l)
+  {
+    assert(l.size() == 3);
+    std::uninitialized_copy(l.begin(), l.end(), data());
+  }
+
+  // convert from different type (e.g., float -> double)
+  template<typename U>
+  Vec3(const Vec3<U>& u)
+  {
+    for (int i = 0; i < 3; i++) {
+      new (&(*this)[i])	T(u[i]); // placement new -- not really necessary
+    }
+  }
+  
+  operator const T* () const { return data(); }
+  operator T* ()             { return data(); }
+};
+
+using Int3 = Vec3<int>;
+
+#endif
+
