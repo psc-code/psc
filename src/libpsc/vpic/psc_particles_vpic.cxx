2e14d788934741e941db6ac4201c5c6fca6479f0
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-17 15:13:24 -0500

    vpic: conversion to/from single -> functors

--- src/libpsc/vpic/psc_particles_vpic.cxx
+++ src/libpsc/vpic/psc_particles_vpic.cxx
@@ -17,9 +17,9 @@ struct copy_ctx {
   float dVi;
 };
 
-static void
-copy_to(struct psc_mparticles *mprts, struct psc_mparticles *mprts_to,
-	void (*put_particle)(struct vpic_mparticles_prt *prt, int n, void *ctx))
+template<typename F>
+static void copy_to(struct psc_mparticles *mprts, struct psc_mparticles *mprts_to,
+		    F convert_from)
 {
   struct psc_mparticles_vpic *sub = psc_mparticles_vpic(mprts);
   Particles *vmprts = sub->vmprts;
@@ -36,15 +36,15 @@ copy_to(struct psc_mparticles *mprts, struct psc_mparticles *mprts_to,
       ctx.im[d] += 2; // add ghost points
     }
     ctx.dVi = 1.f / (ctx.dx[0] * ctx.dx[1] * ctx.dx[2]);
-    vpic_mparticles_get_particles(vmprts, n_prts, off, put_particle, &ctx);
+    vpic_mparticles_get_particles(vmprts, n_prts, off, convert_from, &ctx);
 
     off += n_prts;
   }
 }
 
-static void
-copy_from(struct psc_mparticles *mprts, struct psc_mparticles *mprts_from,
-	  void (*get_particle)(struct vpic_mparticles_prt *prt, int n, void *ctx))
+template<typename F>
+static void copy_from(struct psc_mparticles *mprts, struct psc_mparticles *mprts_from,
+		      F convert_to)
 {
   struct psc_mparticles_vpic *sub = psc_mparticles_vpic(mprts);
   Particles *vmprts = sub->vmprts;
@@ -68,7 +68,7 @@ copy_from(struct psc_mparticles *mprts, struct psc_mparticles *mprts_from,
 
     int n_prts = n_prts_by_patch[p];
     for (int n = 0; n < n_prts; n++) {
-      get_particle(&prt, n, &ctx);
+      convert_to(&prt, n, &ctx);
       Simulation_mprts_push_back(sub->sim, vmprts, &prt);
     }
   }
@@ -77,65 +77,70 @@ copy_from(struct psc_mparticles *mprts, struct psc_mparticles *mprts_from,
 // ======================================================================
 // conversion to "single"
 
-static void
-convert_from_single(struct vpic_mparticles_prt *prt, int n, void *_ctx)
+struct ConvertFromSingle
 {
-  struct copy_ctx *ctx = (struct copy_ctx *) _ctx;
-  particle_single_t *part = &mparticles_single_t(ctx->mprts)[ctx->p][n];
-
-  assert(part->kind() < ppsc->nr_kinds);
-  int *im = ctx->im;
-  float *dx = ctx->dx;
-  int i3[3];
-  for (int d = 0; d < 3; d++) {
-    float val = (&part->xi)[d] / dx[d];
-    i3[d] = (int) val;
-    //mprintf("d %d val %g xi %g\n", d, val, part->xi);
-    assert(i3[d] >= -1 && i3[d] < im[d] + 1);
-    prt->dx[d] = (val - i3[d]) * 2.f - 1.f;
-    i3[d] += 1;
+  void operator()(struct vpic_mparticles_prt *prt, int n, void *_ctx)
+  {
+    struct copy_ctx *ctx = (struct copy_ctx *) _ctx;
+    particle_single_t *part = &mparticles_single_t(ctx->mprts)[ctx->p][n];
+    
+    assert(part->kind() < ppsc->nr_kinds);
+    int *im = ctx->im;
+    float *dx = ctx->dx;
+    int i3[3];
+    for (int d = 0; d < 3; d++) {
+      float val = (&part->xi)[d] / dx[d];
+      i3[d] = (int) val;
+      //mprintf("d %d val %g xi %g\n", d, val, part->xi);
+      assert(i3[d] >= -1 && i3[d] < im[d] + 1);
+      prt->dx[d] = (val - i3[d]) * 2.f - 1.f;
+      i3[d] += 1;
+    }
+    prt->i     = (i3[2] * im[1] + i3[1]) * im[0] + i3[0];
+    prt->ux[0] = part->pxi;
+    prt->ux[1] = part->pyi;
+    prt->ux[2] = part->pzi;
+    prt->w     = part->qni_wni / ppsc->kinds[part->kind_].q / ctx->dVi;
+    prt->kind  = part->kind();
   }
-  prt->i     = (i3[2] * im[1] + i3[1]) * im[0] + i3[0];
-  prt->ux[0] = part->pxi;
-  prt->ux[1] = part->pyi;
-  prt->ux[2] = part->pzi;
-  prt->w     = part->qni_wni / ppsc->kinds[part->kind_].q / ctx->dVi;
-  prt->kind  = part->kind();
-}
+};
 
-static void
-convert_to_single(struct vpic_mparticles_prt *prt, int n, void *_ctx)
+struct ConvertToSingle
 {
-  struct copy_ctx *ctx = (struct copy_ctx *) _ctx;
-  particle_single_t *part = &mparticles_single_t(ctx->mprts)[ctx->p][n];
-
-  assert(prt->kind < ppsc->nr_kinds);
-  int *im = ctx->im;
-  float *dx = ctx->dx;
-  int i = prt->i;
-  int i3[3];
-  i3[2] = i / (im[0] * im[1]); i -= i3[2] * (im[0] * im[1]);
-  i3[1] = i / im[0]; i-= i3[1] * im[0];
-  i3[0] = i;
-  part->xi      = (i3[0] - 1 + .5f * (1.f + prt->dx[0])) * dx[0];
-  part->yi      = (i3[1] - 1 + .5f * (1.f + prt->dx[1])) * dx[1];
-  part->zi      = (i3[2] - 1 + .5f * (1.f + prt->dx[2])) * dx[2];
-  float w = part->zi / dx[2];
-  if (!(w >= 0 && w <= im[2] - 2)) {
-    printf("w %g im %d i3 %d dx %g\n", w, im[2], i3[2], prt->dx[2]);
+  void operator()(struct vpic_mparticles_prt *prt, int n, void *_ctx)
+  {
+    struct copy_ctx *ctx = (struct copy_ctx *) _ctx;
+    particle_single_t *part = &mparticles_single_t(ctx->mprts)[ctx->p][n];
+    
+    assert(prt->kind < ppsc->nr_kinds);
+    int *im = ctx->im;
+    float *dx = ctx->dx;
+    int i = prt->i;
+    int i3[3];
+    i3[2] = i / (im[0] * im[1]); i -= i3[2] * (im[0] * im[1]);
+    i3[1] = i / im[0]; i-= i3[1] * im[0];
+    i3[0] = i;
+    part->xi      = (i3[0] - 1 + .5f * (1.f + prt->dx[0])) * dx[0];
+    part->yi      = (i3[1] - 1 + .5f * (1.f + prt->dx[1])) * dx[1];
+    part->zi      = (i3[2] - 1 + .5f * (1.f + prt->dx[2])) * dx[2];
+    float w = part->zi / dx[2];
+    if (!(w >= 0 && w <= im[2] - 2)) {
+      printf("w %g im %d i3 %d dx %g\n", w, im[2], i3[2], prt->dx[2]);
+    }
+    assert(w >= 0 && w <= im[2] - 2);
+    part->kind_   = prt->kind;
+    part->pxi     = prt->ux[0];
+    part->pyi     = prt->ux[1];
+    part->pzi     = prt->ux[2];
+    part->qni_wni = ppsc->kinds[prt->kind].q * prt->w * ctx->dVi;
   }
-  assert(w >= 0 && w <= im[2] - 2);
-  part->kind_   = prt->kind;
-  part->pxi     = prt->ux[0];
-  part->pyi     = prt->ux[1];
-  part->pzi     = prt->ux[2];
-  part->qni_wni = ppsc->kinds[prt->kind].q * prt->w * ctx->dVi;
-}
+};
 
 static void
 psc_mparticles_vpic_copy_from_single(struct psc_mparticles *mprts,
 				    struct psc_mparticles *mprts_single, unsigned int flags)
 {
+  ConvertFromSingle convert_from_single;
   copy_from(mprts, mprts_single, convert_from_single);
 }
 
@@ -143,6 +148,7 @@ static void
 psc_mparticles_vpic_copy_to_single(struct psc_mparticles *mprts,
 				  struct psc_mparticles *mprts_single, unsigned int flags)
 {
+  ConvertToSingle convert_to_single;
   copy_to(mprts, mprts_single, convert_to_single);
 }
 
