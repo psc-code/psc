8d87b04c65f4039af0ad1b9749c3c83de0b50012
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-17 11:34:54 -0500

    cuda: use new interface in test_cuda_mparticles_1.cu

--- src/libpsc/cuda/tests/test_cuda_mparticles_1.cu
+++ src/libpsc/cuda/tests/test_cuda_mparticles_1.cu
@@ -22,48 +22,57 @@ prof_register(const char *name, float simd, int flops, int bytes)
 // patch (C order, but to get them ordered by block, they need to be reordered
 // into Fortran order, a.k.a., this will exercise the initial sorting
 
-static void
-set_particle_test_1(struct cuda_mparticles_prt *prt, int n, void *ctx)
+struct SetParticleTest1
 {
-  const Grid_t& grid = *static_cast<Grid_t*>(ctx);
+  SetParticleTest1(const Grid_t& grid)
+  : grid_(grid)
+  {
+  }
+  
+  cuda_mparticles_prt operator()(int n)
+  {
+    Int3 ldims = grid_.ldims;
+    Vec3<double> dx = grid_.dx;
+    
+    int k = n % ldims[2];
+    n /= ldims[2];
+    int j = n % ldims[1];
+    n /= ldims[1];
+    int i = n;
 
-  Int3 ldims = grid.ldims;
-  Vec3<double> dx = grid.dx;
+    cuda_mparticles_prt prt;
+    prt.xi[0] = dx[0] * (i + .5f);
+    prt.xi[1] = dx[1] * (j + .5f);
+    prt.xi[2] = dx[2] * (k + .5f);
+    prt.pxi[0] = i;
+    prt.pxi[1] = j;
+    prt.pxi[2] = k;
+    prt.kind = 0;
+    prt.qni_wni = 1.;
+    return prt;
+  }
 
-  int k = n % ldims[2];
-  n /= ldims[2];
-  int j = n % ldims[1];
-  n /= ldims[1];
-  int i = n;
+private:
+  const Grid_t& grid_;
+};
 
-  prt->xi[0] = dx[0] * (i + .5f);
-  prt->xi[1] = dx[1] * (j + .5f);
-  prt->xi[2] = dx[2] * (k + .5f);
-  prt->pxi[0] = i;
-  prt->pxi[1] = j;
-  prt->pxi[2] = k;
-  prt->kind = 0;
-  prt->qni_wni = 1.;
-}
-
-void
-cuda_mparticles_add_particles_test_1(struct cuda_mparticles *cmprts,
-				     const Grid_t& grid,
-				     uint *n_prts_by_patch)
+void cuda_mparticles_add_particles_test_1(cuda_mparticles *cmprts, uint *n_prts_by_patch)
 {
+  const Grid_t& grid = cmprts->grid_;
   Int3 ldims = grid.ldims;
 
   for (int p = 0; p < grid.patches.size(); p++) {
     n_prts_by_patch[p] = ldims[0] * ldims[1] * ldims[2];
   }
 
+  SetParticleTest1 set_particles(grid);
+  
   cmprts->reserve_all(n_prts_by_patch);
   cmprts->resize_all(n_prts_by_patch);
   
   uint off = 0;
   for (int p = 0; p < grid.patches.size(); p++) {
-    cmprts->set_particles(n_prts_by_patch[p], off, set_particle_test_1,
-			  (void*) &grid);
+    cmprts->set_particles(n_prts_by_patch[p], off, set_particles);
     off += n_prts_by_patch[p];
   }
 }
@@ -71,22 +80,23 @@ cuda_mparticles_add_particles_test_1(struct cuda_mparticles *cmprts,
 // ----------------------------------------------------------------------
 // get_particles_test
 
-static void
-get_particle(struct cuda_mparticles_prt *prt, int n, void *ctx)
+struct GetParticlesTest1
 {
-  printf("prt[%d] xi %g %g %g // pxi %g %g %g // kind %d // qni_wni %g\n",
-	 n, prt->xi[0], prt->xi[1], prt->xi[2],
-	 prt->pxi[0], prt->pxi[1], prt->pxi[2],
-	 prt->kind, prt->qni_wni);
-}
+  void operator()(int n, cuda_mparticles_prt& prt) {
+    printf("prt[%d] xi %g %g %g // pxi %g %g %g // kind %d // qni_wni %g\n",
+	   n, prt.xi[0], prt.xi[1], prt.xi[2],
+	   prt.pxi[0], prt.pxi[1], prt.pxi[2],
+	   prt.kind, prt.qni_wni);
+  }
+};
 
 void
-get_particles_test(struct cuda_mparticles *cmprts, int n_patches,
-		   uint *n_prts_by_patch)
+get_particles_test(cuda_mparticles *cmprts, uint *n_prts_by_patch)
 {
+  GetParticlesTest1 get_particles;
   uint off = 0;
-  for (int p = 0; p < n_patches; p++) {
-    cmprts->get_particles(n_prts_by_patch[p], off, get_particle, NULL);
+  for (int p = 0; p < cmprts->n_patches; p++) {
+    cmprts->get_particles(n_prts_by_patch[p], off, get_particles);
     off += n_prts_by_patch[p];
   }
 }
@@ -117,7 +127,7 @@ main(void)
   struct cuda_mparticles *cmprts = new cuda_mparticles(grid, bs);
 
   uint n_prts_by_patch[grid.patches.size()];
-  cuda_mparticles_add_particles_test_1(cmprts, grid, n_prts_by_patch);
+  cuda_mparticles_add_particles_test_1(cmprts, n_prts_by_patch);
   printf("added particles\n");
   cmprts->dump_by_patch(n_prts_by_patch);
 
@@ -126,7 +136,7 @@ main(void)
   cmprts->dump();
 
   printf("get_particles_test\n");
-  get_particles_test(cmprts, grid.patches.size(), n_prts_by_patch);
+  get_particles_test(cmprts, n_prts_by_patch);
 
   delete cmprts;
 }
