2bd1e33891c5a7fc30c56ef820b91c491f0eb4fc
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-23 22:51:38 -0500

    cuda: split off _checks.cu, _gold.cu

---
+++ src/libpsc/cuda/cuda_mparticles_checks.cu
@@ -0,0 +1,96 @@
+
+#include "cuda_mparticles.h"
+#include "cuda_bits.h"
+
+// ----------------------------------------------------------------------
+// check_in_patch_unordered_slow
+
+bool cuda_mparticles::check_in_patch_unordered_slow()
+{
+  uint n_prts_by_patch[n_patches];
+  get_size_all(n_prts_by_patch);
+
+  uint off = 0;
+  for (int p = 0; p < n_patches; p++) {
+    for (int n = 0; n < n_prts_by_patch[p]; n++) {
+      int bidx = get_block_idx(d_xi4[off + n], p);
+      if (!(bidx >= 0 && bidx <= n_blocks)) return false;
+    }
+    off += n_prts_by_patch[p];
+  }
+
+  if (!(off == n_prts)) return false;
+  // printf("PASS: cuda_mparticles_check_in_patch_unordered_slow()\n");
+  return true;
+}
+
+// ----------------------------------------------------------------------
+// check_bix_id_unordered_slow
+//
+// checks that block indices are correct,
+// id is just enumerating particles
+
+bool cuda_mparticles::check_bidx_id_unordered_slow()
+{
+  uint n_prts_by_patch[n_patches];
+  get_size_all(n_prts_by_patch);
+
+  uint off = 0;
+  for (int p = 0; p < n_patches; p++) {
+    for (int n = 0; n < n_prts_by_patch[p]; n++) {
+      int bidx = get_block_idx(d_xi4[off + n], p);
+      if (!(bidx == d_bidx[off+n])) return false;
+      if (!(off+n == d_id[off+n])) return false;
+    }
+    off += n_prts_by_patch[p];
+  }
+
+  if (!(off == n_prts)) return false;
+  // printf("PASS: cuda_mparticles_check_bidx_id_unordered_slow()\n");
+  return true;
+}
+
+// ----------------------------------------------------------------------
+// check_ordered
+
+bool cuda_mparticles::check_ordered()
+{
+  thrust::host_vector<float4> h_xi4(d_xi4.data(), d_xi4.data() + n_prts);
+  thrust::host_vector<uint> h_off(d_off);
+  thrust::host_vector<uint> h_id(d_id.data(), d_id.data() + n_prts);
+
+  //printf("check_ordered: need_reorder %s\n", need_reorder ? "true" : "false");
+
+  uint off = 0;
+  for (int b = 0; b < n_blocks; b++) {
+    int p = b / n_blocks_per_patch;
+    uint off_b = h_off[b], off_e = h_off[b+1];
+    if (!(off_e >= off_b)) return false;
+    //printf("check_ordered: block %d: %d -> %d (patch %d)\n", b, off_b, off_e, p);
+    if (!(off_b == off)) return false;
+    for (int n = h_off[b]; n < h_off[b+1]; n++) {
+      float4 xi4;
+      if (need_reorder) {
+	xi4 = h_xi4[h_id[n]];
+      } else {
+	xi4 = h_xi4[n];
+      }
+      uint bidx = get_block_idx(xi4, p);
+      //printf("check_ordered: bidx %d\n", bidx);
+      if (b != bidx) {
+	printf("b %d bidx %d n %d p %d xi4 %g %g %g\n",
+	       b, bidx, n, p, xi4.x, xi4.y, xi4.z);
+	uint block_pos_y = (int) floorf(xi4.y * indexer.b_dxi_[1]);
+	uint block_pos_z = (int) floorf(xi4.z * indexer.b_dxi_[2]);
+	printf("block_pos %d %d %g %g\n", block_pos_y, block_pos_z, xi4.y * indexer.b_dxi_[1],
+	       xi4.z * indexer.b_dxi_[2]);
+      }
+      if (!(b == bidx)) return false;
+    }
+    off += off_e - off_b;
+  }
+  if (!(off == n_prts)) return false;
+  // printf("PASS: cuda_mparticles_check_ordered:\n");
+  return true;
+}
+
---
+++ src/libpsc/cuda/cuda_mparticles_gold.cu
@@ -0,0 +1,47 @@
+
+// ----------------------------------------------------------------------
+// reorder_and_offsets_slow
+
+void cuda_mparticles::reorder_and_offsets_slow()
+{
+  if (n_patches == 0) {
+    return;
+  }
+
+  thrust::host_vector<float4> h_xi4(d_xi4.data(), d_xi4.data() + n_prts);
+  thrust::host_vector<float4> h_pxi4(d_pxi4.data(), d_pxi4.data() + n_prts);
+  thrust::host_vector<float4> h_alt_xi4(d_alt_xi4.data(), d_alt_xi4.data() + n_prts);
+  thrust::host_vector<float4> h_alt_pxi4(d_alt_pxi4.data(), d_alt_pxi4.data() + n_prts);
+  thrust::host_vector<uint> h_off(d_off);
+  thrust::host_vector<uint> h_bidx(d_bidx.data(), d_bidx.data() + n_prts);
+  thrust::host_vector<uint> h_id(d_id.data(), d_id.data() + n_prts);
+
+  for (int i = 0; i <= n_prts; i++) {
+    //    uint bidx;
+    uint block;
+    if (i < n_prts) {
+      h_alt_xi4[i] = h_xi4[h_id[i]];
+      h_alt_pxi4[i] = h_pxi4[h_id[i]];
+      //bidx = get_block_idx(cmprts, h_alt_xi4[i], 0);
+      block = h_bidx[i];
+    } else {
+      //bidx = n_blocks;
+      block = n_blocks;
+    }
+    // if (i < 10) {
+    //   printf("i %d bidx %d block %d xi4 %g %g\n", bidx, block, h_alt_xi4[i].y, h_alt_xi4[i].z);
+    // }
+    int prev_block = (i > 0) ? (int) h_bidx[i-1] : -1;
+    for (int b = prev_block + 1; b <= block; b++) {
+      h_off[b] = i;
+    }
+  }
+
+  thrust::copy(h_alt_xi4.begin(), h_alt_xi4.end(), d_alt_xi4.begin());
+  thrust::copy(h_alt_pxi4.begin(), h_alt_pxi4.end(), d_alt_pxi4.begin());
+  thrust::copy(h_off.begin(), h_off.end(), d_off.begin());
+  
+  swap_alt();
+  need_reorder = false;
+}
+
