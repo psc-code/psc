6147bef35085400862cef2b1bcec983205f110de
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-27 01:40:59 -0500

    bndp: consolidate fortran

--- src/libpsc/psc_bnd_particles/psc_bnd_particles_impl.cxx
+++ src/libpsc/psc_bnd_particles/psc_bnd_particles_impl.cxx
@@ -2,6 +2,7 @@
 #include "psc_bnd_particles_private.h"
 #include "psc_particles_single.h"
 #include "psc_particles_double.h"
+#include "psc_particles_fortran.h"
 
 //#include "psc_bnd_particles_open.cxx"
 
@@ -36,3 +37,18 @@ struct psc_bnd_particles_ops_double : psc_bnd_particles_ops {
     //open_calc_moments       = psc_bnd_particles_sub_open_calc_moments;
   }
 } psc_bnd_particles_double_ops;
+
+// ======================================================================
+// psc_bnd_particles: subclass "fortran"
+
+struct psc_bnd_particles_ops_fortran : psc_bnd_particles_ops {
+  using sub = psc_bnd_particles_sub<mparticles_fortran_t>;
+  psc_bnd_particles_ops_fortran() {
+    name                    = "fortran";
+    size                    = sizeof(sub);
+    setup                   = sub::setup;
+    unsetup                 = sub::unsetup;
+    exchange_particles      = sub::exchange_particles;
+    //open_calc_moments       = psc_bnd_particles_sub_open_calc_moments;
+  }
+} psc_bnd_particles_fortran_ops;
