d22e440c3a28174254bb15c128c5c6c2d3742bb4
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-27 01:47:15 -0500

    bndp: mv all of "ordered" policy into the class

--- src/libpsc/psc_bnd_particles/psc_bnd_particles_common2.cxx
+++ src/libpsc/psc_bnd_particles/psc_bnd_particles_common2.cxx
@@ -5,129 +5,6 @@
 
 #include "ddc_particles_inc.c"
 
-// ----------------------------------------------------------------------
-// find_block_indices_count
-
-static inline void
-find_block_indices_count(unsigned int *b_idx, unsigned int *b_cnts,
-			 struct psc_mparticles *mprts, int p, int off)
-{
-  mparticles_t::patch_t& prts = mparticles_t(mprts)[p];
-
-  unsigned int n_prts = prts.size();
-  int *b_mx = prts.b_mx;
-  for (int i = off; i < n_prts; i++) {
-    particle_t *part = &prts[i];
-    int b_pos[3];
-    prts.get_block_pos(&part->xi, b_pos);
-    assert(b_pos[0] >= 0 && b_pos[0] < b_mx[0] &&
-	   b_pos[1] >= 0 && b_pos[1] < b_mx[1] &&
-	   b_pos[2] >= 0 && b_pos[2] < b_mx[2]);
-    b_idx[i] = (b_pos[2] * b_mx[1] + b_pos[1]) * b_mx[0] + b_pos[0];
-    b_cnts[b_idx[i]]++;
-  }
-}
-
-// ----------------------------------------------------------------------
-// find_block_indices_count_reorder
-
-static void _mrc_unused
-find_block_indices_count_reorder(struct psc_mparticles *mprts, int p)
-{
-  mparticles_t::patch_t& prts = mparticles_t(mprts)[p];
-
-  unsigned int n_prts = prts.size();
-  unsigned int cnt = n_prts;
-  int *b_mx = prts.b_mx;
-  memset(prts.b_cnt, 0, (prts.nr_blocks + 1) * sizeof(*prts.b_cnt));
-
-  for (int i = 0; i < n_prts; i++) {
-    particle_t *part = &prts[i];
-    int b_pos[3];
-    prts.get_block_pos(&part->xi, b_pos);
-    if (b_pos[0] >= 0 && b_pos[0] < b_mx[0] &&
-	b_pos[1] >= 0 && b_pos[1] < b_mx[1] &&
-	b_pos[2] >= 0 && b_pos[2] < b_mx[2]) {
-      prts.b_idx[i] = (b_pos[2] * b_mx[1] + b_pos[1]) * b_mx[0] + b_pos[0];
-    } else { // out of bounds
-      prts.b_idx[i] = prts.nr_blocks;
-      prts[cnt] = *part;
-      cnt++;
-    }
-    prts.b_cnt[prts.b_idx[i]]++;
-  }
-}
-
-static void _mrc_unused
-count_and_reorder_to_back(struct psc_mparticles *mprts, int p)
-{
-  mparticles_t::patch_t& prts = mparticles_t(mprts)[p];
-
-  memset(prts.b_cnt, 0, (prts.nr_blocks + 1) * sizeof(*prts.b_cnt));
-  unsigned int n_prts = prts.size();
-  unsigned int cnt = n_prts;
-  for (int i = 0; i < n_prts; i++) {
-    if (prts.b_idx[i] == prts.nr_blocks) {
-      prts[cnt] = prts[i];
-      cnt++;
-    }
-    prts.b_cnt[prts.b_idx[i]]++;
-  }
-}
-
-static void _mrc_unused
-reorder_to_back(struct psc_mparticles *mprts, int p)
-{
-  mparticles_t::patch_t& prts = mparticles_t(mprts)[p];
-
-  unsigned int n_prts = prts.size();
-  unsigned int cnt = n_prts;
-  for (int i = 0; i < n_prts; i++) {
-    if (prts.b_idx[i] == prts.nr_blocks) {
-      prts[cnt] = prts[i];
-      cnt++;
-    }
-  }
-}
-
-// ----------------------------------------------------------------------
-// count_block_indices
-
-static inline void
-count_block_indices(unsigned int *b_cnts, unsigned int *b_idx, int n_prts, int off)
-{
-  for (int i = off; i < n_prts; i++) {
-    b_cnts[b_idx[i]]++;
-  }
-}
-
-// ----------------------------------------------------------------------
-// exclusive_scan
-
-static inline void
-exclusive_scan(unsigned int *b_cnts, int n)
-{
-  unsigned int sum = 0;
-  for (int i = 0; i < n; i++) {
-    unsigned int cnt = b_cnts[i];
-    b_cnts[i] = sum;
-    sum += cnt;
-  }
-}
-
-// ----------------------------------------------------------------------
-// sort_indices
-
-static inline void
-sort_indices(unsigned int *b_idx, unsigned int *b_sum, unsigned int *b_ids, int n_prts)
-{
-  for (int n = 0; n < n_prts; n++) {
-    unsigned int n_new = b_sum[b_idx[n]]++;
-    assert(n_new < n_prts);
-    b_ids[n_new] = n;
-  }
-}
-
 template<typename MP>
 struct bnd_particles_policy_ordered
 {
@@ -135,6 +12,129 @@ struct bnd_particles_policy_ordered
   using ddcp_t = ddc_particles<mparticles_t>;
   using ddcp_patch = typename ddcp_t::patch;
   
+  // ----------------------------------------------------------------------
+  // find_block_indices_count
+
+  static void
+  find_block_indices_count(unsigned int *b_idx, unsigned int *b_cnts,
+			   struct psc_mparticles *mprts, int p, int off)
+  {
+    typename mparticles_t::patch_t& prts = mparticles_t(mprts)[p];
+
+    unsigned int n_prts = prts.size();
+    int *b_mx = prts.b_mx;
+    for (int i = off; i < n_prts; i++) {
+      particle_t *part = &prts[i];
+      int b_pos[3];
+      prts.get_block_pos(&part->xi, b_pos);
+      assert(b_pos[0] >= 0 && b_pos[0] < b_mx[0] &&
+	     b_pos[1] >= 0 && b_pos[1] < b_mx[1] &&
+	     b_pos[2] >= 0 && b_pos[2] < b_mx[2]);
+      b_idx[i] = (b_pos[2] * b_mx[1] + b_pos[1]) * b_mx[0] + b_pos[0];
+      b_cnts[b_idx[i]]++;
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  // find_block_indices_count_reorder
+
+  static void _mrc_unused
+  find_block_indices_count_reorder(struct psc_mparticles *mprts, int p)
+  {
+    typename mparticles_t::patch_t& prts = mparticles_t(mprts)[p];
+
+    unsigned int n_prts = prts.size();
+    unsigned int cnt = n_prts;
+    int *b_mx = prts.b_mx;
+    memset(prts.b_cnt, 0, (prts.nr_blocks + 1) * sizeof(*prts.b_cnt));
+
+    for (int i = 0; i < n_prts; i++) {
+      particle_t *part = &prts[i];
+      int b_pos[3];
+      prts.get_block_pos(&part->xi, b_pos);
+      if (b_pos[0] >= 0 && b_pos[0] < b_mx[0] &&
+	  b_pos[1] >= 0 && b_pos[1] < b_mx[1] &&
+	  b_pos[2] >= 0 && b_pos[2] < b_mx[2]) {
+	prts.b_idx[i] = (b_pos[2] * b_mx[1] + b_pos[1]) * b_mx[0] + b_pos[0];
+      } else { // out of bounds
+	prts.b_idx[i] = prts.nr_blocks;
+	prts[cnt] = *part;
+	cnt++;
+      }
+      prts.b_cnt[prts.b_idx[i]]++;
+    }
+  }
+
+  static void _mrc_unused
+  count_and_reorder_to_back(struct psc_mparticles *mprts, int p)
+  {
+    typename mparticles_t::patch_t& prts = mparticles_t(mprts)[p];
+
+    memset(prts.b_cnt, 0, (prts.nr_blocks + 1) * sizeof(*prts.b_cnt));
+    unsigned int n_prts = prts.size();
+    unsigned int cnt = n_prts;
+    for (int i = 0; i < n_prts; i++) {
+      if (prts.b_idx[i] == prts.nr_blocks) {
+	prts[cnt] = prts[i];
+	cnt++;
+      }
+      prts.b_cnt[prts.b_idx[i]]++;
+    }
+  }
+
+  static void _mrc_unused
+  reorder_to_back(struct psc_mparticles *mprts, int p)
+  {
+    typename mparticles_t::patch_t& prts = mparticles_t(mprts)[p];
+
+    unsigned int n_prts = prts.size();
+    unsigned int cnt = n_prts;
+    for (int i = 0; i < n_prts; i++) {
+      if (prts.b_idx[i] == prts.nr_blocks) {
+	prts[cnt] = prts[i];
+	cnt++;
+      }
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  // count_block_indices
+
+  static void
+  count_block_indices(unsigned int *b_cnts, unsigned int *b_idx, int n_prts, int off)
+  {
+    for (int i = off; i < n_prts; i++) {
+      b_cnts[b_idx[i]]++;
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  // exclusive_scan
+
+  static void
+  exclusive_scan(unsigned int *b_cnts, int n)
+  {
+    unsigned int sum = 0;
+    for (int i = 0; i < n; i++) {
+      unsigned int cnt = b_cnts[i];
+      b_cnts[i] = sum;
+      sum += cnt;
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  // sort_indices
+
+  static void
+  sort_indices(unsigned int *b_idx, unsigned int *b_sum, unsigned int *b_ids, int n_prts)
+  {
+    for (int n = 0; n < n_prts; n++) {
+      unsigned int n_new = b_sum[b_idx[n]]++;
+      assert(n_new < n_prts);
+      b_ids[n_new] = n;
+    }
+  }
+
   // ----------------------------------------------------------------------
   // exchange_mprts_prep
   
