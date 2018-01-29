d1c7c91060927c57418d659110e860dfd07a06a2
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-01-28 23:43:08 -0500

    bndp: use regular setup in cuda_bndp interface

--- src/libpsc/cuda/cuda_bndp_iface.h
+++ src/libpsc/cuda/cuda_bndp_iface.h
@@ -25,16 +25,20 @@ struct bnd_particles_policy_cuda
     : cbndp_(new cuda_bndp())
   {}
 
+  // ----------------------------------------------------------------------
+  // setup
+
+  void setup(Grid_t& grid)
+  {
+    cbndp_->setup(grid);
+  }
+
 protected:
   // ----------------------------------------------------------------------
   // exchange_mprts_prep
   
   void exchange_mprts_prep(ddcp_t* ddcp, mparticles_t mprts)
   {
-    if (!is_setup_) {
-      cbndp_->setup(ppsc->grid);
-      is_setup_ = true;
-    }
     cbndp_->prep(ddcp, mprts->cmprts_);
   }
 
@@ -48,12 +52,16 @@ protected:
 
 private:
   std::unique_ptr<cuda_bndp> cbndp_;
-  bool is_setup_ = false; // FIXME, setup should be done at ctor time
 };
 
 struct psc_bnd_particles_cuda : psc_bnd_particles_sub<mparticles_cuda_t,
 						      bnd_particles_policy_cuda<mparticles_cuda_t>>
 {
+  using Base = psc_bnd_particles_sub<mparticles_cuda_t,
+				     bnd_particles_policy_cuda<mparticles_cuda_t>>;
+
+  using Base::setup;
+  
   // ======================================================================
   // interface to psc_bnd_particles
   // repeated here since there's no way to do this somehow virtual at
@@ -69,6 +77,17 @@ struct psc_bnd_particles_cuda : psc_bnd_particles_sub<mparticles_cuda_t,
     new(sub) psc_bnd_particles_cuda();
   }
   
+  // ----------------------------------------------------------------------
+  // setup
+  
+  static void setup(struct psc_bnd_particles *bnd)
+  {
+    auto sub = static_cast<psc_bnd_particles_cuda*>(bnd->obj.subctx);
+    
+    sub->setup(bnd->psc->mrc_domain);
+    static_cast<policy_t*>(sub)->setup(bnd->psc->grid);
+  }
+
   // ----------------------------------------------------------------------
   // destroy
 
