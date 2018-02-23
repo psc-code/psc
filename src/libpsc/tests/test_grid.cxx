a76c233f0cf18ca790517892abd490412a25c5df
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-02-22 23:09:12 -0500

    tests: RngPool / uniform

--- src/libpsc/tests/test_grid.cxx
+++ src/libpsc/tests/test_grid.cxx
@@ -93,3 +93,19 @@ TEST(mfields, Set)
   EXPECT_EQ(F(EY, 0, 0, 0), 2.);
 }
 
+#include "../vpic/PscRng.h"
+
+using Rng = PscRng;
+using RngPool = PscRngPool<Rng>;
+
+TEST(Rng, RngPool)
+{
+  RngPool rngpool;
+  Rng *rng = rngpool[0];
+
+  for (int i = 0; i < 100; ++i) {
+    double r = rng->uniform(0., 1000.);
+    EXPECT_GE(r, 0.);
+    EXPECT_LE(r, 1000.);
+  }
+}
