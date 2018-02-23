c5fac1def3f23460fff7c110983f7cd4b1b1a74e
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-02-23 11:47:57 -0500

    pushf: make sub() object, call through it

--- src/libpsc/psc_push_fields/psc_push_fields_impl.cxx
+++ src/libpsc/psc_push_fields/psc_push_fields_impl.cxx
@@ -4,31 +4,41 @@
 #include "psc_fields_single.h"
 #include "psc_fields_c.h"
 
+#include "push_fields.hxx"
 #include "psc_push_fields_impl.hxx"
 
 // ----------------------------------------------------------------------
-// psc_push_fields_sub_push_mflds_E
-//
 
-template<typename mfields_t>
+template<typename PushFields_t>
+static void psc_push_fields_sub_setup(struct psc_push_fields *push)
+{
+  PscPushFieldsBase<PushFields_t> pushf(push);
+  new(pushf.sub()) PushFields_t;
+}
+
+template<typename PushFields_t>
+static void psc_push_fields_sub_destroy(struct psc_push_fields *push)
+{
+  PscPushFieldsBase<PushFields_t> pushf(push);
+  pushf.sub()->~PushFields_t();
+}
+
+template<typename PushFields_t>
 static void psc_push_fields_sub_push_mflds_E(struct psc_push_fields *push,
 					     struct psc_mfields *mflds_base,
 					     double dt_fac)
 {
-  PushFields<mfields_t> pushf;
-  pushf.push_E(push, mflds_base, dt_fac);
+  PscPushFieldsBase<PushFields_t> pushf(push);
+  pushf->push_E(push, mflds_base, dt_fac);
 }
 
-// ----------------------------------------------------------------------
-// psc_push_fields_sub_push_mflds_H
-
-template<typename mfields_t>
+template<typename PushFields_t>
 static void psc_push_fields_sub_push_mflds_H(struct psc_push_fields *push,
 					     struct psc_mfields *mflds_base,
 					     double dt_fac)
 {
-  PushFields<mfields_t> pushf;
-  pushf.push_H(push, mflds_base, dt_fac);
+  PscPushFieldsBase<PushFields_t> pushf(push);
+  pushf->push_H(push, mflds_base, dt_fac);
 }
 
 // ======================================================================
@@ -36,9 +46,13 @@ static void psc_push_fields_sub_push_mflds_H(struct psc_push_fields *push,
 
 struct psc_push_fields_ops_single : psc_push_fields_ops {
   psc_push_fields_ops_single() {
+    using PushFields_t = PushFields<mfields_single_t>;
     name                  = "single";
-    push_mflds_E          = psc_push_fields_sub_push_mflds_E<mfields_single_t>;
-    push_mflds_H          = psc_push_fields_sub_push_mflds_H<mfields_single_t>;
+    size                  = sizeof(PushFields_t);
+    setup                 = psc_push_fields_sub_setup<PushFields_t>;
+    destroy               = psc_push_fields_sub_destroy<PushFields_t>;
+    push_mflds_E          = psc_push_fields_sub_push_mflds_E<PushFields_t>;
+    push_mflds_H          = psc_push_fields_sub_push_mflds_H<PushFields_t>;
   }
 } psc_push_fields_single_ops;
 
@@ -47,8 +61,12 @@ struct psc_push_fields_ops_single : psc_push_fields_ops {
 
 struct psc_push_fields_ops_c : psc_push_fields_ops {
   psc_push_fields_ops_c() {
+    using PushFields_t = PushFields<mfields_single_t>;
     name                  = "c";
-    push_mflds_E          = psc_push_fields_sub_push_mflds_E<mfields_c_t>;
-    push_mflds_H          = psc_push_fields_sub_push_mflds_H<mfields_c_t>;
+    size                  = sizeof(PushFields_t);
+    setup                 = psc_push_fields_sub_setup<PushFields_t>;
+    destroy               = psc_push_fields_sub_destroy<PushFields_t>;
+    push_mflds_E          = psc_push_fields_sub_push_mflds_E<PushFields_t>;
+    push_mflds_H          = psc_push_fields_sub_push_mflds_H<PushFields_t>;
   }
 } psc_push_fields_c_ops;
