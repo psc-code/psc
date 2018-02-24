29e971a38e7f573f5c6f7b8bcf2a575173ece51c
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-02-23 22:32:19 -0500

    pushp/1vbec: put static boilerplate into class

--- src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_double.cxx
+++ src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_double.cxx
@@ -26,55 +26,53 @@ public:
 
   void push_mprts_1(struct psc_mparticles *mprts, struct psc_mfields *mflds_base) override
   { push_p_ops_1vbec_double<dim_1>::push_mprts(nullptr, mprts, mflds_base); }
+
+  static void setup(struct psc_push_particles *push)
+  {
+    PscPushParticles<PushParticles1vbecDouble> pushp(push);
+    new(pushp.sub()) PushParticles1vbecDouble;
+  }
+
+  static void destroy(struct psc_push_particles *push)
+  {
+    PscPushParticles<PushParticles1vbecDouble> pushp(push);
+    pushp.sub()->~PushParticles1vbecDouble();
+  }
+
+  static void push_mprts_xyz(struct psc_push_particles *push,
+			     struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
+  {
+    PscPushParticles<PushParticles1vbecDouble> pushp(push);
+    pushp->push_mprts_xyz(mprts, mflds_base);
+  }
+  
+  static void push_mprts_yz(struct psc_push_particles *push,
+			    struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
+  {
+    PscPushParticles<PushParticles1vbecDouble> pushp(push);
+    pushp->push_mprts_yz(mprts, mflds_base);
+  }
+  
+  static void push_mprts_1(struct psc_push_particles *push,
+			   struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
+  {
+    PscPushParticles<PushParticles1vbecDouble> pushp(push);
+    pushp->push_mprts_1(mprts, mflds_base);
+  }
+
 };
 
-static void pushp_1vbec_double_setup(struct psc_push_particles *push)
-{
-  psc_push_particles_setup_super(push);
-
-  PscPushParticles<PushParticles1vbecDouble> pushp(push);
-  new(pushp.sub()) PushParticles1vbecDouble;
-}
-
-static void pushp_1vbec_double_destroy(struct psc_push_particles *push)
-{
-  PscPushParticles<PushParticles1vbecDouble> pushp(push);
-  pushp.sub()->~PushParticles1vbecDouble();
-}
-
-static void pushp_1vbec_double_push_mprts_xyz(struct psc_push_particles *push,
-					     struct psc_mparticles *mprts,
-					     struct psc_mfields *mflds_base)
-{
-  PscPushParticles<PushParticles1vbecDouble> pushp(push);
-  pushp->push_mprts_xyz(mprts, mflds_base);
-}
-  
-static void pushp_1vbec_double_push_mprts_yz(struct psc_push_particles *push,
-					     struct psc_mparticles *mprts,
-					     struct psc_mfields *mflds_base)
-{
-  PscPushParticles<PushParticles1vbecDouble> pushp(push);
-  pushp->push_mprts_yz(mprts, mflds_base);
-}
-  
-static void pushp_1vbec_double_push_mprts_1(struct psc_push_particles *push,
-					    struct psc_mparticles *mprts,
-					    struct psc_mfields *mflds_base)
-{
-  PscPushParticles<PushParticles1vbecDouble> pushp(push);
-  pushp->push_mprts_1(mprts, mflds_base);
-}
+using PushParticles_t = PushParticles1vbecDouble;
   
 struct psc_push_particles_ops_1vbec_double : psc_push_particles_ops {
   psc_push_particles_ops_1vbec_double() {
     name                  = "1vbec_double";
     size                  = sizeof(PushParticles1vbecDouble);
-    setup                 = pushp_1vbec_double_setup;
-    destroy               = pushp_1vbec_double_destroy;
-    push_mprts_xyz        = push_p_ops_1vbec_double<dim_xyz>::push_mprts;
-    push_mprts_yz         = pushp_1vbec_double_push_mprts_yz;
-    push_mprts_1          = push_p_ops_1vbec_double<dim_1>::push_mprts;
+    setup                 = PushParticles_t::setup;
+    destroy               = PushParticles_t::destroy;
+    push_mprts_xyz        = PushParticles_t::push_mprts_xyz;
+    push_mprts_yz         = PushParticles_t::push_mprts_yz;
+    push_mprts_1          = PushParticles_t::push_mprts_1;
     //stagger_mprts_1      = push_p_ops_1vbec_double<dim_1>::stagger_mprts;
     particles_type        = PARTICLE_TYPE;
   }
