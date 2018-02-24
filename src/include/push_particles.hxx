c0e04fa5d60dab5bb7d0a836b936e7a9467c41bc
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-02-24 00:09:18 -0500

    pushp: get rid of most wrapper provided ops functions

--- src/include/push_particles.hxx
+++ src/include/push_particles.hxx
@@ -101,68 +101,12 @@ public:
     pushp.sub()->~PushParticles_t();
   }
 
-  static void push_mprts_xyz(struct psc_push_particles *push,
-			     struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
+  static void push_mprts(struct psc_push_particles *push,
+			 struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
   {
     PscPushParticles<PushParticles_t> pushp(push);
-    pushp->push_mprts_xyz(mprts, mflds_base);
+    pushp->push_mprts(mprts, mflds_base);
   }
 
-  static void push_mprts_xy(struct psc_push_particles *push,
-			    struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
-  {
-    PscPushParticles<PushParticles_t> pushp(push);
-    pushp->push_mprts_xy(mprts, mflds_base);
-  }
-  
-  static void push_mprts_xz(struct psc_push_particles *push,
-			    struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
-  {
-    PscPushParticles<PushParticles_t> pushp(push);
-    pushp->push_mprts_xz(mprts, mflds_base);
-  }
-  
-  static void push_mprts_yz(struct psc_push_particles *push,
-			    struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
-  {
-    PscPushParticles<PushParticles_t> pushp(push);
-    pushp->push_mprts_yz(mprts, mflds_base);
-  }
-  
-  static void push_mprts_x(struct psc_push_particles *push,
-			   struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
-  {
-    PscPushParticles<PushParticles_t> pushp(push);
-    pushp->push_mprts_x(mprts, mflds_base);
-  }
-
-  static void push_mprts_y(struct psc_push_particles *push,
-			   struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
-  {
-    PscPushParticles<PushParticles_t> pushp(push);
-    pushp->push_mprts_y(mprts, mflds_base);
-  }
-
-  static void push_mprts_z(struct psc_push_particles *push,
-			   struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
-  {
-    PscPushParticles<PushParticles_t> pushp(push);
-    pushp->push_mprts_z(mprts, mflds_base);
-  }
-
-  static void push_mprts_1(struct psc_push_particles *push,
-			   struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
-  {
-    PscPushParticles<PushParticles_t> pushp(push);
-    pushp->push_mprts_1(mprts, mflds_base);
-  }
-
-  static void stagger_mprts_yz(struct psc_push_particles *push,
-			       struct psc_mparticles *mprts, struct psc_mfields *mflds_base)
-  {
-    PscPushParticles<PushParticles_t> pushp(push);
-    pushp->stagger_mprts_yz(mprts, mflds_base);
-  }
-  
 };
 
