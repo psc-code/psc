7957be0b2f19525cc71789211c6fdbf897bbfc61
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-02-26 13:54:25 -0500

    pushp: make push_p_config, Config alike

--- src/libpsc/psc_push_particles/push_config.hxx
+++ src/libpsc/psc_push_particles/push_config.hxx
@@ -9,7 +9,9 @@ struct CacheFields;
 
 template<typename MP, typename MF, typename D,
 	 typename IP, typename O,
-	 typename CALCJ, typename OPT_EXT = opt_ext_none>
+	 typename CALCJ = opt_calcj_esirkepov,
+	 typename OPT_EXT = opt_ext_none,
+	 typename CF = CacheFieldsNone>
 struct push_p_config
 {
   using mparticles_t = MP;
@@ -19,10 +21,13 @@ struct push_p_config
   using order = O;
   using calcj = CALCJ;
   using ext = OPT_EXT;
+  using CacheFields = CF;
 };
 
 template<typename MP, typename MF, typename D,
-	 typename IP, typename O = opt_order_2nd,
+	 typename IP, typename O,
+	 typename CALCJ = opt_calcj_esirkepov,
+	 typename OPT_EXT = opt_ext_none,
 	 typename CF = CacheFieldsNone>
 struct Config
 {
@@ -31,21 +36,23 @@ struct Config
   using dim = D;
   using ip = IP;
   using order = O;
+  using calcj = CALCJ;
   using CacheFields = CF;
 };
 
 #include "psc_particles_double.h"
 #include "psc_fields_c.h"
 
-using Config2ndXYZ = Config<PscMparticlesDouble, PscMfieldsC, dim_xyz, opt_ip_2nd>;
-using Config2ndXY = Config<PscMparticlesDouble, PscMfieldsC, dim_xy, opt_ip_2nd>;
-using Config2ndXZ = Config<PscMparticlesDouble, PscMfieldsC, dim_xz, opt_ip_2nd>;
-using Config2ndYZ = Config<PscMparticlesDouble, PscMfieldsC, dim_yz, opt_ip_2nd>;
-using Config2ndY = Config<PscMparticlesDouble, PscMfieldsC, dim_y, opt_ip_2nd>;
-using Config2ndZ = Config<PscMparticlesDouble, PscMfieldsC, dim_z, opt_ip_2nd>;
-using Config2nd1 = Config<PscMparticlesDouble, PscMfieldsC, dim_1, opt_ip_2nd>;
+using Config2ndXYZ = Config<PscMparticlesDouble, PscMfieldsC, dim_xyz, opt_ip_2nd, opt_order_2nd>;
+using Config2ndXY = Config<PscMparticlesDouble, PscMfieldsC, dim_xy, opt_ip_2nd, opt_order_2nd>;
+using Config2ndXZ = Config<PscMparticlesDouble, PscMfieldsC, dim_xz, opt_ip_2nd, opt_order_2nd>;
+using Config2ndYZ = Config<PscMparticlesDouble, PscMfieldsC, dim_yz, opt_ip_2nd, opt_order_2nd>;
+using Config2ndY = Config<PscMparticlesDouble, PscMfieldsC, dim_y, opt_ip_2nd, opt_order_2nd>;
+using Config2ndZ = Config<PscMparticlesDouble, PscMfieldsC, dim_z, opt_ip_2nd, opt_order_2nd>;
+using Config2nd1 = Config<PscMparticlesDouble, PscMfieldsC, dim_1, opt_ip_2nd, opt_order_2nd>;
 
-using Config2ndDoubleYZ = Config<PscMparticlesDouble, PscMfieldsC, dim_yz, opt_ip_2nd, opt_order_2nd, CacheFields>;
+using Config2ndDoubleYZ = Config<PscMparticlesDouble, PscMfieldsC, dim_yz, opt_ip_2nd, opt_order_2nd,
+				 opt_calcj_esirkepov, opt_ext_none, CacheFields>;
 
 using Config1stXZ = Config<PscMparticlesDouble, PscMfieldsC, dim_xz, opt_ip_1st, opt_order_1st>;
 using Config1stYZ = Config<PscMparticlesDouble, PscMfieldsC, dim_yz, opt_ip_1st, opt_order_1st>;
