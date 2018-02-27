40487fe567037201ee53318516827a9232ceb172
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-02-26 23:25:15 -0500

    pushp: template template parameter for CacheFields

--- src/libpsc/psc_push_particles/push_config.hxx
+++ src/libpsc/psc_push_particles/push_config.hxx
@@ -16,7 +16,7 @@ template<typename MP, typename MF, typename D,
 	 typename IP, typename O,
 	 typename CALCJ = opt_calcj_esirkepov,
 	 typename OPT_EXT = opt_ext_none,
-	 typename CF = CacheFieldsNone<typename MF::fields_t, D>,
+	 template<typename, typename> class CF = CacheFieldsNone,
 	 typename EM = Fields3d<typename MF::fields_t>>
 struct push_p_config
 {
@@ -27,7 +27,7 @@ struct push_p_config
   using order = O;
   using calcj = CALCJ;
   using ext = OPT_EXT;
-  using CacheFields = CF;
+  using CacheFields = CF<typename MF::fields_t, D>;
 
   using FieldsEM = EM;
 };
@@ -41,7 +41,7 @@ template<typename dim>
 using Config2nd = push_p_config<MparticlesDouble, MfieldsC, dim, opt_ip_2nd, opt_order_2nd>;
 
 using Config2ndDoubleYZ = push_p_config<MparticlesDouble, MfieldsC, dim_yz, opt_ip_2nd, opt_order_2nd,
-					opt_calcj_esirkepov, opt_ext_none, CacheFields<MfieldsC::fields_t, dim_yz>>;
+					opt_calcj_esirkepov, opt_ext_none, CacheFields>;
 
 template<typename dim>
 using Config1st = push_p_config<MparticlesDouble, MfieldsC, dim, opt_ip_1st, opt_order_1st>;
@@ -55,8 +55,8 @@ using Config1vbecSingle = push_p_config<MparticlesSingle, MfieldsSingle, dim, op
 					opt_order_1st, opt_calcj_1vb_var1>;
 
 using Config1vbecSingleXZ = push_p_config<MparticlesSingle, MfieldsSingle, dim_xyz, opt_ip_1st_ec, opt_order_1st,
-					  opt_calcj_1vb_split, opt_ext_none, CacheFieldsNone<MfieldsC::fields_t, dim_xyz>,
+					  opt_calcj_1vb_split, opt_ext_none, CacheFieldsNone,
 					  Fields3d<typename MfieldsSingle::fields_t, dim_xz>>;
 using Config1vbecSingle1 = push_p_config<MparticlesSingle, MfieldsSingle, dim_1, opt_ip_1st_ec, opt_order_1st,
-					 opt_calcj_1vb_var1, opt_ext_none, CacheFieldsNone<MfieldsC::fields_t, dim_1>,
+					 opt_calcj_1vb_var1, opt_ext_none, CacheFieldsNone,
 					 Fields3d<typename MfieldsSingle::fields_t, dim_1>>;
