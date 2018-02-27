5d535015efeeca62fdc434d2b9b5c6c3485e14f5
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-02-26 23:48:30 -0500

    pushp: test yz / calcj_split

---
+++ src/libpsc/psc_push_particles/1vb/psc_push_particles_1vbec_split_single_yz.cxx
@@ -0,0 +1,20 @@
+
+#include "psc_push_particles_private.h"
+
+#include "psc_particles_as_single.h"
+#include "psc_fields_as_single.h"
+
+#include "psc_push_particles_1vb.h"
+
+#define DIM DIM_YZ
+
+#include "../inc_defs.h"
+#include "../push_config.hxx"
+
+#define CALC_J CALC_J_1VB_SPLIT
+
+using push_p_conf = push_p_config<MparticlesSingle, MfieldsSingle, dim_yz, opt_ip_1st_ec, opt_order_1st,
+				  opt_calcj_1vb_var1>;
+
+#include "../1vb.c"
+
