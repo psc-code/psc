d1e3ac8f9dc860b656ce2f979a1f3c50c0026c97
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-02-28 15:00:51 -0500

    pushp: make an actual curr_cache_t

--- src/libpsc/psc_push_particles/inc_curr_cache_none.c
+++ src/libpsc/psc_push_particles/inc_curr_cache_none.c
@@ -1,5 +1,20 @@
 
-typedef flds_curr_t curr_cache_t;
+struct curr_cache_t
+{
+  using real_t = typename flds_curr_t::real_t;
+
+  curr_cache_t(flds_curr_t& flds)
+    : f_(flds)
+  {}
+  
+  real_t* data()      { return f_.data(); }
+  int n_comps() const { return f_.n_comps(); }
+  Int3 ib() const     { return f_.ib(); }
+  Int3 im() const     { return f_.im(); }
+
+private:
+  flds_curr_t f_;
+};
 
 #ifndef CURR_CACHE_DIM
 #define CURR_CACHE_DIM DIM_XYZ
@@ -8,7 +23,7 @@ typedef flds_curr_t curr_cache_t;
 CUDA_DEVICE static inline curr_cache_t
 curr_cache_create(flds_curr_t flds_curr, int ci0[3])
 {
-  return flds_curr;
+  return curr_cache_t{flds_curr};
 }
 
 CUDA_DEVICE static inline void
@@ -16,11 +31,11 @@ curr_cache_add(curr_cache_t curr_cache, int m, int i, int j, int k,
 	       curr_cache_t::real_t val)
 {
 #if CURR_CACHE_DIM == DIM_XYZ
-  using FieldsJ = Fields3d<flds_curr_t, dim_xyz>;
+  using FieldsJ = Fields3d<curr_cache_t, dim_xyz>;
 #elif CURR_CACHE_DIM == DIM_XZ
-  using FieldsJ = Fields3d<flds_curr_t, dim_xz>;
+  using FieldsJ = Fields3d<curr_cache_t, dim_xz>;
 #elif CURR_CACHE_DIM == DIM_1
-  using FieldsJ = Fields3d<flds_curr_t, dim_1>;
+  using FieldsJ = Fields3d<curr_cache_t, dim_1>;
 #else
 #error unhandled CURR_CACHE_DIM
 #endif
