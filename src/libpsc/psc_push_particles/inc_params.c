2523f63d4f36e63fac1039d60b7a549778a69a79
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-01 18:05:37 -0500

    pushp: global opt_calcj gone

--- src/libpsc/psc_push_particles/inc_params.c
+++ src/libpsc/psc_push_particles/inc_params.c
@@ -25,14 +25,6 @@
 #define IF_NOT_DIM_Z(s) s do{} while(0)
 #endif
 
-#if CALC_J == CALC_J_1VB_SPLIT
-using opt_calcj = opt_calcj_1vb_split;
-#elif CALC_J == CALC_J_1VB_VAR1
-using opt_calcj = opt_calcj_1vb_var1;
-#elif CALC_J == CALC_J_1VB_2D
-using opt_calcj = opt_calcj_1vb_2d;
-#endif
-
 #define CUDA_CONSTANT
 #define CUDA_DEVICE
 #define __forceinline__
