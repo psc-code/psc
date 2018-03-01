e1e3a077495afb497cfd92d0986918d2c0e586a6
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-01 13:46:55 -0500

    pushp: 1vb_split tag dispatch

--- src/libpsc/psc_push_particles/inc_curr_1vb_split.c
+++ src/libpsc/psc_push_particles/inc_curr_1vb_split.c
@@ -15,11 +15,9 @@ struct Current1vb
     fnqys_ = grid.dx[1] * grid.fnqs / grid.dt;
     fnqzs_ = grid.dx[2] * grid.fnqs / grid.dt;
   }
-  
-#if DIM == DIM_YZ
 
   void calc_j2_one_cell(curr_cache_t curr_cache, real_t qni_wni,
-			real_t xm[3], real_t xp[3])
+			real_t xm[3], real_t xp[3], dim_yz tag_dim)
   {
 
     real_t dx[3] = { xp[0] - xm[0], xp[1] - xm[1], xp[2] - xm[2] };
@@ -73,10 +71,8 @@ struct Current1vb
 #endif
   }
 
-#elif DIM == DIM_XYZ
-
   void calc_j2_one_cell(curr_cache_t curr_cache, real_t qni_wni,
-			real_t xm[3], real_t xp[3])
+			real_t xm[3], real_t xp[3], dim_xyz tag_dim)
   {
     real_t dx[3] = { xp[0] - xm[0], xp[1] - xm[1], xp[2] - xm[2] };
     real_t xa[3]= { .5f * (xm[0] + xp[0]),
@@ -109,8 +105,16 @@ struct Current1vb
     curr_cache.add(2, i[0]+1,i[1]+1,i[2]  , fnqz * (dx[2] * (      xa[0]) * (      xa[1]) + h));
   }
 
+  void calc_j2_one_cell(curr_cache_t curr_cache, real_t qni_wni,
+			real_t xm[3], real_t xp[3])
+  {
+#if DIM == DIM_YZ
+    calc_j2_one_cell(curr_cache, qni_wni, xm, xp, dim_yz{});
+#elif DIM == DIM_XYZ
+    calc_j2_one_cell(curr_cache, qni_wni, xm, xp, dim_xyz{});
 #endif
-
+  }
+  
   CUDA_DEVICE __forceinline__ static void
   calc_j2_split_along_dim(int dim, int im, real_t x1[3],
 			  real_t xm[3], real_t xp[3])
@@ -132,31 +136,8 @@ struct Current1vb
     }
   }
 
-#if DIM == DIM_YZ
-
-#if 0
-  CUDA_DEVICE __forceinline__ static void 
-  calc_j2_split_dim(curr_cache_t curr_cache, real_t qni_wni,
-		    real_t *xm, real_t *xp, int dim)
-  {
-    if (dim == 0) {
-      calc_j2_one_cell(curr_cache, qni_wni, xm, xp);
-    } else {
-      int im = fint(xm[dim]);
-      if (xp[dim] > im + 1 || xp[dim] < im) {
-	real_t x1[3];
-	calc_j2_split_along_dim(dim, im, x1, xm, xp);
-	calc_j2_split_dim(curr_cache, qni_wni, xm, x1, dim - 1);
-	calc_j2_split_dim(curr_cache, qni_wni, x1, xp, dim - 1);
-      } else {
-	calc_j2_split_dim(curr_cache, qni_wni, xm, xp, dim - 1);
-      }
-    }
-  }
-#endif
-
   void calc_j2_split_dim_y(curr_cache_t curr_cache, real_t qni_wni,
-			   real_t *xm, real_t *xp)
+			   real_t *xm, real_t *xp, dim_yz tag_dim)
   {
     const int dim = 1;
     int im = fint(xm[dim]);
@@ -171,42 +152,19 @@ struct Current1vb
   }
 
   void calc_j2_split_dim_z(curr_cache_t curr_cache, real_t qni_wni,
-			   real_t *xm, real_t *xp)
+			   real_t *xm, real_t *xp, dim_yz tag_dim)
   {
     const int dim = 2;
     int im = fint(xm[dim]);
     if (xp[dim] > im + 1 || xp[dim] < im) {
       real_t x1[3];
       calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, x1);
-      calc_j2_split_dim_y(curr_cache, qni_wni, x1, xp);
+      calc_j2_split_dim_y(curr_cache, qni_wni, xm, x1, tag_dim);
+      calc_j2_split_dim_y(curr_cache, qni_wni, x1, xp, tag_dim);
     } else {
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, xp);
+      calc_j2_split_dim_y(curr_cache, qni_wni, xm, xp, tag_dim);
     }
   }
-
-#elif DIM == DIM_XYZ
-
-#if 0
-  CUDA_DEVICE __forceinline__ static void
-  calc_j2_split_dim(curr_cache_t curr_cache, real_t qni_wni,
-		    real_t *xm, real_t *xp, int dim)
-  {
-    if (dim == -1) {
-      calc_j2_one_cell(curr_cache, qni_wni, xm, xp);
-    } else {
-      int im = fint(xm[dim]);
-      if (xp[dim] > im + 1 || xp[dim] < im) {
-	real_t x1[3];
-	calc_j2_split_along_dim(dim, im, x1, xm, xp);
-	calc_j2_split_dim(curr_cache, qni_wni, xm, x1, dim - 1);
-	calc_j2_split_dim(curr_cache, qni_wni, x1, xp, dim - 1);
-      } else {
-	calc_j2_split_dim(curr_cache, qni_wni, xm, xp, dim - 1);
-      }
-    }
-  }
-#endif
 
   void calc_j2_split_dim_x(curr_cache_t curr_cache, real_t qni_wni,
 			   real_t *xm, real_t *xp)
@@ -224,7 +182,7 @@ struct Current1vb
   }
 
   void calc_j2_split_dim_y(curr_cache_t curr_cache, real_t qni_wni,
-			   real_t *xm, real_t *xp)
+			   real_t *xm, real_t *xp, dim_xyz tag_dim)
   {
     const int dim = 1;
     int im = fint(xm[dim]);
@@ -239,22 +197,30 @@ struct Current1vb
   }
 
   void calc_j2_split_dim_z(curr_cache_t curr_cache, real_t qni_wni,
-			   real_t *xm, real_t *xp)
+			   real_t *xm, real_t *xp, dim_xyz tag_dim)
   {
     const int dim = 2;
     int im = fint(xm[dim]);
     if (xp[dim] > im + 1 || xp[dim] < im) {
       real_t x1[3];
       calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, x1);
-      calc_j2_split_dim_y(curr_cache, qni_wni, x1, xp);
+      calc_j2_split_dim_y(curr_cache, qni_wni, xm, x1, tag_dim);
+      calc_j2_split_dim_y(curr_cache, qni_wni, x1, xp, tag_dim);
     } else {
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, xp);
+      calc_j2_split_dim_y(curr_cache, qni_wni, xm, xp, tag_dim);
     }
   }
 
+  void calc_j2_split_dim_z(curr_cache_t curr_cache, real_t qni_wni,
+			   real_t *xm, real_t *xp)
+  {
+#if DIM == DIM_YZ
+    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp, dim_yz{});
+#elif DIM == DIM_XYZ
+    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp, dim_xyz{});
 #endif
-
+  }
+  
   // ----------------------------------------------------------------------
   // calc_j
 
