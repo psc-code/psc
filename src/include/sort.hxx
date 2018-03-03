c2aa2138435435d4ce5d6349a2a2b145a3fe1b19
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-03 16:27:33 -0500

    sort: mv wrappers -> header

--- src/include/sort.hxx
+++ src/include/sort.hxx
@@ -4,6 +4,8 @@
 #include "particles.hxx"
 #include "psc_sort_private.h"
 
+#include <mrc_profile.h>
+
 // ======================================================================
 // PscSort
 
@@ -39,3 +41,60 @@ public:
 };
 
 using PscSortBase = PscSort<SortBase>;
+
+// ======================================================================
+// PscSort_
+//
+// wraps get_as / put_as
+
+template<class Sort>
+class PscSort_ : SortBase
+{
+public:
+  using mparticles_t = typename Sort::mparticles_t;
+  
+  void run(struct psc_mparticles *mprts_base) override
+  {
+    mparticles_t mprts = mprts_base->get_as<mparticles_t>();
+    
+    sort_(mprts);
+    
+    mprts.put_as(mprts_base);
+  }
+
+private:
+  Sort sort_{};
+};
+
+template<typename Sort>
+class PscSortWrapper
+{
+public:
+  const static size_t size = sizeof(Sort);
+  
+  static void setup(psc_sort* _sort)
+  {
+    PscSort<Sort> sort(_sort);
+    new(sort.sub()) Sort;
+  }
+
+  static void destroy(psc_sort* _sort)
+  {
+    PscSort<Sort> sort(_sort);
+    sort->~Sort();
+  }
+
+  static void run(struct psc_sort* _sort, struct psc_mparticles* mprts)
+  {
+    static int pr;
+    if (!pr) {
+      pr = prof_register("sort", 1., 0, 0);
+    }
+
+    PscSort<Sort> sort(_sort);
+    prof_start(pr);
+    sort->run(mprts);
+    prof_stop(pr);
+  }
+};
+
--- src/libpsc/psc_sort/psc_sort_impl.cxx
+++ src/libpsc/psc_sort/psc_sort_impl.cxx
@@ -2,62 +2,6 @@
 #include "psc_sort_impl.hxx"
 #include "sort.hxx"
 
-// ======================================================================
-// PscSort_
-//
-// wraps get_as / put_as
-
-template<class Sort>
-class PscSort_ : SortBase
-{
-public:
-  using mparticles_t = typename Sort::mparticles_t;
-  
-  void run(struct psc_mparticles *mprts_base) override
-  {
-    mparticles_t mprts = mprts_base->get_as<mparticles_t>();
-    
-    sort_(mprts);
-    
-    mprts.put_as(mprts_base);
-  }
-
-private:
-  Sort sort_{};
-};
-
-template<typename Sort>
-class PscSortWrapper
-{
-public:
-  const static size_t size = sizeof(Sort);
-  
-  static void setup(psc_sort* _sort)
-  {
-    PscSort<Sort> sort(_sort);
-    new(sort.sub()) Sort;
-  }
-
-  static void destroy(psc_sort* _sort)
-  {
-    PscSort<Sort> sort(_sort);
-    sort->~Sort();
-  }
-
-  static void run(struct psc_sort* _sort, struct psc_mparticles* mprts)
-  {
-    static int pr;
-    if (!pr) {
-      pr = prof_register("sort", 1., 0, 0);
-    }
-
-    PscSort<Sort> sort(_sort);
-    prof_start(pr);
-    sort->run(mprts);
-    prof_stop(pr);
-  }
-};
-
 #include "psc_particles_single.h"
 #include "psc_particles_double.h"
 
