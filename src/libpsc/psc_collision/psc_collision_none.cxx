f42eb533cf932d80b35cc5436a881b12068af5e8
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-03 22:03:01 -0500

    collision: PscCollisionNone

--- src/libpsc/psc_collision/psc_collision_none.cxx
+++ src/libpsc/psc_collision/psc_collision_none.cxx
@@ -1,23 +1,32 @@
 
 #include "psc_collision_private.h"
+#include "collision.hxx"
 
 #include <mrc_profile.h>
 
-// ----------------------------------------------------------------------
-// psc_collision_none_run
+// ======================================================================
+// psc_collision_none
 
-static void
-psc_collision_none_run(struct psc_collision *collision,
-		       struct psc_mparticles *mprts_base)
+struct PscCollisionNone
 {
-}
+  PscCollisionNone(MPI_Comm comm)
+  {}
+  
+  void run(struct psc_mparticles *mprts_base)
+  {}
+};
 
 // ======================================================================
 // psc_collision: subclass "none"
 
-struct psc_collision_ops_none : psc_collision_ops {
-  psc_collision_ops_none() {
+struct psc_collision_none_ops : psc_collision_ops {
+  using Collision = CollisionWrapper<PscCollisionNone>;
+  psc_collision_none_ops() {
     name                  = "none";
-    run                   = psc_collision_none_run;
+    size                  = Collision::size;
+    setup                 = Collision::setup;
+    destroy               = Collision::destroy;
+    run                   = Collision::run;
   }
 } psc_collision_none_ops;
+
