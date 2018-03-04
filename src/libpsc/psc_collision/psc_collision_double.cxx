7ed04d32e5d34aa2e3ef5d08d604ecce975ebcf9
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-03 23:06:32 -0500

    collision: no more #define

--- src/libpsc/psc_collision/psc_collision_double.cxx
+++ src/libpsc/psc_collision/psc_collision_double.cxx
@@ -2,9 +2,6 @@
 #include <psc_fields_as_c.h>
 #include <psc_particles_as_double.h>
 
-#define psc_output_fields_item_coll_stats_ops psc_output_fields_item_coll_stats_double_ops
-#define psc_output_fields_item_coll_rei_ops psc_output_fields_item_coll_rei_double_ops
-
 #include "psc_collision_common.cxx"
 
 // ======================================================================
@@ -15,9 +12,9 @@ psc_collision_ops_<Collision_<mparticles_t>> psc_collision_double_ops;
 // ======================================================================
 // psc_output_fields_item: subclass "coll_stats"
 
-struct psc_output_fields_item_ops_coll : psc_output_fields_item_ops {
+struct psc_output_fields_item_ops_coll_double : psc_output_fields_item_ops {
   using Collision = Collision_<mparticles_t>;
-  psc_output_fields_item_ops_coll() {
+  psc_output_fields_item_ops_coll_double() {
     name      = "coll_stats_" PARTICLE_TYPE;
     nr_comp   = Collision::NR_STATS;
     fld_names[0] = "coll_nudt_min";
@@ -27,14 +24,14 @@ struct psc_output_fields_item_ops_coll : psc_output_fields_item_ops {
     fld_names[4] = "coll_ncoll";
     run_all   = CollisionWrapper<Collision>::copy_stats;
   }
-} psc_output_fields_item_coll_stats_ops;
+} psc_output_fields_item_coll_stats_double_ops;
 
 // ======================================================================
 // psc_output_fields_item: subclass "coll_rei"
 
-struct psc_output_fields_item_ops_coll_rei : psc_output_fields_item_ops {
+struct psc_output_fields_item_ops_coll_rei_double : psc_output_fields_item_ops {
   using Collision = Collision_<mparticles_t>;
-  psc_output_fields_item_ops_coll_rei() {
+  psc_output_fields_item_ops_coll_rei_double() {
     name      = "coll_rei_" PARTICLE_TYPE;
     nr_comp   = 3;
     fld_names[0] = "coll_rei_x";
@@ -42,4 +39,4 @@ struct psc_output_fields_item_ops_coll_rei : psc_output_fields_item_ops {
     fld_names[2] = "coll_rei_z";
     run_all   = CollisionWrapper<Collision>::copy_rei;
   }
-} psc_output_fields_item_coll_rei_ops;
+} psc_output_fields_item_coll_rei_double_ops;
--- src/libpsc/psc_collision/psc_collision_single.cxx
+++ src/libpsc/psc_collision/psc_collision_single.cxx
@@ -2,9 +2,6 @@
 #include <psc_fields_as_single.h>
 #include <psc_particles_as_single.h>
 
-#define psc_output_fields_item_coll_stats_ops psc_output_fields_item_coll_stats_single_ops
-#define psc_output_fields_item_coll_rei_ops psc_output_fields_item_coll_rei_single_ops
-
 #include "psc_collision_common.cxx"
 
 // ======================================================================
@@ -15,9 +12,9 @@ psc_collision_ops_<Collision_<mparticles_t>> psc_collision_single_ops;
 // ======================================================================
 // psc_output_fields_item: subclass "coll_stats"
 
-struct psc_output_fields_item_ops_coll : psc_output_fields_item_ops {
+struct psc_output_fields_item_ops_coll_single : psc_output_fields_item_ops {
   using Collision = Collision_<mparticles_t>;
-  psc_output_fields_item_ops_coll() {
+  psc_output_fields_item_ops_coll_single() {
     name      = "coll_stats_" PARTICLE_TYPE;
     nr_comp   = Collision::NR_STATS;
     fld_names[0] = "coll_nudt_min";
@@ -27,14 +24,14 @@ struct psc_output_fields_item_ops_coll : psc_output_fields_item_ops {
     fld_names[4] = "coll_ncoll";
     run_all   = CollisionWrapper<Collision>::copy_stats;
   }
-} psc_output_fields_item_coll_stats_ops;
+} psc_output_fields_item_coll_stats_single_ops;
 
 // ======================================================================
 // psc_output_fields_item: subclass "coll_rei"
 
-struct psc_output_fields_item_ops_coll_rei : psc_output_fields_item_ops {
+struct psc_output_fields_item_ops_coll_rei_single : psc_output_fields_item_ops {
   using Collision = Collision_<mparticles_t>;
-  psc_output_fields_item_ops_coll_rei() {
+  psc_output_fields_item_ops_coll_rei_single() {
     name      = "coll_rei_" PARTICLE_TYPE;
     nr_comp   = 3;
     fld_names[0] = "coll_rei_x";
@@ -42,6 +39,6 @@ struct psc_output_fields_item_ops_coll_rei : psc_output_fields_item_ops {
     fld_names[2] = "coll_rei_z";
     run_all   = CollisionWrapper<Collision>::copy_rei;
   }
-} psc_output_fields_item_coll_rei_ops;
+} psc_output_fields_item_coll_rei_single_ops;
 
 
