2d3613dfc6040bacd058edad5802058b5c808a02
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-07 01:02:31 -0500

    inject: instantiate from one file

--- src/libpsc/psc_inject/psc_inject_impl.cxx
+++ src/libpsc/psc_inject/psc_inject_impl.cxx
@@ -1,8 +1,9 @@
 
 #include <psc_particles_single.h>
+#include <psc_particles_double.h>
 #include <psc_fields_c.h>
 
-#include "psc_inject_common.cxx"
+#include "psc_inject_impl.hxx"
 
 // ----------------------------------------------------------------------
 // psc_inject "single"
@@ -16,4 +17,15 @@ struct psc_inject_ops_single : psc_inject_ops {
   }
 } psc_inject_ops_single;
 
+// ----------------------------------------------------------------------
+// psc_inject "double"
+
+struct psc_inject_ops_double : psc_inject_ops {
+  using Inject_t = Inject_<MparticlesDouble, MfieldsC>;
+  psc_inject_ops_double() {
+    name                = "double";
+    create              = Inject_t::create;
+    run                 = Inject_t::run;
+  }
+} psc_inject_ops_double;
 
