17017793c618166532e12afb5574f5e27ae72409
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-09 21:58:11 -0500

    item/dive_cuda

--- src/libpsc/cuda/psc_output_fields_item_flds_cuda.cxx
+++ src/libpsc/cuda/psc_output_fields_item_flds_cuda.cxx
@@ -1,36 +1,37 @@
 
 #include "psc_output_fields_item_private.h"
 #include "psc_fields_cuda.h"
+#include "fields_item.hxx"
 
 #include "cuda_iface.h"
 
 // ======================================================================
 
-static void
-calc_dive_nc(struct psc_output_fields_item *item, struct psc_mfields *mflds_base,
-	     struct psc_mparticles *mprts, struct psc_mfields *mres_base)
+struct FieldsItem_dive_cuda
 {
-  assert(ppsc->domain.gdims[0] == 1);
+  static const char* name() { return "dive_cuda"; }
+  constexpr static int n_comps = 1;
+  constexpr static fld_names_t fld_names() { return { "dive" }; } // FIXME
+  constexpr static int flags = 0;
 
-  PscMfieldsCuda mf = mflds_base->get_as<PscMfieldsCuda>(EX, EX+3);
-  PscMfieldsCuda mf_res = mres_base->get_as<PscMfieldsCuda>(0, 0);
-  cuda_mfields *cmflds = mf->cmflds;
-  cuda_mfields *cmres = mf_res->cmflds;
+  static void run(struct psc_output_fields_item *item, struct psc_mfields *mflds_base,
+		  struct psc_mparticles *mprts_base, struct psc_mfields *mres_base)
+  {
+    assert(ppsc->domain.gdims[0] == 1);
 
-  for (int p = 0; p < mf_res->n_patches(); p++) {
-    cuda_mfields_calc_dive_yz(cmflds, cmres, p);
+    PscMfieldsCuda mf = mflds_base->get_as<PscMfieldsCuda>(EX, EX+3);
+    PscMfieldsCuda mf_res = mres_base->get_as<PscMfieldsCuda>(0, 0);
+    cuda_mfields *cmflds = mf->cmflds;
+    cuda_mfields *cmres = mf_res->cmflds;
+    
+    for (int p = 0; p < mf_res->n_patches(); p++) {
+      cuda_mfields_calc_dive_yz(cmflds, cmres, p);
+    }
+    
+    mf.put_as(mflds_base, 0, 0);
+    mf_res.put_as(mres_base, 0, 1);
   }
+};
 
-  mf.put_as(mflds_base, 0, 0);
-  mf_res.put_as(mres_base, 0, 1);
-}
-
-struct psc_output_fields_item_ops_dive_cuda : psc_output_fields_item_ops {
-  psc_output_fields_item_ops_dive_cuda() {
-    name         = "dive_cuda";
-    nr_comp      = 1;
-    fld_names[0] = "dive";
-    run_all      = calc_dive_nc;
-  }
-} psc_output_fields_item_dive_cuda_ops;
+FieldsItemOps<FieldsItem_dive_cuda> psc_output_fields_item_dive_cuda_ops;
 
