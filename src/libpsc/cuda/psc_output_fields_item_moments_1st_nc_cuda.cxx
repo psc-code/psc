82a9856b96a12724ce3742e73602890563b2b91d
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-10 16:00:21 -0500

    item/moment cuda

--- src/libpsc/cuda/psc_output_fields_item_moments_1st_nc_cuda.cxx
+++ src/libpsc/cuda/psc_output_fields_item_moments_1st_nc_cuda.cxx
@@ -7,64 +7,52 @@
 #include "fields_item.hxx"
 
 // ======================================================================
-// rho_1st_nc_cuda
+// Moment_rho_1st_nc_cuda
 
-struct FieldsItem_rho_1st_nc_cuda : FieldsItemCRTP<FieldsItem_rho_1st_nc_cuda>
+struct Moment_rho_1st_nc_cuda
 {
-  using Base = FieldsItemCRTP<FieldsItem_rho_1st_nc_cuda>;
-  using Base::Base;
+  using mfields_t = PscMfieldsCuda;
+  using mparticles_t = PscMparticlesCuda;
   
-  using mres_t = MfieldsCuda;
-  constexpr static const char* name() { return "rho_1st_nc_cuda"; }
-  constexpr static int n_comps() { return 1; }
+  constexpr static const char* name = "rho_1st_nc";
+  constexpr static int n_comps = 1;
   constexpr static fld_names_t fld_names() { return { "rho_nc_cuda" }; } // FIXME
   constexpr static int flags = POFI_ADD_GHOSTS;
 
-  void run(PscMfieldsBase mflds_base, PscMparticlesBase mprts_base) override
+  static void run(mfields_t mres, mparticles_t mprts)
   {
-    PscMparticlesCuda mprts = mprts_base.get_as<PscMparticlesCuda>();
-    PscMfieldsCuda mf_res = mres_base_->get_as<PscMfieldsCuda>(0, 0);
-    cuda_mparticles *cmprts = mprts->cmprts();
-    cuda_mfields *cmres = mf_res->cmflds;
-    
-    mf_res->zero();
-
-    cuda_moments_yz_rho_1st_nc(cmprts, cmres);
-    
-    mprts.put_as(mprts_base, MP_DONT_COPY);
-    mf_res.put_as(mres_base_, 0, 1);
-  }
-};
-
-FieldsItemOps<FieldsItem_rho_1st_nc_cuda> psc_output_fields_item_rho_1st_nc_cuda_ops;
-
-// ======================================================================
-// n_1st_cuda
-
-struct FieldsItem_n_1st_cuda : FieldsItemCRTP<FieldsItem_n_1st_cuda>
-{
-  using Base = FieldsItemCRTP<FieldsItem_n_1st_cuda>;
-  using Base::Base;
-  
-  static const char* name() { return "n_1st_cuda"; }
-  constexpr static int n_comps() { return 1; }
-  constexpr static fld_names_t fld_names() { return { "n_1st_cuda" }; }
-  constexpr static int flags = POFI_ADD_GHOSTS | POFI_BY_KIND;
-
-  void run(PscMfieldsBase mflds_base, PscMparticlesBase mprts_base) override
-  {
-    PscMparticlesCuda mprts = mprts_base.get_as<PscMparticlesCuda>();
-    PscMfieldsCuda mres = mres_base_->get_as<PscMfieldsCuda>(0, 0);
     cuda_mparticles *cmprts = mprts->cmprts();
     cuda_mfields *cmres = mres->cmflds;
     
     mres->zero();
-    
-    cuda_moments_yz_n_1st(cmprts, cmres);
-    
-    mprts.put_as(mprts_base, MP_DONT_COPY);
-    mres.put_as(mres_base_, 0, 1);
+    cuda_moments_yz_rho_1st_nc(cmprts, cmres);
   }
 };
 
-FieldsItemOps<FieldsItem_n_1st_cuda> psc_output_fields_item_n_1st_cuda_ops;
+FieldsItemOps<ItemMoment<Moment_rho_1st_nc_cuda>> psc_output_fields_item_rho_1st_nc_cuda_ops;
+
+// ======================================================================
+// n_1st_cuda
+
+struct Moment_n_1st_cuda
+{
+  using mfields_t = PscMfieldsCuda;
+  using mparticles_t = PscMparticlesCuda;
+  
+  constexpr static const char* name = "n_1st";
+  constexpr static int n_comps = 1;
+  constexpr static fld_names_t fld_names() { return { "n_1st_cuda" }; } // FIXME
+  constexpr static int flags = POFI_ADD_GHOSTS;
+
+  static void run(mfields_t mres, mparticles_t mprts)
+  {
+    cuda_mparticles *cmprts = mprts->cmprts();
+    cuda_mfields *cmres = mres->cmflds;
+    
+    mres->zero();
+    cuda_moments_yz_n_1st(cmprts, cmres);
+  }
+};
+
+FieldsItemOps<ItemMoment<Moment_n_1st_cuda>> psc_output_fields_item_n_1st_cuda_ops;
+
