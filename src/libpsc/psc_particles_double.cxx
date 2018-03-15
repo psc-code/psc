06ead41e51b2f40f1566291de403eaee0a77f9c0
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-15 16:56:50 -0400

    cleanup: don't use psc_mparticles_as_*.h to generate ops

--- src/libpsc/psc_particles_double.cxx
+++ src/libpsc/psc_particles_double.cxx
@@ -1,6 +1,5 @@
 
-#include "psc.h"
-#include "psc_particles_as_double.h"
+#include "psc_particles_double.h"
 #include "psc_particles_inc.h"
 
 // ======================================================================
@@ -9,5 +8,5 @@
 template<> const MparticlesBase::Convert MparticlesDouble::convert_to_ = {};
 template<> const MparticlesBase::Convert MparticlesDouble::convert_from_ = {};
 
-psc_mparticles_ops_<mparticles_t::sub_t> psc_mparticles_double_ops;
+psc_mparticles_ops_<MparticlesDouble> psc_mparticles_double_ops;
 
--- src/libpsc/psc_particles_single.cxx
+++ src/libpsc/psc_particles_single.cxx
@@ -1,31 +1,8 @@
 
 #include "psc.h"
-#include "psc_particles_as_single.h"
+#include "psc_particles_single.h"
 #include "psc_particles_inc.h"
 #include "psc_particles_double.h"
-
-#if 0
-static void _mrc_unused // FIXME
-psc_particles_single_reorder(struct psc_mparticles *mprts)
-{
-  struct psc_particles_single *sub = psc_particles_single(prts);
-
-  if (!sub->need_reorder) {
-    return;
-  }
-f
-  int n_prts = patch->n_prts;
-  for (int n = 0; n < n_prts; n++) {
-    sub->particles_alt[n] = sub->particles[sub->b_ids[n]];
-  }
-  
-  // swap in alt array
-  particle_single_t *tmp = sub->particles;
-  sub->particles = sub->particles_alt;
-  sub->particles_alt = tmp;
-  sub->need_reorder = false;
-}
-#endif
 
 // ======================================================================
 // psc_mparticles: subclass "single"
@@ -41,4 +18,4 @@ template<> const MparticlesBase::Convert MparticlesSingle::convert_from_ = {
   { std::type_index(typeid(MparticlesDouble)), psc_mparticles_copy_from<MparticlesSingle, MparticlesDouble> },
 };
 
-psc_mparticles_ops_<mparticles_t::sub_t> psc_mparticles_single_ops;
+psc_mparticles_ops_<MparticlesSingle> psc_mparticles_single_ops;
