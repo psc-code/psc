6de8661233795fa3986c84c9b74c131ea1d13eb5
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-15 17:03:13 -0400

    cleanup: psc_particles_inc.h gone

--- src/libpsc/psc_particles_impl.cxx
+++ src/libpsc/psc_particles_impl.cxx
@@ -1,7 +1,65 @@
 
 #include "psc_particles_single.h"
 #include "psc_particles_double.h"
-#include "psc_particles_inc.h"
+
+template<typename MP_FROM, typename MP_TO>
+struct Convert
+{
+  using MparticlesFrom = MP_FROM;
+  using MparticlesTo = MP_TO;
+  using particle_from_t = typename MP_FROM::particle_t;
+  using particle_to_t = typename MP_TO::particle_t;
+
+  particle_to_t operator()(const particle_from_t& prt_from)
+  {
+    particle_to_t prt_to;
+    
+    prt_to.xi       = prt_from.xi;
+    prt_to.yi       = prt_from.yi;
+    prt_to.zi       = prt_from.zi;
+    prt_to.pxi      = prt_from.pxi;
+    prt_to.pyi      = prt_from.pyi;
+    prt_to.pzi      = prt_from.pzi;
+    prt_to.qni_wni_ = prt_from.qni_wni_;
+    prt_to.kind_    = prt_from.kind_;
+
+    return prt_to;
+  }
+};
+
+template<typename MP_FROM, typename MP_TO>
+void psc_mparticles_copy(MP_FROM& mp_from, MP_TO& mp_to)
+{
+  Convert<MP_FROM, MP_TO> convert;
+  int n_patches = mp_to.n_patches();
+  uint n_prts_by_patch[n_patches];
+  mp_from.get_size_all(n_prts_by_patch);
+  mp_to.reserve_all(n_prts_by_patch);
+  mp_to.resize_all(n_prts_by_patch);
+  
+  for (int p = 0; p < n_patches; p++) {
+    auto& prts_from = mp_from[p];
+    auto& prts_to = mp_to[p];
+    int n_prts = prts_from.size();
+    for (int n = 0; n < n_prts; n++) {
+      prts_to[n] = convert(prts_from[n]);
+    }
+  }
+}
+
+template<typename MP_FROM, typename MP_TO>
+void psc_mparticles_copy_to(MparticlesBase& mp_from, MparticlesBase& mp_to)
+{
+  psc_mparticles_copy<MP_FROM, MP_TO>(dynamic_cast<MP_FROM&>(mp_from),
+				      dynamic_cast<MP_TO&>(mp_to));
+}
+
+template<typename MP_FROM, typename MP_TO>
+void psc_mparticles_copy_from(MparticlesBase& mp_from, MparticlesBase& mp_to)
+{
+  psc_mparticles_copy<MP_TO, MP_FROM>(dynamic_cast<MP_TO&>(mp_to),
+				      dynamic_cast<MP_FROM&>(mp_from));
+}
 
 // ======================================================================
 // psc_mparticles: subclass "single"
