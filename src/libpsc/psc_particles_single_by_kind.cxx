e2e760734a89d3972862c1ac81ddc12c83283bdc
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-15 17:06:17 -0400

    cleanup: minor single_by_kind cleanup

--- src/libpsc/psc_particles_single_by_kind.cxx
+++ src/libpsc/psc_particles_single_by_kind.cxx
@@ -1,18 +1,10 @@
 
 #include "bk_mparticles_iface.h"
 
-#include "psc.h"
 #include "psc_particles_single_by_kind.h"
-
-#define PARTICLE_TYPE "single_by_kind"
-#define PFX(x) psc_mparticles_single_by_kind_ ## x
-#define psc_mparticles_sub psc_mparticles_single_by_kind
 
 // ======================================================================
 // psc_mparticles: subclass "single_by_kind"
-
-// ----------------------------------------------------------------------
-// psc_mparticles_ops
 
 psc_mparticles_ops_<MparticlesSingleByKind> psc_mparticles_single_by_kind_ops;
 
