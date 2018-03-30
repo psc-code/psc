ced8464f23b530694b8d534c9a5caea06290286f
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-03-30 11:22:56 -0400

    get rid of particles_impl.hxx

--- src/libpsc/mparticles.cxx
+++ src/libpsc/mparticles.cxx
@@ -10,7 +10,6 @@
 // ======================================================================
 
 #include "particles.hxx"
-#include "particles_impl.hxx"
 
 #include "psc_particles_single.h"
 #include "psc_particles_double.h"
@@ -18,6 +17,28 @@
 #ifdef HAVE_VPIC
 #include "psc_particles_vpic.h"
 #endif
+
+void MparticlesBase::convert(MparticlesBase& mp_from, MparticlesBase& mp_to)
+{
+  // FIXME, implementing == wouldn't hurt
+  assert(&mp_from.grid() == &mp_to.grid());
+  
+  auto convert_to = mp_from.convert_to().find(std::type_index(typeid(mp_to)));
+  if (convert_to != mp_from.convert_to().cend()) {
+    convert_to->second(mp_from, mp_to);
+    return;
+  }
+  
+  auto convert_from = mp_to.convert_from().find(std::type_index(typeid(mp_from)));
+  if (convert_from != mp_to.convert_from().cend()) {
+    convert_from->second(mp_to, mp_from);
+    return;
+  }
+
+  fprintf(stderr, "ERROR: no conversion known from %s to %s!\n",
+	  typeid(mp_from).name(), typeid(mp_to).name());
+  assert(0);
+}
 
 // ======================================================================
 // psc_mparticles base class
