e7a523b52c64c8f3e82be8efaec745c0edd5af71
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-04-02 11:05:28 -0400

    cuda/push_fields: do tag dispatch on dim

--- src/libpsc/cuda/psc_push_fields_cuda.cxx
+++ src/libpsc/cuda/psc_push_fields_cuda.cxx
@@ -4,6 +4,7 @@
 #include "cuda_iface.h"
 
 #include "push_fields.hxx"
+#include "dim.hxx"
 
 struct PushFieldsCuda : PushFieldsBase
 {
@@ -22,17 +23,30 @@ struct PushFieldsCuda : PushFieldsBase
     
     mflds_base->put_as(mflds, EX, EX + 3);
   }
-  
+
+  // ----------------------------------------------------------------------
+  // push_H
+
+  void push_H(MfieldsCuda& mflds, double dt_fac, dim_yz tag)
+  {
+    cuda_push_fields_H_yz(mflds.cmflds, dt_fac * ppsc->dt);
+  }
+
+  // dispatch -- FIXME, mostly same as non-cuda dispatch
   void push_H(PscMfieldsBase mflds_base, double dt_fac) override
   {
     auto& mflds = mflds_base->get_as<MfieldsCuda>(JXI, HX + 3);
     
-    if (mflds.grid().isInvar(0)) {
-      cuda_push_fields_H_yz(mflds.cmflds, dt_fac * ppsc->dt);
+    const auto& grid = mflds.grid();
+    using Bool3 = Vec3<bool>;
+    Bool3 invar{grid.isInvar(0), grid.isInvar(1), grid.isInvar(2)};
+
+    if (invar == Bool3{true, false, false}) {
+      push_H(mflds, dt_fac, dim_yz{});
     } else {
       assert(0);
     }
-    
+
     mflds_base->put_as(mflds, HX, HX + 3);
   }
 };
