6809db94d31031f5d44308f961325bc0753a06f9
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-04-02 10:27:25 -0400

    cuda: add test_cuda_base

---
+++ src/libpsc/cuda/tests/test_cuda_base.cu
@@ -0,0 +1,13 @@
+
+#include "cuda_iface.h"
+
+#include "gtest/gtest.h"
+
+// ======================================================================
+// test cuda_base_init
+
+TEST(CudaBase, Init)
+{
+  cuda_base_init();
+}
+
