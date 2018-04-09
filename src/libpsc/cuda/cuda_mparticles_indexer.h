dcca390ed301e42a09859c642756103f9305f1ab
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-04-09 15:02:33 -0400

    cmprts: some dpi cleanup

--- src/libpsc/cuda/cuda_mparticles_indexer.h
+++ src/libpsc/cuda/cuda_mparticles_indexer.h
@@ -20,40 +20,46 @@ struct DParticleIndexer
   using real_t = float;
 
   DParticleIndexer() = default; // FIXME, delete
-  DParticleIndexer(const cuda_mparticles_indexer& cpi);
+  DParticleIndexer(const int b_mx[3], const real_t b_dxi[3])
+  {
+    for (int d = 0; d < 3; d++) {
+      b_mx_[d]  = b_mx[d];
+      b_dxi_[d] = b_dxi[d];
+    }
+  }
 
   __device__ int blockIndex(float4 xi4, int p) const
   {
-    uint block_pos_y = __float2int_rd(xi4.y * b_dxi[1]);
-    uint block_pos_z = __float2int_rd(xi4.z * b_dxi[2]);
+    uint block_pos_y = __float2int_rd(xi4.y * b_dxi_[1]);
+    uint block_pos_z = __float2int_rd(xi4.z * b_dxi_[2]);
     
-    //assert(block_pos_y < b_mx[1] && block_pos_z < b_mx[2]); FIXME, assert doesn't work (on macbook)
-    return (p * b_mx[2] + block_pos_z) * b_mx[1] + block_pos_y;
+    //assert(block_pos_y < b_mx_[1] && block_pos_z < b_mx_[2]); FIXME, assert doesn't work (on macbook)
+    return (p * b_mx_[2] + block_pos_z) * b_mx_[1] + block_pos_y;
   }
 
   __device__ int blockShift(float xi[3], int p_nr, int bid) const
   {
-    uint block_pos_y = __float2int_rd(xi[1] * b_dxi[1]);
-    uint block_pos_z = __float2int_rd(xi[2] * b_dxi[2]);
+    uint block_pos_y = __float2int_rd(xi[1] * b_dxi_[1]);
+    uint block_pos_z = __float2int_rd(xi[2] * b_dxi_[2]);
     
-    if (block_pos_y >= b_mx[1] || block_pos_z >= b_mx[2]) {
+    if (block_pos_y >= b_mx_[1] || block_pos_z >= b_mx_[2]) {
       return CUDA_BND_S_OOB;
     } else {
-      int bidx = (p_nr * b_mx[2] + block_pos_z) * b_mx[1] + block_pos_y;
-      int b_diff = bid - bidx + b_mx[1] + 1;
-      int d1 = b_diff % b_mx[1];
-      int d2 = b_diff / b_mx[1];
+      int bidx = (p_nr * b_mx_[2] + block_pos_z) * b_mx_[1] + block_pos_y;
+      int b_diff = bid - bidx + b_mx_[1] + 1;
+      int d1 = b_diff % b_mx_[1];
+      int d2 = b_diff / b_mx_[1];
       return d2 * 3 + d1;
     }
   }
   
-  __device__ static uint block_pos_to_block_idx(int block_pos[3], uint b_mx[3])
+  __device__ uint block_pos_to_block_idx(int block_pos[3])
   {
 #define NO_CHECKERBOARD
 #ifdef NO_CHECKERBOARD
-    return block_pos[2] * b_mx[1] + block_pos[1];
+    return block_pos[2] * b_mx_[1] + block_pos[1];
 #else
-    int dimy = b_mx[1] >> 1;
+    int dimy = b_mx_[1] >> 1;
     return
       ((block_pos[1] & 1) << 0) |
       ((block_pos[2] & 1) << 1) | 
@@ -63,46 +69,46 @@ struct DParticleIndexer
 
   __device__ int find_bid()
   {
-    return blockIdx.y * b_mx[1] + blockIdx.x;
+    return blockIdx.y * b_mx_[1] + blockIdx.x;
   }
 
   __device__ int find_bid_q(int p, int *block_pos)
   {
-    // FIXME won't work if b_mx[1,2] not even (?)
-    return block_pos_to_block_idx(block_pos, b_mx) + p * b_mx[1] * b_mx[2];
+    // FIXME won't work if b_mx_[1,2] not even (?)
+    return block_pos_to_block_idx(block_pos) + p * b_mx_[1] * b_mx_[2];
   }
 
   template<int BLOCKSIZE_X, int BLOCKSIZE_Y, int BLOCKSIZE_Z>
   __device__ int find_block_pos_patch(int *block_pos, int *ci0)
   {
     block_pos[1] = blockIdx.x;
-    block_pos[2] = blockIdx.y % b_mx[2];
+    block_pos[2] = blockIdx.y % b_mx_[2];
     
     ci0[0] = 0;
     ci0[1] = block_pos[1] * BLOCKSIZE_Y;
     ci0[2] = block_pos[2] * BLOCKSIZE_Z;
     
-    return blockIdx.y / b_mx[2];
+    return blockIdx.y / b_mx_[2];
   }
 
   template<int BLOCKSIZE_X, int BLOCKSIZE_Y, int BLOCKSIZE_Z>
   __device__ int find_block_pos_patch(int *block_pos)
   {
     block_pos[1] = blockIdx.x;
-    block_pos[2] = blockIdx.y % b_mx[2];
+    block_pos[2] = blockIdx.y % b_mx_[2];
     
-    return blockIdx.y / b_mx[2];
+    return blockIdx.y / b_mx_[2];
   }
 
   template<int BLOCKSIZE_X, int BLOCKSIZE_Y, int BLOCKSIZE_Z>
   __device__ int find_block_pos_patch_q(int *block_pos, int *ci0, int block_start)
   {
-    int grid_dim_y = (b_mx[2] + 1) / 2;
+    int grid_dim_y = (b_mx_[2] + 1) / 2;
     block_pos[1] = blockIdx.x * 2;
     block_pos[2] = (blockIdx.y % grid_dim_y) * 2;
     block_pos[1] += block_start & 1;
     block_pos[2] += block_start >> 1;
-    if (block_pos[1] >= b_mx[1] || block_pos[2] >= b_mx[2])
+    if (block_pos[1] >= b_mx_[1] || block_pos[2] >= b_mx_[2])
       return -1;
     
     ci0[0] = 0;
@@ -113,8 +119,8 @@ struct DParticleIndexer
   }
 
 private:
-  uint b_mx[3];
-  real_t b_dxi[3];
+  uint b_mx_[3];
+  real_t b_dxi_[3];
 };
 
 // ======================================================================
@@ -148,7 +154,7 @@ struct cuda_mparticles_indexer
 
   operator DParticleIndexer()
   {
-    return DParticleIndexer(*this);
+    return DParticleIndexer(pi_.b_mx_, pi_.b_dxi_);
   }
   
 private:
@@ -170,14 +176,6 @@ private:
   ParticleIndexer<real_t> pi_;
 };
 
-
-inline DParticleIndexer::DParticleIndexer(const cuda_mparticles_indexer& cpi)
-{
-  for (int d = 0; d < 3; d++) {
-    b_mx[d]  = cpi.b_mx()[d];
-    b_dxi[d] = cpi.b_dxi()[d];
-  }
-}
 
 #endif
 
