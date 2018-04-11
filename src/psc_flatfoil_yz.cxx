3de021e5c8e2d6aaa86885c1b0a7c69bfc99f95c
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-04-11 11:06:38 -0400

    bndp: back to testing cuda

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -1,5 +1,5 @@
 
-//#define DO_CUDA 1
+#define DO_CUDA 1
 
 #include <psc.h>
 
@@ -634,7 +634,7 @@ PscFlatfoil* PscFlatfoilBuilder::makePscFlatfoil()
   auto grid_domain = Grid_t::Domain{{1, 32, 128}, // global number of grid points
 				    {1., LLy, LLz}, {0., -.5*LLy, -.5*LLz}, // domain size, origin
 				    {1, 1, 4}, // division into patches
-                                    {1, 1, 1}}; // blocksize FIXME needs to match cuda pusher
+                                    {1, 4, 4}}; // blocksize FIXME needs to match cuda pusher
 
   auto grid_bc = GridBc{{ BND_FLD_PERIODIC, BND_FLD_PERIODIC, BND_FLD_PERIODIC },
 			{ BND_FLD_PERIODIC, BND_FLD_PERIODIC, BND_FLD_PERIODIC },
