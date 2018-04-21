7903bcda50ff27fa8f4acf0d9d237f76929c65b1
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-04-20 23:16:41 -0400

    range: add and test strided range

--- src/include/range.hxx
+++ src/include/range.hxx
@@ -36,6 +36,38 @@ private:
 };
 
 template<typename T>
+struct RangeStrided
+{
+  struct iterator : std::iterator<std::input_iterator_tag, T>
+  {
+    explicit iterator(T val, T stride) : val_(val), stride_(stride) {}
+    
+    T operator*() const { return val_; }
+    T const* operator->() const { return &val_; }
+
+    iterator& operator++() { val_ += stride_; return *this; }
+    iterator operator++(int) { auto retval = *this; ++*this; return retval; }
+
+    bool operator!=(const iterator& other) const { return val_ < other.val_; }
+
+  private:
+    T val_;
+    T stride_;
+  };
+
+  RangeStrided(T begin, T end, T stride)
+    : begin_(begin, stride), end_(end, stride)
+  {}
+
+  iterator begin() const { return begin_; }
+  iterator end() const { return end_; }
+  
+private:
+  iterator begin_;
+  iterator end_;
+};
+
+template<typename T>
 Range<T> range(T begin, T end)
 {
   return {begin, end};
@@ -45,5 +77,11 @@ template<typename T>
 Range<T> range(T length)
 {
   return {0, length};
+}
+
+template<typename T>
+RangeStrided<T> range(T begin, T end, T stride)
+{
+  return {begin, end, stride};
 }
 
--- src/libpsc/cuda/tests/range_unittest.cu
+++ src/libpsc/cuda/tests/range_unittest.cu
@@ -38,5 +38,22 @@ namespace {
     EXPECT_EQ(vals, std::vector<T>({ 10, 11, 12, 13, 14 }));
   }
 
+  TYPED_TEST(RangeTest, ThreeParams)
+  {
+    using T = TypeParam;
+
+    std::vector<T> vals;
+    for (auto i : range(T{10}, T{15}, T{2})) {
+      vals.push_back(i);
+    }
+    EXPECT_EQ(vals, std::vector<T>({ 10, 12, 14 }));
+
+    vals.clear();
+    for (auto i : range(T{10}, T{14}, T{2})) {
+      vals.push_back(i);
+    }
+    EXPECT_EQ(vals, std::vector<T>({ 10, 12 }));
+}
+
 }  // namespace
 
