d52c98d9913f0a4c0bb66badb79d20560f234e21
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-04-20 23:04:27 -0400

    range: add unittest

---
+++ src/libpsc/cuda/tests/range_unittest.cu
@@ -0,0 +1,42 @@
+
+#include "range.hxx"
+#include "gtest/gtest.h"
+
+namespace {
+
+  // ----------------------------------------------------------------------
+  // RangeTest
+  
+  template <typename T>
+  class RangeTest : public ::testing::Test
+  {
+  };
+
+  using RangeTypes = ::testing::Types<int>;
+
+  TYPED_TEST_CASE(RangeTest, RangeTypes);
+
+  TYPED_TEST(RangeTest, OneParam)
+  {
+    using T = TypeParam;
+
+    std::vector<T> vals;
+    for (auto i : range(T{5})) {
+      vals.push_back(i);
+    }
+    EXPECT_EQ(vals, std::vector<T>({ 0, 1, 2, 3, 4 }));
+  }
+
+  TYPED_TEST(RangeTest, TwoParams)
+  {
+    using T = TypeParam;
+
+    std::vector<T> vals;
+    for (auto i : range(T{10}, T{15})) {
+      vals.push_back(i);
+    }
+    EXPECT_EQ(vals, std::vector<T>({ 10, 11, 12, 13, 14 }));
+  }
+
+}  // namespace
+
