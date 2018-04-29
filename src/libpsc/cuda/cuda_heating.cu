01e9e6a72d24eb101817e0bb934d434d7d225991
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-04-28 23:34:35 -0400

    cuda/heating: no more cuda_heating_iface.hxx

--- src/libpsc/cuda/cuda_heating.cu
+++ src/libpsc/cuda/cuda_heating.cu
@@ -4,7 +4,7 @@
 #include "cuda_mfields.h"
 #include "cuda_bits.h"
 #include "psc_bits.h"
-#include "cuda_heating_iface.hxx"
+#include "heating_spot_foil.hxx"
 #include "heating_cuda_impl.hxx"
 
 #include <thrust/host_vector.h>
@@ -51,6 +51,33 @@ static void cuda_heating_params_free()
   ierr = cudaFree(&h_prm.d_xb_by_patch);
   cudaCheck(ierr);
 }
+
+struct cuda_heating_foil;
+
+template<typename BS>
+void cuda_heating_run_foil(cuda_heating_foil& foil, cuda_mparticles<BS>* cmprts);
+
+// ======================================================================
+// cuda_heating_foil
+
+struct cuda_heating_foil : HeatingSpotFoilParams
+{
+  cuda_heating_foil() = default;
+  
+  cuda_heating_foil(const HeatingSpotFoilParams& params, int kind, double heating_dt)
+    : HeatingSpotFoilParams(params), kind(kind), heating_dt(heating_dt)
+  {
+    float width = zh - zl;
+    fac = (8.f * pow(T, 1.5)) / (sqrt(Mi) * width);
+  }
+  
+  // params
+  int kind;
+
+  // state (FIXME, shouldn't be part of the interface)
+  float fac;
+  float heating_dt;
+};
 
 // ----------------------------------------------------------------------
 // foil_get_H
