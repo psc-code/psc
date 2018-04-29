c5dedeeb6a91da35b886b74b7dac19252b080d47
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-04-28 23:29:10 -0400

    cuda/heating: HeatingSpotFoil -> heating_spot_foil.hxx

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -120,36 +120,6 @@ struct InjectFoil : InjectFoilParams
 };
 
 // ======================================================================
-// HeatingSpotFoil
-
-struct HeatingSpotFoil : HeatingSpotFoilParams
-{
-  HeatingSpotFoil() = default;
-  
-  HeatingSpotFoil(const HeatingSpotFoilParams& params)
-    : HeatingSpotFoilParams(params)
-  {
-    double width = zh - zl;
-    fac = (8.f * pow(T, 1.5)) / (sqrt(Mi) * width);
-    // FIXME, I don't understand the sqrt(Mi) in here
-  }
-  
-  double operator()(const double *crd)
-  {
-    double x = crd[0], y = crd[1], z = crd[2];
-
-    if (z <= zl || z >= zh) {
-      return 0;
-    }
-    
-    return fac * exp(-(sqr(x-xc) + sqr(y-yc)) / sqr(rH));
-  }
-
-private:
-  double fac;
-};
-
-// ======================================================================
 // PscFlatfoilParams
 
 struct PscFlatfoilParams
