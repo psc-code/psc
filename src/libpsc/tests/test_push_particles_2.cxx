91f14cf623ca3c8229ee9634a0a4755f79be2e62
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-05-19 23:58:19 -0400

    tests/pushp: mv accel test, too

--- src/libpsc/tests/test_push_particles_2.cxx
+++ src/libpsc/tests/test_push_particles_2.cxx
@@ -10,6 +10,76 @@ using PushParticlesTestTypes = ::testing::Types<TestConfig2ndDouble,
 TYPED_TEST_CASE(PushParticlesTest, PushParticlesTestTypes);
 
 // ======================================================================
+// Accel test
+
+TYPED_TEST(PushParticlesTest, Accel)
+{
+  using Mparticles = typename TypeParam::Mparticles;
+  using Mfields = typename TypeParam::Mfields;
+  using PushParticles = typename TypeParam::PushParticles;
+  using Checks = typename TypeParam::Checks;
+
+  const int n_prts = 131;
+  const int n_steps = 10;
+  const typename Mparticles::real_t eps = 1e-5;
+
+  auto kinds = Grid_t::Kinds{Grid_t::Kind(1., 1., "test_species")};
+  this->make_psc(kinds);
+  const auto& grid = this->grid();
+  
+  // init fields
+  auto mflds = Mfields{grid, NR_FIELDS, this->ibn};
+  SetupFields<Mfields>::set(mflds, [&](int m, double crd[3]) {
+      switch (m) {
+      case EX: return 1.;
+      case EY: return 2.;
+      case EZ: return 3.;
+      default: return 0.;
+      }
+    });
+
+  // init particles
+  RngPool rngpool;
+  Rng *rng = rngpool[0];
+  auto n_prts_by_patch = std::vector<uint>{n_prts};
+
+  Mparticles mprts{grid};
+  SetupParticles<Mparticles>::setup_particles(mprts, n_prts_by_patch, [&](int p, int n) -> typename Mparticles::particle_t {
+      typename Mparticles::particle_t prt{};
+      prt.xi = rng->uniform(0, this->L);
+      prt.yi = rng->uniform(0, this->L);
+      prt.zi = rng->uniform(0, this->L);
+      prt.qni_wni_ = 1.;
+      prt.pxi = 0.;
+      prt.pyi = 0.;
+      prt.pzi = 0.;
+      prt.kind_ = 0;
+      return prt;
+    });
+
+  // run test
+  PushParticles pushp_;
+  ChecksParams checks_params{};
+  checks_params.continuity_threshold = 1e-10;
+  checks_params.continuity_verbose = false;
+  Checks checks_{grid, MPI_COMM_WORLD, checks_params};
+  for (int n = 0; n < n_steps; n++) {
+    //checks_.continuity_before_particle_push(mprts);
+    pushp_.push_mprts(mprts, mflds);
+    //checks_.continuity_after_particle_push(mprts, mflds);
+
+    for (auto& prt : make_getter(mprts)[0]) {
+      EXPECT_NEAR(prt.pxi, 1*(n+1), eps);
+      EXPECT_NEAR(prt.pyi, 2*(n+1), eps);
+      EXPECT_NEAR(prt.pzi, 3*(n+1), eps);
+      prt.xi = this->L/2;
+      prt.yi = this->L/2;
+      prt.zi = this->L/2;
+    }
+  }
+}
+
+// ======================================================================
 // Cyclo test
 
 TYPED_TEST(PushParticlesTest, Cyclo)
