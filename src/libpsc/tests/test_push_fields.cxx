985da3e79f2ff1b2be7ae014e84f72aa65115ab2
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-05-21 19:18:49 -0400

    test/pushf: cuda xyz passes push_E, push_H

--- src/libpsc/tests/test_push_fields.cxx
+++ src/libpsc/tests/test_push_fields.cxx
@@ -56,6 +56,42 @@ TYPED_TEST(PushFieldsTest, Pushf1)
     });
 }
 
+TYPED_TEST(PushFieldsTest, Pushf2)
+{
+  using Mparticles = typename TypeParam::Mparticles;
+  using Mfields = typename TypeParam::Mfields;
+  using dim = typename TypeParam::dim;
+  using PushFields = typename TypeParam::PushFields;
+
+  const typename Mparticles::real_t eps = 1e-2;
+
+  this->make_psc({});
+  const auto& grid = this->grid();
+
+  const double ky = 2. * M_PI / grid.domain.length[1];
+  
+  // init fields
+  auto mflds = Mfields{grid, NR_FIELDS, this->ibn};
+  SetupFields<Mfields>::set(mflds, [&](int m, double crd[3]) {
+      switch (m) {
+      case HX: return cos(ky*crd[1]);
+      default: return 0.;
+      }
+    });
+
+  // run test
+  PushFields pushf_;
+  pushf_.push_E(mflds, 1., dim{});
+
+  auto flds = mflds[0];
+  grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
+      double y = grid.patches[0].y_cc(j);
+      // printf("ijk %d:%d:%d %g %g dt %g\n", i,j,k, flds(HX, i,j,k),
+      // 	     kz*cos(kz*z), ppsc->dt);
+      EXPECT_NEAR(flds(EZ, i,j,k), ky*sin(ky*y), eps);
+    });
+}
+
 
 int main(int argc, char **argv)
 {
