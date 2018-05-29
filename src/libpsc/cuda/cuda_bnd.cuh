9b657e7a390cc6c5f3358af3328e1661911dee91
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-05-29 12:00:38 -0400

    cuda/bnd3: replicate most of mrc_ddc_multi.c

--- src/libpsc/cuda/cuda_bnd.cuh
+++ src/libpsc/cuda/cuda_bnd.cuh
@@ -5,6 +5,165 @@
 #include "psc_fields_single.h"
 #include "fields.hxx"
 
+#include "mrc_ddc_private.h"
+
+static void
+mrc_ddc_multi_set_mpi_type(struct mrc_ddc *ddc)
+{
+  if (ddc->size_of_type == sizeof(float)) {
+    ddc->mpi_type = MPI_FLOAT;
+  } else if (ddc->size_of_type == sizeof(double)) {
+    ddc->mpi_type = MPI_DOUBLE;
+  } else {
+    assert(0);
+  }
+}
+
+// ----------------------------------------------------------------------
+// mrc_ddc_multi_free_buffers
+
+static void
+mrc_ddc_multi_free_buffers(struct mrc_ddc *ddc, struct mrc_ddc_pattern2 *patt2)
+{
+  free(patt2->send_buf);
+  free(patt2->recv_buf);
+  free(patt2->local_buf);
+}
+
+// ----------------------------------------------------------------------
+// mrc_ddc_multi_alloc_buffers
+
+static void
+mrc_ddc_multi_alloc_buffers(struct mrc_ddc *ddc, struct mrc_ddc_pattern2 *patt2,
+			    int n_fields)
+{
+  if (ddc->size_of_type > patt2->max_size_of_type ||
+      n_fields > patt2->max_n_fields) {
+
+    if (ddc->size_of_type > patt2->max_size_of_type) {
+      patt2->max_size_of_type = ddc->size_of_type;
+    }
+    if (n_fields > patt2->max_n_fields) {
+      patt2->max_n_fields = n_fields;
+    }
+
+    mrc_ddc_multi_free_buffers(ddc, patt2);
+
+    patt2->recv_buf = malloc(patt2->n_recv * patt2->max_n_fields * patt2->max_size_of_type);
+    patt2->send_buf = malloc(patt2->n_send * patt2->max_n_fields * patt2->max_size_of_type);
+    patt2->local_buf = malloc(patt2->local_buf_size * patt2->max_n_fields * patt2->max_size_of_type);
+  }
+}
+
+// ----------------------------------------------------------------------
+// ddc_run_begin
+
+static void
+ddc_run_begin(struct mrc_ddc *ddc, struct mrc_ddc_pattern2 *patt2,
+	      int mb, int me, void *ctx,
+	      void (*to_buf)(int mb, int me, int p, int ilo[3], int ihi[3], void *buf, void *ctx))
+{
+  struct mrc_ddc_multi *sub = mrc_ddc_multi(ddc);
+  struct mrc_ddc_rank_info *ri = patt2->ri;
+
+  // communicate aggregated buffers
+  // post receives
+  patt2->recv_cnt = 0;
+  char* p = (char*) patt2->recv_buf;
+  for (int r = 0; r < sub->mpi_size; r++) {
+    if (r != sub->mpi_rank && ri[r].n_recv_entries) {
+      MPI_Irecv(p, ri[r].n_recv * (me - mb), ddc->mpi_type,
+		r, 0, ddc->obj.comm, &patt2->recv_req[patt2->recv_cnt++]);
+      p += ri[r].n_recv * (me - mb) * ddc->size_of_type;
+    }
+  }  
+  assert(p == (char*)patt2->recv_buf + patt2->n_recv * (me - mb) * ddc->size_of_type);
+
+  // post sends
+  patt2->send_cnt = 0;
+  p = (char*) patt2->send_buf;
+  for (int r = 0; r < sub->mpi_size; r++) {
+    if (r != sub->mpi_rank && ri[r].n_send_entries) {
+      void *p0 = p;
+      for (int i = 0; i < ri[r].n_send_entries; i++) {
+	struct mrc_ddc_sendrecv_entry *se = &ri[r].send_entry[i];
+	to_buf(mb, me, se->patch, se->ilo, se->ihi, p, ctx);
+	p += se->len * (me - mb) * ddc->size_of_type;
+      }
+      MPI_Isend(p0, ri[r].n_send * (me - mb), ddc->mpi_type,
+		r, 0, ddc->obj.comm, &patt2->send_req[patt2->send_cnt++]);
+    }
+  }  
+  assert(p == (char*) patt2->send_buf + patt2->n_send * (me - mb) * ddc->size_of_type);
+}
+
+// ----------------------------------------------------------------------
+// ddc_run_end
+
+static void
+ddc_run_end(struct mrc_ddc *ddc, struct mrc_ddc_pattern2 *patt2,
+	    int mb, int me, void *ctx,
+	    void (*from_buf)(int mb, int me, int p, int ilo[3], int ihi[3], void *buf, void *ctx))
+{
+  struct mrc_ddc_multi *sub = mrc_ddc_multi(ddc);
+  struct mrc_ddc_rank_info *ri = patt2->ri;
+
+  MPI_Waitall(patt2->recv_cnt, patt2->recv_req, MPI_STATUSES_IGNORE);
+
+  char* p = (char*) patt2->recv_buf;
+  for (int r = 0; r < sub->mpi_size; r++) {
+    if (r != sub->mpi_rank) {
+      for (int i = 0; i < ri[r].n_recv_entries; i++) {
+	struct mrc_ddc_sendrecv_entry *re = &ri[r].recv_entry[i];
+	from_buf(mb, me, re->patch, re->ilo, re->ihi, p, ctx);
+	p += re->len * (me - mb) * ddc->size_of_type;
+      }
+    }
+  }
+
+  MPI_Waitall(patt2->send_cnt, patt2->send_req, MPI_STATUSES_IGNORE);
+}
+
+// ----------------------------------------------------------------------
+// ddc_run_local
+
+static void
+ddc_run_local(struct mrc_ddc *ddc, struct mrc_ddc_pattern2 *patt2,
+	      int mb, int me, void *ctx,
+	      void (*to_buf)(int mb, int me, int p, int ilo[3], int ihi[3], void *buf, void *ctx),
+	      void (*from_buf)(int mb, int me, int p, int ilo[3], int ihi[3], void *buf, void *ctx))
+{
+  struct mrc_ddc_multi *sub = mrc_ddc_multi(ddc);
+  struct mrc_ddc_rank_info *ri = patt2->ri;
+
+  // overlap: local exchange
+  for (int i = 0; i < ri[sub->mpi_rank].n_send_entries; i++) {
+    struct mrc_ddc_sendrecv_entry *se = &ri[sub->mpi_rank].send_entry[i];
+    struct mrc_ddc_sendrecv_entry *re = &ri[sub->mpi_rank].recv_entry[i];
+    if (se->ilo[0] == se->ihi[0] ||
+	se->ilo[1] == se->ihi[1] ||
+	se->ilo[2] == se->ihi[2]) { // FIXME, we shouldn't even create these
+      continue;
+    }
+    to_buf(mb, me, se->patch, se->ilo, se->ihi, patt2->local_buf, ctx);
+    from_buf(mb, me, se->nei_patch, re->ilo, re->ihi, patt2->local_buf, ctx);
+  }
+}
+
+// ----------------------------------------------------------------------
+// ddc_run
+
+static void
+ddc_run(struct mrc_ddc *ddc, struct mrc_ddc_pattern2 *patt2,
+	int mb, int me, void *ctx,
+	void (*to_buf)(int mb, int me, int p, int ilo[3], int ihi[3], void *buf, void *ctx),
+	void (*from_buf)(int mb, int me, int p, int ilo[3], int ihi[3], void *buf, void *ctx))
+{
+  ddc_run_begin(ddc, patt2, mb, me, ctx, to_buf);
+  ddc_run_local(ddc, patt2, mb, me, ctx, to_buf, from_buf);
+  ddc_run_end(ddc, patt2, mb, me, ctx, from_buf);
+}
+
 // ======================================================================
 // CudaBnd
 
@@ -60,7 +219,19 @@ struct CudaBnd
     // I don't think we need as many points, and only stencil star
     // rather then box
     auto& mflds_single = mflds.get_as<MfieldsSingle>(mb, me);
+
+#if 1
+#define mrc_ddc_multi(ddc) mrc_to_subobj(ddc, struct mrc_ddc_multi)
+    struct mrc_ddc_multi *sub = mrc_ddc_multi(ddc_);
+    
+    mrc_ddc_multi_set_mpi_type(ddc_);
+    mrc_ddc_multi_alloc_buffers(ddc_, &sub->fill_ghosts2, me - mb);
+    ddc_run(ddc_, &sub->fill_ghosts2, mb, me, &mflds_single,
+	    ddc_->funcs->copy_to_buf, ddc_->funcs->copy_from_buf);
+#else
     mrc_ddc_fill_ghosts(ddc_, mb, me, &mflds_single);
+#endif
+    
     mflds.put_as(mflds_single, mb, me);
   }
 
