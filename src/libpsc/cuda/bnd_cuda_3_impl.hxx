c14578588f5b4539d87ba5a5985c25fca4b0fdd7
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-05-29 11:42:00 -0400

    bnd/cuda3: reorder

--- src/libpsc/cuda/bnd_cuda_3_impl.hxx
+++ src/libpsc/cuda/bnd_cuda_3_impl.hxx
@@ -9,6 +9,43 @@
 #include <mrc_profile.h>
 #include <mrc_ddc.h>
 
+// ======================================================================
+// BndCuda3
+//
+// just wrapping CudaBnd doing the actual work
+
+struct CudaBnd;
+
+template<typename MF>
+struct BndCuda3 : BndBase
+{
+  using Mfields = MF;
+
+  BndCuda3(const Grid_t& grid, mrc_domain* domain, int ibn[3]);
+  ~BndCuda3();
+  
+  void reset();
+  void add_ghosts(Mfields& mflds, int mb, int me);
+  void fill_ghosts(Mfields& mflds, int mb, int me);
+
+  void add_ghosts(PscMfieldsBase mflds_base, int mb, int me) override
+  {
+    assert(0);
+  }
+
+  void fill_ghosts(PscMfieldsBase mflds_base, int mb, int me) override
+  {
+    assert(0);
+  }
+
+private:
+  CudaBnd* cbnd_;
+  int balance_generation_cnt_;
+};
+
+// ======================================================================
+// CudaBnd
+
 struct CudaBnd
 {
   using Mfields = MfieldsCuda;
@@ -132,33 +169,6 @@ struct CudaBnd
 
 private:
   mrc_ddc* ddc_;
-};
-
-template<typename MF>
-struct BndCuda3 : BndBase
-{
-  using Mfields = MF;
-
-  BndCuda3(const Grid_t& grid, mrc_domain* domain, int ibn[3]);
-  ~BndCuda3();
-  
-  void reset();
-  void add_ghosts(Mfields& mflds, int mb, int me);
-  void fill_ghosts(Mfields& mflds, int mb, int me);
-
-  void add_ghosts(PscMfieldsBase mflds_base, int mb, int me) override
-  {
-    assert(0);
-  }
-
-  void fill_ghosts(PscMfieldsBase mflds_base, int mb, int me) override
-  {
-    assert(0);
-  }
-
-private:
-  CudaBnd* cbnd_;
-  int balance_generation_cnt_;
 };
 
 // ----------------------------------------------------------------------
