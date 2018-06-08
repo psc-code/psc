9c1592f0cc5567ce7fa1301efd243d09d10ab0a3
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-06-08 15:03:57 -0400

    collision: avoid propagation to double precision
    (when using single prec)

--- src/libpsc/psc_collision/psc_collision_impl.hxx
+++ src/libpsc/psc_collision/psc_collision_impl.hxx
@@ -64,8 +64,8 @@ struct BinaryCollision
     q2 =prts.prt_qni(prt2);
     m2 =prts.prt_mni(prt2);
 
-    if (q1*q2 == 0.) {
-      return 0.; // no Coulomb collisions with neutrals
+    if (q1*q2 == 0.f) {
+      return 0.f; // no Coulomb collisions with neutrals
     }
  
     px1=m1*px1;
@@ -81,13 +81,13 @@ struct BinaryCollision
     p01=std::sqrt(m1*m1+px1*px1+py1*py1+pz1*pz1);
     p02=std::sqrt(m2*m2+px2*px2+py2*py2+pz2*pz2);
     h1=p01*p02-px1*px2-py1*py2-pz1*pz2;
-    ss=m1*m1+m2*m2+2.0*h1;
+    ss=m1*m1+m2*m2+2.f*h1;
     h2=ss-m1*m1-m2*m2;
-    h3=(h2*h2-4.0*m1*m1*m2*m2)/(4.0*ss);
-    if (h3 < 0.) {
-      mprintf("WARNING: ss %g (m1+m1)^2 %g in psc_collision_c.c\n",
+    h3=(h2*h2-4.f*m1*m1*m2*m2)/(4.f*ss);
+    if (h3 < 0.f) {
+      mprintf("WARNING: ss %g (m1+m1)^2 %g in BinaryCollision\n",
 	      ss, (m1+m2)*(m1+m2));
-      return 0.; // nudt = 0 because no collision
+      return 0.f; // nudt = 0 because no collision
     }
     ppc=std::sqrt(h3);
   
@@ -99,17 +99,17 @@ struct BinaryCollision
     vcz=(pz1+pz2)/(p01+p02);
   
     nnorm=std::sqrt(vcx*vcx+vcy*vcy+vcz*vcz);
-    if (nnorm>0.0) {
+    if (nnorm>0.f) {
       nx=vcx/nnorm;
       ny=vcy/nnorm;
       nz=vcz/nnorm;
     } else {
-      nx=0.0;
-      ny=0.0;
-      nz=0.0;
+      nx=0.f;
+      ny=0.f;
+      nz=0.f;
     }
     bet=nnorm;
-    gam=1.0/std::sqrt(1.0-bet*bet);
+    gam=1.f/std::sqrt(1.f-bet*bet);
   
   
     // determine pre-collision momenta in cm-frame
@@ -118,13 +118,13 @@ struct BinaryCollision
     pn1=px1*nx+py1*ny+pz1*nz;
     pn2=px2*nx+py2*ny+pz2*nz;
     pc01=std::sqrt(m1*m1+ppc*ppc);
-    pcx1=px1+(gam-1.0)*pn1*nx-gam*vcx*p01;
-    pcy1=py1+(gam-1.0)*pn1*ny-gam*vcy*p01;
-    pcz1=pz1+(gam-1.0)*pn1*nz-gam*vcz*p01;
+    pcx1=px1+(gam-1.f)*pn1*nx-gam*vcx*p01;
+    pcy1=py1+(gam-1.f)*pn1*ny-gam*vcy*p01;
+    pcz1=pz1+(gam-1.f)*pn1*nz-gam*vcz*p01;
     pc02=std::sqrt(m2*m2+ppc*ppc);
-    pcx2=px2+(gam-1.0)*pn2*nx-gam*vcx*p02;
-    pcy2=py2+(gam-1.0)*pn2*ny-gam*vcy*p02;
-    pcz2=pz2+(gam-1.0)*pn2*nz-gam*vcz*p02;
+    pcx2=px2+(gam-1.f)*pn2*nx-gam*vcx*p02;
+    pcy2=py2+(gam-1.f)*pn2*ny-gam*vcy*p02;
+    pcz2=pz2+(gam-1.f)*pn2*nz-gam*vcz*p02;
       
       
     //  introduce right-handed coordinate system
@@ -134,30 +134,30 @@ struct BinaryCollision
     nn2=std::sqrt(pcx1*pcx1+pcy1*pcy1);
     nn3=nn1*nn2;
   
-    if (nn2 != 0.0) {
+    if (nn2 != 0.f) {
       nx1=pcx1/nn1;
       ny1=pcy1/nn1;
       nz1=pcz1/nn1;
     
       nx2=pcy1/nn2;
       ny2=-pcx1/nn2;
-      nz2=0.0;
+      nz2=0.f;
     
       nx3=-pcx1*pcz1/nn3;
       ny3=-pcy1*pcz1/nn3;
       nz3=nn2*nn2/nn3;
     } else {
-      nx1=0.0;
-      ny1=0.0;
-      nz1=1.0;
+      nx1=0.f;
+      ny1=0.f;
+      nz1=1.f;
     
-      nx2=0.0;
-      ny2=1.0;
-      nz2=0.0;
+      nx2=0.f;
+      ny2=1.f;
+      nz2=0.f;
     
-      nx3=1.0;
-      ny3=0.0;
-      nz3=0.0;
+      nx3=1.f;
+      ny3=0.f;
+      nz3=0.f;
     }
     
           
@@ -171,13 +171,13 @@ struct BinaryCollision
     vcy2=pcy2/pc02;
     vcz2=pcz2/pc02;
   
-    vcn=1.0/(1.0-(vcx1*vcx2+vcy1*vcy2+vcz1*vcz2));
+    vcn=1.f/(1.f-(vcx1*vcx2+vcy1*vcy2+vcz1*vcz2));
     vcxr=vcn*(vcx1-vcx2);
     vcyr=vcn*(vcy1-vcy2);
     vczr=vcn*(vcz1-vcz2);
     vcr = std::sqrt(vcxr*vcxr+vcyr*vcyr+vczr*vczr);
-    if (vcr < 1.0e-20) {
-      vcr=1.0e-20;
+    if (vcr < 1.e-20f) {
+      vcr=1.e-20f;
     }
   
     m3=m1;
@@ -191,11 +191,11 @@ struct BinaryCollision
     // determine absolute value of post-collision momentum in cm-frame
           
     h2=ss-m3*m3-m4*m4;
-    h3=(h2*h2-4.0*m3*m3*m4*m4)/(4.0*ss);
-    if (h3 < 0.) {
-      mprintf("WARNING: ss %g (m3+m4)^2 %g in psc_collision_c.c\n",
+    h3=(h2*h2-4.f*m3*m3*m4*m4)/(4.f*ss);
+    if (h3 < 0.f) {
+      mprintf("WARNING: ss %g (m3+m4)^2 %g in BinaryCollision\n",
 	      ss, (m3+m4)*(m3+m4));
-      return 0.; // nudt = 0 because no collision
+      return 0.f; // nudt = 0 because no collision
     }
           
     qqc=std::sqrt(h3);
@@ -206,26 +206,26 @@ struct BinaryCollision
   
     // event generator of angles for post collision vectors
   
-    ran1 = (1.0 * random()) / RAND_MAX ;
-    ran2 = (1.0 * random()) / RAND_MAX ;
-    if (ran2 < 1e-20) {
-      ran2 = 1e-20;
+    ran1 = (1.f * random()) / RAND_MAX;
+    ran2 = (1.f * random()) / RAND_MAX;
+    if (ran2 < 1e-20f) {
+      ran2 = 1e-20f;
     }
   
-    nu = 6.28318530717958623200 * ran1;
+    nu = real_t(2.*M_PI) * ran1;
   
-    if(nudt<1.0) {                   // small angle collision
-      psi=2.0*atan(std::sqrt(-0.5*nudt*log(1.0-ran2)));
+    if(nudt<1.f) {                   // small angle collision
+      psi=2.f*std::atan(std::sqrt(-.5f*nudt*std::log(1.f-ran2)));
     } else {
-      psi=acos(1.0-2.0*ran2);          // isotropic angles
+      psi=std::acos(1.f-2.f*ran2);          // isotropic angles
     }
             
     // determine post-collision momentum in cm-frame
                     
-    h1=cos(psi);
-    h2=sin(psi);
-    h3=sin(nu);
-    h4=cos(nu);
+    h1=std::cos(psi);
+    h2=std::sin(psi);
+    h3=std::sin(nu);
+    h4=std::cos(nu);
   
     pc03=std::sqrt(m3*m3+qqc*qqc);
     pcx3=qqc*(h1*nx1+h2*h3*nx2+h2*h4*nx3);
@@ -246,13 +246,13 @@ struct BinaryCollision
     pn3=pcx3*nx+pcy3*ny+pcz3*nz;
     pn4=pcx4*nx+pcy4*ny+pcz4*nz;
     /* p03=gam*(pc03+bet*pn3); */
-    px3=pcx3+(gam-1.0)*pn3*nx+gam*vcx*pc03;
-    py3=pcy3+(gam-1.0)*pn3*ny+gam*vcy*pc03;
-    pz3=pcz3+(gam-1.0)*pn3*nz+gam*vcz*pc03;
+    px3=pcx3+(gam-1.f)*pn3*nx+gam*vcx*pc03;
+    py3=pcy3+(gam-1.f)*pn3*ny+gam*vcy*pc03;
+    pz3=pcz3+(gam-1.f)*pn3*nz+gam*vcz*pc03;
     /* p04=gam*(pc04+bet*pn4); */
-    px4=pcx4+(gam-1.0)*pn4*nx+gam*vcx*pc04;
-    py4=pcy4+(gam-1.0)*pn4*ny+gam*vcy*pc04;
-    pz4=pcz4+(gam-1.0)*pn4*nz+gam*vcz*pc04;
+    px4=pcx4+(gam-1.f)*pn4*nx+gam*vcx*pc04;
+    py4=pcy4+(gam-1.f)*pn4*ny+gam*vcy*pc04;
+    pz4=pcz4+(gam-1.f)*pn4*nz+gam*vcz*pc04;
   
     px3=px3/m3;
     py3=py3/m3;
