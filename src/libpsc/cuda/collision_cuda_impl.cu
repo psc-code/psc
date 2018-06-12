9f1c6537cf5da7c9929f27dcacc1f83eb8ecab9d
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-06-12 13:46:16 -0400

    collision/cuda: separate CollisionCudaHost

--- src/libpsc/cuda/collision_cuda_impl.cu
+++ src/libpsc/cuda/collision_cuda_impl.cu
@@ -8,21 +8,13 @@
 
 template<typename BS>
 CollisionCuda<BS>::CollisionCuda(MPI_Comm comm, int interval, double nu)
-  : coll_{comm, interval, nu},
-    fwd_{new cuda_collision<cuda_mparticles<BS>>{interval, nu, ppsc->prm.nicell, ppsc->dt}}
+  : fwd_{new cuda_collision<cuda_mparticles<BS>>{interval, nu, ppsc->prm.nicell, ppsc->dt}}
 {}
 
 template<typename BS>
 void CollisionCuda<BS>::operator()(MparticlesCuda<BS>& _mprts)
 {
-#if 1
   (*fwd_)(*_mprts.cmprts());
-#else
-  auto& mprts = _mprts.template get_as<MparticlesSingle>();
-  sort_(mprts);
-  coll_(mprts);
-  _mprts.put_as(mprts);
-#endif
 }
 
 template struct CollisionCuda<BS144>;
--- src/libpsc/cuda/collision_cuda_impl.hxx
+++ src/libpsc/cuda/collision_cuda_impl.hxx
@@ -3,11 +3,6 @@
 
 #include "collision.hxx"
 #include "psc_particles_cuda.h"
-#include "psc_particles_single.h"
-#include "psc_fields_single.h"
-
-#include "../libpsc/psc_sort/psc_sort_impl.hxx"
-#include "../libpsc/psc_collision/psc_collision_impl.hxx"
 
 template<typename cuda_mparticles>
 struct cuda_collision;
@@ -28,8 +23,6 @@ public:
   void operator()(Mparticles& _mprts);
 
 private:
-  SortCountsort2<MparticlesSingle> sort_;
-  Collision_<MparticlesSingle, MfieldsSingle> coll_;
   cuda_collision<cuda_mparticles<BS>> *fwd_;
 };
 
