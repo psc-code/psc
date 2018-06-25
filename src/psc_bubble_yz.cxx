3ef3a398686b04dbe69f7560728943941575754f
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-06-25 15:21:47 -0400

    bubble: copy in psc_main()

--- src/psc_bubble_yz.cxx
+++ src/psc_bubble_yz.cxx
@@ -248,5 +248,55 @@ struct psc_ops_bubble : psc_ops {
 int
 main(int argc, char **argv)
 {
-  return psc_main(&argc, &argv, &psc_bubble_ops);
+#ifdef USE_VPIC
+  vpic_base_init(&argc, &argv);
+#else
+  MPI_Init(&argc, &argv);
+#endif
+  libmrc_params_init(argc, argv);
+  mrc_set_flags(MRC_FLAG_SUPPRESS_UNPREFIXED_OPTION_WARNING);
+
+  mrc_class_register_subclass(&mrc_class_psc, &psc_bubble_ops);
+
+  int from_checkpoint = -1;
+  mrc_params_get_option_int("from_checkpoint", &from_checkpoint);
+
+  struct psc *psc;
+
+  // regular start-up (not from checkpoint)
+  
+  // psc_create() will create the psc object, create the sub-objects
+  // (particle, field pusher and many others) and set the parameter defaults.
+  // It will also set the psc subtype defaults and call psc_subtype_create(),
+  // which will change some of the general defaults to match this case.
+  psc = psc_create(MPI_COMM_WORLD);
+  
+  // psc_set_from_options() will override general and bubble psc parameters
+  // if given on the command line. It will also call
+  // psc_bubble_set_from_options()
+  psc_set_from_options(psc);
+  
+  // psc_setup() will set up the various sub-objects (particle pusher, ...)
+  // and set up the initial domain partition, the particles and the fields.
+  // The standard implementation, used here, will set particles using
+  // psc_bubble_init_npt and the fields using setup_field()
+  psc_setup(psc);
+
+  // psc_view() will just print a whole lot of info about the psc object and
+  // sub-objects, in particular all the parameters.
+  psc_view(psc);
+  psc_mparticles_view(psc->particles);
+  psc_mfields_view(psc->flds);
+  
+  // psc_integrate() uses the standard implementation, which does the regular
+  // classic PIC time integration loop
+  psc_integrate(psc);
+  
+  // psc_destroy() just cleans everything up when we're done.
+  psc_destroy(psc);
+  
+  libmrc_params_finalize();
+  MPI_Finalize();
+
+  return 0;
 }
