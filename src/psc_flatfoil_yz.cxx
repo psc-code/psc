8d056954ed665bbe0376a22a4f98e015f98d0be9
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-06-25 16:41:38 -0400

    flatfoil: switch to Single

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -290,7 +290,7 @@ using dim_t = dim_xyz;
 #else
 using dim_t = dim_yz;
 #endif
-using PscConfig = PscConfig1vbecCuda<dim_t>;
+using PscConfig = PscConfig1vbecSingle<dim_t>;
 
 // ======================================================================
 // PscFlatfoil
