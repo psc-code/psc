4c18e73b4ae59ef1c1b6be14ed608c7fcd7a8fcb
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-06-26 11:29:03 -0400

    bubble: own integrate() function, like flatfoil

--- src/psc_bubble_yz.cxx
+++ src/psc_bubble_yz.cxx
@@ -7,6 +7,8 @@
 #include <psc_particles_single.h>
 #include <psc_fields_single.h>
 
+#include "push_particles.hxx"
+
 #include <mrc_params.h>
 
 #include <math.h>
@@ -264,11 +266,104 @@ struct PscBubble : PscBubbleParams
       psc_{psc},
       mprts_{dynamic_cast<Mparticles_t&>(*PscMparticlesBase{psc->particles}.sub())},
       mflds_{dynamic_cast<Mfields_t&>(*PscMfieldsBase{psc->flds}.sub())}
-  {}
+  {
+    setup_stats();
+  }
+
+  // ----------------------------------------------------------------------
+  // setup_stats
+  
+  void setup_stats()
+  {
+    st_nr_particles = psc_stats_register("nr particles");
+    st_time_step = psc_stats_register("time entire step");
+
+    // generic stats categories
+    st_time_particle = psc_stats_register("time particle update");
+    st_time_field = psc_stats_register("time field update");
+    st_time_comm = psc_stats_register("time communication");
+    st_time_output = psc_stats_register("time output");
+  }
+  
+  // ----------------------------------------------------------------------
+  // integrate
 
   void integrate()
   {
-    psc_integrate(ppsc);
+    //psc_method_initialize(psc_->method, psc_);
+    psc_output(psc_);
+    psc_stats_log(psc_);
+    psc_print_profiling(psc_);
+
+    mpi_printf(psc_comm(psc_), "Initialization complete.\n");
+
+    static int pr;
+    if (!pr) {
+      pr = prof_register("psc_step", 1., 0, 0);
+    }
+
+    mpi_printf(psc_comm(psc_), "*** Advancing\n");
+    double elapsed = MPI_Wtime();
+
+    bool first_iteration = true;
+    while (psc_->timestep < psc_->prm.nmax) {
+      prof_start(pr);
+      psc_stats_start(st_time_step);
+
+      if (!first_iteration &&
+	  psc_->prm.write_checkpoint_every_step > 0 &&
+	  psc_->timestep % psc_->prm.write_checkpoint_every_step == 0) {
+	psc_write_checkpoint(psc_);
+      }
+      first_iteration = false;
+
+      mpi_printf(psc_comm(psc_), "**** Step %d / %d, Code Time %g, Wall Time %g\n", psc_->timestep + 1,
+		 psc_->prm.nmax, psc_->timestep * psc_->dt, MPI_Wtime() - psc_->time_start);
+
+      prof_start(pr_time_step_no_comm);
+      prof_stop(pr_time_step_no_comm); // actual measurements are done w/ restart
+
+      void psc_step(struct psc *psc);
+      psc_step(psc_);
+      
+      psc_->timestep++; // FIXME, too hacky
+      psc_output(psc_);
+
+      psc_stats_stop(st_time_step);
+      prof_stop(pr);
+
+      psc_stats_val[st_nr_particles] = mprts_.get_n_prts();
+
+      if (psc_->timestep % psc_->prm.stats_every == 0) {
+	psc_stats_log(psc_);
+	psc_print_profiling(psc_);
+      }
+
+      if (psc_->prm.wallclock_limit > 0.) {
+	double wallclock_elapsed = MPI_Wtime() - psc_->time_start;
+	double wallclock_elapsed_max;
+	MPI_Allreduce(&wallclock_elapsed, &wallclock_elapsed_max, 1, MPI_DOUBLE, MPI_MAX,
+		      MPI_COMM_WORLD);
+      
+	if (wallclock_elapsed_max > psc_->prm.wallclock_limit) {
+	  mpi_printf(MPI_COMM_WORLD, "WARNING: Max wallclock time elapsed!\n");
+	  break;
+	}
+      }
+    }
+
+    if (psc_->prm.write_checkpoint) {
+      psc_write_checkpoint(psc_);
+    }
+
+    // FIXME, merge with existing handling of wallclock time
+    elapsed = MPI_Wtime() - elapsed;
+
+    int  s = (int)elapsed, m  = s/60, h  = m/60, d  = h/24, w = d/ 7;
+    /**/ s -= m*60,        m -= h*60, h -= d*24, d -= w*7;
+    mpi_printf(psc_comm(psc_), "*** Finished (%gs / %iw:%id:%ih:%im:%is elapsed)\n",
+	       elapsed, w, d, h, m, s );
+    
   }
 
 private:
@@ -276,6 +371,9 @@ private:
 
   Mparticles_t& mprts_;
   Mfields_t& mflds_;
+
+  int st_nr_particles;
+  int st_time_step;
 };
 
 // ======================================================================
