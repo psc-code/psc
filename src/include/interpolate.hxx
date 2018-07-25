c1fab9c6c75d06ba1eacdb11df4eec1ab791812d
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-07-25 05:55:28 -0400

    ip: add dim_xz variant (untested...)

--- src/include/interpolate.hxx
+++ src/include/interpolate.hxx
@@ -188,6 +188,52 @@ struct InterpolateEM_Helper<F, IP, opt_ip_1st_ec, dim_xyz>
 };
 
 // ----------------------------------------------------------------------
+// InterpolateEM_Helper: 1st EC, dim_xz
+
+template<typename F, typename IP>
+struct InterpolateEM_Helper<F, IP, opt_ip_1st_ec, dim_xz>
+{
+  using real_t = typename F::real_t;
+
+  __host__ __device__ static real_t ex(const IP& ip, const F& EM)
+  {
+    return (ip.cz.g.v0*(EM(EX, ip.cx.g.l  ,ip.cy.g.l  ,ip.cz.g.l  )) +
+	    ip.cz.g.v1*(EM(EX, ip.cx.g.l  ,ip.cy.g.l  ,ip.cz.g.l+1)));
+  }
+
+  __host__ __device__ static real_t ey(const IP& ip, const F& EM)
+  {
+    return (ip.cx.g.v0*(ip.cz.g.v0*EM(EY, ip.cx.g.l  ,ip.cy.g.l  ,ip.cz.g.l  ) +
+			ip.cz.g.v1*EM(EY, ip.cx.g.l  ,ip.cy.g.l  ,ip.cz.g.l+1)) +
+	    ip.cx.g.v1*(ip.cz.g.v0*EM(EY, ip.cx.g.l+1,ip.cy.g.l  ,ip.cz.g.l  ) +
+			ip.cz.g.v1*EM(EY, ip.cx.g.l+1,ip.cy.g.l  ,ip.cz.g.l+1)));
+  }
+
+  __host__ __device__ static real_t ez(const IP& ip, const F& EM)
+  {
+    return (ip.cx.g.v0*EM(EZ, ip.cx.g.l  ,ip.cy.g.l  ,ip.cz.g.l  ) +
+	    ip.cx.g.v1*EM(EZ, ip.cx.g.l+1,ip.cy.g.l  ,ip.cz.g.l  ));
+  }
+
+  __host__ __device__ static real_t hx(const IP& ip, const F& EM)
+  {
+    return (ip.cx.g.v0*EM(HX, ip.cx.g.l  ,ip.cy.g.l  ,ip.cz.g.l  ) +
+	    ip.cx.g.v1*EM(HX, ip.cx.g.l+1,ip.cy.g.l  ,ip.cz.g.l  ));
+  }
+
+  __host__ __device__ static real_t hy(const IP& ip, const F& EM)
+  {
+    return (EM(HY, ip.cx.g.l  ,ip.cy.g.l  ,ip.cz.g.l  ));
+  }
+
+  __host__ __device__ static real_t hz(const IP& ip, const F& EM)
+  {
+    return (ip.cz.g.v0*EM(HZ, ip.cx.g.l  ,ip.cy.g.l  ,ip.cz.g.l  ) +
+	    ip.cz.g.v1*EM(HZ, ip.cx.g.l  ,ip.cy.g.l  ,ip.cz.g.l+1));	     
+  }
+};
+
+// ----------------------------------------------------------------------
 // InterpolateEM_Helper: 1st EC, dim_yz
 
 template<typename F, typename IP>
