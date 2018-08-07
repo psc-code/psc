7af8b7fed5382da12d507bdc5231559ad650b8b8
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-08-07 15:28:23 -0400

    cuda/mparticles: start const_accessor_range etc

--- src/libpsc/cuda/psc_particles_cuda.h
+++ src/libpsc/cuda/psc_particles_cuda.h
@@ -90,6 +90,66 @@ struct MparticlesCuda : MparticlesBase
 
   struct patch_t
   {
+    using Double3 = Vec3<double>;
+    
+    struct const_accessor
+    {
+      const_accessor(const particle_t& prt, const patch_t& prts)
+	: prt_{prt}, prts_{prts}
+      {}
+      
+      Real3 u()   const { return prt_.p; }
+      real_t w()  const { return prt_.w; }
+      int kind()  const { return prt_.kind; }
+      
+      Double3 position() const
+      {
+	auto& patch = prts_.mp_.grid().patches[prts_.p_];
+	
+	return patch.xb + Double3{prt_.x};
+      }
+    
+    private:
+      const particle_t& prt_;
+      const patch_t& prts_;
+    };
+  
+    struct const_accessor_range
+    {
+      struct const_iterator : std::iterator<std::random_access_iterator_tag,
+					    const_accessor,  // value type
+					    ptrdiff_t,       // difference type
+					    const_accessor*, // pointer type
+					    const_accessor&> // reference type
+      
+      {
+	const_iterator(const patch_t& prts, uint n)
+	  : prts_{prts}, n_{n}
+	{}
+	
+	bool operator==(const_iterator other) const { return n_ == other.n_; }
+	bool operator!=(const_iterator other) const { return !(*this == other); }
+	
+	const_iterator& operator++() { n_++; return *this; }
+	const_iterator operator++(int) { auto retval = *this; ++(*this); return retval; }
+	//const_accessor operator*() { return {prts_[n_], prts_}; }
+	
+      private:
+	const patch_t& prts_;
+	uint n_;
+      };
+    
+      const_accessor_range(const patch_t& prts)
+	: prts_{prts}
+      {}
+
+      const_iterator begin() const;
+      const_iterator end()   const;
+      
+    private:
+      const patch_t& prts_;
+    };
+
     patch_t(const MparticlesCuda& mp, int p)
       : mp_(mp), p_(p)
     {}
@@ -98,6 +158,15 @@ struct MparticlesCuda : MparticlesBase
 
     void push_back(const particle_t& prt) { assert(0); }
 
+    uint size()
+    {
+      uint n_prts_by_patch[mp_.grid().n_patches()];
+      mp_.get_size_all(n_prts_by_patch);
+      return n_prts_by_patch[p_];
+    }
+
+    const_accessor_range get() const { return {*this}; }
+    
   private:
     const MparticlesCuda& mp_;
     int p_;
