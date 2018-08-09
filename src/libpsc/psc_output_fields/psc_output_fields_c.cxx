56c7edb7bc43cf40de2e00ecd9322ad1ba6188f9
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-08-08 23:48:54 -0400

    outf: get rid of psc_output_fields_c.h

--- src/libpsc/psc_output_fields/psc_output_fields_c.cxx
+++ src/libpsc/psc_output_fields/psc_output_fields_c.cxx
@@ -1,5 +1,5 @@
 
-#include "psc_output_fields_c.h"
+#include "psc_output_fields_private.h"
 #include "psc_output_fields_item.h"
 #include "psc_fields_as_c.h"
 #include "bnd.hxx"
@@ -10,6 +10,53 @@
 #include <mrc_params.h>
 #include <mrc_io.h>
 #include <string.h>
+
+
+#include "fields3d.hxx"
+#include "fields_item.hxx"
+
+// ----------------------------------------------------------------------
+
+enum {
+  IO_TYPE_PFD,
+  IO_TYPE_TFD,
+  NR_IO_TYPES,
+};
+
+struct OutputFieldsItem
+{
+  OutputFieldsItem(PscFieldsItemBase item, const std::string& name,
+		   std::vector<std::string>& comp_names, MfieldsBase& pfd,
+		   MfieldsBase& tfd)
+    : item(item), name(name), comp_names(comp_names), pfd(pfd), tfd(tfd)
+  {}
+
+  PscFieldsItemBase item;
+  MfieldsBase& pfd;
+  MfieldsBase& tfd;
+  std::string name;
+  std::vector<std::string> comp_names;
+};
+
+struct psc_output_fields_c {
+  char *data_dir;
+  char *output_fields;
+  char *pfd_s;
+  char *tfd_s;
+  bool dowrite_pfield, dowrite_tfield;
+  int pfield_first, tfield_first;
+  int pfield_step, tfield_step;
+  int tfield_length;
+  int tfield_every;
+  int rn[3];
+  int rx[3];
+	
+  int pfield_next, tfield_next;
+  // storage for output
+  unsigned int naccum;
+  std::vector<OutputFieldsItem> items;
+  struct mrc_io *ios[NR_IO_TYPES];
+};
 
 using PscOutputFields_t = PscOutputFields<psc_output_fields_c>;
 
