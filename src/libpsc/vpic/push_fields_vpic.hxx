c0eac238ff89f069b4a3de5c109aff83802d759e
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-08-10 15:48:36 -0400

    Revert "rmdr push_fields"
    This reverts commit 97892685fe763b056a2a422f9da5458f24485bee.

--- src/libpsc/vpic/push_fields_vpic.hxx
+++ src/libpsc/vpic/push_fields_vpic.hxx
@@ -27,28 +27,22 @@ struct PushFieldsVpic : PushFieldsBase
     TIC PushFieldsOps::advance_b(mflds.vmflds(), dt_fac); TOC(advance_b, 1);
   }
 
-  void push_E(MfieldsStateBase& mflds_base, double dt_fac) override
+  void push_E(MfieldsBase& mflds_base, double dt_fac) override
   {
-    assert(0);
-#if 0
     // needs J, E, B, TCA, material
     auto& mflds = mflds_base.get_as<MfieldsStateVpic>(JXI, MfieldsStateVpic::N_COMP);
     push_E(mflds, dt_fac);
     // updates E, TCA, and B ghost points FIXME 9 == TCAX
     mflds_base.put_as(mflds, EX, 9 + 3);
-#endif
   }
 
-  void push_H(MfieldsStateBase& mflds_base, double dt_fac) override
+  void push_H(MfieldsBase& mflds_base, double dt_fac) override
   {
-    assert(0);
-#if 0
     // needs E, B
     auto& mflds = mflds_base.get_as<MfieldsStateVpic>(EX, HX + 6);
     push_H(mflds, dt_fac);
     // updates B
     mflds_base.put_as(mflds, HX, HX + 3);
-#endif
   }
 
 private:
