8ebaac7c5371e805b73b1f8e0a8066ced9e7efe1
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-08-14 17:24:38 -0400

    resurrect working MfieldsInterpolatorVpic

--- src/libpsc/vpic/VpicInterpolatorBase.h
+++ src/libpsc/vpic/VpicInterpolatorBase.h
@@ -12,16 +12,6 @@ struct VpicInterpolatorBase : interpolator_array_t {
   typedef G Grid;
   typedef interpolator_t Element;
 
-  static VpicInterpolatorBase* create(Grid *grid)
-  {
-    return static_cast<VpicInterpolatorBase*>(new_interpolator_array(grid));
-  }
-  
-  static void destroy(VpicInterpolatorBase* interpolator)
-  {
-    delete_interpolator_array(interpolator);
-  }
-  
   Element  operator[](int idx) const { return i[idx]; }
   Element& operator[](int idx)       { return i[idx]; }
 
--- src/libpsc/vpic/mfields_interpolator_vpic.hxx
+++ src/libpsc/vpic/mfields_interpolator_vpic.hxx
@@ -43,12 +43,49 @@ private:
 };
 
 // ======================================================================
+// MfieldsInterpolatorPsc
 
 template<typename Grid>
 using MfieldsInterpolatorPsc = MfieldsInterpolator_<PscInterpolatorBase<Grid>>;
 
-using MfieldsInterpolatorVpic = MfieldsInterpolator_<PscInterpolatorBase<VpicGridBase>>;
+// ======================================================================
+// MfieldsInterpolatorVpic
 
+struct MfieldsInterpolatorVpic
+{
+  using Interpolator = VpicInterpolatorBase<VpicGridBase>;
+  using Grid = VpicGridBase;
+  using Element = typename Interpolator::Element;
 
+  struct Patch
+  {
+    using Element = Element;
+    
+    Patch(Grid* vgrid)
+      : ip_{static_cast<Interpolator*>(new_interpolator_array(vgrid))}
+    {}
 
+    ~Patch()
+    {
+      delete_interpolator_array(ip_);
+    }
+    
+    Element* data() { return ip_->data(); }
+    Element  operator[](int idx) const { return (*ip_)[idx]; }
+    Element& operator[](int idx)       { return (*ip_)[idx]; }
+
+    Grid* grid() { return ip_->grid(); }
+
+    Interpolator *ip_;
+  };
+
+  MfieldsInterpolatorVpic(Grid* vgrid)
+    : patch_{vgrid}
+  {}
+
+  Patch& getPatch(int p) { return patch_; }
   
+private:
+  Patch patch_;
+};
+
