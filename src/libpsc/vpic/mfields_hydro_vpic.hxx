ce71d0d826dac1435f5ef11fc02fbe07fe84526a
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-08-14 01:09:23 -0400

    vpic: MfieldsHydroVpic now pretty

--- src/libpsc/vpic/mfields_hydro_vpic.hxx
+++ src/libpsc/vpic/mfields_hydro_vpic.hxx
@@ -13,66 +13,67 @@ struct MfieldsHydroVpic
   using Element = hydro_t;
   using fields_t = fields3d<float, LayoutAOS>;
 
+  enum {
+    N_COMP = 16,
+  };
+
+  static_assert(N_COMP == sizeof(Element) / sizeof(real_t), "N_COMP doesn't match Element");
+
   struct Patch
   {
     using Element = Element;
 
     Patch(Grid* vgrid)
-      : vgrid_{vgrid}
-    {}
+      : vgrid_{vgrid},
+	ha_{::new_hydro_array(vgrid)}
+    {
+      ::clear_hydro_array(ha_);
+    }
 
-    Element* data() { return ha->h; }
+    ~Patch()
+    {
+      ::delete_hydro_array(ha_);
+    }
     
-    Element  operator[](int idx) const { return ha->h[idx]; }
-    Element& operator[](int idx)       { return ha->h[idx]; }
+    Element* data() { return ha_->h; }
+    
+    Element  operator[](int idx) const { return ha_->h[idx]; }
+    Element& operator[](int idx)       { return ha_->h[idx]; }
 
     Grid* grid() { return vgrid_; }
+
+    operator hydro_array_t*() { return ha_; }
     
-    hydro_array_t* ha;
   private:
+    hydro_array_t* ha_;
     Grid* vgrid_;
  };
     
-  enum {
-    N_COMP = 16,
-  };
-
   MfieldsHydroVpic(const Grid_t& grid, Grid* vgrid)
     : grid_{grid},
       patch_{vgrid}
   {
     assert(grid.n_patches() == 1);
 
-    patch_.ha = ::new_hydro_array(vgrid);
-
     const int B = 1; // VPIC always uses one ghost cell (on c.c. grid)
     im_ = { vgrid->nx + 2*B, vgrid->ny + 2*B, vgrid->nz + 2*B };
     ib_ = { -B, -B, -B };
-    data_ = &patch_.ha->h[0].jx;
-
-    ::clear_hydro_array(patch_.ha);
-  }
-
-  ~MfieldsHydroVpic()
-  {
-    ::delete_hydro_array(patch_.ha);
   }
 
   int n_patches() const { return grid_.n_patches(); }
   int n_comps() const { return N_COMP; }
 
-  real_t* data() { return data_; }
-  fields_t operator[](int p) { return {ib_, im_, N_COMP, data_}; }
+  real_t* data() { return reinterpret_cast<real_t*>(patch_.data()); }
+  fields_t operator[](int p) { return {ib_, im_, N_COMP, data()}; }
   Patch& getPatch(int p) { return patch_; }
 
   Grid* vgrid() { return patch_.grid(); }
 
-  operator hydro_array_t*() { return patch_.ha; }
+  operator hydro_array_t*() { return patch_; }
 
 private:
-  real_t* data_;
-  Int3 ib_, im_;
   const Grid_t& grid_;
   Patch patch_;
+  Int3 ib_, im_;
 };
 
