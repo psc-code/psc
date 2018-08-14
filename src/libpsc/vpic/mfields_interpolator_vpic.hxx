4fb6d14ec991061a51b00847c297f90726ca22c3
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-08-14 17:27:24 -0400

    vpic/ip: MfieldsInterpolatorVpic done

--- src/libpsc/vpic/mfields_interpolator_vpic.hxx
+++ src/libpsc/vpic/mfields_interpolator_vpic.hxx
@@ -53,16 +53,15 @@ using MfieldsInterpolatorPsc = MfieldsInterpolator_<PscInterpolatorBase<Grid>>;
 
 struct MfieldsInterpolatorVpic
 {
-  using Interpolator = VpicInterpolatorBase<VpicGridBase>;
   using Grid = VpicGridBase;
-  using Element = typename Interpolator::Element;
+  using Element = interpolator_t;
 
   struct Patch
   {
     using Element = Element;
     
     Patch(Grid* vgrid)
-      : ip_{static_cast<Interpolator*>(new_interpolator_array(vgrid))}
+      : ip_{new_interpolator_array(vgrid)}
     {}
 
     ~Patch()
@@ -70,13 +69,13 @@ struct MfieldsInterpolatorVpic
       delete_interpolator_array(ip_);
     }
     
-    Element* data() { return ip_->data(); }
-    Element  operator[](int idx) const { return (*ip_)[idx]; }
-    Element& operator[](int idx)       { return (*ip_)[idx]; }
+    Element* data() { return ip_->i; }
+    Element  operator[](int idx) const { return ip_->i[idx]; }
+    Element& operator[](int idx)       { return ip_->i[idx]; }
 
-    Grid* grid() { return ip_->grid(); }
+    Grid* grid() { return static_cast<Grid*>(ip_->g); }
 
-    Interpolator *ip_;
+    interpolator_array_t *ip_;
   };
 
   MfieldsInterpolatorVpic(Grid* vgrid)
