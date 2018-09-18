5531a9fb6a55d3a92e75a376766a03438d0b7861
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-09-18 11:45:30 -0400

    cuda: make SetupParticlesCuda work again

--- src/libpsc/cuda/setup_particles_cuda.hxx
+++ src/libpsc/cuda/setup_particles_cuda.hxx
@@ -8,20 +8,26 @@
 
 template<>
 template<typename FUNC>
-void SetupParticles<MparticlesCuda<BS144>>::setup_particles2(Mparticles& mprts,
+void SetupParticles<MparticlesCuda<BS144>>::setup_particles(Mparticles& mprts,
 							     std::vector<uint>& n_prts_by_patch,
 							     FUNC func)
 {
+  SetupParticles<MparticlesSingle> setup_particles;
+  setup_particles.neutralizing_population = neutralizing_population;
+  setup_particles.fractional_n_particles_per_cell = fractional_n_particles_per_cell;
+  setup_particles.const_num_particles_per_cell = const_num_particles_per_cell;
+  setup_particles.initial_momentum_gamma_correction = initial_momentum_gamma_correction;
+
   auto& mp = mprts.get_as<MparticlesSingle>();
-  SetupParticles<MparticlesSingle>::setup_particles2(mp, n_prts_by_patch, func);
+  setup_particles.setup_particles(mp, n_prts_by_patch, func);
   mprts.put_as(mp);
 }
 
 template<>
 template<typename FUNC>
-void SetupParticles<MparticlesCuda<BS144>>::setup_particles(MparticlesCuda<BS144>& mprts,
-							    std::vector<uint>& n_prts_by_patch,
-							    FUNC func)
+void SetupParticles<MparticlesCuda<BS144>>::setup_particles2(MparticlesCuda<BS144>& mprts,
+							     std::vector<uint>& n_prts_by_patch,
+							     FUNC func)
 {
   using particle_t = typename Mparticles::particle_t;
 
