fb9c30fae1d735d93f39a1a30e0decf5b4bda3a3
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-09-18 23:19:14 -0400

    inject: test now actually tests something

--- src/libpsc/tests/test_inject.cxx
+++ src/libpsc/tests/test_inject.cxx
@@ -11,7 +11,7 @@ struct InjectTestTarget
 {
   bool is_inside(double crd[3])
   {
-    return (crd[1] >= .25 && crd[1] <= .75);
+    return (crd[1] >= 40. && crd[1] <= 120.);
   }
 
   void init_npt(int pop, double crd[3], struct psc_particle_npt *npt)
@@ -77,36 +77,63 @@ using InjectTestTypes = ::testing::Types<
 TYPED_TEST_CASE(InjectTest, InjectTestTypes);
 
 // ======================================================================
-// SingleParticle test
+// InjectTest/Test1
 
-TYPED_TEST(InjectTest, SingleParticle)
+TYPED_TEST(InjectTest, Test1)
 {
   using Mparticles = typename TypeParam::Mparticles;
   using Mfields = typename TypeParam::Mfields;
   using PushParticles = typename TypeParam::PushParticles;
   using Inject = Inject_<Mparticles, Mfields, InjectTestTarget>;
+  using ItemMoment = typename Inject::ItemMoment_t;
+  using real_t = typename Mparticles::real_t;
+
+  const real_t eps = 1e-5;
 
   auto kinds = Grid_t::Kinds{Grid_t::Kind(1., 1., "test_species")};
   this->make_psc(kinds);
   const auto& grid = this->grid();
 
-  auto inject = Inject{grid, 1, 10, 0, InjectTestTarget{}};
+  const int inject_interval = 1;
+  const int inject_tau = 10;
+  auto target = InjectTestTarget{};
+  auto inject = Inject{grid, 1, 10, 0, target};
 
   // let's start with no particles
   Mparticles mprts{grid};
-  mprts.inject(0, {{1., 0., 0.}, {}, 1., 0});
 
-#if 0
-  const typename Mparticles::real_t eps = 1e-5;
+  // should have no particles
+  EXPECT_EQ(mprts[0].size(), 0);
 
-  // check particle
-  EXPECT_EQ(mprts[0].size(), 1);
-  auto prt = *mprts[0].get().begin();
-  EXPECT_NEAR(prt.position()[0], 1., eps);
-  EXPECT_NEAR(prt.position()[1], 0., eps);
-  EXPECT_NEAR(prt.position()[2], 0., eps);
-  EXPECT_NEAR(prt.w(), 1., eps);
-#endif
+  // density should be all zero
+  ItemMoment moment_n{grid, grid.comm()};
+  auto& mflds_n = moment_n.result();
+
+  moment_n.run(mprts);
+  for (int p = 0; p < grid.n_patches(); p++) {
+    grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
+	EXPECT_EQ(mflds_n[p](0, i,j,k), 0.);
+      });
+  }
+
+  // do one injection
+  inject(mprts);
+
+  // density should be equal to n_injected inside target
+  real_t fac = 1. / grid.norm.cori * 
+    (inject_interval * grid.dt / inject_tau) / (1. + inject_interval * grid.dt / inject_tau);
+  real_t n_injected = int(fac) * grid.norm.cori;
+  // FIXME, it's worth noting that given the wrong choice of interval / tau / nicell, one may end up never injecting anything because the # particles to be injected turns out to be < 1 and we always round down
+  mprintf("fac = %g, n_injected = %g\n", fac, n_injected);
+  
+  moment_n.run(mprts);
+  for (int p = 0; p < grid.n_patches(); p++) {
+    grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
+	double xx[3] = {grid.patches[p].x_cc(i), grid.patches[p].y_cc(j), grid.patches[p].z_cc(k)};
+	real_t n_expected = target.is_inside(xx) ? n_injected : 0.;
+	EXPECT_NEAR(mflds_n[p](0, i,j,k), n_expected, eps) << "ijk " << i << ":" << j << ":" << k;
+      });
+  }
 }
 
 // ======================================================================
