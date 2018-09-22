24bf3b789632c6adc6e19afdb76442929d7585fb
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-09-22 00:23:12 -0400

    setup_particles/cuda: broken BS444 implementation gone

--- src/libpsc/cuda/setup_particles_cuda.hxx
+++ src/libpsc/cuda/setup_particles_cuda.hxx
@@ -23,27 +23,5 @@ void SetupParticles<MparticlesCuda<BS144>>::setup_particles(Mparticles& mprts,
   mprts.put_as(mp);
 }
 
-// FIXME, exactly duplicated for BS444
+// FIXME, missing for BS444
 
-template<>
-template<typename FUNC>
-void SetupParticles<MparticlesCuda<BS444>>::setup_particles(MparticlesCuda<BS444>& mprts,
-							    std::vector<uint>& n_prts_by_patch,
-							    FUNC func)
-{
-  using particle_t = typename Mparticles::particle_t;
-
-  auto& grid = mprts.grid();
-
-  std::vector<cuda_mparticles_prt> buf;
-  for (int p = 0; p < mprts.n_patches(); p++) {
-    for (int n = 0; n < n_prts_by_patch[p]; n++) {
-      particle_t prt = func(p, n);
-      cuda_mparticles_prt cprt{prt.x, prt.p, prt.w, prt.kind};
-      buf.push_back(cprt);
-    }
-  }
-    
-  mprts.reserve_all(n_prts_by_patch.data());
-  mprts.inject_buf(buf.data(), n_prts_by_patch.data());
-}
