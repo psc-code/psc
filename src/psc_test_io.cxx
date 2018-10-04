e1719d5f084dc4f8dd3fd28d73f6556dfc01c352
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-10-04 12:13:26 -0400

    test/io: fewer patches make it easier to know what's supposed to happen

--- src/psc_test_io.cxx
+++ src/psc_test_io.cxx
@@ -110,8 +110,8 @@ struct PscTestIo
     Int3 gdims = { 400, 800, 2400}; // global number of grid points
     Int3 np = { 40, 80, 4 }; // division into patches
 #else
-    Int3 gdims = { 40, 80, 24}; // global number of grid points
-    Int3 np = { 4, 8, 4 }; // division into patches
+    Int3 gdims = { 20, 20, 80}; // global number of grid points
+    Int3 np = { 2, 2, 8 }; // division into patches
 #endif
     
     if (dim::InvarX::value) { ibn[0] = 0; } // FIXME, wrong place, not for VPIC...
