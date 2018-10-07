d88d8a5417ac8f08233f6f36d0d45031adc2832b
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-10-07 13:35:26 -0400

    test/io: log mpi calls

--- src/psc_test_io.cxx
+++ src/psc_test_io.cxx
@@ -7,6 +7,33 @@
 
 #include <string>
 
+int MPI_Irecv(void *buf, int count, MPI_Datatype datatype, int source,
+	      int tag, MPI_Comm comm, MPI_Request *request)
+{
+  mprintf("MPI_Irecv buf %p count %d source %d tag %d request %p\n",
+	  buf, count, source, tag, request);
+  int ierr = PMPI_Irecv(buf, count, datatype, source, tag, comm, request);
+  return ierr;
+}
+
+
+int MPI_Isend(void *buf, int count, MPI_Datatype datatype, int dest,
+	      int tag, MPI_Comm comm, MPI_Request *request)
+{
+  mprintf("MPI_Irecv buf %p count %d dest %d tag %d request %p\n",
+	  buf, count, dest, tag, request);
+  int ierr = PMPI_Irecv(buf, count, datatype, dest, tag, comm, request);
+  return ierr;
+}
+
+int MPI_Waitall(int count, MPI_Request array_of_requests[],
+		MPI_Status array_of_statuses[])
+{
+  mprintf("MPI_Waitall count %d req %p status %p\n", count, array_of_requests, array_of_statuses);
+  int ierr = PMPI_Waitall(count, array_of_requests, array_of_statuses);
+  return ierr;
+}
+
 // ======================================================================
 // PscTestIo
 
