484540f2dfa6514c651f59df638127b8e8cf3756
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-10-18 16:58:19 -0400

    io2: split test_redist, test_io

--- src/psc_test_io_2.cxx
+++ src/psc_test_io_2.cxx
@@ -11,7 +11,7 @@
 
 struct PscTestIo
 {
-  static void run()
+  static mrc_domain* make_domain()
   {
     auto comm = MPI_COMM_WORLD;
 
@@ -41,10 +41,12 @@ struct PscTestIo
     
     mrc_domain_set_from_options(domain);
     mrc_domain_setup(domain);
-    
-    int n_patches;
-    mrc_patch* patches = mrc_domain_get_patches(domain, &n_patches);
 
+    return domain;
+  }
+
+  static mrc_fld* make_fld(mrc_domain* domain)
+  {
     mrc_fld* fld = mrc_domain_m3_create(domain);
     mrc_fld_set_name(fld, "e");
     mrc_fld_set_param_int(fld, "nr_ghosts", 0);
@@ -53,6 +55,30 @@ struct PscTestIo
     mrc_fld_set_comp_name(fld, 0, "ex");
     mrc_fld_set_comp_name(fld, 1, "ey");
     mrc_fld_set_comp_name(fld, 2, "ez");
+    
+    int n_patches;
+    mrc_patch* patches = mrc_domain_get_patches(domain, &n_patches);
+
+    for (int p = 0; p < n_patches; p++) {
+      mrc_fld_patch *m3p = mrc_fld_patch_get(fld, p);
+      mrc_fld_foreach(fld, i,j,k, 0,0) {
+	int ii = i + patches[p].off[0];
+	int jj = j + patches[p].off[1];
+	int kk = k + patches[p].off[2];
+	for (int m = 0; m < mrc_fld_nr_comps(fld); m++) {
+	  MRC_M3(m3p, m , i,j,k) = ii + 100 * jj + 10000 * kk;
+	}
+      } mrc_fld_foreach_end;
+      mrc_fld_patch_put(fld);
+    }
+    
+    return fld;
+  }
+
+  static void test_redist()
+  {
+    mrc_domain* domain = make_domain();
+    mrc_fld* fld = make_fld(domain);
     
     mpi_printf(MPI_COMM_WORLD, "*** Testing redist\n");
 
@@ -73,8 +99,17 @@ struct PscTestIo
     mrc_redist_put_ndarray(redist, nd);
 
     mrc_redist_destroy(redist);
-    
-    mpi_printf(MPI_COMM_WORLD, "*** Writing output\n");
+    mrc_fld_destroy(fld);
+
+    mpi_printf(MPI_COMM_WORLD, "*** Testing redist done\n");
+  }
+
+  static void test_io()
+  {
+    mrc_domain* domain = make_domain();
+    mrc_fld* fld = make_fld(domain);
+
+    mpi_printf(MPI_COMM_WORLD, "*** Testing output\n");
 
     mrc_io* io = mrc_io_create(MPI_COMM_WORLD);
     mrc_io_set_param_string(io, "basename", "pfd");
@@ -84,28 +119,16 @@ struct PscTestIo
 
     mrc_io_open(io, "w", 0, 0.);
     
-    for (int p = 0; p < n_patches; p++) {
-      mrc_fld_patch *m3p = mrc_fld_patch_get(fld, p);
-      mrc_fld_foreach(fld, i,j,k, 0,0) {
-	int ii = i + patches[p].off[0];
-	int jj = j + patches[p].off[1];
-	int kk = k + patches[p].off[2];
-	for (int m = 0; m < mrc_fld_nr_comps(fld); m++) {
-	  MRC_M3(m3p, m , i,j,k) = ii + 100 * jj + 10000 * kk;
-	}
-      } mrc_fld_foreach_end;
-      mrc_fld_patch_put(fld);
-    }
-    
     mrc_fld_write(fld, io);
 
     mrc_io_close(io);
     mrc_io_destroy(io);
 
-    mpi_printf(mrc_domain_comm(domain), "*** Writing output done.\n");
-
     mrc_fld_destroy(fld);
+
+    mpi_printf(mrc_domain_comm(domain), "*** Testing output done.\n");
   }
+  
 };
 
 
@@ -117,7 +140,8 @@ main(int argc, char **argv)
 {
   psc_init(argc, argv);
 
-  PscTestIo::run();
+  PscTestIo::test_redist();
+  PscTestIo::test_io();
 
   libmrc_params_finalize();
   MPI_Finalize();
