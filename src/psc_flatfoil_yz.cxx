d880c68e57eb5e4f8a85b8e2868f0f732fdd4719
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-10-24 15:37:19 -0400

    flatfoil: less frequent cont checks

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -179,9 +179,9 @@ struct PscFlatfoil : Psc<PscConfig>
 
     // -- Checks
     ChecksParams checks_params{};
-    checks_params.continuity_every_step = 1;
+    checks_params.continuity_every_step = 50;
     checks_params.continuity_threshold = 1e-5;
-    checks_params.continuity_verbose = true;
+    checks_params.continuity_verbose = false;
     checks_.reset(new Checks_t{grid(), comm, checks_params});
 
     // -- Marder correction
