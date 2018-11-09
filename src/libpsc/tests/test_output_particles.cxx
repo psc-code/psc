e91dd5c361015f18a04242b959e4a62ce78e65a3
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-11-08 22:00:51 -0500

    test/outp: start OutputParticlesTest

---
+++ src/libpsc/tests/test_output_particles.cxx
@@ -0,0 +1,91 @@
+
+#include "gtest/gtest.h"
+
+#include "testing.hxx"
+
+#include "grid.hxx"
+#include "fields.hxx"
+#include "psc_particles_double.h"
+#include "psc_fields_c.h"
+#include "push_particles.hxx"
+#include "setup_fields.hxx"
+#include "setup_particles.hxx"
+
+template<typename T>
+struct OutputParticlesTest : ::testing::Test
+{
+  using Dim = typename T::dim;
+
+  const double L = 160;
+
+  void make_psc(const Grid_t::Kinds& kinds)
+  {
+    Int3 gdims = {16, 16, 16};
+    if (Dim::InvarX::value) { gdims[0] = 1; ibn[0] = 0; }
+    if (Dim::InvarY::value) { gdims[1] = 1; ibn[1] = 0; }
+    if (Dim::InvarZ::value) { gdims[2] = 1; ibn[2] = 0; }
+
+    auto grid_domain = Grid_t::Domain{gdims, {L, L, L}};
+    auto grid_bc = GridBc{{ BND_FLD_PERIODIC, BND_FLD_PERIODIC, BND_FLD_PERIODIC },
+			  { BND_FLD_PERIODIC, BND_FLD_PERIODIC, BND_FLD_PERIODIC },
+			  { BND_PRT_PERIODIC, BND_PRT_PERIODIC, BND_PRT_PERIODIC },
+			  { BND_PRT_PERIODIC, BND_PRT_PERIODIC, BND_PRT_PERIODIC }};
+    
+    auto norm_params = Grid_t::NormalizationParams::dimensionless();
+    norm_params.nicell = 200;
+    auto coeff = Grid_t::Normalization{norm_params};
+
+    grid_ = Grid_t::psc_make_grid(grid_domain, grid_bc, kinds, coeff, 1., {});
+  }
+
+  const Grid_t& grid() const
+  {
+    assert(grid_);
+    return *grid_;
+  }
+
+private:
+  Grid_t* grid_;
+  Int3 ibn = { 2, 2, 2 };
+};
+
+using OutputParticlesTestTypes = ::testing::Types<TestConfig1vbec3dSingle>;
+
+TYPED_TEST_CASE(OutputParticlesTest, OutputParticlesTestTypes);
+
+// ======================================================================
+// Test1
+
+TYPED_TEST(OutputParticlesTest, Test1)
+{
+  using Mparticles = typename TypeParam::Mparticles;
+
+  auto kinds = Grid_t::Kinds{{1., 100., "ion"}, {-1., 1., "electron"}};
+  this->make_psc(kinds);
+  const auto& grid = this->grid();
+  
+  // init particle
+  auto n_prts_by_patch = std::vector<uint>{1};
+
+  Mparticles mprts{grid};
+  {
+    auto injector = mprts[0].injector();
+    injector({{1., 0., 0.}, {}, 1., 0});
+    injector({{2., 0., 0.}, {}, 1., 1});
+  }
+
+}
+
+// ----------------------------------------------------------------------
+// main
+
+int main(int argc, char **argv)
+{
+  MPI_Init(&argc, &argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  int rc = RUN_ALL_TESTS();
+  MPI_Finalize();
+  return rc;
+}
+
+
