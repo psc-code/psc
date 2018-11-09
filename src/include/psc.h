1c342982503181e9bd4c88e52ab423e820bae92d
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-11-08 21:41:52 -0500

    psc: get rid of psc_foreach_3d_g

--- src/include/psc.h
+++ src/include/psc.h
@@ -49,18 +49,6 @@ struct psc_particle_npt {
   double T[3]; ///< temperature
 };
 
-#define psc_foreach_3d_g(psc, p, ix, iy, iz) {				\
-  int __ilo[3] = { -psc->ibn[0], -psc->ibn[1], -psc->ibn[2] };		\
-  int __ihi[3] = { psc->grid().ldims[0] + psc->ibn[0],			\
-		   psc->grid().ldims[1] + psc->ibn[1],			\
-		   psc->grid().ldims[2] + psc->ibn[2] };		\
-  for (int iz = __ilo[2]; iz < __ihi[2]; iz++) {			\
-    for (int iy = __ilo[1]; iy < __ihi[1]; iy++) {			\
-      for (int ix = __ilo[0]; ix < __ihi[0]; ix++)
-
-#define psc_foreach_3d_g_end				\
-  } } }
-
 // ----------------------------------------------------------------------
 // we keep this info global for now.
 
