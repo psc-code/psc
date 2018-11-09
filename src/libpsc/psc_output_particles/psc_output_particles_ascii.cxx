b2b0bf574f74d46d692d0a603a80a898d0f70dc4
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-11-08 22:43:12 -0500

    outp/ascii: mv -> output_particles_ascii_impl.hxx

--- src/libpsc/psc_output_particles/psc_output_particles_ascii.cxx
+++ src/libpsc/psc_output_particles/psc_output_particles_ascii.cxx
@@ -1,57 +1,5 @@
 
-#include "psc_particles_double.h"
-#include "output_particles.hxx"
-
-#include <mrc_params.h>
-#include <string.h>
-
-#include <string.h>
-
-struct psc_output_particles_ascii : OutputParticlesParams, OutputParticlesBase
-{
-  psc_output_particles_ascii(const Grid_t& grid, const OutputParticlesParams& params)
-    : OutputParticlesParams(params),
-      comm_{grid.comm()}
-  {}
-
-  // ----------------------------------------------------------------------
-  // run
-
-  void run(MparticlesBase& mprts_base) override
-  {
-    const auto& grid = mprts_base.grid();
-    if (every_step < 0 || grid.timestep() % every_step != 0) {
-      return;
-    }
-    
-    int rank;
-    MPI_Comm_rank(comm_, &rank);
-    char filename[strlen(data_dir) + strlen(basename) + 21];
-    sprintf(filename, "%s/%s.%06d_p%06d.asc", data_dir,
-	    basename, grid.timestep(), rank);
-    
-    auto& mprts = mprts_base.get_as<MparticlesDouble>();
-    
-    FILE *file = fopen(filename, "w");
-    for (int p = 0; p < mprts.n_patches(); p++) {
-      int n = 0;
-      for (auto& prt : mprts[p]) {
-	fprintf(file, "%d %g %g %g %g %g %g %g %d\n",
-		n, prt.x[0], prt.x[1], prt.x[2],
-		prt.p[0], prt.p[1], prt.p[2],
-		prt.w, prt.kind);
-	n++;
-      }
-    }
-    
-    mprts_base.put_as(mprts, MP_DONT_COPY);
-    
-    fclose(file);
-  }
-
-private:
-  MPI_Comm comm_;
-};
+#include "output_particles_ascii_impl.hxx"
 
 // ======================================================================
 // psc_output_particles: subclass "ascii"
