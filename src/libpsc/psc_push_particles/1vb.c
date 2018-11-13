11a4d676b37028180efbf4ad319cb2e6e353db3c
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-11-12 22:04:47 -0500

    pushp: smaller diff

--- src/libpsc/psc_push_particles/1vb.c
+++ src/libpsc/psc_push_particles/1vb.c
@@ -55,7 +55,7 @@ private:
     InterpolateEM_t ip;
     AdvanceParticle_t advance(prts.grid().dt);
     Current current(prts.grid());
-
+    
     PI<real_t> pi(prts.grid());
     Real3 dxi = Real3{ 1., 1., 1. } / Real3(prts.grid().domain.dx);
     real_t dq_kind[MAX_NR_KINDS];
@@ -66,8 +66,8 @@ private:
     }
     
     for (auto& prt: prts) {
-      // field interpolation
       real_t *x = prt.x;
+      real_t vv[3];
 
       real_t xm[3];
       for (int d = 0; d < 3; d++) {
@@ -75,6 +75,7 @@ private:
       }
       ip.set_coeffs(xm);
       
+      // FIELD INTERPOLATION
       real_t E[3] = { ip.ex(EM), ip.ey(EM), ip.ez(EM) };
       real_t H[3] = { ip.hx(EM), ip.hy(EM), ip.hz(EM) };
 
@@ -82,14 +83,12 @@ private:
       real_t dq = dq_kind[prt.kind];
       advance.push_p(prt.p, E, H, dq);
 
-      real_t vv[3];
+      // x^(n+0.5), p^(n+1.0) -> x^(n+1.5), p^(n+1.0)
       advance.calc_v(vv, prt.p);
+      advance.push_x(x, vv);
 
       int lf[3];
       real_t of[3], xp[3];
-      // x^(n+0.5), p^(n+1.0) -> x^(n+1.5), p^(n+1.0)
-      advance.push_x(prt.x, vv);
-  
       pi.find_idx_off_pos_1st_rel(prt.x, lf, of, xp, real_t(0.));
 
       // CURRENT DENSITY BETWEEN (n+.5)*dt and (n+1.5)*dt
--- src/libpsc/psc_push_particles/push_part_common.c
+++ src/libpsc/psc_push_particles/push_part_common.c
@@ -57,7 +57,7 @@ private:
     InterpolateEM_t ip;
     AdvanceParticle_t advance(prts.grid().dt);
     Current current(prts.grid());
-  
+    
     Real3 dxi = Real3{ 1., 1., 1. } / Real3(prts.grid().domain.dx);
     real_t dq_kind[MAX_NR_KINDS];
     auto& kinds = prts.grid().kinds;
@@ -70,17 +70,16 @@ private:
       real_t *x = prt.x;
       real_t vv[3];
 
-      // CHARGE DENSITY FORM FACTOR AT (n+.5)*dt
-      // FIELD INTERPOLATION
-
       real_t xm[3];
       for (int d = 0; d < 3; d++) {
 	xm[d] = x[d] * dxi[d];
       }
       ip.set_coeffs(xm);
       
+      // CHARGE DENSITY FORM FACTOR AT (n+.5)*dt
       current.charge_before(ip);
       
+      // FIELD INTERPOLATION
       real_t E[3] = { ip.ex(EM), ip.ey(EM), ip.ez(EM) };
       real_t H[3] = { ip.hx(EM), ip.hy(EM), ip.hz(EM) };
 
@@ -88,7 +87,7 @@ private:
       real_t dq = dq_kind[prt.kind];
       advance.push_p(prt.p, E, H, dq);
 
-      // x^(n+0.5), p^(n+1.0) -> x^(n+1.0), p^(n+1.0)
+      // x^(n+0.5), p^(n+1.0) -> x^(n+1.5), p^(n+1.0)
       advance.calc_v(vv, prt.p);
       advance.push_x(x, vv);
 
