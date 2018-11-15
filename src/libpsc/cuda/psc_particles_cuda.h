ab722f76a3cd34c7e9a8b8964b19628389c47174
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-11-15 11:41:45 -0500

    cuda: mv DParticleCuda

--- src/libpsc/cuda/psc_particles_cuda.h
+++ src/libpsc/cuda/psc_particles_cuda.h
@@ -29,6 +29,53 @@ struct BS444
 using particle_cuda_t = psc_particle<float>;
 
 // ======================================================================
+// DParticleCuda
+
+struct DParticleCuda
+{
+  using real_t = float;
+  using Real3 = Vec3<real_t>;
+
+  __host__ __device__
+  DParticleCuda() = default;
+
+  __host__ __device__
+  DParticleCuda(const DParticleCuda&) = default;
+
+  __host__ __device__
+  DParticleCuda(Real3 x, int kind, Real3 u, real_t qni_wni)
+    : xi_{x},
+      pxi_{u},
+      kind_{kind},
+      qni_wni_{qni_wni}
+  {}
+
+  __host__ __device__
+  Real3 x() const { return xi_; }
+  
+  __host__ __device__
+  Real3& x() { return xi_; }
+  
+  __host__ __device__
+  real_t kind() const { return kind_; }
+
+  __host__ __device__
+  Real3 u() const { return pxi_; }
+  
+  __host__ __device__
+  Real3& u() { return pxi_; }
+  
+  __host__ __device__
+  real_t qni_wni() const { return qni_wni_; }
+  
+private:
+  Real3 xi_;
+  Real3 pxi_;
+  int kind_;
+  real_t qni_wni_;
+};
+
+// ======================================================================
 // cuda_mparticles_prt
 
 struct cuda_mparticles_prt
