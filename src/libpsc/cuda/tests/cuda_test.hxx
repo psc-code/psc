6c80fff3a3179ecff93b734be7de309fec7ffd0f
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-11-21 03:21:50 -0500

    cuda: remove some unused code from make_cmprts

--- src/libpsc/cuda/tests/cuda_test.hxx
+++ src/libpsc/cuda/tests/cuda_test.hxx
@@ -14,32 +14,6 @@ struct TestBase
     
     return cmprts;
   }
-
-  CudaMparticles* make_cmprts(Grid_t& grid, const std::vector<particle_t>& prts)
-  {
-    auto cmprts = new CudaMparticles(grid);
-
-    std::vector<uint> n_prts_by_patch = { uint(prts.size()) };
-
-    //cmprts->reserve_all(n_prts_by_patch); FIXME
-    cmprts->inject_buf(prts.data(), n_prts_by_patch);
-  
-    return cmprts;
-  }
-  
-  template<typename S>
-  CudaMparticles* make_cmprts(Grid_t& grid, uint n_prts, S setter)
-  {
-    std::vector<particle_t> prts;
-    prts.reserve(n_prts);
-  
-    for (int i = 0; i < n_prts; i++) {
-      auto prt = setter(i);
-      prts.push_back(prt);
-    }
-
-    return make_cmprts(grid, prts);
-  }
 };
 
 
