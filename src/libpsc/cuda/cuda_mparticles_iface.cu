b4c22d02f50a9994c492585b63448f1ba15ef091
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-11-22 11:31:44 -0500

    cmprts: remove two unused files

--- src/libpsc/cuda/cuda_mparticles_iface.cu
+++
@@ -1,8 +0,0 @@
-
-#include "cuda_iface.h"
-#include "cuda_mparticles.cuh"
-#include "cuda_bits.h"
-
-#include "psc_particles_single.h"
-#include "psc_particles_double.h"
-
--- src/libpsc/cuda/psc_heating_cuda.cxx
+++
@@ -1,6 +0,0 @@
-
-#include "cuda_iface.h"
-
-#include "heating_cuda_impl.hxx"
-
-
