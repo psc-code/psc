1d61d1ef7334414fa417ccc74ac20fe0c8e58750
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-12-14 14:38:49 -0500

    mprts/simple: rename ParticleSimple vars -> x_, u_

--- src/include/particle_simple.hxx
+++ src/include/particle_simple.hxx
@@ -18,8 +18,8 @@ struct ParticleSimple
 
   __host__ __device__
   ParticleSimple(Real3 x, Real3 u, real_t qni_wni, int kind)
-    : xi_{x},
-      pxi_{u},
+    : x_{x},
+      u_{u},
       kind_{kind},
       qni_wni_{qni_wni}
   {}
@@ -27,23 +27,23 @@ struct ParticleSimple
   __host__ __device__
   bool operator==(const ParticleSimple& other) const
   {
-    return (xi_ == other.xi_ && qni_wni_ == other.qni_wni_ &&
-	    pxi_ == other.pxi_ && kind_ == other.kind_);
+    return (x_ == other.x_ && qni_wni_ == other.qni_wni_ &&
+	    u_ == other.u_ && kind_ == other.kind_);
   }
 
   __host__ __device__
   bool operator!=(const ParticleSimple& other) const { return !(*this == other); }
 
-  __host__ __device__ Real3  x() const { return xi_; }
-  __host__ __device__ Real3& x()       { return xi_; }
-  __host__ __device__ Real3  u() const { return pxi_; }
-  __host__ __device__ Real3& u()       { return pxi_; }
+  __host__ __device__ Real3  x() const { return x_; }
+  __host__ __device__ Real3& x()       { return x_; }
+  __host__ __device__ Real3  u() const { return u_; }
+  __host__ __device__ Real3& u()       { return u_; }
   __host__ __device__ int kind() const { return kind_; }
   __host__ __device__ real_t qni_wni() const { return qni_wni_; }
   
 private:
-  Real3 xi_;
-  Real3 pxi_;
+  Real3 x_;
+  Real3 u_;
   int kind_;
   real_t qni_wni_;
 };
