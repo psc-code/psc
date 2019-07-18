ffde86679fa485f961745c41405948531daf4ad2
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-07-17 21:25:21 -0400

    fields3d: some more cleanup, clang-format

--- src/libpsc/psc_output_fields/Deposit1stCc.h
+++ src/libpsc/psc_output_fields/Deposit1stCc.h
@@ -1,6 +1,12 @@
 
 #pragma once
 
+// FIXME, this is still too intermingled, both doing the actual deposit as well as
+// the particle / patch processing
+// OTOH, there is still opportunity for optimization, in particular when
+// operator() gets called back multiple times, we don't have to find the IP coefficients again
+// Obviously, the rest of the IP macro should be converted, too
+
 template <typename Mparticles, typename Mfields>
 class Deposit1stCc
 {
@@ -8,47 +14,42 @@ public:
   using FE = typename Mfields::fields_view_t;
   using R = typename Mfields::real_t;
   using ConstAccessor = typename Mparticles::ConstAccessor;
-  
+
   Deposit1stCc(const Mparticles& mprts, Mfields& mflds)
     : mprts_{mprts},
       mflds_{mflds},
-      accessor_{mprts.accessor()},
       flds_{mflds[0]},
       fnqs_{R(mprts.grid().norm.fnqs)},
       dxi_{R(1.f / mprts.grid().domain.dx[0]),
-	   R(1.f / mprts.grid().domain.dx[1]),
-	   R(1.f / mprts.grid().domain.dx[2])},
-      is_invar_{mprts.grid().isInvar(0), mprts.grid().isInvar(1), mprts.grid().isInvar(2)},
+           R(1.f / mprts.grid().domain.dx[1]),
+           R(1.f / mprts.grid().domain.dx[2])},
+      is_invar_{mprts.grid().isInvar(0), mprts.grid().isInvar(1),
+                mprts.grid().isInvar(2)},
       ldims_{mprts.grid().ldims}
   {}
 
-  void setPatch(int p)
-  {
-    flds_ = mflds_[p];
-  }
-  
-  template<typename PRT>
+  template <typename PRT>
   void operator()(const PRT& prt, int m, R val)
   {
     auto xi = prt.x(); /* don't shift back in time */
     R u = xi[0] * dxi_[0] - .5f;
     R v = xi[1] * dxi_[1] - .5f;
     R w = xi[2] * dxi_[2] - .5f;
-    
+
     int jx = fint(u);
     int jy = fint(v);
     int jz = fint(w);
     R h1 = u - jx;
     R h2 = v - jy;
     R h3 = w - jz;
-    
+
     R g0x = 1.f - h1;
     R g0y = 1.f - h2;
     R g0z = 1.f - h3;
     R g1x = h1;
     R g1y = h2;
     R g1z = h3;
-    
+
     int jxd = 1, jyd = 1, jzd = 1;
     if (is_invar_[0]) {
       jx = 0;
@@ -68,13 +69,13 @@ public:
       g1z = 0.;
       jzd = 0;
     }
-    
+
     assert(jx >= -1 && jx < ldims_[0]);
     assert(jy >= -1 && jy < ldims_[1]);
     assert(jz >= -1 && jz < ldims_[2]);
-    
+
     R fnq = prt.w() * fnqs_;
-    
+
     flds_(m, jx, jy, jz) += fnq * g0x * g0y * g0z * (val);
     flds_(m, jx + jxd, jy, jz) += fnq * g1x * g0y * g0z * (val);
     flds_(m, jx, jy + jyd, jz) += fnq * g0x * g1y * g0z * (val);
@@ -85,20 +86,21 @@ public:
     flds_(m, jx + jxd, jy + jyd, jz + jzd) += fnq * g1x * g1y * g1z * (val);
   }
 
-  template<typename F>
+  template <typename F>
   void process(F&& func)
   {
+    auto accessor = mprts_.accessor();
+
     for (int p = 0; p < mprts_.n_patches(); p++) {
-      setPatch(p);
-      for (auto prt: accessor_[p]) {
-	func(prt);
+      flds_ = mflds_[p];
+      for (auto prt : accessor[p]) {
+        func(prt);
       }
     }
   }
 
 private:
   const Mparticles& mprts_;
-  ConstAccessor accessor_;
   Mfields& mflds_;
   Fields3d<FE> flds_;
   R fnqs_;
@@ -106,4 +108,3 @@ private:
   Vec3<bool> is_invar_;
   Int3 ldims_;
 };
-  
