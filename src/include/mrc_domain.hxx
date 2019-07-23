44aff0091dd492f36664d8b5ac616ed5de7e3649
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-07-22 23:00:49 -0400

    flatfoil/mrc_domain: make MrcDomain simple pointer based

--- src/include/mrc_domain.hxx
+++ src/include/mrc_domain.hxx
@@ -8,24 +8,27 @@
 // ======================================================================
 // MrcDomain
 
-struct mrc_domain_Deleter {
-  void operator()(mrc_domain* domain) { mrc_domain_destroy(domain); }
-};
-
 struct MrcDomain
 {
-  MrcDomain(mrc_domain *domain = {}) : domain_{domain} {}
+  MrcDomain() = default;
+  MrcDomain(mrc_domain *domain) : domain_{domain} {}
+  ~MrcDomain()
+  {
+    if (domain_) {
+      mrc_domain_destroy(domain_);
+    }
+  }
+  
+  MPI_Comm comm() const { return mrc_domain_comm(domain_); }
+  void view() const { mrc_domain_view(domain_); }
 
-  MPI_Comm comm() const { return mrc_domain_comm(domain_.get()); }
-  void view() const { mrc_domain_view(domain_.get()); }
-
-  int nGlobalPatches() const { int n_global_patches; mrc_domain_get_nr_global_patches(domain_.get(), &n_global_patches); return n_global_patches; }
-  int nPatches() const { int n_patches; mrc_domain_get_patches(domain_.get(), &n_patches); return n_patches; }
-  mrc_patch_info globalPatchInfo(int p) const { mrc_patch_info info; mrc_domain_get_global_patch_info(domain_.get(), p, &info); return info; }
-  mrc_patch_info localPatchInfo(int p) const { mrc_patch_info info; mrc_domain_get_local_patch_info(domain_.get(), p, &info); return info; }
-  mrc_patch_info levelIdx3PatchInfo(int level, int idx[3]) const { mrc_patch_info info; mrc_domain_get_level_idx3_patch_info(domain_.get(), level, idx, &info); return info; }
-  const mrc_patch* getPatches(int* n_patches) const { return mrc_domain_get_patches(domain_.get(), n_patches); }
-  void neighborRankPatch(int p, int dir[3], int* nei_rank, int* nei_patch) const { mrc_domain_get_neighbor_rank_patch(domain_.get(), p, dir, nei_rank, nei_patch); }
+  int nGlobalPatches() const { int n_global_patches; mrc_domain_get_nr_global_patches(domain_, &n_global_patches); return n_global_patches; }
+  int nPatches() const { int n_patches; mrc_domain_get_patches(domain_, &n_patches); return n_patches; }
+  mrc_patch_info globalPatchInfo(int p) const { mrc_patch_info info; mrc_domain_get_global_patch_info(domain_, p, &info); return info; }
+  mrc_patch_info localPatchInfo(int p) const { mrc_patch_info info; mrc_domain_get_local_patch_info(domain_, p, &info); return info; }
+  mrc_patch_info levelIdx3PatchInfo(int level, int idx[3]) const { mrc_patch_info info; mrc_domain_get_level_idx3_patch_info(domain_, level, idx, &info); return info; }
+  const mrc_patch* getPatches(int* n_patches) const { return mrc_domain_get_patches(domain_, n_patches); }
+  void neighborRankPatch(int p, int dir[3], int* nei_rank, int* nei_patch) const { mrc_domain_get_neighbor_rank_patch(domain_, p, dir, nei_rank, nei_patch); }
 
   std::vector<Int3> offs() const
   {
@@ -44,9 +47,9 @@ struct MrcDomain
     return offs;
   }
   
-  mrc_fld* m3_create() const { return mrc_domain_m3_create(domain_.get()); }
-  mrc_ddc* create_ddc() const { return mrc_domain_create_ddc(domain_.get()); }
+  mrc_fld* m3_create() const { return mrc_domain_m3_create(domain_); }
+  mrc_ddc* create_ddc() const { return mrc_domain_create_ddc(domain_); }
   
 private:
-  std::unique_ptr<mrc_domain, mrc_domain_Deleter> domain_;
+  mrc_domain* domain_ = nullptr;
 };
