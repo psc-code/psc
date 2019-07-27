1f975529dadcb917fad85f8a7522bc488fd7f54d
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-07-27 14:00:08 -0400

    whistler: new-style done

--- src/psc_whistler.cxx
+++ src/psc_whistler.cxx
@@ -1,14 +1,34 @@
 
-#include "../src/libpsc/psc_output_fields/fields_item_moments_1st.hxx"
 #include <psc.hxx>
 #include <setup_fields.hxx>
 #include <setup_particles.hxx>
 
 #include "psc_config.hxx"
+#include "psc_integrator.hxx"
 
-// EDIT to change particle shape order / floating point type / 2d/3d / ...
+// ======================================================================
+// PSC configuration
+//
+// This sets up compile-time configuration for the code, in particular
+// what data structures and algorithms to use
+//
+// EDIT to change order / floating point type / cuda / 2d/3d
+
 using Dim = dim_xyz;
 using PscConfig = PscConfig1vbecSingle<Dim>;
+
+// ----------------------------------------------------------------------
+
+using MfieldsState = PscConfig::MfieldsState;
+using Mparticles = PscConfig::Mparticles;
+using Balance = PscConfig::Balance_t;
+using Collision = PscConfig::Collision_t;
+using Checks = PscConfig::Checks_t;
+using Marder = PscConfig::Marder_t;
+using OutputParticles = PscConfig::OutputParticles;
+
+// ======================================================================
+// PscWhistlerParams
 
 struct PscWhistlerParams
 {
@@ -99,188 +119,6 @@ Grid_t* setupGrid()
 }
 
 // ======================================================================
-// PscWhistler
-
-struct PscWhistler : Psc<PscConfig>
-{
-  using Base = Psc<PscConfig>;
-  
-  // ----------------------------------------------------------------------
-  // ctor
-
-  PscWhistler(const PscParams& params, Grid_t& grid)
-  {
-    auto comm = grid.comm();
-
-    Base::p_ = params;
-
-    Base::define_grid(grid);
-
-    define_field_array();
-
-    mprts_.reset(new Mparticles{grid});
-
-    // -- Balance
-    p_.balance_interval = -1;
-    balance_.reset(new Balance_t{p_.balance_interval, .1, false});
-
-    // -- Sort
-    // FIXME, needs a way to make sure it gets set?
-    p_.sort_interval = 100;
-
-    // -- Collision
-    int collision_interval = -1;
-    double collision_nu = .1;
-    collision_.reset(new Collision_t{grid, collision_interval, collision_nu});
-
-    // -- Checks
-    ChecksParams checks_params{};
-    checks_params.continuity_every_step = 50;
-    checks_params.continuity_threshold = 1e-5;
-    checks_params.continuity_verbose = false;
-    checks_.reset(new Checks_t{grid, comm, checks_params});
-
-    // -- Marder correction
-    double marder_diffusion = 0.9;
-    int marder_loop = 3;
-    bool marder_dump = false;
-    p_.marder_interval = -1;
-    marder_.reset(
-      new Marder_t(grid, marder_diffusion, marder_loop, marder_dump));
-
-    // -- output fields
-    OutputFieldsCParams outf_params;
-    outf_params.pfield_step = 200;
-    std::vector<std::unique_ptr<FieldsItemBase>> outf_items;
-    outf_items.emplace_back(
-      new FieldsItemFields<ItemLoopPatches<Item_e_cc>>(grid));
-    outf_items.emplace_back(
-      new FieldsItemFields<ItemLoopPatches<Item_h_cc>>(grid));
-    outf_items.emplace_back(
-      new FieldsItemFields<ItemLoopPatches<Item_j_cc>>(grid));
-    outf_items.emplace_back(
-      new FieldsItemMoment<
-        ItemMomentAddBnd<Moment_n_1st<Mparticles, MfieldsC>>>(grid));
-    outf_items.emplace_back(
-      new FieldsItemMoment<
-        ItemMomentAddBnd<Moment_v_1st<Mparticles, MfieldsC>>>(grid));
-    outf_items.emplace_back(
-      new FieldsItemMoment<
-        ItemMomentAddBnd<Moment_T_1st<Mparticles, MfieldsC>>>(grid));
-    outf_.reset(new OutputFieldsC{grid, outf_params, std::move(outf_items)});
-
-    // --- partition particles and initial balancing
-    mpi_printf(comm, "**** Partitioning...\n");
-    auto n_prts_by_patch = setup_initial_partition();
-    balance_->initial(grid_, n_prts_by_patch);
-    // balance::initial does not rebalance particles, because the old way of
-    // doing this does't even have the particle data structure created yet --
-    // FIXME?
-    mprts_->reset(grid);
-
-    mpi_printf(comm, "**** Setting up particles...\n");
-    setup_initial_particles(*mprts_, n_prts_by_patch);
-
-    mpi_printf(comm, "**** Setting up fields...\n");
-    setup_initial_fields(*mflds_);
-
-    // do remainder of generic initialization
-    init();
-  }
-
-private:
-  void init_npt(int kind, double crd[3], psc_particle_npt& npt)
-  {
-    double kz = (4. * M_PI / grid().domain.length[2]);
-    double kperp = (2. * M_PI / grid().domain.length[0]);
-    double x = crd[0], y = crd[1], z = crd[2];
-
-    double envelope1 = exp(-(z - 25.) * (z - 25.) / 40.);
-    double envelope2 = exp(-(z - 75.) * (z - 75.) / 40.);
-
-    switch (kind) {
-      case KIND_ELECTRON:
-        npt.n = 1.;
-
-        npt.T[0] = g.Te_perp;
-        npt.T[1] = g.Te_perp;
-        npt.T[2] = g.Te_par;
-
-        // Set velocities for first wave:
-        npt.p[0] = -g.amplitude * envelope1 * g.B0 * cos(kperp * y + kz * z);
-        npt.p[2] = 0.;
-
-        // Set velocities for second wave:
-        npt.p[1] = g.amplitude * envelope2 * g.B0 * sin(kperp * x - kz * z);
-        break;
-      case KIND_ION:
-        npt.n = 1.;
-
-        npt.T[0] = g.Ti_perp;
-        npt.T[1] = g.Ti_perp;
-        npt.T[2] = g.Ti_par;
-
-        // Set velocities for first wave:
-        npt.p[0] = -g.amplitude * envelope1 * g.B0 * cos(kperp * y + kz * z);
-        npt.p[2] = 0.;
-
-        // Set velocities for second wave:
-        npt.p[1] = g.amplitude * envelope2 * g.B0 * sin(kperp * x - kz * z);
-        break;
-      default: assert(0);
-    }
-  }
-
-  // ----------------------------------------------------------------------
-  // setup_initial_partition
-
-  std::vector<uint> setup_initial_partition()
-  {
-    SetupParticles<Mparticles> setup_particles(grid());
-    return setup_particles.partition(
-      grid(), [&](int kind, double crd[3], psc_particle_npt& npt) {
-        this->init_npt(kind, crd, npt);
-      });
-  }
-
-  // ----------------------------------------------------------------------
-  // setup_initial_particles
-
-  void setup_initial_particles(Mparticles& mprts,
-                               std::vector<uint>& n_prts_by_patch)
-  {
-    SetupParticles<Mparticles> setup_particles(grid());
-    setup_particles(mprts, [&](int kind, double crd[3], psc_particle_npt& npt) {
-      this->init_npt(kind, crd, npt);
-    });
-  }
-
-  // ----------------------------------------------------------------------
-  // setup_initial_fields
-
-  void setup_initial_fields(MfieldsState& mflds)
-  {
-    double kz = (4. * M_PI / grid().domain.length[2]);
-    double kperp = (2. * M_PI / grid().domain.length[0]);
-
-    setupFields(mflds, [&](int m, double crd[3]) {
-      double x = crd[0], y = crd[1], z = crd[2];
-      double envelope1 = exp(-(z - 25.) * (z - 25.) / 40.);
-      double envelope2 = exp(-(z - 75.) * (z - 75.) / 40.);
-
-      switch (m) {
-        case HX:
-          return g.amplitude * envelope1 * cos(kperp * y + kz * z) * g.B0;
-        case HY:
-          return g.amplitude * envelope2 * sin(kperp * x - kz * z) * g.B0;
-        case HZ: return g.B0;
-        default: return 0.;
-      }
-    });
-  }
-};
-
-// ======================================================================
 // run
 
 static void run()
@@ -342,10 +180,154 @@ static void run()
 
   auto grid_ptr = setupGrid();
   auto& grid = *grid_ptr;
-  
-  PscWhistler psc(psc_params, *grid_ptr);
+  auto& mflds = *new MfieldsState{grid};
+  auto& mprts = *new Mparticles{grid};
 
-  psc.initialize();
+  // ----------------------------------------------------------------------
+  // Set up various objects needed to run this case
+
+  // -- Balance
+  psc_params.balance_interval = 0;
+  auto& balance = *new Balance{psc_params.balance_interval, .1, false};
+
+  // -- Sort
+  psc_params.sort_interval = 100;
+
+  // -- Collision
+  int collision_interval = 1;
+  double collision_nu = .1;
+  auto& collision = *new Collision{grid, collision_interval, collision_nu};
+
+  // -- Checks
+  ChecksParams checks_params{};
+  checks_params.continuity_every_step = 50;
+  checks_params.continuity_threshold = 1e-5;
+  checks_params.continuity_verbose = false;
+  auto& checks = *new Checks{grid, MPI_COMM_WORLD, checks_params};
+
+  // -- Marder correction
+  double marder_diffusion = 0.9;
+  int marder_loop = 3;
+  bool marder_dump = false;
+  psc_params.marder_interval = 0;
+  auto& marder = *new Marder(grid, marder_diffusion, marder_loop, marder_dump);
+
+  // ----------------------------------------------------------------------
+  // Set up output
+  //
+  // FIXME, this really is too complicated and not very flexible
+
+  // -- output fields
+  OutputFieldsCParams outf_params{};
+  outf_params.pfield_step = 200;
+  std::vector<std::unique_ptr<FieldsItemBase>> outf_items;
+  outf_items.emplace_back(new FieldsItem_E_cc(grid));
+  outf_items.emplace_back(new FieldsItem_H_cc(grid));
+  outf_items.emplace_back(new FieldsItem_J_cc(grid));
+  outf_items.emplace_back(new FieldsItem_n_1st_cc<Mparticles>(grid));
+  outf_items.emplace_back(new FieldsItem_v_1st_cc<Mparticles>(grid));
+  outf_items.emplace_back(new FieldsItem_T_1st_cc<Mparticles>(grid));
+  auto& outf = *new OutputFieldsC{grid, outf_params, std::move(outf_items)};
+
+  // -- output particles
+  OutputParticlesParams outp_params{};
+  outp_params.every_step = 0;
+  outp_params.data_dir = ".";
+  outp_params.basename = "prt";
+  auto& outp = *new OutputParticles{grid, outp_params};
+
+  // ----------------------------------------------------------------------
+  // Initial conditions
+
+  double kz = (4. * M_PI / grid.domain.length[2]);
+  double kperp = (2. * M_PI / grid.domain.length[0]);
+
+  auto lf_init_npt = [&](int kind, double crd[3], psc_particle_npt& npt) {
+    double x = crd[0], y = crd[1], z = crd[2];
+
+    double envelope1 = exp(-(z - 25.) * (z - 25.) / 40.);
+    double envelope2 = exp(-(z - 75.) * (z - 75.) / 40.);
+
+    switch (kind) {
+      case KIND_ELECTRON:
+        npt.n = 1.;
+
+        npt.T[0] = g.Te_perp;
+        npt.T[1] = g.Te_perp;
+        npt.T[2] = g.Te_par;
+
+        // Set velocities for first wave:
+        npt.p[0] = -g.amplitude * envelope1 * g.B0 * cos(kperp * y + kz * z);
+        npt.p[2] = 0.;
+
+        // Set velocities for second wave:
+        npt.p[1] = g.amplitude * envelope2 * g.B0 * sin(kperp * x - kz * z);
+        break;
+      case KIND_ION:
+        npt.n = 1.;
+
+        npt.T[0] = g.Ti_perp;
+        npt.T[1] = g.Ti_perp;
+        npt.T[2] = g.Ti_par;
+
+        // Set velocities for first wave:
+        npt.p[0] = -g.amplitude * envelope1 * g.B0 * cos(kperp * y + kz * z);
+        npt.p[2] = 0.;
+
+        // Set velocities for second wave:
+        npt.p[1] = g.amplitude * envelope2 * g.B0 * sin(kperp * x - kz * z);
+        break;
+      default: assert(0);
+    }
+  };
+
+  auto lf_init_fields = [&](int m, double crd[3]) {
+    double x = crd[0], y = crd[1], z = crd[2];
+    double envelope1 = exp(-(z - 25.) * (z - 25.) / 40.);
+    double envelope2 = exp(-(z - 75.) * (z - 75.) / 40.);
+
+    switch (m) {
+      case HX: return g.amplitude * envelope1 * cos(kperp * y + kz * z) * g.B0;
+      case HY: return g.amplitude * envelope2 * sin(kperp * x - kz * z) * g.B0;
+      case HZ: return g.B0;
+      default: return 0.;
+    }
+  };
+
+  if (read_checkpoint_filename.empty()) {
+    // --- partition particles and initial balancing
+    mpi_printf(MPI_COMM_WORLD, "**** Partitioning...\n");
+
+    SetupParticles<Mparticles> setup_particles(grid);
+
+    auto n_prts_by_patch = setup_particles.partition(grid, lf_init_npt);
+
+    balance.initial(grid_ptr, n_prts_by_patch);
+    // !!! FIXME! grid is now invalid
+    // balance::initial does not rebalance particles, because the old way of
+    // doing this does't even have the particle data structure created yet --
+    // FIXME?
+    mprts.reset(*grid_ptr);
+
+    // -- set up particles
+    mpi_printf(MPI_COMM_WORLD, "**** Setting up particles...\n");
+    setup_particles(mprts, lf_init_npt);
+
+    // -- set up fields
+    mpi_printf(MPI_COMM_WORLD, "**** Setting up fields...\n");
+    setupFields(mflds, lf_init_fields);
+  }
+
+  auto psc =
+    makePscIntegrator<PscConfig>(psc_params, *grid_ptr, mflds, mprts, balance,
+                                 collision, checks, marder, outf, outp);
+
+  // FIXME, checkpoint reading should be moved to before the integrator
+  if (!read_checkpoint_filename.empty()) {
+    mpi_printf(MPI_COMM_WORLD, "**** Reading checkpoint...\n");
+    psc.read_checkpoint(read_checkpoint_filename);
+  }
+
   psc.integrate();
 }
 
