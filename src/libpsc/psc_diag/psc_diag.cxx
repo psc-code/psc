b467c9b72894bdab2cb30c5f917b85d9e13fb7d7
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-07-30 16:53:55 -0400

    diag: clang-format

--- src/libpsc/psc_diag/psc_diag.cxx
+++ src/libpsc/psc_diag/psc_diag.cxx
@@ -1,6 +1,6 @@
 
-#include "psc_diag_private.h"
 #include "psc_diag_item_private.h"
+#include "psc_diag_private.h"
 
 #include <mrc_params.h>
 #include <stdlib.h>
@@ -9,17 +9,15 @@
 // ----------------------------------------------------------------------
 // psc_diag_setup
 
-static void
-_psc_diag_setup(struct psc_diag *diag)
+static void _psc_diag_setup(struct psc_diag* diag)
 {
   // parse "items" parameter
   char *s_orig = strdup(diag->items), *p, *s = s_orig;
   while ((p = strsep(&s, ", "))) {
-    struct psc_diag_item *item =
-      psc_diag_item_create(psc_diag_comm(diag));
+    struct psc_diag_item* item = psc_diag_item_create(psc_diag_comm(diag));
     psc_diag_item_set_type(item, p);
     psc_diag_item_set_name(item, p);
-    psc_diag_add_child(diag, (struct mrc_obj *) item);
+    psc_diag_add_child(diag, (struct mrc_obj*)item);
   }
   free(s);
   free(s_orig);
@@ -29,12 +27,13 @@ _psc_diag_setup(struct psc_diag *diag)
 
   if (rank != 0)
     return;
-  
+
   diag->file = fopen("diag.asc", "w");
   fprintf(diag->file, "# time");
 
-  struct psc_diag_item *item;
-  mrc_obj_for_each_child(item, diag, struct psc_diag_item) {
+  struct psc_diag_item* item;
+  mrc_obj_for_each_child(item, diag, struct psc_diag_item)
+  {
     int nr_values = psc_diag_item_nr_values(item);
     for (int i = 0; i < nr_values; i++) {
       fprintf(diag->file, " %s", psc_diag_item_title(item, i));
@@ -46,49 +45,49 @@ _psc_diag_setup(struct psc_diag *diag)
 // ----------------------------------------------------------------------
 // psc_diag_destroy
 
-static void
-_psc_diag_destroy(struct psc_diag *diag)
+static void _psc_diag_destroy(struct psc_diag* diag)
 {
   int rank;
   MPI_Comm_rank(psc_diag_comm(diag), &rank);
 
   if (rank != 0)
     return;
-  
+
   fclose(diag->file);
 }
 
 // ----------------------------------------------------------------------
 // psc_diag_run
 
-void
-psc_diag_run(struct psc_diag *diag, MparticlesBase& mprts, MfieldsStateBase& mflds)
+void psc_diag_run(struct psc_diag* diag, MparticlesBase& mprts,
+                  MfieldsStateBase& mflds)
 {
   const auto& grid = mprts.grid();
-  
-  if (diag->every_step < 0 || 
-      grid.timestep() % diag->every_step != 0)
+
+  if (diag->every_step < 0 || grid.timestep() % diag->every_step != 0)
     return;
 
   int rank;
   MPI_Comm_rank(psc_diag_comm(diag), &rank);
 
-  struct psc_diag_item *item;
+  struct psc_diag_item* item;
   if (rank == 0) {
     fprintf(diag->file, "%g", grid.timestep() * grid.dt);
   }
-  mrc_obj_for_each_child(item, diag, struct psc_diag_item) {
+  mrc_obj_for_each_child(item, diag, struct psc_diag_item)
+  {
     int nr_values = psc_diag_item_nr_values(item);
-    double *result = (double *) calloc(nr_values, sizeof(*result));
+    double* result = (double*)calloc(nr_values, sizeof(*result));
     psc_diag_item_run(item, mprts, mflds, result);
     if (rank == 0) {
-      MPI_Reduce(MPI_IN_PLACE, result, nr_values, MPI_DOUBLE, MPI_SUM, 0, grid.comm());
+      MPI_Reduce(MPI_IN_PLACE, result, nr_values, MPI_DOUBLE, MPI_SUM, 0,
+                 grid.comm());
     } else {
       MPI_Reduce(result, NULL, nr_values, MPI_DOUBLE, MPI_SUM, 0, grid.comm());
     }
     if (rank == 0) {
       for (int i = 0; i < nr_values; i++) {
-	fprintf(diag->file, " %g", result[i]);
+        fprintf(diag->file, " %g", result[i]);
       }
     }
     free(result);
@@ -101,12 +100,11 @@ psc_diag_run(struct psc_diag *diag, MparticlesBase& mprts, MfieldsStateBase& mfl
 
 // ======================================================================
 
-#define VAR(x) (void *)offsetof(struct psc_diag, x)
+#define VAR(x) (void*)offsetof(struct psc_diag, x)
 
 static struct param psc_diag_descr[] = {
-  { "items"            , VAR(items)              ,
-    PARAM_STRING("field_energy,particle_energy")                              },
-  { "every_step"       , VAR(every_step)         , PARAM_INT(-1)              },
+  {"items", VAR(items), PARAM_STRING("field_energy,particle_energy")},
+  {"every_step", VAR(every_step), PARAM_INT(-1)},
   {},
 };
 #undef VAR
@@ -114,13 +112,14 @@ static struct param psc_diag_descr[] = {
 // ======================================================================
 // psc_diag class
 
-struct mrc_class_psc_diag_ : mrc_class_psc_diag {
-  mrc_class_psc_diag_() {
-    name             = "psc_diag";
-    size             = sizeof(struct psc_diag);
-    param_descr      = psc_diag_descr;
-    setup            = _psc_diag_setup;
-    destroy          = _psc_diag_destroy;
+struct mrc_class_psc_diag_ : mrc_class_psc_diag
+{
+  mrc_class_psc_diag_()
+  {
+    name = "psc_diag";
+    size = sizeof(struct psc_diag);
+    param_descr = psc_diag_descr;
+    setup = _psc_diag_setup;
+    destroy = _psc_diag_destroy;
   }
 } mrc_class_psc_diag;
-
--- src/libpsc/psc_diag/psc_diag_item_particle_energy.cxx
+++ src/libpsc/psc_diag/psc_diag_item_particle_energy.cxx
@@ -4,10 +4,10 @@
 
 #include <math.h>
 
-static void
-psc_diag_item_particle_energy_run(struct psc_diag_item *item,
-				  MparticlesBase& mprts_base, MfieldsStateBase& mflds_base,
-				  double *result)
+static void psc_diag_item_particle_energy_run(struct psc_diag_item* item,
+                                              MparticlesBase& mprts_base,
+                                              MfieldsStateBase& mflds_base,
+                                              double* result)
 {
   auto& mprts = mprts_base.get_as<MparticlesDouble>();
 
@@ -19,16 +19,17 @@ psc_diag_item_particle_energy_run(struct psc_diag_item *item,
     auto accessor = mprts.accessor();
     for (int p = 0; p < mprts.n_patches(); p++) {
       for (auto prt : accessor[p]) {
-	double gamma = sqrt(1.f + sqr(prt.u()[0]) + sqr(prt.u()[1]) + sqr(prt.u()[2]));
-	double Ekin = (gamma - 1.) * prt.m() * prt.w() * fnqs;
-	double q = prt.q();
-	if (q < 0.) {
-	  result[0] += Ekin * fac;
-	} else if (q > 0.) {
-	  result[1] += Ekin * fac;
-	} else {
-	  assert(0);
-	}
+        double gamma =
+          sqrt(1.f + sqr(prt.u()[0]) + sqr(prt.u()[1]) + sqr(prt.u()[2]));
+        double Ekin = (gamma - 1.) * prt.m() * prt.w() * fnqs;
+        double q = prt.q();
+        if (q < 0.) {
+          result[0] += Ekin * fac;
+        } else if (q > 0.) {
+          result[1] += Ekin * fac;
+        } else {
+          assert(0);
+        }
       }
     }
   }
@@ -39,13 +40,14 @@ psc_diag_item_particle_energy_run(struct psc_diag_item *item,
 // ======================================================================
 // psc_diag_item_particle_energy
 
-struct psc_diag_item_ops_p : psc_diag_item_ops {
-  psc_diag_item_ops_p() {
-    name      = "particle_energy";
-    run       = psc_diag_item_particle_energy_run;
+struct psc_diag_item_ops_p : psc_diag_item_ops
+{
+  psc_diag_item_ops_p()
+  {
+    name = "particle_energy";
+    run = psc_diag_item_particle_energy_run;
     nr_values = 2;
-    title[0]  = "E_electron";
-    title[1]  = "E_ion";
+    title[0] = "E_electron";
+    title[1] = "E_ion";
   }
 } psc_diag_item_particle_energy_ops;
-
