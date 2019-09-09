a2412028a62448bd43da0fe9c577fc5737088886
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-09-09 16:22:16 -0400

    fix cuda collision crash; use unique random numbers
    This really needs more work, but this should at least not crash.

--- src/libpsc/cuda/collision_cuda_impl.cu
+++ src/libpsc/cuda/collision_cuda_impl.cu
@@ -3,19 +3,32 @@
 
 #include "cuda_collision.cuh"
 #include "mparticles_cuda.hxx"
+#include "balance.hxx"
 
 // ======================================================================
 // CollisionCuda
 
 template<typename Mparticles, typename RngState>
 CollisionCuda<Mparticles, RngState>::CollisionCuda(const Grid_t& grid, int interval, double nu)
-  : fwd_{new CudaCollision<typename Mparticles::CudaMparticles, RngState>{interval, nu, int(1. / grid.norm.cori + .5), grid.dt}} // FIXME nicell hack
+  : fwd_{new CudaCollision<typename Mparticles::CudaMparticles, RngState>{interval, nu, int(1. / grid.norm.cori + .5), grid.dt}}, // FIXME nicell hack
+    balance_generation_cnt_{-1}
 {}
 
 template<typename Mparticles, typename RngState>
-void CollisionCuda<Mparticles, RngState>::operator()(Mparticles& _mprts)
+void CollisionCuda<Mparticles, RngState>::operator()(Mparticles& mprts)
 {
-  (*fwd_)(*_mprts.cmprts());
+  if (psc_balance_generation_cnt > this->balance_generation_cnt_) {
+    balance_generation_cnt_ = psc_balance_generation_cnt;
+    reset(mprts);
+  }
+  
+  (*fwd_)(*mprts.cmprts());
+}
+
+template<typename Mparticles, typename RngState>
+void CollisionCuda<Mparticles, RngState>::reset(Mparticles& mprts)
+{
+  fwd_->reset(*mprts.cmprts());
 }
 
 template<typename Mparticles, typename RngState>
