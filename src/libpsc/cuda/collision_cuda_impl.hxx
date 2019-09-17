bf9ddac155a127fa2db9d9599784e3f9842528b7
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-09-16 21:15:08 -0400

    cuda/collision: run clang-format

--- src/libpsc/cuda/collision_cuda_impl.hxx
+++ src/libpsc/cuda/collision_cuda_impl.hxx
@@ -3,7 +3,7 @@
 
 #include "collision.hxx"
 
-template<typename cuda_mparticles, typename RngState>
+template <typename cuda_mparticles, typename RngState>
 struct CudaCollision;
 
 struct RngStateCuda;
@@ -12,20 +12,19 @@ struct RngStateFake;
 // ----------------------------------------------------------------------
 // CollisionCuda
 
-template<typename MP, typename RngState = RngStateCuda>
+template <typename MP, typename RngState = RngStateCuda>
 struct CollisionCuda : CollisionBase
 {
   using Mparticles = MP;
-  
+
   CollisionCuda(const Grid_t& grid, int interval, double nu);
-  
+
   void operator()(MparticlesBase& mprts_base) override { assert(0); }
   void operator()(Mparticles& _mprts);
   int interval() const;
   void reset(Mparticles& mprts);
-  
+
 private:
-  CudaCollision<typename Mparticles::CudaMparticles, RngState> *fwd_;
+  CudaCollision<typename Mparticles::CudaMparticles, RngState>* fwd_;
   int balance_generation_cnt_;
 };
-
