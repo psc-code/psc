cff73a1496d876103147955894ce06e9ee5eb752
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-04-06 10:48:59 -0400

    outf: support separate field and moment output
    This should be redone to avoid all the duplication, as well as
    the existing ugliness

--- src/include/OutputFieldsDefault.h
+++ src/include/OutputFieldsDefault.h
@@ -22,10 +22,18 @@ struct OutputFieldsParams
   int pfield_interval = 0;
   int pfield_first = 0;
 
+  int pfield_moments_interval = -1;
+  int pfield_moments_first = -1;
+
   int tfield_interval = 0;
   int tfield_first = 0;
   int tfield_average_length = 1000000;
   int tfield_average_every = 1;
+
+  int tfield_moments_interval = -1;
+  int tfield_moments_first = -1;
+  int tfield_moments_average_length = -1;
+  int tfield_moments_average_every = -1;
 
   Int3 rn = {};
   Int3 rx = {1000000, 1000000, 100000};
@@ -48,13 +56,41 @@ public:
       tfd_jeh_{grid, Item_jeh<MfieldsFake>::n_comps(), grid.ibn},
       tfd_moments_{grid, FieldsItem_Moments_1st_cc<MparticlesFake>::n_comps(grid), grid.ibn},
       pfield_next_{pfield_first},
-      tfield_next_{tfield_first}
+      pfield_moments_next_{pfield_moments_first},
+      tfield_next_{tfield_first},
+      tfield_moments_next_{tfield_moments_first}
   {
+    if (pfield_moments_interval < 0) {
+      pfield_moments_interval = pfield_interval;
+    }
+    if (pfield_moments_first < 0) {
+      pfield_moments_first = pfield_first;
+    }
+    
+    if (tfield_moments_interval < 0) {
+      tfield_moments_interval = tfield_interval;
+    }
+    if (tfield_moments_first < 0) {
+      tfield_moments_first = tfield_first;
+    }
+    if (tfield_moments_average_length < 0) {
+      tfield_moments_average_length = tfield_average_length;
+    }
+    if (tfield_moments_average_every < 0) {
+      tfield_moments_average_every = tfield_average_every;
+    }
+    
     if (pfield_interval > 0) {
       io_pfd_.reset(new MrcIo{"pfd", data_dir});
     }
+    if (pfield_moments_interval > 0) {
+      io_pfd_moments_.reset(new MrcIo{"pfd_moments", data_dir});
+    }
     if (tfield_interval > 0) {
       io_tfd_.reset(new MrcIo{"tfd", data_dir});
+    }
+    if (tfield_moments_interval > 0) {
+      io_tfd_moments_.reset(new MrcIo{"tfd_moments", data_dir});
     }
   }
 
@@ -82,22 +118,19 @@ public:
 
     if (do_pfield || doaccum_tfield) {
       Item_jeh<MfieldsState> pfd_jeh{mflds};
-      FieldsItem_Moments_1st_cc<Mparticles> pfd_moments{mprts};
       
       if (do_pfield) {
 	mpi_printf(grid.comm(), "***** Writing PFD output\n");
 	pfield_next_ += pfield_interval;
 	
 	io_pfd_->open(grid, rn, rx);
-	_write_pfd(pfd_jeh);
-	_write_pfd(pfd_moments);
+	_write_pfd(*io_pfd_, pfd_jeh);
 	io_pfd_->close();
       }
 
       if (doaccum_tfield) {
 	// tfd += pfd
 	tfd_jeh_ += pfd_jeh;
-	tfd_moments_ += pfd_moments;
 	naccum_++;
       }
       if (do_tfield) {
@@ -105,10 +138,44 @@ public:
 	tfield_next_ += tfield_interval;
 
 	io_tfd_->open(grid, rn, rx);
-	_write_tfd(tfd_jeh_, pfd_jeh);
-	_write_tfd(tfd_moments_, pfd_moments);
+	_write_tfd(*io_tfd_, tfd_jeh_, pfd_jeh, naccum_);
 	io_tfd_->close();
 	naccum_ = 0;
+      }
+    }
+
+    bool do_pfield_moments = pfield_moments_interval > 0 && timestep >= pfield_moments_next_;
+    bool do_tfield_moments = tfield_moments_interval > 0 && timestep >= tfield_moments_next_;
+    bool doaccum_tfield_moments =
+      tfield_moments_interval > 0 && (((timestep >= (tfield_moments_next_ - tfield_moments_average_length + 1)) &&
+					timestep % tfield_moments_average_every == 0) ||
+				       timestep == 0);
+    
+    if (do_pfield_moments || doaccum_tfield_moments) {
+      FieldsItem_Moments_1st_cc<Mparticles> pfd_moments{mprts};
+      
+      if (do_pfield_moments) {
+	mpi_printf(grid.comm(), "***** Writing PFD moment output\n");
+	pfield_moments_next_ += pfield_moments_interval;
+	
+	io_pfd_moments_->open(grid, rn, rx);
+	_write_pfd(*io_pfd_moments_, pfd_moments);
+	io_pfd_moments_->close();
+      }
+
+      if (doaccum_tfield_moments) {
+	// tfd += pfd
+	tfd_moments_ += pfd_moments;
+	naccum_moments_++;
+      }
+      if (do_tfield_moments) {
+	mpi_printf(grid.comm(), "***** Writing TFD moment output\n");
+	tfield_moments_next_ += tfield_moments_interval;
+
+	io_tfd_moments_->open(grid, rn, rx);
+	_write_tfd(*io_tfd_moments_, tfd_moments_, pfd_moments, naccum_moments_);
+	io_tfd_moments_->close();
+	naccum_moments_ = 0;
       }
     }
 
@@ -117,18 +184,18 @@ public:
 
 private:
   template <typename EXP>
-  void _write_pfd(EXP& pfd)
+  static void _write_pfd(MrcIo& io, EXP& pfd)
   {
-    MrcIo::write_mflds(io_pfd_->io_, adaptMfields(pfd), pfd.grid(), pfd.name(),
+    MrcIo::write_mflds(io.io_, adaptMfields(pfd), pfd.grid(), pfd.name(),
                        pfd.comp_names());
   }
 
   template <typename EXP>
-  void _write_tfd(MfieldsC& tfd, EXP& pfd)
+  static void _write_tfd(MrcIo& io, MfieldsC& tfd, EXP& pfd, int naccum)
   {
     // convert accumulated values to correct temporal mean
-    tfd.scale(1. / naccum_);
-    tfd.write_as_mrc_fld(io_tfd_->io_, pfd.name(), pfd.comp_names());
+    tfd.scale(1. / naccum);
+    tfd.write_as_mrc_fld(io.io_, pfd.name(), pfd.comp_names());
     tfd.zero();
   }
 
@@ -137,7 +204,13 @@ private:
   MfieldsC tfd_jeh_;
   MfieldsC tfd_moments_;
   std::unique_ptr<MrcIo> io_pfd_;
+  std::unique_ptr<MrcIo> io_pfd_moments_;
   std::unique_ptr<MrcIo> io_tfd_;
-  int pfield_next_, tfield_next_;
+  std::unique_ptr<MrcIo> io_tfd_moments_;
+  int pfield_next_;
+  int pfield_moments_next_;
+  int tfield_next_;
+  int tfield_moments_next_;
   int naccum_ = 0;
+  int naccum_moments_ = 0;
 };
