5f806b140216c40d902c189fcfa2f37c94f8fd85
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-08-03 17:22:07 -0400

    profiling: details in inject

--- src/include/inject_impl.hxx
+++ src/include/inject_impl.hxx
@@ -37,12 +37,35 @@ struct Inject_ : InjectBase
 
   void operator()(Mparticles& mprts)
   {
+    static int pr, pr_0, pr_1, pr_2, pr_3, pr_4;
+    if (!pr) {
+      pr = prof_register("inject_impl", 1., 0, 0);
+      pr_0 = prof_register("inject_barrer", 1., 0, 0);
+      pr_1 = prof_register("inject_moment_n", 1., 0, 0);
+      pr_2 = prof_register("inject_eval", 1., 0, 0);
+      pr_3 = prof_register("inject_get_as", 1., 0, 0);
+      pr_4 = prof_register("inject_setup_prts", 1., 0, 0);
+    }
+
+    prof_start(pr);
     const auto& grid = mprts.grid();
 
+    prof_start(pr_0);
+    MPI_Barrier(MPI_COMM_WORLD);
+    prof_stop(pr_0);
+    
+    prof_start(pr_1);
     ItemMoment_t moment_n(mprts);
+    prof_stop(pr_1);
+    
+    prof_start(pr_2);
     auto mres = evalMfields(moment_n);
+    prof_stop(pr_2);
+    
+    prof_start(pr_3);
     auto& mf_n = mres.template get_as<Mfields>(kind_n, kind_n + 1);
-
+    prof_stop(pr_3);
+    
     real_t fac = (interval * grid.dt / tau) / (1. + interval * grid.dt / tau);
 
     auto lf_init_npt = [&](int kind, Double3 pos, int p, Int3 idx,
@@ -57,9 +80,12 @@ struct Inject_ : InjectBase
       }
     };
 
+    prof_start(pr_4);
     setup_particles_.setupParticles(mprts, lf_init_npt);
-
+    prof_stop(pr_4);
+    
     mres.put_as(mf_n, 0, 0);
+    prof_stop(pr);
   }
 
 private:
