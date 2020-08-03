d5ed029d84215315b95eb8cdb8c5f461f8f0a609
John Donaghy <john.donaghy@protonmail.com>
2020-08-02 23:31:06 -0400

    rmm::mr:cnmem_memory_resource and device_vector

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -10,7 +10,14 @@
 #include "../libpsc/psc_heating/psc_heating_impl.hxx"
 #include "heating_spot_foil.hxx"
 #include "inject_impl.hxx"
+#ifdef PSC_HAVE_RMM
+#include "rmm/thrust_rmm_allocator.h"
+#include "rmm/mr/device/default_memory_resource.hpp"
+#include "rmm/mr/device/cuda_memory_resource.hpp"
+#include "rmm/mr/device/cnmem_memory_resource.hpp"
+rmm::mr::cnmem_memory_resource* pool_mr;
 
+#endif
 #define DIM_3D
 
 // ======================================================================
@@ -513,7 +520,10 @@ void run()
 int main(int argc, char** argv)
 {
   psc_init(argc, argv);
-
+#ifdef PSC_HAVE_RMM
+  pool_mr = new rmm::mr::cnmem_memory_resource{(1<<30)}; 
+  rmm::mr::set_default_resource(pool_mr);
+#endif
   run();
 
   psc_finalize();
