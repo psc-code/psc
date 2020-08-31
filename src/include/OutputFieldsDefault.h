179a0cad5978727166cd8283601761b2740104bc
John <john.donaghy@protonmail.com>
2020-08-31 11:15:34 -0400

    properly set the next write-out flag after restart.  without setting flds get written out at each "average every" interval

--- src/include/OutputFieldsDefault.h
+++ src/include/OutputFieldsDefault.h
@@ -129,6 +129,10 @@ public:
     if(first_time){
         first_time = false;
         if(timestep != 0)
+            pfield_next_ = timestep + pfield_interval;
+            tfield_next_ = timestep + tfield_interval;
+            pfield_moments_next_ = timestep + pfield_moments_interval;
+            tfield_moments_next_ = timestep + tfield_moments_interval;
             return;
     }
 
