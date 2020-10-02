970c5e92b7a0063632f45741f372cd7cb4b97dd1
John <john.donaghy@protonmail.com>
2020-10-02 17:36:11 -0400

    declaration

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -41,6 +41,7 @@ struct PscFlatfoilParams
   double mass_ratio;
   double lambda0;
   double target_n;  // target density
+  double target_Te_HE; // target high energy electron temperature
   double target_Te; // target electron temperature
   double target_Ti; // target ion_temperatore
   double target_Te_heat;
