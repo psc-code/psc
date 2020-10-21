9e940df7aa66d17dd64e2bdef47463b71cecb962
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-10-21 15:21:59 -0400

    cuda/mparticles: no direct xi4 access from cuda_mparticles_gold.cu

--- src/libpsc/cuda/cuda_mparticles_gold.cu
+++ src/libpsc/cuda/cuda_mparticles_gold.cu
@@ -19,17 +19,16 @@ void cuda_mparticles<BS>::reorder_and_offsets_slow()
     //    uint bidx;
     uint block;
     if (i < this->n_prts) {
-      h_alt_storage.xi4[i] = h_storage.xi4[h_id[i]];
-      h_alt_storage.pxi4[i] = h_storage.pxi4[h_id[i]];
-      // bidx = blockIndex(h_alt_xi4[i], 0);
+      h_alt_storage.store(h_storage[h_id[i]], i);
+      // bidx = blockIndex(h_alt_storage[i], 0);
       block = h_bidx[i];
     } else {
       // bidx = n_blocks;
       block = this->n_blocks;
     }
     // if (i < 10) {
-    //   printf("i %d bidx %d block %d xi4 %g %g\n", bidx, block,
-    //   h_alt_xi4[i].y, h_alt_xi4[i].z);
+    //   printf("i %d bidx %d block %d x %g %g\n", bidx, block,
+    //   h_alt_storage[i].x[1], h_alt_storage[i].x[2]);
     // }
     int prev_block = (i > 0) ? (int)h_bidx[i - 1] : -1;
     for (int b = prev_block + 1; b <= block; b++) {
@@ -38,11 +37,7 @@ void cuda_mparticles<BS>::reorder_and_offsets_slow()
   }
 
   h_alt_storage.resize(this->n_prts);
-  // h_alt_storage.pxi4.resize(this->n_prts);
-  thrust::copy(h_alt_storage.xi4.begin(), h_alt_storage.xi4.end(),
-               alt_storage.xi4.begin());
-  thrust::copy(h_alt_storage.pxi4.begin(), h_alt_storage.pxi4.end(),
-               alt_storage.pxi4.begin());
+  thrust::copy(h_alt_storage.begin(), h_alt_storage.end(), alt_storage.begin());
   thrust::copy(h_off.begin(), h_off.end(), this->by_block_.d_off.begin());
 
   swap_alt();
