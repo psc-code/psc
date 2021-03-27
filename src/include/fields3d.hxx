5650f114f760e94fc8a0d098579b318e87d415d9
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-03-26 23:41:04 -0400

    sarray: replicate SArrayView

--- src/include/fields3d.hxx
+++ src/include/fields3d.hxx
@@ -20,6 +20,57 @@
 #include <list>
 #include <string>
 
+// ======================================================================
+// SArrayView
+
+namespace detail
+{
+template <typename L>
+gt::shape_type<4> strides(const kg::Box3& box, int n_comps);
+
+template <>
+GT_INLINE gt::shape_type<4> strides<kg::LayoutSOA>(const kg::Box3& box,
+                                                   int n_comps)
+{
+  return gt::shape(1, box.im(0), box.im(0) * box.im(1),
+                   box.im(0) * box.im(1) * box.im(2));
+}
+
+template <>
+GT_INLINE gt::shape_type<4> strides<kg::LayoutAOS>(const kg::Box3& box,
+                                                   int n_comps)
+{
+  return gt::shape(n_comps, n_comps * box.im(0),
+                   n_comps * box.im(0) * box.im(1), 1);
+}
+
+} // namespace detail
+
+template <typename T, typename L = kg::LayoutSOA>
+struct SArrayView
+{
+  using Storage = gt::gtensor_span<T, 4>;
+  using value_type = typename Storage::value_type;
+  using reference = typename Storage::reference;
+  using const_reference = typename Storage::const_reference;
+  using pointer = typename Storage::pointer;
+  using const_pointer = typename Storage::const_pointer;
+
+  KG_INLINE SArrayView(const kg::Box3& box, int n_comps, pointer data)
+    : ib_(box.ib()),
+      storage_(data, gt::shape(box.im(0), box.im(1), box.im(2), n_comps),
+               detail::strides<L>(box, n_comps))
+  {}
+
+  KG_INLINE const Int3& ib() const { return ib_; }
+  KG_INLINE Storage& storage() { return storage_; }
+  KG_INLINE const Storage& storage() const { return storage_; }
+
+private:
+  Storage storage_;
+  Int3 ib_; //> lower bounds per direction
+};
+
 template <typename Derived>
 class MFexpression
 {
@@ -231,7 +282,7 @@ public:
   using InnerTypes = MfieldsCRTPInnerTypes<D>;
   using Storage = typename InnerTypes::Storage;
   using Real = typename Storage::value_type;
-  using fields_view_t = kg::SArrayView<Real>;
+  using fields_view_t = SArrayView<Real>;
 
   KG_INLINE const kg::Box3& box() const { return box_; }
   KG_INLINE const Int3& ib() const { return box_.ib(); }
