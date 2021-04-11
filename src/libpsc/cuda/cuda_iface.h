bde6fd72c354b018e31ba8188dbfb2fe95cf9423
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-10 21:27:23 -0400

    cuda/pushf: mv yz implementation

--- src/libpsc/cuda/cuda_iface.h
+++ src/libpsc/cuda/cuda_iface.h
@@ -29,8 +29,6 @@ struct cuda_mfields;
 void cuda_mfields_calc_dive_yz(struct cuda_mfields* cmflds,
                                struct cuda_mfields* cmf, int p);
 
-void cuda_push_fields_E_yz(struct cuda_mfields* cmflds, float dt);
-void cuda_push_fields_H_yz(struct cuda_mfields* cmflds, float dt);
 void cuda_marder_correct_yz(struct cuda_mfields* cmflds,
                             struct cuda_mfields* cmf, int p, float fac[3],
                             int ly[3], int ry[3], int lz[3], int rz[3]);
