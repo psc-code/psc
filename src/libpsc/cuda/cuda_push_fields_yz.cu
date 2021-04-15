8b31891b2f651564cd5c78a4faccf07496be6c0c
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-14 23:13:53 -0400

    cuda/dive: use array exp, all patches at once

--- src/libpsc/cuda/cuda_push_fields_yz.cu
+++ src/libpsc/cuda/cuda_push_fields_yz.cu
@@ -13,21 +13,19 @@ void cuda_mfields_calc_dive_yz(MfieldsStateCuda& mflds, MfieldsCuda& mdive)
 {
   auto dx = mflds.grid().domain.dx;
 
-  assert(mflds.ibn() == Int3({0, BND, BND}));
-  for (int p = 0; p < mflds.grid().n_patches(); p++) {
-    auto flds = mflds.gt().view(_all, _all, _all, _all, p).to_kernel();
-    auto bd = mdive.ibn();
-    auto dive =
-      mdive.gt()
-        .view(_s(bd[0], -bd[0]), _s(bd[1], -bd[1]), _s(bd[2], -bd[2]), 0, p)
-        .to_kernel();
-    gt::launch<3>(dive.shape(), GT_LAMBDA(int i, int j, int k) {
-      int iy = j + BND;
-      int iz = k + BND;
+  auto bnd = mflds.ibn();
+  auto flds = mflds.gt().view(_s(bnd[0], -bnd[0]), _s(-1 + bnd[1], -bnd[1]),
+                              _s(-1 + bnd[2], -bnd[2]));
 
-      dive(0, j, k) = ((flds(0, iy, iz, EY) - flds(0, iy - 1, iz, EY)) / dx[1] +
-                       (flds(0, iy, iz, EZ) - flds(0, iy, iz - 1, EZ)) / dx[2]);
-    });
-  }
+  auto bd = mdive.ibn();
+  auto dive =
+    mdive.gt().view(_s(bd[0], -bd[0]), _s(bd[1], -bd[1]), _s(bd[2], -bd[2]), 0);
+
+  auto ey = flds.view(_all, _s(1, _), _s(1, _), EY);
+  auto eym = flds.view(_all, _s(_, -1), _s(1, _), EY);
+  auto ez = flds.view(_all, _s(1, _), _s(1, _), EZ);
+  auto ezm = flds.view(_all, _s(1, _), _s(_, -1), EZ);
+  dive = (ey - eym) / dx[1] + (ez - ezm) / dx[2];
+
   cuda_sync_if_enabled();
 }
