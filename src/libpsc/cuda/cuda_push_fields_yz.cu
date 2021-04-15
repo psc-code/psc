d0c4125210dacac5d14da15d60ee7678f0d6ecd1
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-14 23:24:08 -0400

    cuda/dive: make a bit prettier
    Well, maybe...

--- src/libpsc/cuda/cuda_push_fields_yz.cu
+++ src/libpsc/cuda/cuda_push_fields_yz.cu
@@ -18,14 +18,12 @@ void cuda_mfields_calc_dive_yz(MfieldsStateCuda& mflds, MfieldsCuda& mdive)
                               _s(-1 + bnd[2], -bnd[2]));
 
   auto bd = mdive.ibn();
-  auto dive =
-    mdive.gt().view(_s(bd[0], -bd[0]), _s(bd[1], -bd[1]), _s(bd[2], -bd[2]), 0);
+  auto dive = mdive.gt().view(0, _s(bd[1], -bd[1]), _s(bd[2], -bd[2]), 0);
 
-  auto ey = flds.view(_all, _s(1, _), _s(1, _), EY);
-  auto eym = flds.view(_all, _s(_, -1), _s(1, _), EY);
-  auto ez = flds.view(_all, _s(1, _), _s(1, _), EZ);
-  auto ezm = flds.view(_all, _s(1, _), _s(_, -1), EZ);
-  dive = (ey - eym) / dx[1] + (ez - ezm) / dx[2];
+  auto s0 = _s(1, _);
+  auto sm = _s(_, -1);
+  dive = (flds.view(0, s0, s0, EY) - flds.view(0, sm, s0, EY)) / dx[1] +
+         (flds.view(0, s0, s0, EZ) - flds.view(0, s0, sm, EZ)) / dx[2];
 
   cuda_sync_if_enabled();
 }
@@ -45,13 +43,9 @@ void cuda_mfields_calc_dive_xyz(MfieldsStateCuda& mflds, MfieldsCuda& mdive)
 
   auto s0 = _s(1, _);
   auto sm = _s(_, -1);
-  auto ex = flds.view(s0, s0, s0, EX);
-  auto exm = flds.view(sm, s0, s0, EX);
-  auto ey = flds.view(s0, s0, s0, EY);
-  auto eym = flds.view(s0, sm, s0, EY);
-  auto ez = flds.view(s0, s0, s0, EZ);
-  auto ezm = flds.view(s0, s0, sm, EZ);
-  dive = (ex - exm) / dx[0] + (ey - eym) / dx[1] + (ez - ezm) / dx[2];
+  dive = (flds.view(s0, s0, s0, EX) - flds.view(sm, s0, s0, EX)) / dx[0] +
+         (flds.view(s0, s0, s0, EY) - flds.view(s0, sm, s0, EY)) / dx[1] +
+         (flds.view(s0, s0, s0, EZ) - flds.view(s0, s0, sm, EZ)) / dx[2];
 
   cuda_sync_if_enabled();
 }
