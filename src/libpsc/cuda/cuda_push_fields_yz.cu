5adbb6baf1eb082bd25689d40f4ee2fe04533a4b
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-14 22:45:10 -0400

    cuda/dive: use GT_LAMBDA

--- src/libpsc/cuda/cuda_push_fields_yz.cu
+++ src/libpsc/cuda/cuda_push_fields_yz.cu
@@ -9,38 +9,20 @@
 
 // ======================================================================
 
-template <typename E1, typename E2>
-__global__ static void calc_dive_yz(E1 flds, E2 f, float dy, float dz,
-                                    int ldimsy, int ldimsz)
-{
-  int iy = blockIdx.x * blockDim.x + threadIdx.x;
-  int iz = blockIdx.y * blockDim.y + threadIdx.y;
-
-  if (iy >= ldimsy || iz >= ldimsz) {
-    return;
-  }
-  iy += BND;
-  iz += BND;
-
-  f(0, iy, iz, 0) = ((flds(0, iy, iz, EY) - flds(0, iy - 1, iz, EY)) / dy +
-                     (flds(0, iy, iz, EZ) - flds(0, iy, iz - 1, EZ)) / dz);
-}
-
 void cuda_mfields_calc_dive_yz(MfieldsStateCuda& mflds, MfieldsCuda& mf, int p)
 {
   auto dx = mflds.grid().domain.dx;
 
-  int my = mflds.gt().shape(1);
-  int mz = mflds.gt().shape(2);
-
-  int grid[2] = {(my + BLOCKSIZE_Y - 1) / BLOCKSIZE_Y,
-                 (mz + BLOCKSIZE_Z - 1) / BLOCKSIZE_Z};
-  dim3 dimBlock(BLOCKSIZE_Y, BLOCKSIZE_Z);
-  dim3 dimGrid(grid[0], grid[1]);
-
   auto flds = mflds.gt().view(_all, _all, _all, _all, p).to_kernel();
   auto f = mf.gt().view(_all, _all, _all, _all, p).to_kernel();
-  calc_dive_yz<<<dimGrid, dimBlock>>>(
-    flds, f, dx[1], dx[2], mflds.grid().ldims[1], mflds.grid().ldims[2]);
+  gt::launch<2>({mflds.grid().ldims[1], mflds.grid().ldims[2]},
+                GT_LAMBDA(int j, int k) {
+                  int iy = j + BND;
+                  int iz = k + BND;
+
+                  f(0, iy, iz, 0) =
+                    ((flds(0, iy, iz, EY) - flds(0, iy - 1, iz, EY)) / dx[1] +
+                     (flds(0, iy, iz, EZ) - flds(0, iy, iz - 1, EZ)) / dx[2]);
+                });
   cuda_sync_if_enabled();
 }
