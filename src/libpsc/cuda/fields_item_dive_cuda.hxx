cfb5b55795e03887c23cc993a55d21f6785da673
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-14 21:53:20 -0400

    cuda/dive: make interface more like non-cuda Item_dive

--- src/libpsc/cuda/fields_item_dive_cuda.hxx
+++ src/libpsc/cuda/fields_item_dive_cuda.hxx
@@ -12,9 +12,8 @@ struct Item_dive_cuda
   constexpr static int n_comps = 1;
   static std::vector<std::string> fld_names() { return {"dive"}; } // FIXME
 
-  Item_dive_cuda(const Grid_t& grid) : mres_{grid, n_comps, grid.ibn} {}
-
-  void operator()(MfieldsState& mflds)
+  Item_dive_cuda(MfieldsState& mflds)
+    : mres_{mflds.grid(), n_comps, mflds.grid().ibn}
   {
     for (int p = 0; p < mflds.n_patches(); p++) {
       cuda_mfields_calc_dive_yz(mflds.cmflds(), mres_.cmflds(), p);
