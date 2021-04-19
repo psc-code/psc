cca83e4973c95845fb2d540259b000e5f197df2a
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-19 11:47:39 -0400

    cuda/checks: use .gt() for item_divj

--- src/libpsc/cuda/checks_cuda_impl.hxx
+++ src/libpsc/cuda/checks_cuda_impl.hxx
@@ -65,15 +65,12 @@ struct ChecksCuda
     auto& h_mflds = mflds.get_as<MfieldsState>(0, mflds._n_comps());
     auto item_divj = Item_divj<MfieldsState>(h_mflds);
 
-    auto& dev_rho_p = item_rho_p_.result();
-    auto& dev_rho_m = item_rho_m_.result();
-    auto&& rho_p = gt::host_mirror(dev_rho_p.gt());
-    auto&& rho_m = gt::host_mirror(dev_rho_m.gt());
-    gt::copy(dev_rho_p.gt(), rho_p);
-    gt::copy(dev_rho_m.gt(), rho_m);
+    auto&& rho_p = gt::host_mirror(item_rho_p_.gt());
+    auto&& rho_m = gt::host_mirror(item_rho_m_.gt());
+    gt::copy(gt::eval(item_rho_p_.gt()), rho_p);
+    gt::copy(gt::eval(item_rho_m_.gt()), rho_m);
 
-    auto&& d_rho = view_interior(rho_p, dev_rho_p.ibn()) -
-                   view_interior(rho_m, dev_rho_m.ibn());
+    auto&& d_rho = rho_p - rho_m;
     auto&& dt_divj = grid.dt * item_divj.gt();
 
     double eps = continuity_threshold;
