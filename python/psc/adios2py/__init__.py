edd25cd264eca95edaf6b67d494ca09073b26f26
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-06-23 22:25:13 -0400

    py: pass slices to var[...]

--- python/psc/adios2py/__init__.py
+++ python/psc/adios2py/__init__.py
@@ -13,11 +13,26 @@ class variable:
     def set_selection(self, start, count):
         self._var.SetSelection((start[::-1], count[::-1]))
 
-    def __getitem__(self, arg):
-        count = self._var.Count()[::-1]
+    def __getitem__(self, args):
+        #print('args', args)
+        shape = self.shape
+        start = np.zeros_like(shape)
+        count = np.zeros_like(shape)
+        for d in range(len(args)):
+            arg = args[d]
+            assert isinstance(arg, slice)
+            assert arg.step is None or arg.step == 1
+            first = 0 if arg.start is None else arg.start
+            last = shape[d] if arg.stop is None else arg.stop
+            start[d] = first
+            count[d] = last - first
+
+        #print(f"d {d} start {start} count {count}")
+        self.set_selection(start, count)
+
         arr = np.empty(count, dtype='f', order='F')
         self._engine.Get(self._var, arr, adios2.Mode.Sync)
-        return arr[arg]
+        return arr
 
 class file:
     def __init__(self, filename):
@@ -32,8 +47,10 @@ class file:
         
     def read(self, varname, sel_start, sel_count):
         var = self[varname]
-        var.set_selection(sel_start, sel_count)
-        return var[:]
+        return var[sel_start[0]:sel_start[0]+sel_count[0],
+                   sel_start[1]:sel_start[1]+sel_count[1],
+                   sel_start[2]:sel_start[2]+sel_count[2],
+                   sel_start[3]:sel_start[3]+sel_count[3]]
     
     def __getitem__(self, varname):
         return variable(self._io.InquireVariable(varname), self._engine)
