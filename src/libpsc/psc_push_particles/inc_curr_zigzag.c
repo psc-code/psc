0e03c587221afc3ff388c1652b5f55b4aa11a779
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-06 15:00:23 -0500

    deposit: implement 2d zigzag

--- src/libpsc/psc_push_particles/inc_curr_zigzag.c
+++ src/libpsc/psc_push_particles/inc_curr_zigzag.c
@@ -59,206 +59,13 @@ struct CurrentZigzag
   }
 
   void calc_j2_one_cell(fields_t curr_cache, real_t qni_wni, real_t xm[3],
-                        real_t xp[3], dim_xz tag_dim)
-  {
-
-    real_t dx[3] = {xp[0] - xm[0], xp[1] - xm[1], xp[2] - xm[2]};
-    real_t xa[3] = {.5f * (xm[0] + xp[0]), .5f * (xm[1] + xp[1]),
-                    .5f * (xm[2] + xp[2])};
-    real_t h = (1.f / 12.f) * dx[0] * dx[1] * dx[2];
-
-    int i[3];
-    for (int d = 0; d < 3; d++) {
-      i[d] = fint(xa[d]);
-    }
-
-    for (int d = 0; d < 3; d++) {
-      xa[d] -= i[d];
-    }
-
-    real_t fnqx = qni_wni * fnqxs_;
-    curr_cache.add(0, i[0], i[1], i[2], fnqx * (dx[0] * (1.f - xa[2])));
-    curr_cache.add(0, i[0], i[1], i[2] + 1, fnqx * (dx[0] * (xa[2])));
-
-    real_t fnqy = qni_wni * fnqys_;
-    curr_cache.add(1, i[0], i[1], i[2],
-                   fnqy * (dx[1] * (1.f - xa[0]) * (1.f - xa[2]) + h));
-    curr_cache.add(1, i[0] + 1, i[1], i[2],
-                   fnqy * (dx[1] * (xa[0]) * (1.f - xa[2]) - h));
-    curr_cache.add(1, i[0], i[1], i[2] + 1,
-                   fnqy * (dx[1] * (1.f - xa[0]) * (xa[2]) - h));
-    curr_cache.add(1, i[0] + 1, i[1], i[2] + 1,
-                   fnqy * (dx[1] * (xa[0]) * (xa[2]) + h));
-
-    real_t fnqz = qni_wni * fnqzs_;
-    curr_cache.add(2, i[0], i[1], i[2], fnqz * (dx[2] * (1.f - xa[0])));
-    curr_cache.add(2, i[0] + 1, i[1], i[2], fnqz * (dx[2] * (xa[0])));
-  }
-
-  void calc_j2_one_cell(fields_t curr_cache, real_t qni_wni, real_t xm[3],
-                        real_t xp[3], dim_xyz tag_dim)
-  {
-    real_t dx[3] = {xp[0] - xm[0], xp[1] - xm[1], xp[2] - xm[2]};
-    real_t xa[3] = {.5f * (xm[0] + xp[0]), .5f * (xm[1] + xp[1]),
-                    .5f * (xm[2] + xp[2])};
-    real_t h = (1.f / 12.f) * dx[0] * dx[1] * dx[2];
-
-    int i[3] = {};
-    for (int d = 0; d < 3; d++) {
-      i[d] = fint(xa[d]);
-      xa[d] -= i[d];
-    }
-
-    int ci0[3] = {};
-    real_t fnq[3] = {qni_wni * fnqxs_, qni_wni * fnqys_, qni_wni * fnqzs_};
-    deposit(curr_cache, i, fnq, dx, xa, h, ci0);
-  }
-
-  void calc_j2_one_cell(fields_t curr_cache, real_t qni_wni, real_t xm[3],
                         real_t xp[3])
   {
     calc_j2_one_cell(curr_cache, qni_wni, xm, xp, Dim{});
   }
 
-  static void calc_j2_split_along_dim(int dim, int im, real_t x1[3],
-                                      real_t xm[3], real_t xp[3])
-  {
-    real_t bnd = 0.f;       // quell warning
-    if (xp[dim] > im + 1) { // crossed boundary to right
-      bnd = im + 1;
-    } else if (xp[dim] < im) { // crosses boundary to left
-      bnd = im;
-    }
-    real_t frac = (bnd - xm[dim]) / (xp[dim] - xm[dim]);
-    // FIXME, set d == dim value to exact boundary?
-    for (int d = 0; d < 3; d++) {
-      if (d == dim) {
-        x1[d] = bnd;
-      } else {
-        x1[d] = xm[d] + frac * (xp[d] - xm[d]);
-      }
-    }
-  }
-
   // ----------------------------------------------------------------------
   // dim_yz
-
-  void calc_j2_split_dim_y(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_yz tag_dim)
-  {
-    const int dim = 1;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_one_cell(curr_cache, qni_wni, xm, x1);
-      calc_j2_one_cell(curr_cache, qni_wni, x1, xp);
-    } else {
-      calc_j2_one_cell(curr_cache, qni_wni, xm, xp);
-    }
-  }
-
-  void calc_j2_split_dim_z(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_yz tag_dim)
-  {
-    const int dim = 2;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, x1, tag_dim);
-      calc_j2_split_dim_y(curr_cache, qni_wni, x1, xp, tag_dim);
-    } else {
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, xp, tag_dim);
-    }
-  }
-
-  // ----------------------------------------------------------------------
-  // dim_xz
-
-  void calc_j2_split_dim_x(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_xz tag_dim)
-  {
-    const int dim = 0;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_one_cell(curr_cache, qni_wni, xm, x1);
-      calc_j2_one_cell(curr_cache, qni_wni, x1, xp);
-    } else {
-      calc_j2_one_cell(curr_cache, qni_wni, xm, xp);
-    }
-  }
-
-  void calc_j2_split_dim_z(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_xz tag_dim)
-  {
-    const int dim = 2;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_split_dim_x(curr_cache, qni_wni, xm, x1, tag_dim);
-      calc_j2_split_dim_x(curr_cache, qni_wni, x1, xp, tag_dim);
-    } else {
-      calc_j2_split_dim_x(curr_cache, qni_wni, xm, xp, tag_dim);
-    }
-  }
-
-  // ----------------------------------------------------------------------
-  // dim_xyz
-
-  void calc_j2_split_dim_x(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp)
-  {
-    const int dim = 0;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_one_cell(curr_cache, qni_wni, xm, x1);
-      calc_j2_one_cell(curr_cache, qni_wni, x1, xp);
-    } else {
-      calc_j2_one_cell(curr_cache, qni_wni, xm, xp);
-    }
-  }
-
-  void calc_j2_split_dim_y(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_xyz tag_dim)
-  {
-    const int dim = 1;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_split_dim_x(curr_cache, qni_wni, xm, x1);
-      calc_j2_split_dim_x(curr_cache, qni_wni, x1, xp);
-    } else {
-      calc_j2_split_dim_x(curr_cache, qni_wni, xm, xp);
-    }
-  }
-
-  void calc_j2_split_dim_z(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_xyz tag_dim)
-  {
-    const int dim = 2;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, x1, tag_dim);
-      calc_j2_split_dim_y(curr_cache, qni_wni, x1, xp, tag_dim);
-    } else {
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, xp, tag_dim);
-    }
-  }
-
-  void calc_j2_split_dim_z(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp)
-  {
-    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp, Dim{});
-  }
 
   void calc_j(fields_t curr_cache, real_t* xm, real_t* xp, int* lf, int* lg,
               real_t qni_wni, real_t* vxi, dim_yz tag)
@@ -266,22 +73,24 @@ struct CurrentZigzag
     xm[0] = .5f; // this way, we guarantee that the average position will remain
                  // in the 0th cell
     xp[0] = xm[0] + vxi[0] * dt_ * dxi_[0];
-    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp);
-  }
-
-  void calc_j(fields_t curr_cache, real_t* xm, real_t* xp, int* lf, int* lg,
-              real_t qni_wni, real_t* vxi, dim_xz tag)
-  {
-    xm[1] = .5f; // this way, we guarantee that the average position will remain
-                 // in the 0th cell
-    xp[1] = xm[1] + vxi[1] * dt_ * dxi_[1];
-    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp);
-  }
-
-  void calc_j(fields_t curr_cache, real_t* xm, real_t* xp, int* lf, int* lg,
-              real_t qni_wni, real_t* vxi, dim_xyz tag)
-  {
-    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp);
+    //    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp);
+    real_t xr[3];
+    bool crossed = false;
+    for (int d = 0; d < 3; d++) {
+      int im = fint(xm[d]), ip = fint(xp[d]);
+      if (im == ip) {
+        xr[d] = .5 * (xm[d] + xp[d]);
+      } else {
+        xr[d] = std::max(im, ip);
+        crossed = true;
+      }
+    }
+    if (crossed) { // could choose this path always
+      calc_j2_one_cell(curr_cache, qni_wni, xm, xr);
+      calc_j2_one_cell(curr_cache, qni_wni, xr, xp);
+    } else {
+      calc_j2_one_cell(curr_cache, qni_wni, xm, xp);
+    }
   }
 
   // ----------------------------------------------------------------------
