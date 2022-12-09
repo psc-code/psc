bacdbc4d17a13f8f4499d80b3c3ee14d46a94fe1
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-09 11:20:38 -0500

    current: consolidate all the splitting in Current1vbSplit

--- src/libpsc/psc_push_particles/inc_curr_1vb_split.c
+++ src/libpsc/psc_push_particles/inc_curr_1vb_split.c
@@ -19,8 +19,8 @@ struct Current1vbSplit
       deposition_(real_t(grid.norm.fnqs / grid.dt) * Real3(grid.domain.dx))
   {}
 
-  void calc_j2_one_cell(fields_t curr_cache, real_t qni_wni, real_t xm[3],
-                        real_t xp[3])
+  void calc_j2_one_cell(fields_t curr_cache, real_t qni_wni, const real_t xm[3],
+                        const real_t xp[3])
   {
     real_t dx[3] = {xp[0] - xm[0], xp[1] - xm[1], xp[2] - xm[2]};
     real_t xa[3] = {.5f * (xm[0] + xp[0]), .5f * (xm[1] + xp[1]),
@@ -36,7 +36,7 @@ struct Current1vbSplit
   }
 
   static void calc_j2_split_along_dim(int dim, int im, real_t x1[3],
-                                      real_t xm[3], real_t xp[3])
+                                      const real_t xm[3], const real_t xp[3])
   {
     real_t bnd = 0.f; // quell warning
     int ip = fint(xp[dim]);
@@ -58,48 +58,12 @@ struct Current1vbSplit
     }
   }
 
-  // ----------------------------------------------------------------------
-  // dim_yz
-
-  void calc_j2_split_dim_y(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_yz tag_dim)
-  {
-    const int dim = 1;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_one_cell(curr_cache, qni_wni, xm, x1);
-      calc_j2_one_cell(curr_cache, qni_wni, x1, xp);
-    } else {
-      calc_j2_one_cell(curr_cache, qni_wni, xm, xp);
-    }
-  }
-
-  void calc_j2_split_dim_z(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_yz tag_dim)
-  {
-    const int dim = 2;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, x1, tag_dim);
-      calc_j2_split_dim_y(curr_cache, qni_wni, x1, xp, tag_dim);
-    } else {
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, xp, tag_dim);
-    }
-  }
-
-  // ----------------------------------------------------------------------
-  // dim_xz
-
-  void calc_j2_split_dim_x(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_xz tag_dim)
+  void calc_j2_split_dim_x(fields_t curr_cache, real_t qni_wni,
+                           const real_t* xm, const real_t* xp)
   {
     const int dim = 0;
     int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
+    if (!Dim::InvarX::value && (fint(xp[dim]) != im)) {
       real_t x1[3];
       calc_j2_split_along_dim(dim, im, x1, xm, xp);
       calc_j2_one_cell(curr_cache, qni_wni, xm, x1);
@@ -109,45 +73,12 @@ struct Current1vbSplit
     }
   }
 
-  void calc_j2_split_dim_z(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_xz tag_dim)
-  {
-    const int dim = 2;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_split_dim_x(curr_cache, qni_wni, xm, x1, tag_dim);
-      calc_j2_split_dim_x(curr_cache, qni_wni, x1, xp, tag_dim);
-    } else {
-      calc_j2_split_dim_x(curr_cache, qni_wni, xm, xp, tag_dim);
-    }
-  }
-
-  // ----------------------------------------------------------------------
-  // dim_xyz
-
-  void calc_j2_split_dim_x(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp)
-  {
-    const int dim = 0;
-    int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
-      real_t x1[3];
-      calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_one_cell(curr_cache, qni_wni, xm, x1);
-      calc_j2_one_cell(curr_cache, qni_wni, x1, xp);
-    } else {
-      calc_j2_one_cell(curr_cache, qni_wni, xm, xp);
-    }
-  }
-
-  void calc_j2_split_dim_y(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_xyz tag_dim)
+  void calc_j2_split_dim_y(fields_t curr_cache, real_t qni_wni,
+                           const real_t* xm, const real_t* xp)
   {
     const int dim = 1;
     int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
+    if (!Dim::InvarY::value && (fint(xp[dim]) != im)) {
       real_t x1[3];
       calc_j2_split_along_dim(dim, im, x1, xm, xp);
       calc_j2_split_dim_x(curr_cache, qni_wni, xm, x1);
@@ -157,43 +88,19 @@ struct Current1vbSplit
     }
   }
 
-  void calc_j2_split_dim_z(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp, dim_xyz tag_dim)
+  void calc_j2_split_dim_z(fields_t curr_cache, real_t qni_wni,
+                           const real_t* xm, const real_t* xp)
   {
     const int dim = 2;
     int im = fint(xm[dim]);
-    if (xp[dim] > im + 1 || xp[dim] < im) {
+    if (!Dim::InvarZ::value && (fint(xp[dim]) != im)) {
       real_t x1[3];
       calc_j2_split_along_dim(dim, im, x1, xm, xp);
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, x1, tag_dim);
-      calc_j2_split_dim_y(curr_cache, qni_wni, x1, xp, tag_dim);
+      calc_j2_split_dim_y(curr_cache, qni_wni, xm, x1);
+      calc_j2_split_dim_y(curr_cache, qni_wni, x1, xp);
     } else {
-      calc_j2_split_dim_y(curr_cache, qni_wni, xm, xp, tag_dim);
+      calc_j2_split_dim_y(curr_cache, qni_wni, xm, xp);
     }
-  }
-
-  void calc_j2_split_dim_z(fields_t curr_cache, real_t qni_wni, real_t* xm,
-                           real_t* xp)
-  {
-    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp, Dim{});
-  }
-
-  void calc_j(fields_t curr_cache, real_t* xm, real_t* xp, real_t* vxi,
-              real_t qni_wni, dim_yz tag)
-  {
-    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp);
-  }
-
-  void calc_j(fields_t curr_cache, real_t* xm, real_t* xp, real_t* vxi,
-              real_t qni_wni, dim_xz tag)
-  {
-    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp);
-  }
-
-  void calc_j(fields_t curr_cache, real_t* xm, real_t* xp, real_t* vxi,
-              real_t qni_wni, dim_xyz tag)
-  {
-    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp);
   }
 
   // ----------------------------------------------------------------------
@@ -216,7 +123,8 @@ struct Current1vbSplit
       xm[2] = .5f;
       xp[2] = xm[2] + vxi[2] * dt_ * dxi_[2];
     }
-    calc_j(curr_cache, xm, xp, vxi, qni_wni, Dim{});
+
+    calc_j2_split_dim_z(curr_cache, qni_wni, xm, xp);
   }
 
 private:
