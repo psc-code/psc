bbbac7014e858aaae95cf5618e368d1adeda6f01
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-09 10:08:29 -0500

    current: mv yz -> CurrentDeposition1vb

--- src/include/psc/current_deposition.hxx
+++ src/include/psc/current_deposition.hxx
@@ -49,6 +49,29 @@ public:
              fnq[2] * (dx[2] * (xa[0]) * (xa[1]) + h));
   };
 
+  GT_INLINE void operator()(Curr& curr, const int i[3], real_t qni_wni,
+                            const real_t dx[3], const real_t xa[3],
+                            dim_yz tag_dim) const
+  {
+    real_t h = (1.f / real_t(12.f)) * dx[0] * dx[1] * dx[2];
+
+    real3_t fnq = qni_wni * fnqs_;
+    curr.add(0, i[0], i[1], i[2],
+             fnq[0] * (dx[0] * (1.f - xa[1]) * (1.f - xa[2]) + h));
+    curr.add(0, i[0], i[1] + 1, i[2],
+             fnq[0] * (dx[0] * (xa[1]) * (1.f - xa[2]) - h));
+    curr.add(0, i[0], i[1], i[2] + 1,
+             fnq[0] * (dx[0] * (1.f - xa[1]) * (xa[2]) - h));
+    curr.add(0, i[0], i[1] + 1, i[2] + 1,
+             fnq[0] * (dx[0] * (xa[1]) * (xa[2]) + h));
+
+    curr.add(1, i[0], i[1], i[2], fnq[1] * (dx[1] * (1.f - xa[2])));
+    curr.add(1, i[0], i[1], i[2] + 1, fnq[1] * (dx[1] * (xa[2])));
+
+    curr.add(2, i[0], i[1], i[2], fnq[2] * (dx[2] * (1.f - xa[1])));
+    curr.add(2, i[0], i[1] + 1, i[2], fnq[2] * (dx[2] * (xa[1])));
+  }
+
 private:
   real3_t fnqs_;
 };
