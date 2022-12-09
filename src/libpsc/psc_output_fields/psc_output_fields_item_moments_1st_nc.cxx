f71fb18d0b2b73488d0ec71bca2527790b365a26
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-09 15:17:56 -0500

    moments: get rid of Moment_v_1st_nc
    It's not used, and doesn't seem particularly useful, either.

--- src/libpsc/psc_output_fields/psc_output_fields_item_moments_1st_nc.cxx
+++ src/libpsc/psc_output_fields/psc_output_fields_item_moments_1st_nc.cxx
@@ -116,50 +116,8 @@ struct Moment_rho_1st_nc : ItemMomentCRTP<Moment_rho_1st_nc<MP, MF>, MF>
   }
 };
 
-// ======================================================================
-// v
-
-template <typename MP, typename MF>
-struct Moment_v_1st_nc
-{
-  using Mparticles = MP;
-  using Mfields = MF;
-  using real_t = typename Mparticles::real_t;
-  using particles_t = typename Mparticles::Patch;
-
-  constexpr static char const* name = "v_1st_nc";
-  constexpr static int n_comps = 3;
-  static std::vector<std::string> fld_names() { return {"vx", "vy", "vz"}; }
-  constexpr static int flags = POFI_BY_KIND;
-
-  static void run(Mfields& mflds, Mparticles& mprts)
-  {
-    const Grid_t& grid = mprts.grid();
-    Deposit1stNc<real_t> deposit(
-      {grid.domain.dx[0], grid.domain.dx[1], grid.domain.dx[2]},
-      grid.norm.fnqs);
-
-    auto accessor = mprts.accessor();
-    for (int p = 0; p < mprts.n_patches(); p++) {
-      auto flds = mflds[p];
-      for (auto prt : accessor[p]) {
-        int mm = prt.kind() * 3;
-
-        real_t vxi[3];
-        particle_calc_vxi(prt, vxi);
-
-        for (int m = 0; m < 3; m++) {
-          deposit(prt, flds, mm + m, vxi[m], grid);
-        }
-      }
-    }
-  }
-};
-
 #define MAKE_POFI_OPS(MP, MF, TYPE)                                            \
   FieldsItemMomentOps<Moment_n_1st_nc<MP, MF>>                                 \
     psc_output_fields_item_n_1st_nc_##TYPE##_ops;                              \
   FieldsItemMomentOps<Moment_rho_1st_nc<MP, MF>>                               \
-    psc_output_fields_item_rho_1st_nc_##TYPE##_ops;                            \
-  FieldsItemMomentOps<Moment_v_1st_nc<MP, MF>>                                 \
-    psc_output_fields_item_v_1st_nc_##TYPE##_ops;
+    psc_output_fields_item_rho_1st_nc_##TYPE##_ops;
