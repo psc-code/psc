b0e927667424fc3c859e2c2d1e03a046d72d0265
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-20 00:33:08 -0500

    moments: reenable cuda, add test for Moments_1st (cuda)

--- src/libpsc/tests/test_moments.cxx
+++ src/libpsc/tests/test_moments.cxx
@@ -5,7 +5,7 @@
 
 using PushParticlesTestTypes =
   ::testing::Types<TestConfig1vbec3dSingleYZ, TestConfig1vbec3dSingle
-#ifdef xUSE_CUDA
+#ifdef USE_CUDA
                    ,
                    TestConfig1vbec3dCudaYZ, TestConfig1vbec3dCuda444
 #endif
@@ -47,6 +47,56 @@ TYPED_TEST(PushParticlesTest, Moment1)
       // if (val) {
       //   printf("ijk %d %d %d val %g\n", i, j, k, val);
       // }
+    });
+  }
+}
+
+template <typename Mfields>
+struct MfieldsToHost
+{
+  using type = Mfields;
+};
+
+#ifdef USE_CUDA
+
+template <>
+struct MfieldsToHost<MfieldsCuda>
+{
+  using type = MfieldsSingle;
+};
+
+#endif
+
+TYPED_TEST(PushParticlesTest, Moments_1st)
+{
+  using Mparticles = typename TypeParam::Mparticles;
+  using Mfields = typename TypeParam::Mfields;
+  using dim_t = typename TypeParam::dim;
+  using MfieldsHost = typename MfieldsToHost<Mfields>::type;
+  using Moments = Moments_1st<Mparticles, MfieldsHost, dim_t>;
+  using real_t = typename Mfields::real_t;
+
+  const real_t eps = 1e-6;
+  auto kinds = Grid_t::Kinds{Grid_t::Kind(1., 1., "test_species")};
+  this->make_psc(kinds);
+  const auto& grid = this->grid();
+
+  // init particles
+  Mparticles mprts{grid};
+  {
+    auto injector = mprts.injector();
+    injector[0]({{5., 5., 5.}, {0., 0., 1.}, 1., 0});
+  }
+  Moments moments{mprts};
+  auto gt = moments.gt();
+  for (int p = 0; p < grid.n_patches(); p++) {
+    grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
+      real_t val = gt(i, j, k, 0, p);
+      if (i == 0 && j == 0 && k == 0) {
+        EXPECT_NEAR(val, .005, eps) << "ijk " << i << " " << j << " " << k;
+      } else {
+        EXPECT_NEAR(val, 0., eps) << "ijk " << i << " " << j << " " << k;
+      }
     });
   }
 }
