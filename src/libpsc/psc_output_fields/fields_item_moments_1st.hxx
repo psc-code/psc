1674e19dc1491adf21953f5e0b8a32a3dccfc6df
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-23 11:26:26 -0500

    moments: remove unused __particle_calc_vxi

--- src/libpsc/psc_output_fields/fields_item_moments_1st.hxx
+++ src/libpsc/psc_output_fields/fields_item_moments_1st.hxx
@@ -6,17 +6,6 @@
 
 #include <cmath>
 
-template <typename Particle>
-static inline void __particle_calc_vxi(const Particle& prt,
-                                       typename Particle::real_t vxi[3])
-{
-  typename Particle::real_t root =
-    1.f / std::sqrt(1.f + sqr(prt.u[0]) + sqr(prt.u[1]) + sqr(prt.u[2]));
-  vxi[0] = prt.u[0] * root;
-  vxi[1] = prt.u[1] * root;
-  vxi[2] = prt.u[2] * root;
-}
-
 // ======================================================================
 // n_1st
 
