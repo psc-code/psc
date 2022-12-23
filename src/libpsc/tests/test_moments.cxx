401a703a7fa3985e2caf0c6b4dd64ff35bd7b844
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-22 22:03:08 -0500

    moments/test: no more (ab)use of ParticlesTest stuff

--- src/libpsc/tests/test_moments.cxx
+++ src/libpsc/tests/test_moments.cxx
@@ -9,13 +9,11 @@
 template <typename T>
 struct MomentTest : ::testing::Test
 {
-  using dim = typename T::dim;
+  using Mfields = typename T::Mfields;
   using Mparticles = typename T::Mparticles;
-  using MfieldsState = typename T::MfieldsState;
-  using PushParticles = typename T::PushParticles;
+  using dim_t = typename T::dim;
   using real_t = typename Mparticles::real_t;
 
-  const real_t eps = 1e-5;
   const double L = 160;
 
   const real_t fnqx = .05, fnqy = .05, fnqz = .05;
@@ -26,15 +24,15 @@ struct MomentTest : ::testing::Test
   void make_psc(const Grid_t::Kinds& kinds)
   {
     Int3 gdims = {16, 16, 16};
-    if (dim::InvarX::value) {
+    if (dim_t::InvarX::value) {
       gdims[0] = 1;
       ibn[0] = 0;
     }
-    if (dim::InvarY::value) {
+    if (dim_t::InvarY::value) {
       gdims[1] = 1;
       ibn[1] = 0;
     }
-    if (dim::InvarZ::value) {
+    if (dim_t::InvarZ::value) {
       gdims[2] = 1;
       ibn[2] = 0;
     }
@@ -62,22 +60,35 @@ struct MomentTest : ::testing::Test
   std::unique_ptr<Grid_t> grid_;
 };
 
-using MomentTestTypes =
-  ::testing::Types<TestConfig1vbec3dSingleYZ, TestConfig1vbec3dSingle
+template <typename MF, typename MP, typename D>
+struct MomentTestConfig
+{
+  using Mfields = MF;
+  using Mparticles = MP;
+  using dim = D;
+};
+
+using MomentTestTypes = ::testing::Types<
 #ifdef USE_CUDA
-                   ,
-                   TestConfig1vbec3dCudaYZ, TestConfig1vbec3dCuda444
+  MomentTestConfig<MfieldsCuda, MparticlesCuda<BS444>, dim_xyz>,
+  MomentTestConfig<MfieldsCuda, MparticlesCuda<BS144>, dim_yz>,
 #endif
-                   >;
+  MomentTestConfig<MfieldsC, MparticlesDouble, dim_xyz>,
+  MomentTestConfig<MfieldsC, MparticlesDouble, dim_yz>,
+  MomentTestConfig<MfieldsSingle, MparticlesSingle, dim_xyz>,
+  MomentTestConfig<MfieldsSingle, MparticlesSingle, dim_yz>>;
 
 TYPED_TEST_SUITE(MomentTest, MomentTestTypes);
 
 TYPED_TEST(MomentTest, Moment_n_1)
 {
-  using Mparticles = typename TypeParam::Mparticles;
-  using Mfields = typename TypeParam::Mfields;
-  using Moment_n = typename TypeParam::Moment_n;
-  using real_t = typename Mfields::real_t;
+  using base_type = MomentTest<TypeParam>;
+  using dim_t = typename base_type::dim_t;
+  using Mfields = typename base_type::Mfields;
+  using Mparticles = typename base_type::Mparticles;
+  using real_t = typename base_type::real_t;
+
+  using Moment = Moment_n_1st<Mfields, dim_t>;
 
   const real_t eps = 1e-6;
   auto kinds = Grid_t::Kinds{Grid_t::Kind(1., 1., "test_species")};
@@ -90,11 +101,11 @@ TYPED_TEST(MomentTest, Moment_n_1)
     auto injector = mprts.injector();
     injector[0]({{5., 5., 5.}, {0., 0., 1.}, 1., 0});
   }
-  Moment_n moment_n{mprts};
-  auto gt_n = moment_n.gt();
+  Moment moment{mprts};
+  auto gt = moment.gt();
   for (int p = 0; p < grid.n_patches(); p++) {
     grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
-      real_t val = gt_n(i, j, k, 0, p);
+      real_t val = gt(i, j, k, 0, p);
       if (i == 0 && j == 0 && k == 0) {
         EXPECT_NEAR(val, .005, eps) << "ijk " << i << " " << j << " " << k;
       } else {
@@ -156,10 +167,13 @@ TYPED_TEST(MomentTest, Moments_1st)
 
 TYPED_TEST(MomentTest, Moment_n_2) // FIXME, mostly copied
 {
-  using Mparticles = typename TypeParam::Mparticles;
-  using Mfields = typename TypeParam::Mfields;
-  using Moment_n = typename TypeParam::Moment_n;
-  using real_t = typename Mfields::real_t;
+  using base_type = MomentTest<TypeParam>;
+  using dim_t = typename base_type::dim_t;
+  using Mfields = typename base_type::Mfields;
+  using Mparticles = typename base_type::Mparticles;
+  using real_t = typename base_type::real_t;
+
+  using Moment = Moment_n_1st<Mfields, dim_t>;
 
   const real_t eps = 1e-6;
   auto kinds = Grid_t::Kinds{Grid_t::Kind(1., 1., "test_species")};
@@ -174,14 +188,14 @@ TYPED_TEST(MomentTest, Moment_n_2) // FIXME, mostly copied
   }
 
   int i0 = 2;
-  if (PushParticlesTest<TypeParam>::dim::InvarX::value)
+  if (dim_t::InvarX::value)
     i0 = 0;
 
-  Moment_n moment_n{mprts};
-  auto gt_n = moment_n.gt();
+  Moment moment{mprts};
+  auto gt = moment.gt();
   for (int p = 0; p < grid.n_patches(); p++) {
     grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
-      real_t val = gt_n(i, j, k, 0, p);
+      real_t val = gt(i, j, k, 0, p);
       if (i == i0 && j == 0 && k == 0) {
         EXPECT_NEAR(val, .005, eps) << "ijk " << i << " " << j << " " << k;
       } else {
@@ -263,7 +277,6 @@ TYPED_TEST(MomentTest, Moment_rho_1st_nc_cc)
 {
   using Mparticles = typename TypeParam::Mparticles;
   using Mfields = typename TypeParam::Mfields;
-  using Bnd = typename TypeParam::Bnd;
   using dim_t = typename TypeParam::dim;
   using Particle = typename Mparticles::Particle;
   using real_t = typename Mfields::real_t;
@@ -285,8 +298,7 @@ TYPED_TEST(MomentTest, Moment_rho_1st_nc_cc)
   for (int p = 0; p < grid.n_patches(); p++) {
     grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
       real_t val = gt(i, j, k, 0, p);
-      if (std::is_same<typename PushParticlesTest<TypeParam>::dim,
-                       dim_xyz>::value) {
+      if (std::is_same<dim_t, dim_xyz>::value) {
         if ((i == 0 && j == 0 && k == 0) || (i == 1 && j == 0 && k == 0) ||
             (i == 0 && j == 1 && k == 0) || (i == 1 && j == 1 && k == 0) ||
             (i == 0 && j == 0 && k == 1) || (i == 1 && j == 0 && k == 1) ||
@@ -296,8 +308,7 @@ TYPED_TEST(MomentTest, Moment_rho_1st_nc_cc)
         } else {
           EXPECT_NEAR(val, 0., eps) << "ijk " << i << " " << j << " " << k;
         }
-      } else if (std::is_same<typename PushParticlesTest<TypeParam>::dim,
-                              dim_yz>::value) {
+      } else if (std::is_same<dim_t, dim_yz>::value) {
         if ((i == 0 && j == 0 && k == 0) || (i == 0 && j == 1 && k == 0) ||
             (i == 0 && j == 0 && k == 1) || (i == 0 && j == 1 && k == 1)) {
           EXPECT_NEAR(val, .005 / 4., eps)
@@ -315,7 +326,6 @@ TYPED_TEST(MomentTest, Moment_rho_1st_nc_nc)
 {
   using Mparticles = typename TypeParam::Mparticles;
   using Mfields = typename TypeParam::Mfields;
-  using Bnd = typename TypeParam::Bnd;
   using dim_t = typename TypeParam::dim;
   using Particle = typename Mparticles::Particle;
   using real_t = typename Mfields::real_t;
@@ -337,15 +347,13 @@ TYPED_TEST(MomentTest, Moment_rho_1st_nc_nc)
   for (int p = 0; p < grid.n_patches(); p++) {
     grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
       real_t val = gt(i, j, k, 0, p);
-      if (std::is_same<typename PushParticlesTest<TypeParam>::dim,
-                       dim_xyz>::value) {
+      if (std::is_same<dim_t, dim_xyz>::value) {
         if (i == 1 && j == 1 && k == 1) {
           EXPECT_NEAR(val, .005, eps) << "ijk " << i << " " << j << " " << k;
         } else {
           EXPECT_NEAR(val, 0., eps) << "ijk " << i << " " << j << " " << k;
         }
-      } else if (std::is_same<typename PushParticlesTest<TypeParam>::dim,
-                              dim_yz>::value) {
+      } else if (std::is_same<dim_t, dim_yz>::value) {
         if (j == 1 && k == 1) {
           EXPECT_NEAR(val, .005, eps) << "ijk " << i << " " << j << " " << k;
         } else {
@@ -360,7 +368,8 @@ TYPED_TEST(MomentTest, Moment_n_2nd_nc)
 {
   using Mparticles = typename TypeParam::Mparticles;
   using Mfields = typename TypeParam::Mfields;
-  using Moment = Moment_n_2nd_nc<Mfields, typename TypeParam::dim>;
+  using dim_t = typename TypeParam::dim;
+  using Moment = Moment_n_2nd_nc<Mfields, dim_t>;
   using real_t = typename Mfields::real_t;
 
   const real_t eps = 1e-6;
@@ -382,16 +391,14 @@ TYPED_TEST(MomentTest, Moment_n_2nd_nc)
   for (int p = 0; p < grid.n_patches(); p++) {
     grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
       real_t val = gt(i, j, k, 0, p);
-      if (std::is_same<typename PushParticlesTest<TypeParam>::dim,
-                       dim_xyz>::value) {
+      if (std::is_same<dim_t, dim_xyz>::value) {
         if ((i == 0 || i == 1) && (j == 0 || j == 1) && (k == 0 || k == 1)) {
           EXPECT_NEAR(val, w * cori / 8., eps)
             << "ijk " << i << " " << j << " " << k;
         } else {
           EXPECT_NEAR(val, 0., eps) << "ijk " << i << " " << j << " " << k;
         }
-      } else if (std::is_same<typename PushParticlesTest<TypeParam>::dim,
-                              dim_yz>::value) {
+      } else if (std::is_same<dim_t, dim_yz>::value) {
         if ((j == 0 || j == 1) && (k == 0 || k == 1)) {
           EXPECT_NEAR(val, w * cori / 4., eps)
             << "ijk " << i << " " << j << " " << k;
@@ -407,7 +414,8 @@ TYPED_TEST(MomentTest, Moment_rho_2nd_nc)
 {
   using Mparticles = typename TypeParam::Mparticles;
   using Mfields = typename TypeParam::Mfields;
-  using Moment = Moment_rho_2nd_nc<Mfields, typename TypeParam::dim>;
+  using dim_t = typename TypeParam::dim;
+  using Moment = Moment_rho_2nd_nc<Mfields, dim_t>;
   using real_t = typename Mfields::real_t;
 
   const real_t eps = 1e-6;
@@ -429,16 +437,14 @@ TYPED_TEST(MomentTest, Moment_rho_2nd_nc)
   for (int p = 0; p < grid.n_patches(); p++) {
     grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
       real_t val = gt(i, j, k, 0, p);
-      if (std::is_same<typename PushParticlesTest<TypeParam>::dim,
-                       dim_xyz>::value) {
+      if (std::is_same<dim_t, dim_xyz>::value) {
         if ((i == 0 || i == 1) && (j == 0 || j == 1) && (k == 0 || k == 1)) {
           EXPECT_NEAR(val, w * cori / 8., eps)
             << "ijk " << i << " " << j << " " << k;
         } else {
           EXPECT_NEAR(val, 0., eps) << "ijk " << i << " " << j << " " << k;
         }
-      } else if (std::is_same<typename PushParticlesTest<TypeParam>::dim,
-                              dim_yz>::value) {
+      } else if (std::is_same<dim_t, dim_yz>::value) {
         if ((j == 0 || j == 1) && (k == 0 || k == 1)) {
           EXPECT_NEAR(val, w * cori / 4., eps)
             << "ijk " << i << " " << j << " " << k;
