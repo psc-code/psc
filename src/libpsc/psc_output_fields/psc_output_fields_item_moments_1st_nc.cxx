ae751912d5902cb1d4b00920dd02f771222c5dc5
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-27 01:05:43 -0500

    moments: remove ItemMomentCRTP

--- src/libpsc/psc_output_fields/psc_output_fields_item_moments_1st_nc.cxx
+++ src/libpsc/psc_output_fields/psc_output_fields_item_moments_1st_nc.cxx
@@ -9,11 +9,5 @@
 // rho
 
 template <typename S, typename D>
-struct Moment_rho_1st_nc : ItemMomentCRTP<Moment_rho_1st_nc<S, D>, S>
-{
-  using Base = ItemMomentCRTP<Moment_rho_1st_nc<S, D>, S>;
-  using moment_type =
-    psc::moment::moment_rho<psc::deposit::code::Deposit1stNc, D>;
-
-  using Base::Base;
-};
+using Moment_rho_1st_nc =
+  ItemMoment<psc::moment::moment_rho<psc::deposit::code::Deposit1stNc, D>, S>;
--- src/libpsc/psc_output_fields/psc_output_fields_item_moments_2nd_nc.cxx
+++ src/libpsc/psc_output_fields/psc_output_fields_item_moments_2nd_nc.cxx
@@ -4,16 +4,6 @@
 
 #include <math.h>
 
-// ======================================================================
-// rho
-
 template <typename S, typename D>
-class Moment_rho_2nd_nc : public ItemMomentCRTP<Moment_rho_2nd_nc<S, D>, S>
-{
-public:
-  using Base = ItemMomentCRTP<Moment_rho_2nd_nc<S, D>, S>;
-  using moment_type =
-    psc::moment::moment_rho<psc::deposit::code::Deposit2ndNc, D>;
-
-  using Base::Base;
-};
+using Moment_rho_2nd_nc =
+  ItemMoment<psc::moment::moment_rho<psc::deposit::code::Deposit2ndNc, D>, S>;
