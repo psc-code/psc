0eaafbc4e26fa787c88bf573aed6086d4ee8cb2f
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-27 22:51:16 -0500

    checks/gauss: do actual calc on device
    Unfortunately, have to do it patch by patch, since
    boundaries may mean we want to skip a layer.

--- src/libpsc/psc_checks/checks_impl.hxx
+++ src/libpsc/psc_checks/checks_impl.hxx
@@ -45,6 +45,23 @@ void print_diff(const E1& e1, const E2& e2, double eps)
   });
 }
 
+template <typename E1, typename E2>
+void print_diff_3d(const E1& e1, const E2& e2, double eps)
+{
+  auto&& h_e1 = gt::host_mirror(e1);
+  auto&& h_e2 = gt::host_mirror(e2);
+  gt::copy(gt::eval(e1), h_e1);
+  gt::copy(gt::eval(e2), h_e2);
+  gt::launch_host<3>(h_e1.shape(), [=](int i, int j, int k) {
+    auto val_e1 = h_e1(i, j, k);
+    auto val_e2 = h_e2(i, j, k);
+    if (std::abs(val_e1 + val_e2) > eps) {
+      mprintf("p%d (%d,%d,%d): %g -- %g diff %g\n", i, j, k, val_e1, val_e2,
+              val_e1 - val_e2);
+    }
+  });
+}
+
 } // namespace helper
 
 namespace checks
