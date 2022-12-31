80cc055e82e9303fa8447dd983bf673771ee975a
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-30 22:13:56 -0500

    marder: rewrite host version using gtensor slicing

--- src/libpsc/psc_push_fields/marder_impl.hxx
+++ src/libpsc/psc_push_fields/marder_impl.hxx
@@ -30,40 +30,21 @@ namespace marder
 // Do the modified marder correction (See eq.(5, 7, 9, 10) in Mardahl and
 // Verboncoeur, CPC, 1997)
 
-#define define_dxdydz(dx, dy, dz)                                              \
-  int dx _mrc_unused = (grid.isInvar(0)) ? 0 : 1;                              \
-  int dy _mrc_unused = (grid.isInvar(1)) ? 0 : 1;                              \
-  int dz _mrc_unused = (grid.isInvar(2)) ? 0 : 1
-
-#define psc_foreach_3d_more(psc, p, ix, iy, iz, l, r)                          \
-  {                                                                            \
-    int __ilo[3] = {-l[0], -l[1], -l[2]};                                      \
-    int __ihi[3] = {grid.ldims[0] + r[0], grid.ldims[1] + r[1],                \
-                    grid.ldims[2] + r[2]};                                     \
-    for (int iz = __ilo[2]; iz < __ihi[2]; iz++) {                             \
-      for (int iy = __ilo[1]; iy < __ihi[1]; iy++) {                           \
-        for (int ix = __ilo[0]; ix < __ihi[0]; ix++)
-
-#define psc_foreach_3d_more_end                                                \
-  }                                                                            \
-  }                                                                            \
-  }
-
 template <typename MfieldsState, typename Mfields>
 inline void correct(MfieldsState& mflds, Mfields& mf,
                     typename MfieldsState::real_t diffusion)
 {
   const auto& grid = mflds.grid();
-  define_dxdydz(dx, dy, dz);
 
   // FIXME: how to choose diffusion parameter properly?
   double deltax = grid.domain.dx[0]; // FIXME double/float
   double deltay = grid.domain.dx[1];
   double deltaz = grid.domain.dx[2];
+  Int3 ib = mflds.ib();
 
   for (int p = 0; p < mf.n_patches(); p++) {
-    int l_cc[3] = {0, 0, 0}, r_cc[3] = {0, 0, 0};
-    int l_nc[3] = {0, 0, 0}, r_nc[3] = {0, 0, 0};
+    Int3 l_cc = {0, 0, 0}, r_cc = {0, 0, 0};
+    Int3 l_nc = {0, 0, 0}, r_nc = {0, 0, 0};
     for (int d = 0; d < 3; d++) {
       if (grid.bc.fld_lo[d] == BND_FLD_CONDUCTING_WALL &&
           grid.atBoundaryLo(p, d)) {
@@ -80,43 +61,42 @@ inline void correct(MfieldsState& mflds, Mfields& mf,
     auto flds_ = make_Fields3d<dim_xyz>(mflds[p]);
     auto f_ = make_Fields3d<dim_xyz>(mf[p]);
     if (!grid.isInvar(0)) {
-      int l[3] = {l_cc[0], l_nc[1], l_nc[2]};
-      int r[3] = {r_cc[0], r_nc[1], r_nc[2]};
-      psc_foreach_3d_more(ppsc, p, ix, iy, iz, l, r)
-      {
-        flds_(EX, ix, iy, iz) += (f_(0, ix + dx, iy, iz) - f_(0, ix, iy, iz)) *
-                                 .5 * grid.dt * diffusion / deltax;
-      }
-      psc_foreach_3d_more_end;
+      Int3 l = -Int3{l_cc[0], l_nc[1], l_nc[2]} - ib;
+      Int3 r = -Int3{r_cc[0], r_nc[1], r_nc[2]} + ib;
+      auto ex = mflds.storage().view(_all, _all, _all, EX, p);
+      auto res = mf.storage().view(_all, _all, _all, 0, p);
+      ex.view(_s(l[0], r[0]), _s(l[1], r[1]), _s(l[2], r[2])) =
+        ex.view(_s(l[0], r[0]), _s(l[1], r[1]), _s(l[2], r[2])) +
+        (res.view(_s(l[0] + 1, r[0] + 1), _s(l[1], r[1]), _s(l[2], r[2])) -
+         res.view(_s(l[0], r[0]), _s(l[1], r[1]), _s(l[2], r[2]))) *
+          .5 * grid.dt * diffusion / deltax;
     }
 
     {
-      int l[3] = {l_nc[0], l_cc[1], l_nc[2]};
-      int r[3] = {r_nc[0], r_cc[1], r_nc[2]};
-      psc_foreach_3d_more(ppsc, p, ix, iy, iz, l, r)
-      {
-        flds_(EY, ix, iy, iz) += (f_(0, ix, iy + dy, iz) - f_(0, ix, iy, iz)) *
-                                 .5 * grid.dt * diffusion / deltay;
-      }
-      psc_foreach_3d_more_end;
+      Int3 l = -Int3{l_nc[0], l_cc[1], l_nc[2]} - ib;
+      Int3 r = -Int3{r_nc[0], r_cc[1], r_nc[2]} + ib;
+      auto ey = mflds.storage().view(_all, _all, _all, EY, p);
+      auto res = mf.storage().view(_all, _all, _all, 0, p);
+      ey.view(_s(l[0], r[0]), _s(l[1], r[1]), _s(l[2], r[2])) =
+        ey.view(_s(l[0], r[0]), _s(l[1], r[1]), _s(l[2], r[2])) +
+        (res.view(_s(l[0], r[0]), _s(l[1] + 1, r[1] + 1), _s(l[2], r[2])) -
+         res.view(_s(l[0], r[0]), _s(l[1], r[1]), _s(l[2], r[2]))) *
+          .5 * grid.dt * diffusion / deltay;
     }
 
     {
-      int l[3] = {l_nc[0], l_nc[1], l_cc[2]};
-      int r[3] = {r_nc[0], r_nc[1], r_cc[2]};
-      psc_foreach_3d_more(ppsc, p, ix, iy, iz, l, r)
-      {
-        flds_(EZ, ix, iy, iz) += (f_(0, ix, iy, iz + dz) - f_(0, ix, iy, iz)) *
-                                 .5 * grid.dt * diffusion / deltaz;
-      }
-      psc_foreach_3d_more_end;
+      Int3 l = -Int3{l_nc[0], l_nc[1], l_cc[2]} - ib;
+      Int3 r = -Int3{r_nc[0], r_nc[1], r_cc[2]} + ib;
+      auto ez = mflds.storage().view(_all, _all, _all, EZ, p);
+      auto res = mf.storage().view(_all, _all, _all, 0, p);
+      ez.view(_s(l[0], r[0]), _s(l[1], r[1]), _s(l[2], r[2])) =
+        ez.view(_s(l[0], r[0]), _s(l[1], r[1]), _s(l[2], r[2])) +
+        (res.view(_s(l[0], r[0]), _s(l[1], r[1]), _s(l[2] + 1, r[2] + 1)) -
+         res.view(_s(l[0], r[0]), _s(l[1], r[1]), _s(l[2], r[2]))) *
+          .5 * grid.dt * diffusion / deltaz;
     }
   }
 }
-
-#undef psc_foreach_3d_more
-#undef psc_foreach_3d_more_end
-#undef define_dxdydz
 
 #ifdef USE_CUDA
 
