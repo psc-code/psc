a501b264ef46f74ae616ea17bb8d99f12b6ebd03
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-31 13:54:37 -0500

    marder: no more marder_cuda_impl.{hxx,cxx}
    Though we still have some separate cuda/non-cuda code.

--- src/libpsc/psc_push_fields/marder_impl.hxx
+++ src/libpsc/psc_push_fields/marder_impl.hxx
@@ -12,16 +12,6 @@
 #include <gtensor/reductions.h>
 #include <mrc_io.h>
 
-#ifdef USE_CUDA
-void cuda_marder_correct_yz(MfieldsStateCuda::Storage& gt_mflds,
-                            MfieldsCuda::Storage& gt_mf, int p, Float3 fac,
-                            Int3 ly, Int3 ry, Int3 lz, Int3 rz);
-void cuda_marder_correct_xyz(MfieldsStateCuda::Storage& gt_mflds,
-                             MfieldsCuda::Storage& gt_mf, int p, Float3 fac,
-                             Int3 lx, Int3 rx, Int3 ly, Int3 ry, Int3 lz,
-                             Int3 rz);
-#endif
-
 namespace psc
 {
 namespace marder
@@ -120,6 +110,64 @@ inline void correct(const Grid_t& grid, S& mflds, const Int3& mflds_ib, S& mf,
 }
 
 #ifdef USE_CUDA
+
+inline void cuda_marder_correct_yz(MfieldsStateCuda::Storage& gt_mflds,
+                                   MfieldsCuda::Storage& gt_mf, int p,
+                                   Float3 fac, Int3 ly, Int3 ry, Int3 lz,
+                                   Int3 rz)
+{
+  auto gt_flds = gt_mflds.view(_all, _all, _all, _all, p).to_kernel();
+  auto gt_f = gt_mf.view(_all, _all, _all, 0, p).to_kernel();
+  gt::launch<2>(
+    {gt_flds.shape(1), gt_flds.shape(2)}, GT_LAMBDA(int iy, int iz) {
+      if ((iy >= ly[1] && iy < ry[1]) && (iz >= ly[2] && iz < ry[2])) {
+        gt_flds(0, iy, iz, EY) =
+          gt_flds(0, iy, iz, EY) +
+          fac[1] * (gt_f(0, iy + 1, iz) - gt_f(0, iy, iz));
+      }
+
+      if ((iy >= lz[1] && iy < rz[1]) && (iz >= lz[2] && iz < rz[2])) {
+        gt_flds(0, iy, iz, EZ) =
+          gt_flds(0, iy, iz, EZ) +
+          fac[2] * (gt_f(0, iy, iz + 1) - gt_f(0, iy, iz));
+      }
+    });
+  cuda_sync_if_enabled();
+}
+
+inline void cuda_marder_correct_xyz(MfieldsStateCuda::Storage& gt_mflds,
+                                    MfieldsCuda::Storage& gt_mf, int p,
+                                    Float3 fac, Int3 lx, Int3 rx, Int3 ly,
+                                    Int3 ry, Int3 lz, Int3 rz)
+{
+  auto gt_flds = gt_mflds.view(_all, _all, _all, _all, p).to_kernel();
+  auto gt_f = gt_mf.view(_all, _all, _all, 0, p).to_kernel();
+  gt::launch<3>(
+    {gt_flds.shape(0), gt_flds.shape(1), gt_flds.shape(2)},
+    GT_LAMBDA(int ix, int iy, int iz) {
+      if ((ix >= lx[0] && ix < rx[0]) && (iy >= lx[1] && iy < rx[1]) &&
+          (iz >= lx[2] && iz < rx[2])) {
+        gt_flds(ix, iy, iz, EX) =
+          gt_flds(ix, iy, iz, EX) +
+          fac[0] * (gt_f(ix, iy + 1, iz) - gt_f(ix, iy, iz));
+      }
+
+      if ((ix >= ly[0] && ix < ry[0]) && (iy >= ly[1] && iy < ry[1]) &&
+          (iz >= ly[2] && iz < ry[2])) {
+        gt_flds(ix, iy, iz, EY) =
+          gt_flds(ix, iy, iz, EY) +
+          fac[1] * (gt_f(ix, iy + 1, iz) - gt_f(ix, iy, iz));
+      }
+
+      if ((ix >= lz[0] && ix < rz[0]) && (iy >= lz[1] && iy < rz[1]) &&
+          (iz >= lz[2] && iz < rz[2])) {
+        gt_flds(ix, iy, iz, EZ) =
+          gt_flds(ix, iy, iz, EZ) +
+          fac[2] * (gt_f(ix, iy, iz + 1) - gt_f(ix, iy, iz));
+      }
+    });
+  cuda_sync_if_enabled();
+}
 
 inline void correct(const Grid_t& grid, MfieldsCuda::Storage& mflds,
                     const Int3& mflds_ib, MfieldsCuda::Storage& mf,
@@ -277,3 +325,14 @@ public:
 template <typename MP, typename MFS, typename MF, typename D>
 using Marder_ =
   MarderCommon<MP, MFS, D, Moment_rho_1st_nc<typename MF::Storage, D>, Bnd_>;
+
+#ifdef USE_CUDA
+
+#include "psc_particles_single.h"
+#include "mparticles_cuda.hxx"
+#include "fields_item_moments_1st_cuda.hxx"
+
+template <typename BS, typename D>
+using MarderCuda = MarderCommon<MparticlesCuda<BS>, MfieldsStateCuda, D,
+                                Moment_rho_1st_nc_cuda<D>, BndCuda3>;
+#endif
