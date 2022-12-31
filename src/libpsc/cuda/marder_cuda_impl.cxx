119d91af53028651c5b2cbe5dda5cf7d4ed9298f
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-30 23:21:18 -0500

    marder: mv sign of l negation out of kernel

--- src/libpsc/cuda/marder_cuda_impl.cxx
+++ src/libpsc/cuda/marder_cuda_impl.cxx
@@ -11,15 +11,15 @@ void cuda_marder_correct_yz(MfieldsStateCuda::Storage& gt_mflds,
   auto gt_f = gt_mf.view(_all, _all, _all, 0, p).to_kernel();
   gt::launch<2>(
     {gt_flds.shape(1), gt_flds.shape(2)}, GT_LAMBDA(int iy, int iz) {
-      if (iy - BND >= -ly[1] && iy - BND < ry[1] && iz - BND >= -ly[2] &&
-          iz - BND < ry[2]) {
+      if ((iy - BND >= ly[1] && iy - BND < ry[1]) &&
+          (iz - BND >= ly[2] && iz - BND < ry[2])) {
         gt_flds(0, iy, iz, EY) =
           gt_flds(0, iy, iz, EY) +
           fac[1] * (gt_f(0, iy + 1, iz) - gt_f(0, iy, iz));
       }
 
-      if (iy - BND >= -lz[1] && iy - BND < rz[1] && iz - BND >= -lz[2] &&
-          iz - BND < rz[2]) {
+      if ((iy - BND >= lz[1] && iy - BND < rz[1]) &&
+          (iz - BND >= lz[2] && iz - BND < rz[2])) {
         gt_flds(0, iy, iz, EZ) =
           gt_flds(0, iy, iz, EZ) +
           fac[2] * (gt_f(0, iy, iz + 1) - gt_f(0, iy, iz));
@@ -38,25 +38,25 @@ void cuda_marder_correct_xyz(MfieldsStateCuda::Storage& gt_mflds,
   gt::launch<3>(
     {gt_flds.shape(0), gt_flds.shape(1), gt_flds.shape(2)},
     GT_LAMBDA(int ix, int iy, int iz) {
-      if ((ix - BND >= -lx[0] && ix - BND < rx[0]) &&
-          (iy - BND >= -lx[1] && iy - BND < rx[1]) &&
-          (iz - BND >= -lx[2] && iz - BND < rx[2])) {
+      if ((ix - BND >= lx[0] && ix - BND < rx[0]) &&
+          (iy - BND >= lx[1] && iy - BND < rx[1]) &&
+          (iz - BND >= lx[2] && iz - BND < rx[2])) {
         gt_flds(ix, iy, iz, EX) =
           gt_flds(ix, iy, iz, EX) +
           fac[0] * (gt_f(ix, iy + 1, iz) - gt_f(ix, iy, iz));
       }
 
-      if ((ix - BND >= -ly[0] && ix - BND < ry[0]) &&
-          (iy - BND >= -ly[1] && iy - BND < ry[1]) &&
-          (iz - BND >= -ly[2] && iz - BND < ry[2])) {
+      if ((ix - BND >= ly[0] && ix - BND < ry[0]) &&
+          (iy - BND >= ly[1] && iy - BND < ry[1]) &&
+          (iz - BND >= ly[2] && iz - BND < ry[2])) {
         gt_flds(ix, iy, iz, EY) =
           gt_flds(ix, iy, iz, EY) +
           fac[1] * (gt_f(ix, iy + 1, iz) - gt_f(ix, iy, iz));
       }
 
-      if ((ix - BND >= -lz[0] && ix - BND < rz[0]) &&
-          (iy - BND >= -lz[1] && iy - BND < rz[1]) &&
-          (iz - BND >= -lz[2] && iz - BND < rz[2])) {
+      if ((ix - BND >= lz[0] && ix - BND < rz[0]) &&
+          (iy - BND >= lz[1] && iy - BND < rz[1]) &&
+          (iz - BND >= lz[2] && iz - BND < rz[2])) {
         gt_flds(ix, iy, iz, EZ) =
           gt_flds(ix, iy, iz, EZ) +
           fac[2] * (gt_f(ix, iy, iz + 1) - gt_f(ix, iy, iz));
