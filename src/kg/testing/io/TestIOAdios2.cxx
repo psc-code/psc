97e84ada7e6c81545c8ef62511717a60ab64f3dc
liangwang0734 <liang.wang.phys@gmail.com>
2023-06-30 23:29:49 -0400

    TestIOAdios2: add beginStep/endStep pairs to support bp5

--- src/kg/testing/io/TestIOAdios2.cxx
+++ src/kg/testing/io/TestIOAdios2.cxx
@@ -34,6 +34,7 @@ TEST(IOAdios2, FilePutGetVariable)
   auto io = kg::io::IOAdios2{};
   {
     auto file = io.openFile("test3.bp", kg::io::Mode::Write);
+    file.beginStep(kg::io::StepMode::Append);
     auto dbl = std::vector<double>{1., 2., 3., 4., 5.};
     file.putVariable("dbl", dbl.data(), kg::io::Mode::NonBlocking, {5},
                      {{0}, {5}}, {});
@@ -42,9 +43,11 @@ TEST(IOAdios2, FilePutGetVariable)
     file.putVariable("dbl2", dbl.data() + 3, kg::io::Mode::NonBlocking, {5},
                      {{3}, {2}}, {});
     file.performPuts();
+    file.endStep();
   }
   {
     auto file = io.openFile("test3.bp", kg::io::Mode::Read);
+    file.beginStep(kg::io::StepMode::Read);
 
     auto shape = file.shapeVariable("dbl");
     EXPECT_EQ(shape, kg::io::Dims{5});
@@ -58,6 +61,7 @@ TEST(IOAdios2, FilePutGetVariable)
     file.getVariable("dbl2", dbl2.data(), kg::io::Mode::NonBlocking, {{0}, {5}},
                      {});
     file.performGets();
+    file.endStep();
 
     EXPECT_EQ(dbl, (std::vector<double>{1., 2., 3., 4., 5.}));
     EXPECT_EQ(dbl2, (std::vector<double>{1., 2., 0., 4., 5.}));
@@ -69,15 +73,19 @@ TEST(IOAdios2, FilePutGetAttribute)
   auto io = kg::io::IOAdios2{};
   {
     auto file = io.openFile("test4.bp", kg::io::Mode::Write);
+    file.beginStep(kg::io::StepMode::Append);
     auto dbl = std::vector<double>{1., 2., 3., 4., 5.};
     file.putAttribute("attr_dbl", dbl.data(), dbl.size());
+    file.endStep();
   }
   {
     auto file = io.openFile("test4.bp", kg::io::Mode::Read);
+    file.beginStep(kg::io::StepMode::Read);
     auto size = file.sizeAttribute("attr_dbl");
     auto dbl = std::vector<double>(size);
     file.getAttribute("attr_dbl", dbl.data());
     EXPECT_EQ(dbl, (std::vector<double>{1., 2., 3., 4., 5.}));
+    file.endStep();
   }
 }
 
