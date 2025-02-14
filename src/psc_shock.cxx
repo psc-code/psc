e2bdf69f0312596112dbed04ab3b36706bde767b
James McClung <james@themcclungs.net>
2025-02-14 12:05:43 -0500

    +psc_shock
    currently just a maxwellian

---
+++ src/psc_shock.cxx
@@ -0,0 +1,258 @@
+#include <psc.hxx>
+#include <setup_fields.hxx>
+#include <setup_particles.hxx>
+
+#include "DiagnosticsDefault.h"
+#include "OutputFieldsDefault.h"
+#include "psc_config.hxx"
+
+#include "psc_bgk_util/bgk_params.hxx"
+#include "psc_bgk_util/table.hxx"
+#include "psc_bgk_util/params_parser.hxx"
+
+// ======================================================================
+// PSC configuration
+//
+// This sets up compile-time configuration for the code, in particular
+// what data structures and algorithms to use
+//
+// EDIT to change order / floating point type / cuda / 2d/3d
+
+using Dim = dim_yz;
+#ifdef USE_CUDA
+using PscConfig = PscConfig1vbecCuda<Dim>;
+#else
+using PscConfig = PscConfig1vbecDouble<Dim>;
+#endif
+
+// ----------------------------------------------------------------------
+
+using BgkMfields = PscConfig::Mfields;
+using MfieldsState = PscConfig::MfieldsState;
+using Mparticles = PscConfig::Mparticles;
+using Balance = PscConfig::Balance;
+using Collision = PscConfig::Collision;
+using Checks = PscConfig::Checks;
+using Marder = PscConfig::Marder;
+using OutputParticles = PscConfig::OutputParticles;
+
+// ======================================================================
+// Global parameters
+
+namespace
+{
+// General PSC parameters
+PscParams psc_params;
+
+double v_thermal = 1.0;
+double box_size = 1.0;
+} // namespace
+
+// ======================================================================
+// setupParameters
+
+void setupParameters(int argc, char** argv)
+{
+  psc_params.nmax = 100;
+  psc_params.stats_every = 10;
+  psc_params.cfl = .75;
+
+  psc_params.write_checkpoint_every_step = 0;
+}
+
+// ======================================================================
+// setupGrid
+//
+// This helper function is responsible for setting up the "Grid",
+// which is really more than just the domain and its decomposition, it
+// also encompasses PC normalization parameters, information about the
+// particle kinds, etc.
+
+Grid_t* setupGrid()
+{
+  // FIXME add a check to catch mismatch between Dim and n grid points early
+  auto domain = Grid_t::Domain{
+    {1, 16, 16},                                         // n grid points
+    {box_size, box_size, box_size},                      // physical lengths
+    {-box_size / 2.0, -box_size / 2.0, -box_size / 2.0}, // origin offset
+    {1, 1, 1}};                                          // n patches
+
+  auto bc =
+    psc::grid::BC{{BND_FLD_PERIODIC, BND_FLD_PERIODIC, BND_FLD_PERIODIC},
+                  {BND_FLD_PERIODIC, BND_FLD_PERIODIC, BND_FLD_PERIODIC},
+                  {BND_PRT_PERIODIC, BND_PRT_PERIODIC, BND_PRT_PERIODIC},
+                  {BND_PRT_PERIODIC, BND_PRT_PERIODIC, BND_PRT_PERIODIC}};
+
+  auto kinds = Grid_t::Kinds(NR_KINDS);
+  kinds[KIND_ELECTRON] = {-1.0, 1.0, "e"};
+  kinds[KIND_ION] = {1.0, 1.0, "i"};
+
+  // --- generic setup
+  auto norm_params = Grid_t::NormalizationParams::dimensionless();
+  norm_params.nicell = 100;
+
+  double dt = psc_params.cfl * courant_length(domain);
+  Grid_t::Normalization norm{norm_params};
+
+  Int3 ibn = {2, 2, 2};
+  if (Dim::InvarX::value) {
+    ibn[0] = 0;
+  }
+  if (Dim::InvarY::value) {
+    ibn[1] = 0;
+  }
+  if (Dim::InvarZ::value) {
+    ibn[2] = 0;
+  }
+
+  return new Grid_t{domain, bc, kinds, norm, dt, -1, ibn};
+}
+
+// ======================================================================
+// initializeParticles
+
+void initializeParticles(Balance& balance, Grid_t*& grid_ptr, Mparticles& mprts)
+{
+  SetupParticles<Mparticles> setup_particles(*grid_ptr);
+  setup_particles.centerer = Centering::Centerer(Centering::NC);
+
+  auto init_np = [&](int kind, Double3 crd, int p, Int3 idx,
+                     psc_particle_np& np) {
+    double temperature = sqr(v_thermal);
+
+    np.n = 1.0;
+    np.p =
+      setup_particles.createMaxwellian({np.kind,
+                                        np.n,
+                                        {0, 0, 0},
+                                        {temperature, temperature, temperature},
+                                        np.tag});
+  };
+
+  partitionAndSetupParticles(setup_particles, balance, grid_ptr, mprts,
+                             init_np);
+}
+
+// ======================================================================
+// fillGhosts
+
+template <typename MF>
+void fillGhosts(MF& mfld, int compBegin, int compEnd)
+{
+  Bnd_ bnd{};
+  bnd.fill_ghosts(mfld, compBegin, compEnd);
+}
+
+// ======================================================================
+// initializeFields
+
+void initializeFields(MfieldsState& mflds)
+{
+  setupFields(mflds, [&](int component, double coords[3]) { return 0.0; });
+}
+
+// ======================================================================
+// run
+
+static void run(int argc, char** argv)
+{
+  mpi_printf(MPI_COMM_WORLD, "*** Setting up...\n");
+
+  // ----------------------------------------------------------------------
+  // setup various parameters first
+
+  setupParameters(argc, argv);
+
+  // ----------------------------------------------------------------------
+  // Set up grid, state fields, particles
+
+  auto grid_ptr = setupGrid();
+  auto& grid = *grid_ptr;
+  MfieldsState mflds{grid};
+  Mparticles mprts{grid};
+  BgkMfields phi{grid, 1, mflds.ibn()};
+  BgkMfields gradPhi{grid, 3, mflds.ibn()};
+  BgkMfields divGradPhi{grid, 1, mflds.ibn()};
+
+  // ----------------------------------------------------------------------
+  // Set up various objects needed to run this case
+
+  // -- Balance
+  psc_params.balance_interval = 0;
+  Balance balance{.1};
+
+  // -- Sort
+  psc_params.sort_interval = 10;
+
+  // -- Collision
+  int collision_interval = 0;
+  double collision_nu = .1;
+  Collision collision{grid, collision_interval, collision_nu};
+
+  // -- Checks
+  ChecksParams checks_params{};
+  checks_params.gauss_every_step = 50;
+  // checks_params.gauss_dump_always = true;
+  checks_params.gauss_threshold = 1e-5;
+
+  Checks checks{grid, MPI_COMM_WORLD, checks_params};
+
+  // -- Marder correction
+  double marder_diffusion = 0.9;
+  int marder_loop = 3;
+  bool marder_dump = false;
+  psc_params.marder_interval = 5;
+  Marder marder(grid, marder_diffusion, marder_loop, marder_dump);
+
+  // ----------------------------------------------------------------------
+  // Set up output
+
+  // -- output fields
+  OutputFieldsParams outf_params{};
+  outf_params.fields.pfield.out_interval = 1;
+  outf_params.moments.pfield.out_interval = 1;
+  OutputFields<MfieldsState, Mparticles, Dim> outf{grid, outf_params};
+
+  // -- output particles
+  OutputParticlesParams outp_params{};
+  outp_params.every_step = 1;
+  outp_params.data_dir = ".";
+  outp_params.basename = "prt";
+  OutputParticles outp{grid, outp_params};
+
+  int oute_interval = -100;
+  DiagEnergies oute{grid.comm(), oute_interval};
+
+  auto diagnostics = makeDiagnosticsDefault(outf, outp, oute);
+
+  // ----------------------------------------------------------------------
+  // set up initial conditions
+
+  initializeParticles(balance, grid_ptr, mprts);
+  initializeFields(mflds);
+
+  // ----------------------------------------------------------------------
+  // run the simulation
+
+  auto psc =
+    makePscIntegrator<PscConfig>(psc_params, *grid_ptr, mflds, mprts, balance,
+                                 collision, checks, marder, diagnostics);
+
+  psc.integrate();
+}
+
+// ======================================================================
+// main
+
+int main(int argc, char** argv)
+{
+  // psc_init(argc, argv);
+  // FIXME restore whatever previous functionality there was with options
+  int temp = 1;
+  psc_init(temp, argv);
+
+  run(argc, argv);
+
+  psc_finalize();
+  return 0;
+}
