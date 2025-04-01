86a50731be6f27497794d8ef1af0bacce5bb4032
James McClung <james@themcclungs.net>
2025-04-01 12:28:53 -0400

    output_particles_hdf5_impl: drop the _p000000
    Since we're breaking the API anyways...

--- src/libpsc/psc_output_particles/output_particles_hdf5_impl.hxx
+++ src/libpsc/psc_output_particles/output_particles_hdf5_impl.hxx
@@ -153,18 +153,18 @@ public:
     // FIXME there must be a safer way to do this.
 
     // Currently, if timestep has too many digits, the filename string gets
-    // truncated to e.g. prt.1234567890_p000000.h (with .h instead of .h5)
+    // truncated to e.g. prt.1234567890.h (with .h instead of .h5)
 
     // This is probably a problem with other outputs, too
-    int slen = strlen(params_.data_dir) + strlen(params_.basename) + 23;
+    int slen = strlen(params_.data_dir) + strlen(params_.basename) + 15;
     char filename[slen];
     if (timestep > 999999999) {
       LOG_WARN("%s step index exceeds digit limit in file name. Data still "
                "written, but file name is truncated.\n",
                params_.basename);
     }
-    snprintf(filename, slen, "%s/%s.%09d_p%06d.h5", params_.data_dir,
-             params_.basename, timestep, 0);
+    snprintf(filename, slen, "%s/%s.%09d.h5", params_.data_dir,
+             params_.basename, timestep);
 
     hid_t file = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, plist);
     H5_CHK(file);
