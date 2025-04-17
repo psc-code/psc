e035122fb743ae4dc284a9877348d9c20b745e8d
James McClung <james@themcclungs.net>
2025-04-17 17:13:33 -0400

    add_ghosts_reflecting: generalize
    revert this if it's horribly inefficient

--- src/include/add_ghosts_reflecting.hxx
+++ src/include/add_ghosts_reflecting.hxx
@@ -43,31 +43,29 @@ void add_ghosts_reflecting_hi(const Int3& ldims, FE& mres_gt, const Int3& ib,
 {
   // FIXME only need to scan 1 cell into the ghost region for 1st-order moments
 
-  if (d == 1) {
-    for (int m = mb; m < me; m++) {
-      for (int iz = ib[2]; iz < ldims[2] - ib[2]; iz++) {
-        for (int iy = ldims[1] + ib[1]; iy < ldims[1]; iy++) {
-          for (int ix = ib[0]; ix < ldims[0] - ib[0]; ix++) {
-            int iy_reflected = 2 * ldims[1] - iy - 1;
-            mres_gt(ix - ib[0], iy - ib[1], iz - ib[2], m, p) +=
-              mres_gt(ix - ib[0], iy_reflected - ib[1], iz - ib[2], m, p);
-          }
+  Int3 idx_end = ldims - ib;
+  Int3 idx_begin = ib;
+
+  idx_begin[d] = ldims[d] + ib[d];
+  idx_end[d] = ldims[d];
+
+  Int3 idx;
+
+  for (int m = mb; m < me; m++) {
+    for (idx[2] = idx_begin[2]; idx[2] < idx_end[2]; idx[2]++) {
+      for (idx[1] = idx_begin[1]; idx[1] < idx_end[1]; idx[1]++) {
+        for (idx[0] = idx_begin[0]; idx[0] < idx_end[0]; idx[0]++) {
+          Int3 idx_reflected = idx;
+          idx_reflected[d] = 2 * ldims[d] - idx[d] - 1;
+
+          auto val_reflected =
+            mres_gt(idx_reflected[0] - ib[0], idx_reflected[1] - ib[1],
+                    idx_reflected[2] - ib[2], m, p);
+
+          mres_gt(idx[0] - ib[0], idx[1] - ib[1], idx[2] - ib[2], m, p) +=
+            val_reflected;
         }
       }
     }
-  } else if (d == 2) {
-    for (int m = mb; m < me; m++) {
-      for (int iz = ldims[2] + ib[2]; iz < ldims[2]; iz++) {
-        for (int iy = ib[1]; iy < ldims[1] - ib[1]; iy++) {
-          for (int ix = ib[0]; ix < ldims[0] - ib[0]; ix++) {
-            int iz_reflected = 2 * ldims[2] - iz - 1;
-            mres_gt(ix - ib[0], iy - ib[1], iz - ib[2], m, p) +=
-              mres_gt(ix - ib[0], iy - ib[1], iz_reflected - ib[2], m, p);
-          }
-        }
-      }
-    }
-  } else {
-    assert(0);
   }
 }
