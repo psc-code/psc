183778807e686a25e892c1804bd3f78d9797ea11
James McClung <james@themcclungs.net>
2025-05-02 13:49:42 -0400

    fields_item:+DomainBoundary

--- src/include/fields_item.hxx
+++ src/include/fields_item.hxx
@@ -33,30 +33,12 @@ inline std::vector<std::string> addKindSuffix(
 // ======================================================================
 // ItemMomentBnd
 
-template <typename S, typename Bnd>
-class ItemMomentBnd
+class DomainBoundary
 {
 public:
-  using storage_type = S;
-
-  ItemMomentBnd(const Grid_t& grid) {}
-
-  void add_ghosts(const Grid_t& grid, storage_type& mres_gt, const Int3& ib)
-  {
-    for (int p = 0; p < mres_gt.shape(4); p++) {
-      add_ghosts_boundary(grid, mres_gt, ib, p, 0, mres_gt.shape(3));
-    }
-
-    bnd_.add_ghosts(grid, mres_gt, ib, 0, mres_gt.shape(3));
-  }
-
-private:
-  // ----------------------------------------------------------------------
-  // boundary stuff FIXME, should go elsewhere...
-
   template <typename FE>
-  void add_ghosts_boundary(const Grid_t& grid, FE& mres_gt, const Int3& ib,
-                           int p, int mb, int me)
+  static void add_ghosts_boundary(const Grid_t& grid, FE& mres_gt,
+                                  const Int3& ib, int p, int mb, int me)
   {
     // lo
     for (int d = 0; d < 3; d++) {
@@ -74,6 +56,28 @@ private:
         add_ghosts_reflecting_hi_cc(grid.ldims, mres_gt, ib, p, d, mb, me);
       }
     }
+  }
+};
+
+// ======================================================================
+// ItemMomentBnd
+
+template <typename S, typename Bnd>
+class ItemMomentBnd
+{
+public:
+  using storage_type = S;
+
+  ItemMomentBnd(const Grid_t& grid) {}
+
+  void add_ghosts(const Grid_t& grid, storage_type& mres_gt, const Int3& ib)
+  {
+    for (int p = 0; p < mres_gt.shape(4); p++) {
+      DomainBoundary::add_ghosts_boundary(grid, mres_gt, ib, p, 0,
+                                          mres_gt.shape(3));
+    }
+
+    bnd_.add_ghosts(grid, mres_gt, ib, 0, mres_gt.shape(3));
   }
 
 private:
