deec837a177db4edba2a0120ad2401a98c26669d
James McClung <james@themcclungs.net>
2025-05-16 11:17:15 -0400

    injector_boundary_inflow; test: pass w too

--- src/libpsc/tests/test_injector_boundary_inflow.cxx
+++ src/libpsc/tests/test_injector_boundary_inflow.cxx
@@ -8,14 +8,16 @@ TEST(InjectorBoundaryInflowTest, ParticleGeneratorMaxwellianTest)
 {
   int kind_idx = 0;
   Grid_t::Kind kind{1.0, 1836.0, "ion"};
+  ParticleGeneratorMaxwellian::Real w = 2.0;
   ParticleGeneratorMaxwellian::Real3 mean_u{0.0, 5.0, 15.0};
   ParticleGeneratorMaxwellian::Real3 temperature{0.0, 0.0, 0.0};
 
-  ParticleGeneratorMaxwellian gen{kind_idx, kind, mean_u, temperature};
+  ParticleGeneratorMaxwellian gen{kind_idx, kind, w, mean_u, temperature};
 
   auto prt = gen.get();
 
   ASSERT_EQ(prt.kind, kind_idx);
+  ASSERT_EQ(prt.w, w);
   ASSERT_EQ(prt.tag, 0);
   ASSERT_EQ(prt.u, mean_u); // zero temperature => exact velocity
   ASSERT_EQ(prt.x, psc::particle::Inject::Real3(0.0, 0.0, 0.0)); // set x later
