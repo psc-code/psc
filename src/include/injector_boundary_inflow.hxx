397c67740e7574dbbe6498ea31089129cafb96a9
James McClung <james@themcclungs.net>
2025-05-16 14:12:39 -0400

    injector_boundary_inflow: pass grid, do injection

--- src/include/injector_boundary_inflow.hxx
+++ src/include/injector_boundary_inflow.hxx
@@ -5,6 +5,8 @@
 #include "grid.hxx"
 #include "rng.hxx"
 #include "particle.h"
+#include "pushp.hxx"
+#include "dim.hxx"
 
 class ParticleGeneratorMaxwellian
 {
@@ -47,6 +49,8 @@ private:
 template <typename PARTICLE_GENERATOR, typename PUSH_PARTICLES>
 class InjectorBoundaryInflow
 {
+  static const int INJECT_DIM_IDX_ = 1;
+
 public:
   using ParticleGenerator = PARTICLE_GENERATOR;
   using PushParticles = PUSH_PARTICLES;
@@ -57,23 +61,67 @@ public:
   using InterpolateEM_t = typename PushParticles::InterpolateEM_t;
   using Current = typename PushParticles::Current;
   using Dim = typename PushParticles::Dim;
-  using Real = typename PushParticles::real_t;
+  using real_t = typename PushParticles::real_t;
   using Real3 = typename PushParticles::Real3;
   using checks_order = typename PushParticles::checks_order;
 
-  InjectorBoundaryInflow(ParticleGenerator particle_generator)
-    : partice_generator{particle_generator}
+  InjectorBoundaryInflow(ParticleGenerator particle_generator, Grid_t& grid)
+    : partice_generator{particle_generator},
+      advance{grid.dt},
+      prts_per_cell{grid.norm.prts_per_unit_density}
   {}
 
   void operator()(Mparticles& mprts, MfieldsState& mflds)
   {
+    const Grid_t& grid = mprts.grid();
+    auto injectors_by_patch = mprts.injector();
+
+    for (int patch_idx = 0; patch_idx < grid.n_patches(); patch_idx++) {
+      const auto& patch = grid.patches[patch_idx];
+
+      if (patch.off[INJECT_DIM_IDX_] != 0) {
+        continue;
+      }
+
+      auto&& injector = injectors_by_patch[patch_idx];
+
+      Int3 ilo = patch.off;
+      Int3 ihi = ilo + grid.ldims;
+
+      assert(INJECT_DIM_IDX_ == 1);
+      assert(ilo[INJECT_DIM_IDX_] == 0);
+
+      for (Int3 cell_idx = ilo; cell_idx[0] < ihi[0]; cell_idx[0]++) {
+        for (cell_idx[2] = ilo[2]; cell_idx[2] < ihi[2]; cell_idx[2]++) {
+          assert(cell_idx[INJECT_DIM_IDX_] == 0);
+          cell_idx[INJECT_DIM_IDX_] = -1;
+
+          for (int prt_count = 0; prt_count < prts_per_cell; prt_count++) {
+            Real3 min_pos =
+              grid.domain.corner + Double3(cell_idx) * grid.domain.dx;
+
+            psc::particle::Inject prt =
+              partice_generator.get(min_pos, grid.domain.dx);
+
+            Real3 v = advance.calc_v(prt.u);
+            advance.push_x(prt.x, v, 1.0);
+
+            if (prt.x[INJECT_DIM_IDX_] < grid.domain.corner[INJECT_DIM_IDX_]) {
+              continue;
+            }
+
+            injector(prt);
+          }
+        }
+      }
+    }
+
     // TODO:
-    // 1. inject particles
-    // 2. advance injected particles
-    // 3. cull injected particles that don't enter the domain
     // 4. update current
   }
 
 private:
   ParticleGenerator partice_generator;
+  AdvanceParticle<real_t, dim_y> advance;
+  real_t prts_per_cell;
 };
