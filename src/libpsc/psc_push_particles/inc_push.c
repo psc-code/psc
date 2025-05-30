a0678c9f336b7f44061ab6dd060a3f292490a356
James McClung <james@themcclungs.net>
2025-05-30 11:03:26 -0400

    inc_push: fix: pass by reference

--- src/libpsc/psc_push_particles/inc_push.c
+++ src/libpsc/psc_push_particles/inc_push.c
@@ -6,8 +6,8 @@
 #include "pushp.hxx"
 
 template <typename real_t, typename Real3 = Vec3<real_t>>
-void find_idx_pos_1st_rel(Real3 final_pos, Real3 dxi, Int3 final_index,
-                          Real3 final_pos_normalized, real_t shift)
+void find_idx_pos_1st_rel(Real3 final_pos, Real3 dxi, Int3& final_index,
+                          Real3& final_pos_normalized, real_t shift)
 {
   for (int d = 0; d < 3; d++) {
     final_pos_normalized[d] = final_pos[d] * dxi[d] + shift;
