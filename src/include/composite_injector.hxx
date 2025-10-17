b98144e4eda1e7b728a6f0e1af51d0e9f4d4da1c
James McClung <james@themcclungs.net>
2025-10-17 17:02:57 -0400

    composite_injector: +make_composite

--- src/include/composite_injector.hxx
+++ src/include/composite_injector.hxx
@@ -28,3 +28,20 @@ private:
   INJECTOR_1 injector_1;
   INJECTOR_2 injector_2;
 };
+
+/// @brief Helper method to deduce the type of a composite injector, enabling
+/// e.g. `auto composite_injector = make_composite(injector_1, injector_2);`
+///
+/// Wouldn't be necessary in C++17; see
+/// https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rt-deduce.
+/// @tparam INJECTOR_1 first type of injector
+/// @tparam INJECTOR_2 second type of injector
+/// @param injector_1 first injector
+/// @param injector_2 second injector
+/// @return
+template <typename INJECTOR_1, typename INJECTOR_2>
+CompositeInjector<INJECTOR_1, INJECTOR_2> make_composite(INJECTOR_1 injector_1,
+                                                         INJECTOR_2 injector_2)
+{
+  return CompositeInjector<INJECTOR_1, INJECTOR_2>(injector_1, injector_2);
+}
