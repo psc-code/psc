2db0ab4c7a1b585e29964fb647a2e51ae516098f
James McClung <james@themcclungs.net>
2026-01-22 09:48:40 -0500

    boundary_injector: use VecRange
    and maybe fix bounds

--- src/include/boundary_injector.hxx
+++ src/include/boundary_injector.hxx
@@ -9,6 +9,7 @@
 #include "pushp.hxx"
 #include "dim.hxx"
 #include "setup_particles.hxx"
+#include "kg/VecRange.hxx"
 #include "../libpsc/psc_push_particles/inc_push.cxx"
 
 /// @brief A particle generator for use with @ref BoundaryInjector. Samples
@@ -99,59 +100,55 @@ public:
     Current current(grid);
 
     for (int patch_idx = 0; patch_idx < grid.n_patches(); patch_idx++) {
-      Int3 ilo = grid.patches[patch_idx].off;
-
-      if (ilo[INJECT_DIM_IDX_] != 0) {
+      if (!grid.atBoundaryLo(patch_idx, INJECT_DIM_IDX_)) {
         continue;
       }
 
-      Int3 ihi = ilo + grid.ldims;
+      Int3 ilo = {0, 0, 0};
+      Int3 ihi = grid.ldims;
+
+      ilo[INJECT_DIM_IDX_] = -1;
+      ihi[INJECT_DIM_IDX_] = 0;
 
       auto&& injector = injectors_by_patch[patch_idx];
       auto flds = mflds[patch_idx];
       typename Current::fields_t J(flds);
 
-      for (Int3 initial_idx = ilo; initial_idx[0] < ihi[0]; initial_idx[0]++) {
-        for (initial_idx[2] = ilo[2]; initial_idx[2] < ihi[2];
-             initial_idx[2]++) {
-          initial_idx[INJECT_DIM_IDX_] = -1;
+      for (Int3 initial_idx : VecRange(ilo, ihi)) {
+        Real3 cell_corner =
+          grid.domain.corner + Double3(initial_idx) * grid.domain.dx;
+        int n_prts_to_try_inject =
+          get_n_in_cell(1.0, prts_per_unit_density, true);
 
-          Real3 cell_corner =
-            grid.domain.corner + Double3(initial_idx) * grid.domain.dx;
-          int n_prts_to_try_inject =
-            get_n_in_cell(1.0, prts_per_unit_density, true);
+        for (int prt_count = 0; prt_count < n_prts_to_try_inject; prt_count++) {
+          psc::particle::Inject prt =
+            partice_generator.get(cell_corner, grid.domain.dx);
 
-          for (int prt_count = 0; prt_count < n_prts_to_try_inject;
-               prt_count++) {
-            psc::particle::Inject prt =
-              partice_generator.get(cell_corner, grid.domain.dx);
+          Real3 v = advance.calc_v(prt.u);
+          Real3 initial_x = prt.x;
+          advance.push_x(prt.x, v, 1.0);
 
-            Real3 v = advance.calc_v(prt.u);
-            Real3 initial_x = prt.x;
-            advance.push_x(prt.x, v, 1.0);
-
-            if (prt.x[INJECT_DIM_IDX_] < grid.domain.corner[INJECT_DIM_IDX_]) {
-              // don't inject a particle that fails to enter the domain
-              continue;
-            }
-
-            injector(prt);
-
-            // Update currents
-            // Taken from push_particles_1vb.hxx PushParticlesVb::push_mprts()
-
-            Real3 initial_normalized_pos = initial_x * dxi;
-            // pretend it came from the edge to inject proper current
-            initial_normalized_pos[INJECT_DIM_IDX_] = -1.0;
-
-            Real3 final_normalized_pos = prt.x * dxi;
-            Int3 final_idx = final_normalized_pos.fint();
-
-            // CURRENT DENSITY BETWEEN (n+.5)*dt and (n+1.5)*dt
-            real_t qni_wni = grid.kinds[prt.kind].q * prt.w;
-            current.calc_j(J, initial_normalized_pos, final_normalized_pos,
-                           final_idx, initial_idx, qni_wni, v);
+          if (prt.x[INJECT_DIM_IDX_] < grid.domain.corner[INJECT_DIM_IDX_]) {
+            // don't inject a particle that fails to enter the domain
+            continue;
           }
+
+          injector(prt);
+
+          // Update currents
+          // Taken from push_particles_1vb.hxx PushParticlesVb::push_mprts()
+
+          Real3 initial_normalized_pos = initial_x * dxi;
+          // pretend it came from the edge to inject proper current
+          initial_normalized_pos[INJECT_DIM_IDX_] = -1.0;
+
+          Real3 final_normalized_pos = prt.x * dxi;
+          Int3 final_idx = final_normalized_pos.fint();
+
+          // CURRENT DENSITY BETWEEN (n+.5)*dt and (n+1.5)*dt
+          real_t qni_wni = grid.kinds[prt.kind].q * prt.w;
+          current.calc_j(J, initial_normalized_pos, final_normalized_pos,
+                         final_idx, initial_idx, qni_wni, v);
         }
       }
     }
