3931fad94fe6c21da695cace94daa37af00b84ae
James McClung <james@themcclungs.net>
2026-01-23 15:41:17 -0500

    +external_current_base

---
+++ src/include/external_current_base.hxx
@@ -0,0 +1,24 @@
+#pragma once
+
+template <typename MFIELDS_STATE>
+struct ExternalCurrentBase
+{
+  using MfieldsState = MFIELDS_STATE;
+
+  virtual void inject_current(MfieldsState& mflds) = 0;
+};
+
+template <typename MFIELDS_STATE>
+struct ExternalCurrentFromLambda : ExternalCurrentBase<MFIELDS_STATE>
+{
+  using MfieldsState = MFIELDS_STATE;
+
+  ExternalCurrentFromLambda(std::function<void(MfieldsState&)> lambda)
+    : lambda{lambda}
+  {}
+
+  void inject_current(MfieldsState& mflds) override { return lambda(mflds); }
+
+private:
+  std::function<void(MfieldsState&)> lambda;
+};
