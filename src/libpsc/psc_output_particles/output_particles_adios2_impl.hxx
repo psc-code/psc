92ba9ed970017ec7e587d7b748e0cbdde4eb7b81
James McClung <james@themcclungs.net>
2026-04-20 12:09:24 -0400

    output_particles_adios2_impl: +OutputParticlesAdios2Params
    configure which columns to write

--- src/libpsc/psc_output_particles/output_particles_adios2_impl.hxx
+++ src/libpsc/psc_output_particles/output_particles_adios2_impl.hxx
@@ -3,15 +3,30 @@
 #include "diagnostic_base.hxx"
 #include "writer_adios2.hxx"
 
+struct OutputParticlesAdios2Params : OutputParticlesParams
+{
+  bool write_x = true;
+  bool write_y = true;
+  bool write_z = true;
+  bool write_px = true;
+  bool write_py = true;
+  bool write_pz = true;
+  bool write_q = false;
+  bool write_m = false;
+  bool write_w = true;
+  bool write_id = false;
+  bool write_tag = false;
+};
+
 template <typename Mparticles>
 class OutputParticlesAdios2
   : OutputParticlesBase
   , public ParticleDiagnosticBase<Mparticles>
 {
-  static OutputParticlesParams adjust_params(
-    const OutputParticlesParams& params_in, const Grid_t& grid)
+  static OutputParticlesAdios2Params adjust_params(
+    const OutputParticlesAdios2Params& params_in, const Grid_t& grid)
   {
-    OutputParticlesParams params = params_in;
+    OutputParticlesAdios2Params params = params_in;
     for (int d = 0; d < 3; d++) {
       if (params.hi[d] == 0) {
         params.hi[d] = grid.domain.gdims[d];
@@ -23,7 +38,8 @@ class OutputParticlesAdios2
   }
 
 public:
-  OutputParticlesAdios2(const Grid_t& grid, const OutputParticlesParams& params)
+  OutputParticlesAdios2(const Grid_t& grid,
+                        const OutputParticlesAdios2Params& params)
     : params_{adjust_params(params, grid)}
   {}
 
@@ -38,6 +54,6 @@ public:
   void operator()(Mparticles& mprts) { this->perform_diagnostic(mprts); }
 
 private:
-  const OutputParticlesParams params_;
+  const OutputParticlesAdios2Params params_;
   WriterADIOS2 io_;
 };
