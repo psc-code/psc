de8dd855bf213da8ba8add5120fd6f91cf04680b
James McClung <james@themcclungs.net>
2026-04-20 16:07:44 -0400

    output_particles_adios2_impl: write x

--- src/libpsc/psc_output_particles/output_particles_adios2_impl.hxx
+++ src/libpsc/psc_output_particles/output_particles_adios2_impl.hxx
@@ -1,7 +1,8 @@
+#include <adios2.h>
+
 #include "grid.hxx"
 #include "output_particles.hxx"
 #include "diagnostic_base.hxx"
-#include "writer_adios2.hxx"
 
 struct OutputParticlesAdios2Params : OutputParticlesParams
 {
@@ -71,10 +72,75 @@ public:
 
   void perform_diagnostic(Mparticles& mprts) override
   {
-    if (!io_) {
-      io_.open(params_.basename, params_.data_dir);
+    const Grid_t& grid = mprts.grid();
+
+    if (!init_) {
+      init(grid);
     }
-    // TODO
+
+    //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\.
+    // count number of particles of each kind
+    int n_kinds = grid.kinds.size();
+    std::vector<unsigned long> local_n_prts_per_kind(n_kinds);
+
+    for (int p = 0; p < grid.n_patches(); p++) {
+      for (auto prt = mprts.begin(p); prt != mprts.end(p); prt++) {
+        local_n_prts_per_kind[prt->kind]++;
+      }
+    }
+    ///////////////////////////////////////////////
+
+    for (int kind_idx = 0; kind_idx < n_kinds; kind_idx++) {
+      Grid_t::Kind kind = grid.kinds[kind_idx];
+      unsigned long local_n_of_kind = local_n_prts_per_kind[kind_idx];
+
+      std::string file_name =
+        get_file_name(params_.basename, kind.name, grid.timestep());
+      auto engine = io_.Open(file_name, adios2::Mode::Write);
+      engine.BeginStep(adios2::StepMode::Append);
+
+      //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\.
+      // data must live until EndStep(), so must be scoped accordingly
+
+      std::vector<real_t> xs;
+
+      //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+      // reserve as needed
+
+      if (params_.write_x)
+        xs.reserve(local_n_of_kind);
+
+      //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+      // populate data
+
+      for (int p = 0; p < grid.n_patches(); p++) {
+        Grid_t::Patch patch = grid.patches[p];
+        for (auto prt = mprts.begin(p); prt != mprts.end(p); prt++) {
+          if (prt->kind != kind_idx)
+            continue;
+
+          if (params_.write_x)
+            xs.push_back(prt->x[0] + patch.xb[0]);
+        }
+      }
+
+      //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+      // schedule puts
+
+      if (params_.write_x) {
+        adios2::Variable<real_t> var = io_.InquireVariable<real_t>("x");
+        var.SetSelection({{}, {local_n_of_kind}});
+        engine.Put(var, xs.data(), adios2::Mode::Deferred);
+      }
+
+      //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+      // performs puts - data must live until this point
+
+      engine.EndStep();
+      engine.Close();
+
+      //////////////////////////////////////////////////////////////////////
+    }
   }
 
   void operator()(Mparticles& mprts) { this->perform_diagnostic(mprts); }
