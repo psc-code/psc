a279ce21650e6bb6415aaebc8ce9936cdce79c78
James McClung <james@themcclungs.net>
2026-04-28 15:27:02 -0400

    output_particles: default to nullptr
    This fixes a cursed bug wherein e.g. increasing the size of BndFields_
    (by adding _hi background constants) caused a segfault in the hdf5
    writer. Perhaps it only happened when using H5 prt output with BP
    params, which is what I was doing.
    
    On the bright side, I finally know how to use lldb/gdb.

--- src/include/output_particles.hxx
+++ src/include/output_particles.hxx
@@ -11,8 +11,8 @@ struct OutputParticlesParams
   Int3 lo;
   Int3 hi;
   bool use_independent_io;
-  const char* romio_cb_write;
-  const char* romio_ds_write;
+  const char* romio_cb_write = nullptr;
+  const char* romio_ds_write = nullptr;
 };
 
 // ======================================================================
