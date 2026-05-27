d60513f12232b6cab76f575811bee86331986d72
James McClung <james@themcclungs.net>
2026-05-27 15:13:47 -0400

    -cell_cbe

--- src/libpsc/cell_be/cbe_push_part_xz.c
+++
@@ -1,5 +0,0 @@
-#include <stdlib.h>
-#include <alf.h>
-#include "psc_cbe.h"
-
-void cbe_push_part_xz() {}
--- src/libpsc/cell_be/spu/spu_test_func.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SPU_TEST_FUNC_H_
-#define _SPU_TEST_FUNC_H_
-
-int spu_psc_hello(void);
-int spu_psc_goodbye(void);
-
-#endif
