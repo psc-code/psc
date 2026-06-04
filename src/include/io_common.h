4425d4eee05bb3ae5c352c12f3bff4c83ea6a654
Kai Germaschewski <kai.germaschewski@gmail.com>
2026-06-04 15:28:22 +0000

    writer/adios2: write separate components

--- src/include/io_common.h
+++ src/include/io_common.h
@@ -13,4 +13,11 @@ inline kg::io::Dims makeDims(int m, const Int3& dims)
                       static_cast<size_t>(dims[0])};
 }
 
+inline kg::io::Dims makeDims(const Int3& dims)
+{
+  return kg::io::Dims{static_cast<size_t>(dims[2]),
+                      static_cast<size_t>(dims[1]),
+                      static_cast<size_t>(dims[0])};
+}
+
 } // namespace
