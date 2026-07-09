f702e02fb1a3280c7c69dde860e95848cee11ee6
James McClung <james@themcclungs.net>
2026-07-09 10:41:41 -0400

    +axis

---
+++ src/libpsc/axis.hxx
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace psc
+{
+
+struct Axis
+{
+  static Axis X;
+  static Axis Y;
+  static Axis Z;
+
+  Axis(int axis) : axis{axis} {}
+
+  operator int() const { return axis; }
+
+  Axis& operator++()
+  {
+    this->axis += 1;
+    return *this;
+  }
+
+  Axis operator++(int)
+  {
+    Axis temp = *this;
+    ++(*this);
+    return temp;
+  }
+
+  int axis;
+};
+
+Axis Axis::X = Axis(0);
+Axis Axis::Y = Axis(1);
+Axis Axis::Z = Axis(2);
+
+} // namespace psc
