65e630b78414589f9b84e1157c2a8c583a48ac14
James McClung <james@themcclungs.net>
2026-07-10 15:27:34 -0400

    radiating: cleanup

--- src/libpsc/psc_bnd_fields/radiating.hxx
+++ src/libpsc/psc_bnd_fields/radiating.hxx
@@ -32,8 +32,15 @@ struct Radiating : FieldBcBase<MfieldsState>
   void apply_h_bcs(MfieldsState& mflds) override
   {
     const Grid_t& grid = mflds.grid();
+    real_t dt = grid.dt;
+    Real3 dtdx = dt * Real3(grid.domain.dx_inv);
 
     pulse.tick(grid.time());
+
+    int d0 = d, d1 = d.next(), d2 = d.prev();
+    int H0 = HX + d0, H1 = HX + d1, H2 = HX + d2;
+    int E1 = EX + d1, E2 = EX + d2;
+    int J1 = JXI + d1, J2 = JXI + d2;
 
     for (int p = 0; p < mflds.n_patches(); p++) {
       if (lohi == Lo && grid.atBoundaryLo(p, d)) {
@@ -41,14 +48,6 @@ struct Radiating : FieldBcBase<MfieldsState>
                                                                 false);
 
         auto F = make_Fields3d<dim_t>(mflds[p]);
-        const Grid_t& grid = mflds.grid();
-        real_t dt = grid.dt;
-        Real3 dtdx = dt * Real3(grid.domain.dx_inv);
-
-        int d0 = (d + 0) % 3, d1 = (d + 1) % 3, d2 = (d + 2) % 3;
-        int H0 = HX + d0, H1 = HX + d1, H2 = HX + d2;
-        int E1 = EX + d1, E2 = EX + d2;
-        int J1 = JXI + d1, J2 = JXI + d2;
 
         Int3 start = mflds.ib();
         Int3 stop = mflds.ib() + mflds.im();
@@ -66,10 +65,10 @@ struct Radiating : FieldBcBase<MfieldsState>
           Real3 x3_p = (Real3(edge_idx) + Real3::unit(d2) * real_t(0.5)) *
                        Real3(grid.domain.dx);
 
-          s = pulse.sample_exterior_field_lo(EX + d1, grid.time(), p, x3_s) +
-              pulse.sample_exterior_field_lo(HX + d2, grid.time(), p, x3_s);
-          p = pulse.sample_exterior_field_lo(EX + d2, grid.time(), p, x3_p) -
-              pulse.sample_exterior_field_lo(HX + d1, grid.time(), p, x3_p);
+          s = pulse.sample_exterior_field_lo(E1, grid.time(), p, x3_s) +
+              pulse.sample_exterior_field_lo(H2, grid.time(), p, x3_s);
+          p = pulse.sample_exterior_field_lo(E2, grid.time(), p, x3_p) -
+              pulse.sample_exterior_field_lo(H1, grid.time(), p, x3_p);
 
           F(H2, i3) =
             (2.f * s - 2.f * F(E1, edge_idx) -
@@ -89,15 +88,6 @@ struct Radiating : FieldBcBase<MfieldsState>
                                                                 false);
 
         auto F = make_Fields3d<dim_t>(mflds[p]);
-        const Grid_t& grid = mflds.grid();
-        Int3 ldims = grid.ldims;
-        real_t dt = grid.dt;
-        Real3 dtdx = dt * Real3(grid.domain.dx_inv);
-
-        int d0 = (d + 0) % 3, d1 = (d + 1) % 3, d2 = (d + 2) % 3;
-        int H0 = HX + d0, H1 = HX + d1, H2 = HX + d2;
-        int E1 = EX + d1, E2 = EX + d2;
-        int J1 = JXI + d1, J2 = JXI + d2;
 
         Int3 start = mflds.ib();
         Int3 stop = mflds.ib() + mflds.im();
@@ -115,10 +105,10 @@ struct Radiating : FieldBcBase<MfieldsState>
           Real3 x3_p = (Real3(edge_idx) + Real3::unit(d2) * real_t(0.5)) *
                        Real3(grid.domain.dx);
 
-          s = pulse.sample_exterior_field_hi(EX + d1, grid.time(), p, x3_s) -
-              pulse.sample_exterior_field_hi(HX + d2, grid.time(), p, x3_s);
-          p = pulse.sample_exterior_field_hi(EX + d2, grid.time(), p, x3_p) +
-              pulse.sample_exterior_field_hi(HX + d1, grid.time(), p, x3_p);
+          s = pulse.sample_exterior_field_hi(E1, grid.time(), p, x3_s) -
+              pulse.sample_exterior_field_hi(H2, grid.time(), p, x3_s);
+          p = pulse.sample_exterior_field_hi(E2, grid.time(), p, x3_p) +
+              pulse.sample_exterior_field_hi(H1, grid.time(), p, x3_p);
 
           F(H2, i3) = (-2.f * s + 2.f * F(E1, i3) +
                        dtdx[d2] * (F(H0, i3) - F(H0, i3 - Int3::unit(d2))) -
